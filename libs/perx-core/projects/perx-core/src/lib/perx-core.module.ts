5977b05f160e9e76f520d1db865139b3ee52974e
Pierre Enriquez <pierre@perxtech.com>
2019-06-10 16:24:06 +0800

    Cleanup current workspace.

--- libs/perx-core/projects/perx-core/src/lib/perx-core.module.ts
+++ libs/perx-core/projects/perx-core/src/lib/perx-core.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
-import { PerxCoreComponent } from './perx-core.component';
 
 @NgModule({
-  declarations: [PerxCoreComponent],
+  declarations: [
+  ],
   imports: [
   ],
-  exports: [PerxCoreComponent]
+  exports: [
+  ]
 })
 export class PerxCoreModule { }
--- libs/perx-core/projects/perx-core/src/public-api.ts
+++ libs/perx-core/projects/perx-core/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of perx-core
  */
 
-export * from './lib/perx-core.service';
-export * from './lib/perx-core.component';
 export * from './lib/perx-core.module';
