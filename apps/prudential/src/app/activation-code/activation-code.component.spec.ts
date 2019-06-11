09bec3d453b69ef95ba5d65984391f974f3f3e74
Nicolas Lagier <nicolas@perxtech.com>
2019-06-11 16:26:56 +0800

    Fix broken tests

--- apps/prudential/src/app/activation-code/activation-code.component.spec.ts
+++ apps/prudential/src/app/activation-code/activation-code.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ActivationCodeComponent } from './activation-code.component';
+import { PerxCoreModule } from '@perx/core/dist/perx-core';
+import { RouterModule } from '@angular/router';
 
 describe('ActivationCodeComponent', () => {
   let component: ActivationCodeComponent;
@@ -8,9 +10,13 @@ describe('ActivationCodeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ActivationCodeComponent ]
+      declarations: [ActivationCodeComponent],
+      imports: [
+        RouterModule.forRoot([]),
+        PerxCoreModule
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
