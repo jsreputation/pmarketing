87f2527b294b74329a07b398c9d5d6caf10ad038
Pierre Enriquez <pierre@perxtech.com>
2019-06-13 10:21:04 +0800

    Add ribbon for redeemed vouchers.

--- libs/perx-core/projects/perx-core/src/lib/vouchers/vouchers.service.ts
+++ libs/perx-core/projects/perx-core/src/lib/vouchers/vouchers.service.ts
@@ -2,8 +2,46 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 
 const vouchers = [
-  { id: 1, name: 'Venti Caffe Latte', img: 'https://picsum.photos/200', description: 'Starbucks', expiresAt: '04 Feb 2019' },
-  { id: 2, name: '100g Cookies', img: 'https://picsum.photos/200', description: 'Famous Amos', expiresAt: '04 Feb 2019' }
+  { 
+    id: 1,
+    state: 'issued',
+    name: 'Venti Caffe Latte',
+    img: 'https://picsum.photos/200',
+    description: 'Starbucks',
+    expiresAt: '04 Feb 2019'
+  },
+  {
+    id: 2,
+    state: 'issued',
+    name: '100g Cookies',
+    img: 'https://picsum.photos/200',
+    description: 'Famous Amos',
+    expiresAt: '04 Feb 2019'
+  },
+  {
+    id: 3,
+    state: 'issued',
+    name: '100g Cookies',
+    img: 'https://picsum.photos/200',
+    description: 'Famous Amos',
+    expiresAt: '04 Feb 2019'
+  },
+  {
+    id: 4,
+    state: 'redeemed',
+    name: '100g Cookies',
+    img: 'https://picsum.photos/200',
+    description: 'Famous Amos',
+    expiresAt: '04 Feb 2019'
+  },
+  {
+    id: 5,
+    state: 'redeemed',
+    name: '100g Cookies',
+    img: 'https://picsum.photos/200',
+    description: 'Famous Amos',
+    expiresAt: '04 Feb 2019'
+  }
 ];
 
 @Injectable({
@@ -13,7 +51,7 @@ export class VouchersService {
 
   constructor() { }
 
-  getAll(): Observable<{ id: number, name: string, img: string, description: string, expiresAt: string }[]> {
+  getAll(): Observable<{ id: number, state: string, name: string, img: string, description: string, expiresAt: string }[]> {
     return of(vouchers);
   }
 }
