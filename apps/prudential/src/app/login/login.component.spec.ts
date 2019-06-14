b4e49d70900af09c93a8170193712192800cbf53
Ryane Chia <ryanechia@outlook.com>
2019-06-14 17:20:25 +0800

    login component tests imported modules

--- apps/prudential/src/app/login/login.component.spec.ts
+++ apps/prudential/src/app/login/login.component.spec.ts
@@ -1,6 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { LoginComponent } from './login.component';
+import {
+  MatCardModule,
+  MatProgressSpinnerModule
+} from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthenticationModule, CognitoModule } from '@perx/core/dist/perx-core';
+import { environment } from '../../environments/environment';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,11 +15,18 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
-  }));
+      imports: [MatCardModule,
+        MatProgressSpinnerModule,
+        RouterTestingModule,
+        HttpClientModule,
+        AuthenticationModule,
+        CognitoModule.forRoot({ env: environment }),
 
+      ],
+      declarations: [LoginComponent],
+    })
+      .compileComponents();
+  }));
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
