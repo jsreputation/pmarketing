5b02e6dede9b33f431191b4aa0e0892e74cf8a25
Ryane Chia <ryanechia@outlook.com>
2019-06-14 16:51:22 +0800

    auto-login when login route is hit

--- apps/prudential/src/app/login/login.component.ts
+++ apps/prudential/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit, PLATFORM_ID} from '@angular/core';
+import {Router} from '@angular/router';
+import {environment} from '../../environments/environment';
+import {isPlatformBrowser} from '@angular/common';
+import { AuthenticationService } from '@perx/core/dist/perx-core';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  authed: boolean;
+  preAuth: boolean;
+  failedAuth: boolean;
+
+  constructor(private router: Router,
+              private authService: AuthenticationService,
+              @Inject(PLATFORM_ID) private platformId: object) {
+    this.preAuth = environment.preAuth;
+    this.failedAuth = false;
+
+  }
 
   ngOnInit() {
+    if (this.preAuth) {
+      if (isPlatformBrowser(this.platformId) && !this.authService.authing) {
+        this.authService.isAuthorized().subscribe(
+          authed => {
+            if (!authed) {
+              this.authService.autoLogin().then(
+                (isAuthed: boolean) => {
+                  this.authed = isAuthed;
+                  if (this.authed) {
+                    this.router.navigateByUrl(this.authService.getInterruptedUrl());
+                  }
+                },
+                (err) => {
+                  this.failedAuth = true;
+                  this.authed = false;
+                }
+              );
+            } else {
+              this.authed = authed;
+            }
+
+          },
+        );
+      }
+    }
   }
 
 }
