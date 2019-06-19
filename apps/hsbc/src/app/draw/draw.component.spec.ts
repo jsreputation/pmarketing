8f48875e434c7a783c33a5f2925035edf81d4d51
Nicolas Lagier <nicolas@perxtech.com>
2019-06-19 12:58:03 +0800

    fix more broken tests

--- apps/hsbc/src/app/draw/draw.component.spec.ts
+++ apps/hsbc/src/app/draw/draw.component.spec.ts
@@ -2,6 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DrawComponent } from './draw.component';
 import { PerxCoreModule } from '@perx/core/dist/perx-core';
+import { RouterModule } from '@angular/router';
+import { MatProgressSpinnerModule } from '@angular/material';
 
 describe('DrawComponent', () => {
   let component: DrawComponent;
@@ -10,7 +12,11 @@ describe('DrawComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [DrawComponent],
-      imports: [PerxCoreModule]
+      imports: [
+        PerxCoreModule,
+        MatProgressSpinnerModule,
+        RouterModule.forRoot([])
+      ]
     })
       .compileComponents();
   }));
