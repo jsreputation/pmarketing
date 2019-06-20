1ac2f782a43353a996fbc4c1d8b9f48e7c493cc5
Ryane Chia <ryanechia@outlook.com>
2019-06-20 14:46:34 +0800

    route guards for prudential

--- apps/prudential/src/app/app-routing.module.ts
+++ apps/prudential/src/app/app-routing.module.ts
@@ -9,15 +9,16 @@ import { ContactUsComponent } from './contact-us/contact-us.component';
 import { VouchersComponent } from './vouchers/vouchers.component';
 import { VoucherComponent } from './vouchers/voucher/voucher.component';
 import { LoginComponent } from './login/login.component';
+import { ProtectedGuard } from 'ngx-auth';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/game', pathMatch: 'full' },
-  { path: 'game', component: GameComponent },
-  { path: 'congrats', component: CongratsComponent },
-  { path: 'vouchers', component: VouchersComponent },
-  { path: 'vouchers/:id', component: VoucherComponent },
-  { path: 'activation', component: ActivationCodeComponent },
-  { path: 'redemption', component: RedemptionComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'game', component: GameComponent, canActivate: [ProtectedGuard] },
+  { path: 'congrats', component: CongratsComponent, canActivate: [ProtectedGuard] },
+  { path: 'vouchers', component: VouchersComponent, canActivate: [ProtectedGuard] },
+  { path: 'vouchers/:id', component: VoucherComponent, canActivate: [ProtectedGuard] },
+  { path: 'activation', component: ActivationCodeComponent, canActivate: [ProtectedGuard] },
+  { path: 'redemption', component: RedemptionComponent, canActivate: [ProtectedGuard] },
   { path: 'tnc', component: TncComponent },
   { path: 'contact-us', component: ContactUsComponent },
   { path: 'login', component: LoginComponent }
