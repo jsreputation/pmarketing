87d6abf6773e686d2dac28b881ff34ece9ec56b2
Nicolas Lagier <nicolas@perxtech.com>
2019-06-20 18:56:11 +0800

    add webpack building of backend

---
+++ backend/appauth-server/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: './server.ts',
+    target: 'node',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+        filename: 'server.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
