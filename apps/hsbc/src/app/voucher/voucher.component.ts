832c12327873827309228212f954bd8052180911
Pierre Enriquez <pierre@perxtech.com>
2019-06-21 16:33:14 +0800

    Wireup bcode redemption component.

--- apps/hsbc/src/app/voucher/voucher.component.ts
+++ apps/hsbc/src/app/voucher/voucher.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-voucher',
@@ -7,13 +7,20 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./voucher.component.scss']
 })
 export class VoucherComponent implements OnInit {
-  firstTime = false;
+  private firstTime = false;
+  private id: string;
 
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) { }
 
   ngOnInit() {
     this.firstTime = this.route.snapshot.paramMap.get('win') === 'true';
+    this.id = this.route.snapshot.params.id;
+  }
+
+  onRedeem() {
+    this.router.navigate([`/redemption/${this.id}`]);
   }
 }
