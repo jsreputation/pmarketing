13d6d6767f43db567fff605cc96f0d5a5539c6f7
pavlovandreyka <andreypavlov1706@gmail.com>
2019-07-02 13:30:12 +0300

    added selected type to engagement popup

--- apps/candyland/src/app/engagements/containers/create-engagement-popup/create-engagement-popup.component.ts
+++ apps/candyland/src/app/engagements/containers/create-engagement-popup/create-engagement-popup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { IEngagementType } from './engagement-type/models/engagement-type.model';
 
 @Component({
   selector: 'cl-create-engagement-popup',
@@ -8,12 +9,17 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CreateEngagementPopupComponent implements OnInit {
+  public selectedType: IEngagementType;
   constructor(
     public dialogRef: MatDialogRef<CreateEngagementPopupComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any) {}
 
   onNoClick(): void {
     this.dialogRef.close();
+  }
+
+  public setType(type: IEngagementType): void {
+    this.selectedType = type;
   }
   ngOnInit() {
   }
--- apps/candyland/src/app/engagements/containers/create-engagement-popup/engagement-type/engagement-type.component.ts
+++ apps/candyland/src/app/engagements/containers/create-engagement-popup/engagement-type/engagement-type.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, QueryList, ViewChildren } from '@angular/core';
 import { EngagementType } from './engagement-types';
 import { IEngagementType } from './models/engagement-type.model';
 import { InkListenerDirective } from '@cl-shared/components/ink/directives/ink-listener.directive';
@@ -10,6 +10,8 @@ import { IInkBarHost } from '@cl-shared/components/ink/models/ink-bar-host';
   styleUrls: ['./engagement-type.component.scss']
 })
 export class EngagementTypeComponent implements OnInit, IInkBarHost {
+  @Output() onSelectedType = new EventEmitter<IEngagementType>();
+
   @ViewChildren(InkListenerDirective) public _inkListener: QueryList<InkListenerDirective>;
   public selectedType: IEngagementType;
   public engagementType: IEngagementType[] = EngagementType;
@@ -20,6 +22,7 @@ export class EngagementTypeComponent implements OnInit, IInkBarHost {
 
   public setTypeEngagement(type: IEngagementType): void {
      this.selectedType = type;
+     this.onSelectedType.emit(type);
   }
 
   public getInkListeners(): any {
