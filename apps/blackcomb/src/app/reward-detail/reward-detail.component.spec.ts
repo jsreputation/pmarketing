30b85368af74401baa87adccd4cf389c495ac975
Salman Khalid <salmankhalid1@gmail.com>
2019-07-03 12:26:59 +0500

    Add missing dependency 'HttpClientTestingModule' in RewardDetail testing script

--- apps/blackcomb/src/app/reward-detail/reward-detail.component.spec.ts
+++ apps/blackcomb/src/app/reward-detail/reward-detail.component.spec.ts
@@ -1,9 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RewardDetailComponent } from './reward-detail.component';
-// import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { VouchersModule, CampaignModule } from '@perx/core/dist/perx-core';
 import { environment } from '../../environments/environment';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 
 describe('RewardDetailComponent', () => {
@@ -15,8 +15,9 @@ describe('RewardDetailComponent', () => {
       declarations: [ RewardDetailComponent ],
       imports: [
         RouterTestingModule,
+        HttpClientTestingModule,
         CampaignModule.forRoot({ env: environment }),
-        VouchersModule.forRoot({ env: environment }),
+        VouchersModule.forRoot({ env: environment })
        ]
     })
     .compileComponents();
