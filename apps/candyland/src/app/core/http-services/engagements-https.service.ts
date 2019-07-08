7922a961c7a3b3df8c641373f6db1f510a0ca62c
pavlovandreyka <andreypavlov1706@gmail.com>
2019-07-08 14:28:53 +0300

    change create engagement popup view

--- apps/candyland/src/app/core/http-services/engagements-https.service.ts
+++ apps/candyland/src/app/core/http-services/engagements-https.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { IGraphic } from '@cl-shared/models/graphick.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +12,20 @@ export class EngagementsService {
 
   public getEngagements() {
     return this.http.get('/assets/mocks/engagements.json');
+  }
+
+  public getEngagementType() {
+    return this.http.get('/assets/actives/engagement-type.json')
+      .pipe(
+        map(res => (res as IGraphic[]))
+      );
+  }
+
+
+  public getGamesType() {
+    return this.http.get('/assets/actives/games-type.json')
+      .pipe(
+        map(res => (res as IGraphic[]))
+      );
   }
 }
--- apps/candyland/src/app/engagements/containers/create-engagement-popup/engagement-type/type-item/type-item.component.ts
+++ apps/candyland/src/app/engagements/containers/create-engagement-popup/engagement-type/type-item/type-item.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { IEngagementType } from '../models/engagement-type.model';
+import { IGraphic } from '@cl-shared/models/graphick.model';
 
 @Component({
   selector: 'cl-type-item',
@@ -7,12 +7,11 @@ import { IEngagementType } from '../models/engagement-type.model';
   styleUrls: ['./type-item.component.scss']
 })
 export class TypeItemComponent implements OnInit {
-  @Input() public typeItem: IEngagementType;
-  @Input() public checkedType: IEngagementType;
+  @Input() public typeItem: IGraphic;
+  @Input() public checkedType: IGraphic;
 
-  @Output() public selectType = new EventEmitter<IEngagementType>();
+  @Output() public selectType = new EventEmitter<IGraphic>();
 
-  public basePath = '/assets/images/engagement-type/';
   constructor() { }
 
   ngOnInit() {
