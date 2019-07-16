2cd75731574827a481a1aed66ed6d8f3a02f53da
Ryane Chia <ryanechia@outlook.com>
2019-07-16 17:56:19 +0800

    import apiHost from env to RewardsService

--- libs/perx-core/projects/perx-core/src/lib/rewards/rewards.service.ts
+++ libs/perx-core/projects/perx-core/src/lib/rewards/rewards.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { EnvConfig } from './env-config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RewardsService {
 
-  constructor() {
+  private apiHost: string;
+
+  constructor(private httpClient: HttpClient, config: EnvConfig) {
+    this.apiHost = config.env.apiHost;
   }
 
   getTags() {
