6d5d37f4b3ffaba5878032cb3e179ef6ee213d7d
Ryane Chia <ryanechia@outlook.com>
2019-07-17 12:56:54 +0800

    v4 api queries

--- libs/perx-core/projects/perx-core/src/lib/rewards/rewards.service.ts
+++ libs/perx-core/projects/perx-core/src/lib/rewards/rewards.service.ts
@@ -1,6 +1,31 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { EnvConfig } from './env-config';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+interface IV4Meta {
+  count?: number;
+  size?: number;
+  total_pages?: number;
+  page?: number;
+}
+
+interface IV4Reward {
+  id: number;
+  name: string;
+  description: string;
+  valid_from: string;
+}
+
+interface IV4GetRewardsResponse {
+  data: IV4Reward[];
+  meta?: IV4Meta;
+}
+
+interface IV4GetRewardResponse {
+  data: IV4Reward[];
+}
 
 @Injectable({
   providedIn: 'root'
@@ -9,23 +34,34 @@ export class RewardsService {
 
   private apiHost: string;
 
-  constructor(private httpClient: HttpClient, config: EnvConfig) {
+  constructor(private http: HttpClient, config: EnvConfig) {
     this.apiHost = config.env.apiHost;
   }
 
   getTags() {
-
+    // todo: api not implemented yet
   }
 
-  getRewards() {
-
+  getRewards(page = 1, pageSize = 25): Observable<IV4Reward[]> {
+    return this.http.get<IV4GetRewardsResponse>(
+      `${ this.apiHost }/v4/rewards`,
+      {
+        params: {
+          page: `${ page }`,
+          size: `${ pageSize }`
+        }
+      }
+    ).pipe(
+      map(res => res.data)
+    );
   }
 
-  getReward(id: number) {
-
+  getReward(id: number): Observable<IV4Reward[]> {
+    return this.http.get<IV4GetRewardResponse>(
+      `${ this.apiHost }/v4/reward/${ id }`
+    ).pipe(
+      map(res => res.data)
+    );
   }
 
-  redeemReward(id: number) {
-
-  }
 }
