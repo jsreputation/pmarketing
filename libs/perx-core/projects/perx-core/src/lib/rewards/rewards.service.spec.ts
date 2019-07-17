b8d43ecac3fb36e1205e9ac9c6acc0823fecc6c3
Ryane Chia <ryanechia@outlook.com>
2019-07-17 15:40:24 +0800

    abstract classes tests need to be typecasted

--- libs/perx-core/projects/perx-core/src/lib/rewards/rewards.service.spec.ts
+++ libs/perx-core/projects/perx-core/src/lib/rewards/rewards.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 
 import { RewardsService } from './rewards.service';
+import { Type } from '@angular/core';
 
 describe('RewardsService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: RewardsService = TestBed.get(RewardsService);
+    // https://github.com/angular/angular/issues/29905 abstract classes need to be typecasted
+    const service: RewardsService = TestBed.get(RewardsService as Type<RewardsService>);
     expect(service).toBeTruthy();
   });
 });
