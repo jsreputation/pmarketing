d116eeaf31de5a75d1c70784fa9bc171f7db7c32
Pierre Enriquez <pierre@perxtech.com>
2019-07-18 16:05:09 +0800

    Implement loyalty summary component.

--- libs/perx-core/projects/perx-core/src/lib/loyalty/loyalty.service.ts
+++ libs/perx-core/projects/perx-core/src/lib/loyalty/loyalty.service.ts
@@ -2,11 +2,11 @@ import { Observable } from 'rxjs';
 import { ILoyalty, IPointHistory } from './models/loyalty.model';
 
 export abstract class LoyaltyService {
-  public abstract getLoyalties(page: number, pageSize: number): Observable<ILoyalty[]>;
+  public abstract getLoyalties(page?: number, pageSize?: number): Observable<ILoyalty[]>;
 
   public abstract getLoyalty(id: number): Observable<ILoyalty>;
 
   public abstract getAllHistory(loyaltyId: number): Observable<IPointHistory[]>;
 
-  public abstract getHistory(loyaltyId: number, page: number, pageSize: number): Observable<IPointHistory[]>;
+  public abstract getHistory(loyaltyId: number, page?: number, pageSize?: number): Observable<IPointHistory[]>;
 }
