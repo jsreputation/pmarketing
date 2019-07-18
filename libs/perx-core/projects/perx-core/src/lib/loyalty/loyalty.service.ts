1acefc72fbf3ed98beb1b3ccab2a1f8a4d8cb928
Pierre Enriquez <pierre@perxtech.com>
2019-07-18 18:17:00 +0800

    Add echange points stub.

--- libs/perx-core/projects/perx-core/src/lib/loyalty/loyalty.service.ts
+++ libs/perx-core/projects/perx-core/src/lib/loyalty/loyalty.service.ts
@@ -1,5 +1,6 @@
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { ILoyalty, IPointHistory } from './models/loyalty.model';
+import { IVoucher } from '../vouchers/models/voucher.model';
 
 export abstract class LoyaltyService {
   public abstract getLoyalties(page?: number, pageSize?: number): Observable<ILoyalty[]>;
@@ -9,4 +10,9 @@ export abstract class LoyaltyService {
   public abstract getAllHistory(loyaltyId: number): Observable<IPointHistory[]>;
 
   public abstract getHistory(loyaltyId: number, page?: number, pageSize?: number): Observable<IPointHistory[]>;
+
+  // @ts-ignore
+  public exchangePoints(rewardId: number): Observable<IVoucher> {
+    return throwError('Not implemented yet');
+  }
 }
