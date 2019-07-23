41dcef8b408d258445d5823de4bee026da85450d
Salman Khalid <salmankhalid1@gmail.com>
2019-07-23 15:00:52 +0500

    Fixed testing script issues

--- apps/merck-customer/src/app/user-info/user-info.component.spec.ts
+++ apps/merck-customer/src/app/user-info/user-info.component.spec.ts
@@ -1,7 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserInfoComponent } from './user-info.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import {
+  FormsModule,
+  ReactiveFormsModule
+  } from '@angular/forms';
 import { ProfileModule } from '@perx/core/dist/perx-core';
+import { environment } from '../../environments/environment';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatRadioModule
+} from '@angular/material';
 
 describe('UserInfoComponent', () => {
   let component: UserInfoComponent;
@@ -10,7 +23,17 @@ describe('UserInfoComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ UserInfoComponent ],
-      imports: [ ProfileModule ]
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        RouterTestingModule,
+        HttpClientTestingModule,
+        MatButtonModule,
+        MatCheckboxModule,
+        MatRadioModule,
+        ProfileModule.forRoot({ env: environment })
+         ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     })
     .compileComponents();
   }));
