141fe8a1103142d530daff195db419ed75794110
Jaye <jaye.tan@flags.fm>
2019-07-24 22:00:28 +0800

    update repeating contents to be component, moved css, added unit test for logout

--- apps/merck-merchant/src/app/home/home.component.ts
+++ apps/merck-merchant/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '@perx/core/dist/perx-core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService
+  ) { }
+
   public ngOnInit(): void {
+  }
+
+  public onSalesScan(): void {}
+
+  public onRedemption(): void {}
+
+  public onLogOut(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
   }
 
 }
