0f7423526ac216eb37222675a862a6f193d2441b
Jaye <jaye.tan@flags.fm>
2019-07-24 15:00:55 +0800

    added login unit test

--- apps/merck-merchant/src/app/login/login.component.spec.ts
+++ apps/merck-merchant/src/app/login/login.component.spec.ts
@@ -1,5 +1,5 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { AuthenticationService } from '@perx/core/dist/perx-core';
 import { LoginComponent } from './login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
@@ -7,6 +7,7 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AuthenticationModule, CognitoModule, OauthModule } from '@perx/core/dist/perx-core';
 import { environment } from '../../environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Router } from '@angular/router';
 import {
   MatButtonModule,
   MatToolbarModule,
@@ -14,13 +15,15 @@ import {
   MatInputModule,
   MatRippleModule,
 } from '@angular/material';
+
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async(() => {
+    const routerStub = { navigateByUrl: () => ({}) };
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ],
+      declarations: [LoginComponent],
       imports: [
         FormsModule,
         ReactiveFormsModule,
@@ -34,19 +37,48 @@ describe('LoginComponent', () => {
         MatRippleModule,
         BrowserAnimationsModule,
         CognitoModule.forRoot({ env: environment }),
-        OauthModule.forRoot({ env: environment })
+        OauthModule.forRoot({ env: environment }),
+      ],
+      providers: [
+        { provide: Router, useValue: routerStub },
+        {
+          provide: AuthenticationService,
+          useValue: {v4GameOauth: () => {}, getInterruptedUrl: () => null}
+        }
       ]
-    })
-    .compileComponents();
+    }).compileComponents();
   }));
 
-  beforeEach(() => {
+  beforeEach(async(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }));
 
-  it('should create', () => {
+  it('can load instance', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should navigate to reset onForgotPassword click', () => {
+    const routerStub: Router = fixture.debugElement.injector.get(Router);
+    spyOn(routerStub, 'navigateByUrl').and.callThrough();
+    component.onForgotPassword();
+    expect(routerStub.navigateByUrl).toHaveBeenCalledWith('/reset');
+  });
+
+  it('should navigate to home if authenticated', fakeAsync(() => {
+    const authenticationService: AuthenticationService = fixture.debugElement.injector.get(
+      AuthenticationService
+    );
+    const authSpy = spyOn(authenticationService, 'v4GameOauth').and.returnValue(Promise.resolve(true));
+    const routerStub: Router = fixture.debugElement.injector.get(Router);
+    const routerSpy = spyOn(routerStub, 'navigateByUrl').and.stub();
+
+    component.onSubmit();
+    tick();
+    fixture.detectChanges();
+    expect(authSpy).toHaveBeenCalled();
+    expect(routerSpy).toHaveBeenCalledWith('/home');
+  }));
+
 });
