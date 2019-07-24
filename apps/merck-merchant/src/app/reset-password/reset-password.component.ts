714a1e5f5f5eb7862c4739b047b61872e702533a
Jaye <jaye.tan@flags.fm>
2019-07-24 15:30:28 +0800

    fixed lint errors

--- apps/merck-merchant/src/app/reset-password/reset-password.component.ts
+++ apps/merck-merchant/src/app/reset-password/reset-password.component.ts
@@ -7,9 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResetPasswordComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  public ngOnInit(): void {
   }
 
 }
