b5d5b59c95d8e8afe1700a9c0719fe2b031208a7
Rostislav Kondratiev <kondratiev.rost@gmail.com>
2019-07-24 15:02:23 +0300

    Wallet page implementation

--- apps/hkbn/src/app/wallet/wallet.component.spec.ts
+++ apps/hkbn/src/app/wallet/wallet.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { WalletComponent } from './wallet.component';
+import { MatTabsModule } from '@angular/material';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { VouchersModule } from '@perx/core/dist/perx-core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('WalletComponent', () => {
   let component: WalletComponent;
@@ -8,9 +13,16 @@ describe('WalletComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ WalletComponent ]
+      imports: [
+        MatTabsModule,
+        NoopAnimationsModule,
+        RouterTestingModule,
+        VouchersModule.forRoot({env: {apiHost: ''}}),
+        HttpClientTestingModule,
+      ],
+      declarations: [WalletComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
