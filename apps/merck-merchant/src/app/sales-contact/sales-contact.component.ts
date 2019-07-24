2b787e2c9db3b992d1d5bce1c06863b95a50323e
Jaye <jaye.tan@flags.fm>
2019-07-24 21:37:31 +0800

    added header and sales-contact component

---
+++ apps/merck-merchant/src/app/sales-contact/sales-contact.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sales-contact',
+  templateUrl: './sales-contact.component.html',
+  styleUrls: ['./sales-contact.component.scss']
+})
+export class SalesContactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
