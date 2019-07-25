461f65a273061de48af79d62abc9111e1cb788b8
Ryane Chia <ryanechia@outlook.com>
2019-07-25 19:15:22 +0800

    add components demo app with rewards/list demo

---
+++ apps/components-demo/src/app/rewards/rewards.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RewardsComponent } from './rewards.component';
+import { RewardsCarouselComponent } from './rewards-carousel/rewards-carousel.component';
+import { RewardsListComponent } from './rewards-list/rewards-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RewardsRoutingModule } from './rewards-routing.module';
+import {
+  AuthenticationModule,
+  RewardsModule as PerxRewardsModule
+} from '@perx/core/dist/perx-core';
+import { environment } from '../../environments/environment';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [
+    RewardsComponent,
+    RewardsCarouselComponent,
+    RewardsListComponent,
+  ],
+  imports: [
+    RouterModule,
+    CommonModule,
+    HttpClientModule,
+    PerxRewardsModule.forRoot({ env: environment }),
+    AuthenticationModule,
+    RewardsRoutingModule
+  ]
+})
+export class RewardsModule {
+}
