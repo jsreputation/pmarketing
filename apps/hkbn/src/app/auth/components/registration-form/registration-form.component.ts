9b63910f9cf2ed64a5e4a2933cbf30d1ef74105f
Rostislav Kondratiev <kondratiev.rost@gmail.com>
2019-07-30 09:39:53 +0300

    Registration form layout (WIP)

--- apps/hkbn/src/app/auth/components/registration-form/registration-form.component.ts
+++ apps/hkbn/src/app/auth/components/registration-form/registration-form.component.ts
@@ -1,21 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'hkbn-registration-form',
   templateUrl: './registration-form.component.html',
   styleUrls: ['./registration-form.component.scss']
 })
-export class RegistrationFormComponent implements OnInit {
+export class RegistrationFormComponent {
 
   public registrationForm: FormGroup = new FormGroup({
-
+    firstName: new FormControl(null, [])
   });
-
-  constructor() {
-  }
-
-  ngOnInit() {
-  }
 
 }
