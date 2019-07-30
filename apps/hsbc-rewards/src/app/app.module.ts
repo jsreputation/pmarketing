e56f35404db5490c471eb65e064670feadb0f45a
Golotin Denis <d.golotin@dunice.net>
2019-07-30 14:25:57 +0300

    pw-507 create component

--- apps/hsbc-rewards/src/app/app.module.ts
+++ apps/hsbc-rewards/src/app/app.module.ts
@@ -1,16 +1,65 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  PerxCoreModule,
+  CognitoModule,
+  OauthModule,
+  AuthenticationModule,
+  CampaignModule,
+  VouchersModule,
+  ProfileModule,
+  StampModule,
+  UtilsModule
+} from '@perx/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './login/login.component';
+import {
+  MatButtonModule,
+  MatListModule,
+  MatTabsModule,
+  MatCardModule,
+  MatRippleModule,
+  MatIconModule,
+  MatToolbarModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatProgressSpinnerModule,
+  MatProgressBarModule,
+  MatSidenavModule
+} from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatListModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatRippleModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatSidenavModule,
+    FormsModule,
+    AuthenticationModule,
+    HttpClientModule,
+    CognitoModule.forRoot({ env: environment }),
+    OauthModule.forRoot({ env: environment }),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ apps/hsbc-rewards/src/app/login/login.component.ts
@@ -0,0 +1,113 @@
+import { Component, OnInit, PLATFORM_ID, Inject } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
+import { AuthenticationService, NotificationService } from '@perx/core';
+import { HttpErrorResponse } from '@angular/common/http';
+
+import { environment } from '../../environments/environment';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+
+  authed: boolean;
+  preAuth: boolean;
+  failedAuth: boolean;
+
+  errorMessage: string;
+
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService,
+    @Inject(PLATFORM_ID) private platformId: object,
+    private fb: FormBuilder,
+    private notificationService: NotificationService
+  ) {
+    this.preAuth = environment.preAuth;
+    this.failedAuth = false;
+    this.initForm();
+  }
+
+  initForm() {
+    this.loginForm = this.fb.group({
+      playerCode: ['', Validators.required],
+      hsbcCardLastFourDigits: ['', Validators.required]
+    });
+  }
+
+  ngOnInit() {
+    if (this.preAuth) {
+      if (isPlatformBrowser(this.platformId) && !this.authService.authing) {
+        this.authService.isAuthorized().subscribe(
+          authed => {
+            if (!authed) {
+              this.authService.autoLogin().then(
+                (isAuthed: boolean) => {
+                  this.authed = isAuthed;
+                  if (this.authed) {
+                    this.router.navigateByUrl(this.authService.getInterruptedUrl());
+                  }
+                },
+                () => {
+                  this.failedAuth = true;
+                  this.authed = false;
+                }
+              );
+            } else {
+              this.authed = authed;
+            }
+
+          },
+        );
+      }
+    }
+  }
+
+  onSubmit() {
+    const username = (this.loginForm.get('playerCode').value as string).toUpperCase();
+    const password: string = this.loginForm.get('hsbcCardLastFourDigits').value;
+    this.errorMessage = null;
+    this.authService.v4GameOauth(username, password)
+      .then((isAuthed: boolean) => {
+        this.authed = isAuthed;
+        if (this.authed) {
+
+          // set global userID var for GA tracking
+          if (!((window as any).primaryIdentifier)) {
+            (window as any).primaryIdentifier = username;
+          }
+
+          if (this.authService.getInterruptedUrl()) {
+            this.router.navigateByUrl(this.authService.getInterruptedUrl());
+          } else {
+            this.router.navigateByUrl('puzzle');
+          }
+        }
+      })
+      .catch((err) => {
+        this.failedAuth = true;
+        this.authed = false;
+
+        if (err instanceof HttpErrorResponse) {
+          if (err.status === 0) {
+            this.notificationService.addPopup({
+              title: 'We could not reach the server',
+              text: 'Please try again soon'
+            });
+          } else if (err.status === 401) {
+            [this.loginForm.controls.playerCode, this.loginForm.controls.hsbcCardLastFourDigits]
+              .forEach(c => c.setErrors({
+                invalid: true
+              }));
+            this.errorMessage = 'Invalid credentials';
+          }
+        }
+      });
+  }
+}
