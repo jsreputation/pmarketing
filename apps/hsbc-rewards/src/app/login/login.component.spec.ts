2a1104bf4d071f6b4e8026bbad4c2ebc99c08f9d
Golotin Denis <d.golotin@dunice.net>
2019-07-31 09:56:24 +0300

    fix unit test

--- apps/hsbc-rewards/src/app/login/login.component.spec.ts
+++ apps/hsbc-rewards/src/app/login/login.component.spec.ts
@@ -1,6 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { LoginComponent } from './login.component';
+import { MatCardModule, MatProgressSpinnerModule } from '@angular/material';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { AuthenticationModule, CognitoModule, OauthModule } from '@perx/core';
+import { environment } from '../../environments/environment';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,9 +16,23 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      providers: [],
+      imports: [
+        MatCardModule,
+        MatProgressSpinnerModule,
+        RouterTestingModule,
+        HttpClientTestingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        AuthenticationModule,
+        NoopAnimationsModule,
+        CognitoModule.forRoot({ env: environment }),
+        OauthModule.forRoot({ env: environment }),
+      ],
+      declarations: [LoginComponent],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
