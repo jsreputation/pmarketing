9bf7f75f88ccfe26679a722e288c9514dc8531bc
Ryane Chia <ryanechia@outlook.com>
2019-08-01 21:07:46 +0800

    fix linting

--- apps/components-demo/src/app/app.component.ts
+++ apps/components-demo/src/app/app.component.ts
@@ -26,7 +26,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
 
-    //initialise notification service
+    // initialise notification service
     this.notificationService.$popup.subscribe(data => {
       this.dialog.open(PopupComponent, { data });
     });
