e066df047042704589f0cd10c315ca04e42a71b6
Golotin Denis <d.golotin@dunice.net>
2019-08-02 12:32:04 +0300

    store progress

--- apps/hsbc-rewards/src/app/home/home.component.ts
+++ apps/hsbc-rewards/src/app/home/home.component.ts
@@ -1,25 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Component, OnInit, ViewChild, AfterViewInit, ChangeDetectorRef } from '@angular/core';
+import { Observable, of, BehaviorSubject } from 'rxjs';
 import { IReward, RewardsService, LoyaltyService, ProfileService } from '@perx/core';
+import { } from '@perx/core'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
   rewards: Observable<IReward[]>;
+  @ViewChild('loyaltySummary', {static: false}) loyaltySummary;
   constructor(
     private rewardsService: RewardsService,
     private loyaltyService: LoyaltyService,
-    private profile: ProfileService
+    private profile: ProfileService,
+    private cd: ChangeDetectorRef
   ) { 
   }
 
   ngOnInit() {
     this.getRewards();
   }
-
+  ngAfterViewInit(): void {
+    this.loyaltySummary.loyalty$ = new BehaviorSubject(['a']);
+    this.cd.detectChanges();
+  }
   getRewards() {
     this.rewardsService.getAllRewards().subscribe(
       (rewards: IReward[]) => {
