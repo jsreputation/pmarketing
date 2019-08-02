f6c0192ee65561cf2cb9fc540b70846f5e4e2428
Jaye <jaye.tan@flags.fm>
2019-08-02 13:01:08 +0800

    updated assigning value to locations

--- apps/merck-customer/src/app/find-pharmacy/find-pharmacy.component.ts
+++ apps/merck-customer/src/app/find-pharmacy/find-pharmacy.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { LocationsService, NotificationService, ILocation } from '@perx/core';
-import { of, Observable } from 'rxjs';
+import { LocationsService, ILocation } from '@perx/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'mc-find-pharmacy',
@@ -11,39 +11,35 @@ export class FindPharmacyComponent implements OnInit {
   public locations: Observable<ILocation[]>;
 
   constructor(
-    private locationsService: LocationsService,
-    private notificationService: NotificationService
+    private locationsService: LocationsService
   ) { }
 
   ngOnInit() {
-    this.locationsService.getAll().subscribe(
-      (res: ILocation[]) => this.locations = of(res),
-      (err) => this.notificationService.addSnack(err)
-    );
-    this.locations = of(
-      [
-        {
-          merchantId: 1,
-          locationId: 1,
-          name: 'Pharmacy Name',
-          tags: ['1', '2'],
-          address: 'Pharmacy Address',
-          latitude: 0,
-          longitude: 1,
-          phone: '+852 3102 4028'
-        },
-        {
-          merchantId: 1,
-          locationId: 1,
-          name: 'Pharmacy Name',
-          tags: ['1', '2'],
-          address: 'Pharmacy Address',
-          latitude: 0,
-          longitude: 1,
-          phone: '+852 3102 4028'
-        }
-      ]
-    )
+    this.locations = this.locationsService.getAll();
+    // this.locations = of(
+    //   [
+    //     {
+    //       merchantId: 1,
+    //       locationId: 1,
+    //       name: 'Pharmacy Name',
+    //       tags: ['1', '2'],
+    //       address: 'Pharmacy Address',
+    //       latitude: 0,
+    //       longitude: 1,
+    //       phone: '+852 3102 4028'
+    //     },
+    //     {
+    //       merchantId: 1,
+    //       locationId: 1,
+    //       name: 'Pharmacy Name',
+    //       tags: ['1', '2'],
+    //       address: 'Pharmacy Address',
+    //       latitude: 0,
+    //       longitude: 1,
+    //       phone: '+852 3102 4028'
+    //     }
+    //   ]
+    // )
   }
 
 }
