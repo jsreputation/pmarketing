bd003b18247d0cc34cd25dc0709074f8ac0caf70
Nicolas Lagier <nicolas@perxtech.com>
2019-08-05 17:42:25 +0800

    PW-591 make voucher list easier to work with

---
+++ apps/components-demo/src/app/voucher/mock.ts
@@ -0,0 +1,61 @@
+import { Voucher, VOUCHER_STATE, REDEMPTION_TYPE } from '@perx/core';
+
+export const mock: Voucher[] = [
+    {
+        id: 123,
+        rewardId: 42,
+        state: VOUCHER_STATE.issued,
+        name: 'Starbucks grande',
+        redemptionType: REDEMPTION_TYPE.pin,
+        thumbnailImg: 'https://picsum.photos/200/300?random=1',
+        rewardBanner: 'https://picsum.photos/200/300?random=2',
+        merchantImg: 'https://picsum.photos/200/300?random=3',
+        merchantName: 'Starbucks',
+        expiry: new Date(),
+        description: {
+            title: '',
+            content: '',
+            tag: []
+        },
+        redemptionSuccessImg: 'https://picsum.photos/200/300?random=4',
+        redemptionSuccessTxt: ''
+    },
+    {
+        id: 124,
+        rewardId: 42,
+        state: VOUCHER_STATE.issued,
+        name: 'Starbucks',
+        redemptionType: REDEMPTION_TYPE.pin,
+        thumbnailImg: 'https://picsum.photos/200/300?random=5',
+        rewardBanner: 'https://picsum.photos/200/300?random=6',
+        merchantImg: 'https://picsum.photos/200/300?random=7',
+        merchantName: 'Starbucks',
+        expiry: new Date(),
+        description: {
+            title: '',
+            content: '',
+            tag: []
+        },
+        redemptionSuccessImg: 'https://picsum.photos/200/300?random=8',
+        redemptionSuccessTxt: ''
+    },
+    {
+        id: 125,
+        rewardId: 42,
+        state: VOUCHER_STATE.issued,
+        name: 'Starbucks',
+        redemptionType: REDEMPTION_TYPE.pin,
+        thumbnailImg: 'https://picsum.photos/200/300?random=9',
+        rewardBanner: 'https://picsum.photos/200/300?random=10',
+        merchantImg: 'https://picsum.photos/200/300?random=11',
+        merchantName: 'Starbucks',
+        expiry: new Date(),
+        description: {
+            title: '',
+            content: '',
+            tag: []
+        },
+        redemptionSuccessImg: 'https://picsum.photos/200/300?random=12',
+        redemptionSuccessTxt: ''
+    },
+];
--- libs/perx-core/projects/perx-core/src/lib/vouchers/vouchers/vouchers.component.spec.ts
+++ libs/perx-core/projects/perx-core/src/lib/vouchers/vouchers/vouchers.component.spec.ts
@@ -3,7 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { VouchersComponent } from './vouchers.component';
 import { MatCardModule, MatRippleModule } from '@angular/material';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { VouchersModule } from './vouchers.module';
+import { VouchersModule } from '../vouchers.module';
 import { RouterTestingModule } from '@angular/router/testing';
 
 describe('VouchersComponent', () => {
