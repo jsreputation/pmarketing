cb0f7c6bbe462544659fcb4d52e28eaa4fb2eb17
Nicolas Lagier <nicolas@perxtech.com>
2019-08-05 14:10:58 +0800

    PW-574

---
+++ apps/components-demo/src/app/stamps/stamps/stamps.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stamps',
+  templateUrl: './stamps.component.html',
+  styleUrls: ['./stamps.component.scss']
+})
+export class StampsComponent {
+}
