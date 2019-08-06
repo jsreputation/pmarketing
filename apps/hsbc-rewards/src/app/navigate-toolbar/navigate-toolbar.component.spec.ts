5b04a20ad63176445f6e0bb2d3fb12d4850292a5
Golotin Denis <d.golotin@dunice.net>
2019-08-06 08:52:20 +0300

    remove fdescribe

--- apps/hsbc-rewards/src/app/navigate-toolbar/navigate-toolbar.component.spec.ts
+++ apps/hsbc-rewards/src/app/navigate-toolbar/navigate-toolbar.component.spec.ts
@@ -3,7 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NavigateToolbarComponent } from './navigate-toolbar.component';
 import { RouterTestingModule } from '@angular/router/testing';
 
-fdescribe('NavigateToolbarComponent', () => {
+describe('NavigateToolbarComponent', () => {
   let component: NavigateToolbarComponent;
   let fixture: ComponentFixture<NavigateToolbarComponent>;
 
