ffed7c47671d7c214ae00e0902418dc0031dfa69
andrew <andrew@perxtech.com>
2019-08-06 15:48:22 +0800

    transaction list

--- apps/components-demo/src/app/loyalty/loyalty-transactions-list/loyalty-transactions-list.component.ts
+++ apps/components-demo/src/app/loyalty/loyalty-transactions-list/loyalty-transactions-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { ITransaction, LoyaltyService } from '@perx/core';
+import { mockLoyalty, mockTransactions } from '../loyalty-mock';
 
 @Component({
   selector: 'app-loyalty-transactions-list',
@@ -8,12 +9,12 @@ import { ITransaction, LoyaltyService } from '@perx/core';
   styleUrls: ['./loyalty-transactions-list.component.scss']
 })
 export class LoyaltyTransactionsListComponent implements OnInit {
-  public transactions: Observable<ITransaction> | undefined;
+  public transactions: Observable<ITransaction[]> | undefined;
 
   constructor(private loyaltyService: LoyaltyService) { }
 
   public ngOnInit(): void {
-    this.loyaltyService.getAllTransactions().subscribe(
+    this.loyaltyService.getAllTransactions(mockLoyalty.id).subscribe(
         (transactions) => this.transactions = of(transactions),
         () => this.transactions = of(mockTransactions)
       );
