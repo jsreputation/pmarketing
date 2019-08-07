e2f4e21183f1eb32a238dc3515725ce1fa59ab19
Nicolas Lagier <nicolas@perxtech.com>
2019-08-07 13:04:14 +0800

    PW-605 fix broken build

--- apps/candyland/src/app/helpers/cl-validators.ts
+++ apps/candyland/src/app/helpers/cl-validators.ts
@@ -2,53 +2,53 @@ import { AbstractControl, ValidatorFn, Validators } from '@angular/forms';
 import { sum } from './total-sum';
 
 export class ClValidators extends Validators {
-  // tslint:disable
-  public static url = /^((?:http|ftp)s?:\/\/)(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|localhost|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(?::\d+)?(?:\/?|[\/?]\S+)$/i;
+  // tslint:disable-next-line
+  public static url: RegExp = /^((?:http|ftp)s?:\/\/)(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|localhost|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(?::\d+)?(?:\/?|[\/?]\S+)$/i;
   constructor() {
     super();
   }
-  static email(control: AbstractControl): { [key: string]: boolean } | null {
+  public static email(control: AbstractControl): { [key: string]: boolean } | null {
     if (!control || !control.value) {
       return null;
     }
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; //tslint:disable-line
 
     if (!re.test(control.value.toLowerCase())) {
-      return {email: true};
+      return { email: true };
     }
     return null;
   }
 
-  static sumMoreThan(options: any): ValidatorFn {
-    return (control: AbstractControl): {[key: string]: boolean} => {
+  public static sumMoreThan(options: any): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: boolean } | null => {
       if (!control || !control.value) {
         return null;
       }
       const total = sum(control.value, options.fieldName);
       if (total > 100) {
-        return {sumMoreThan: true};
+        return { sumMoreThan: true };
       }
       return null;
     };
   }
 
-  static sumLessThan(options: any): ValidatorFn {
-    return (control: AbstractControl): {[key: string]: boolean} => {
+  public static sumLessThan(options: any): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: boolean } | null => {
       if (!control || !control.value) {
         return null;
       }
       const total = sum(control.value, options.fieldName);
       if (total < 100) {
-        return {sumLessThan: true};
+        return { sumLessThan: true };
       }
       return null;
     };
   }
 
-  public static checkUrl(control: AbstractControl) {
+  public static checkUrl(control: AbstractControl): { [key: string]: boolean } | null {
     if (control.value !== null) {
       if (control.value.length > 0) {
-        return !ClValidators.url.test(control.value) ? {patternUrl: true} : null;
+        return !ClValidators.url.test(control.value) ? { patternUrl: true } : null;
       }
     }
   }
