d3a2b0826d8730c5f8f592846a5ba88f85fa5842
Nicolas Lagier <nicolas@perxtech.com>
2019-08-07 14:15:00 +0800

    PW-606 fix broken apps

--- apps/blackcomb/src/app/home/home.component.ts
+++ apps/blackcomb/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ICampaign, CAMPAIGN_STATE, CAMPAIGN_TYPE } from '@perx/core';
+import { ICampaign, CampaignState, CampaignType } from '@perx/core';
 import { Router } from '@angular/router';
 import { IVoucher } from '@perx/core/dist/perx-core/lib/stamp/models/stamp.model';
 
@@ -14,8 +14,8 @@ export class HomeComponent {
       id: 1,
       name: 'Smash that Pinata',
       description: '',
-      type: CAMPAIGN_TYPE.stamp,
-      state: CAMPAIGN_STATE.active
+      type: CampaignType.stamp,
+      state: CampaignState.active
     }
   ];  // test Array
 
