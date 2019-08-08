1e5b1200dedf3ff70d05881ccb23c3bd1b38ed5a
Jaye <jaye.tan@flags.fm>
2019-08-08 19:09:13 +0800

    added account unit test

--- apps/merck-customer/src/app/account/account.component.spec.ts
+++ apps/merck-customer/src/app/account/account.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 
 import { AccountComponent } from './account.component';
 import {
@@ -8,27 +8,30 @@ import {
 import { ProfileService, AuthenticationService } from '@perx/core';
 import { of } from 'rxjs';
 import { Router } from '@angular/router';
+import { Type } from '@angular/core';
 
 describe('AccountComponent', () => {
   let component: AccountComponent;
   let fixture: ComponentFixture<AccountComponent>;
+  const userInfo = {
+    id: 59431,
+    state: 'active',
+    firstName: 'Perx',
+    lastName: 'PERX',
+    middleName: null,
+    phone: null,
+    email: null,
+    birthDate: null,
+    gender: null,
+    joinedDate: '2019-07-01T03:37:50.049Z',
+    passwordExpiryDate: null,
+    customProperties: {
+      last_4: '1234'
+    }
+  };
+
   const profileServiceStub = {
-    whoAmI: () => of({
-      id: 59431,
-      state: 'active',
-      firstName: null,
-      lastName: 'PERX',
-      middleName: null,
-      phone: null,
-      email: null,
-      birthDate: null,
-      gender: null,
-      joinedDate: '2019-07-01T03:37:50.049Z',
-      passwordExpiryDate: null,
-      customProperties: {
-        last_4: '1234'
-      }
-    })
+    whoAmI: () => of(userInfo)
   };
 
   const authenticationServiceStub = {
@@ -60,6 +63,50 @@ describe('AccountComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should return get current user info on init', fakeAsync(() => {
+    const profileService: ProfileService = fixture.debugElement.injector.get<ProfileService>(ProfileService as Type<ProfileService>);
+    const authSpy = spyOn(profileService, 'whoAmI').and.returnValue(of(userInfo));
+
+    component.ngOnInit();
+    tick();
+    expect(authSpy).toHaveBeenCalled();
+    expect(component.profile).toBe(userInfo);
+  }));
+
+  it('should redirect to login after logging out', () => {
+    const authenticationService: AuthenticationService = fixture.debugElement.injector.get<AuthenticationService>
+      (AuthenticationService as Type<AuthenticationService>);
+    const router: Router = fixture.debugElement.injector.get<Router>(Router as Type<Router>);
+    const logoutSpy = spyOn(authenticationService, 'logout');
+    const routerSpy = spyOn(router, 'navigate');
+    component.onLogout();
+    expect(logoutSpy).toHaveBeenCalled();
+    expect(routerSpy).toHaveBeenCalledWith(['login']);
+  });
+
+  describe('onAccountScreenNavigate', () => {
+    it('should navigate to profile', () => {
+      const router: Router = fixture.debugElement.injector.get<Router>(Router as Type<Router>);
+      const routerSpy = spyOn(router, 'navigate');
+      component.onAccountScreenNavigate('profile');
+      expect(routerSpy).toHaveBeenCalledWith(['profile']);
+    });
+
+    it('should navigate to transaction history', () => {
+      const router: Router = fixture.debugElement.injector.get<Router>(Router as Type<Router>);
+      const routerSpy = spyOn(router, 'navigate');
+      component.onAccountScreenNavigate('transaction-history');
+      expect(routerSpy).toHaveBeenCalledWith(['transaction-history']);
+    });
+
+    it('should navigate to transaction privacy policy', () => {
+      const router: Router = fixture.debugElement.injector.get<Router>(Router as Type<Router>);
+      const routerSpy = spyOn(router, 'navigate');
+      component.onAccountScreenNavigate('privacy-policy');
+      expect(routerSpy).toHaveBeenCalledWith(['privacy-policy']);
+    });
   });
 
 });
