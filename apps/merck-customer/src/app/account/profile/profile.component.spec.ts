19e7bb454032d20b15d4f6987c0b14d884b82ec3
Jaye <jaye.tan@flags.fm>
2019-08-12 13:00:47 +0800

    added condition screen and functionality, removed subscreen toolbar and imports, added condition unit test

--- apps/merck-customer/src/app/account/profile/profile.component.spec.ts
+++ apps/merck-customer/src/app/account/profile/profile.component.spec.ts
@@ -1,7 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProfileComponent } from './profile.component';
-import { SubscreenToolbarComponent } from '../subscreen-toolbar/subscreen-toolbar.component';
 import {
   MatIconModule,
   MatToolbarModule
@@ -17,7 +16,7 @@ describe('ProfileComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProfileComponent, SubscreenToolbarComponent ],
+      declarations: [ ProfileComponent ],
       imports: [ MatIconModule, MatToolbarModule ],
       providers: [
         { provide: Location, useValue: locationStub }
