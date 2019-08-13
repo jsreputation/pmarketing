382d846501da92d3780b1c2693f82bdc3c1e3954
Nicolas Lagier <nicolas@perxtech.com>
2019-08-13 20:49:21 +0800

    PW-631 finish cleaning up tests of components-demo

--- apps/components-demo/src/app/rewards/rewards-collection/rewards-collection.component.spec.ts
+++ apps/components-demo/src/app/rewards/rewards-collection/rewards-collection.component.spec.ts
@@ -2,7 +2,6 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RewardsCollectionComponent } from './rewards-collection.component';
 import { RewardsModule as PerxRewardsModule } from '@perx/core';
-import { environment } from '../../../environments/environment';
 import { RewardsService } from '@perx/core';
 import { of } from 'rxjs';
 
@@ -17,7 +16,7 @@ describe('RewardsCollectionComponent', () => {
     TestBed.configureTestingModule({
       declarations: [RewardsCollectionComponent],
       imports: [
-        PerxRewardsModule.forRoot({ env: environment }),
+        PerxRewardsModule,
       ],
       providers: [
         { provide: RewardsService, useValue: rewardsServiceStub }
