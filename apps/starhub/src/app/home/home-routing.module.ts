f4f86f82ffe4e4e370343355217a8acdf6b92f5e
Nicolas Lagier <nicolas@perxtech.com>
2019-08-14 16:45:59 +0800

    PW-647 temp

--- apps/starhub/src/app/home/home-routing.module.ts
+++ apps/starhub/src/app/home/home-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DiscoverComponent } from './discover/discover.component';
+import { VouchersComponent } from './vouchers/vouchers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', children: [
+      { path: 'discover', component: DiscoverComponent },
+      { path: 'vouchers', component: VouchersComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- apps/starhub/src/app/home/home.module.ts
+++ apps/starhub/src/app/home/home.module.ts
@@ -4,12 +4,19 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { DiscoverComponent } from './discover/discover.component';
 import { VouchersComponent } from './vouchers/vouchers.component';
+import { HomeComponent } from './home/home.component';
+import { MatToolbarModule, MatTabsModule } from '@angular/material';
 
 @NgModule({
-  declarations: [DiscoverComponent, VouchersComponent],
+  declarations: [DiscoverComponent, VouchersComponent, HomeComponent],
   imports: [
     CommonModule,
+    MatToolbarModule,
+    MatTabsModule,
     HomeRoutingModule
+  ],
+  bootstrap: [
+    HomeComponent
   ]
 })
 export class HomeModule { }
