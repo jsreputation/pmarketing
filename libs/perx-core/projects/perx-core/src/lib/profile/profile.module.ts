c143138f77d8b2ca99ca40d34bd3d983614938f5
andrew <andrew@perxtech.com>
2019-08-14 16:29:06 +0800

    add user profile component

--- libs/perx-core/projects/perx-core/src/lib/profile/profile.module.ts
+++ libs/perx-core/projects/perx-core/src/lib/profile/profile.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ProfileService } from './profile.service';
 import { V4ProfileService } from './v4-profile.service';
 import { EnvConfig } from '../shared/env-config';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @NgModule({
   imports: [
     CommonModule
-  ]
+  ],
+  declarations: [UserProfileComponent]
 })
 export class ProfileModule {
   public static forRoot(config: EnvConfig): ModuleWithProviders {
