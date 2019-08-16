bcf7a2ecdf5d4db64d508d6e4d76307ecab1567c
Ryane Chia <ryanechia@outlook.com>
2019-08-16 23:02:34 +0800

    add base href config to app module

--- apps/hsbc-rewards/src/app/app.module.ts
+++ apps/hsbc-rewards/src/app/app.module.ts
@@ -1,6 +1,6 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {
   CognitoModule,
   OauthModule,
@@ -9,13 +9,13 @@ import {
   ProfileModule,
   LoyaltyModule,
   LocationModule,
-  VouchersModule
 } from '@perx/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import {environment} from 'src/environments/environment';
+import {APP_BASE_HREF} from "@angular/common";
 
 @NgModule({
   declarations: [
@@ -27,15 +27,15 @@ import { environment } from 'src/environments/environment';
     BrowserAnimationsModule,
     AuthenticationModule,
     HttpClientModule,
-    CognitoModule.forRoot({ env: environment }),
-    OauthModule.forRoot({ env: environment }),
-    RewardsModule.forRoot({ env: environment }),
-    ProfileModule.forRoot({ env: environment }),
-    LoyaltyModule.forRoot({ env: environment }),
-    LocationModule.forRoot({ env: environment })
+    CognitoModule.forRoot({env: environment}),
+    OauthModule.forRoot({env: environment}),
+    RewardsModule.forRoot({env: environment}),
+    ProfileModule.forRoot({env: environment}),
+    LoyaltyModule.forRoot({env: environment}),
+    LocationModule.forRoot({env: environment})
   ],
-  providers: [
-  ],
+  providers: [{provide: APP_BASE_HREF, useValue: environment.baseHref}],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
