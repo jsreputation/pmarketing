406b29a9980b61e8d40fc7d9bd17040b48e28527
Ryane Chia <ryanechia@outlook.com>
2019-08-19 18:07:32 +0800

    add param from rewards api to voucher model

--- libs/perx-core/projects/perx-core/src/lib/vouchers/vouchers.service.ts
+++ libs/perx-core/projects/perx-core/src/lib/vouchers/vouchers.service.ts
@@ -45,6 +45,7 @@ interface IV4Voucher {
     timer: any;
     type: RedemptionType;
   };
+  reserved_expires_at: any; // reserve api somehow has a different named duplicate param
   reservation_expires_at: any;
   reward?: IV4Reward;
   state: VoucherState;
