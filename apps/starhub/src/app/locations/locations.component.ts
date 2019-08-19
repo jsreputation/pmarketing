072edb653e85b2af9bb2f9410afad89a80217733
Nicolas Lagier <nicolas@perxtech.com>
2019-08-19 21:10:23 +0800

    PW-647 locations sorting

--- apps/starhub/src/app/locations/locations.component.ts
+++ apps/starhub/src/app/locations/locations.component.ts
@@ -1,18 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
-import { ILocation } from '@perx/core';
+import { ILocation, GeoLocationService, sortByDistance } from '@perx/core';
 import { locations } from '../locations.mock';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-locations',
   templateUrl: './locations.component.html',
   styleUrls: ['./locations.component.scss']
 })
-export class LocationsComponent {
-  public locations: ILocation[];
+export class LocationsComponent implements OnInit {
+  public locations: Observable<ILocation[]>;
 
-  constructor(private location: Location) {
-    this.locations = locations;
+  constructor(private location: Location, private currentPosition: GeoLocationService) {
+  }
+
+  public ngOnInit(): void {
+    this.locations = sortByDistance(this.currentPosition.positions(), of(locations), true);
   }
 
   public back(): void {
