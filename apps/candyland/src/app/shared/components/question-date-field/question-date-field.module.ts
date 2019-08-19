2f123336230c820a506cc85373612988e8959484
AndyG00D <achicunov@gmail.com>
2019-08-19 13:04:48 +0300

    hide stepper and add field name in New/Edit User popup

--- apps/candyland/src/app/shared/components/question-date-field/question-date-field.module.ts
+++ apps/candyland/src/app/shared/components/question-date-field/question-date-field.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { QuestionDateFieldComponent } from './question-date-field.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatCheckboxModule, MatDatepickerModule, MatFormFieldModule, MatInputModule } from '@angular/material';
-import { DatePickerModule } from '@cl-shared/components/date-picker/date-picker.module';
+import {
+  MatFormFieldModule,
+  MatInputModule,
+  MatSlideToggleModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -18,12 +21,9 @@ import { DatePickerModule } from '@cl-shared/components/date-picker/date-picker.
   imports: [
     CommonModule,
     ReactiveFormsModule,
-
-    MatDatepickerModule,
     MatInputModule,
     MatFormFieldModule,
-    DatePickerModule,
-    MatCheckboxModule,
+    MatSlideToggleModule
   ]
 })
 export class QuestionDateFieldModule { }
