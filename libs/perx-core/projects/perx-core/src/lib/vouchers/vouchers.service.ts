4ff09179d349d00ad59aa6cf984539b1356381f9
Pierre Enriquez <pierre@perxtech.com>
2019-08-19 22:43:55 +0800

    Add implementation to notfiy when new vouchers are created for a reward.

--- libs/perx-core/projects/perx-core/src/lib/vouchers/vouchers.service.ts
+++ libs/perx-core/projects/perx-core/src/lib/vouchers/vouchers.service.ts
@@ -1,8 +1,8 @@
 import { Injectable, Inject } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, of, interval } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { IVoucher, VoucherState, RedemptionType } from './models/voucher.model';
-import { map, tap, flatMap, mergeAll, scan } from 'rxjs/operators';
+import { map, tap, flatMap, mergeAll, scan, filter } from 'rxjs/operators';
 import { IVoucherService } from './ivoucher.service';
 
 interface IV4VouchersResponse {
@@ -132,7 +132,7 @@ export class VouchersService implements IVoucherService {
   }
 
   public getAllFromPage(page: number): Observable<IV4Voucher[]> {
-    return this.http.get<IV4VouchersResponse>(`${this.vouchersUrl}&page=${page}`)
+    return this.http.get<IV4VouchersResponse>(`${this.vouchersUrl}&page=${page}&sort_by=id&order=desc`)
       .pipe(
         map(res => res.data)
       );
@@ -169,5 +169,47 @@ export class VouchersService implements IVoucherService {
   // resets the current cache to a new list or by default nothing, and it will filled during the next call to getAll
   public reset(vouchers: IVoucher[] = []): void {
     this.vouchers = vouchers;
+  }
+
+  public newVouchersCreatedForReward(rewardId: number, intervalPeriod: number = 1000): Observable<IVoucher[]> {
+    let current = 0;
+    let firstPageVouchers: number[] = [];
+    let newIssued: IVoucher[] = [];
+    return interval(intervalPeriod).pipe(
+      map(val => {
+        current = val;
+        return this.getAllFromPage(1);
+      }),
+      mergeAll(1),
+      map((v4Vouchers: IV4Voucher[]) => v4Vouchers.map((v4Voucher: IV4Voucher) => VouchersService.voucherToVoucher(v4Voucher))),
+      map((vouchers: IVoucher[]) => vouchers.filter(v => v.rewardId === rewardId && v.state === 'issued')),
+      filter((vouchers: IVoucher[]) => {
+        if (current === 0) {
+          firstPageVouchers = [
+            ...vouchers.map(v => v.id)
+          ];
+          return false;
+        }
+
+        if (vouchers && vouchers.length <= 0) {
+          firstPageVouchers = [];
+          return false;
+        }
+
+        newIssued = vouchers.filter(v => !firstPageVouchers.includes(v.id));
+        if (newIssued && newIssued.length <= 0) {
+          return false;
+        }
+
+        firstPageVouchers = [
+          ...vouchers.map(v => v.id)
+        ];
+
+        return true;
+      }),
+      map((_: IVoucher[]) => {
+        return newIssued;
+      })
+    );
   }
 }
