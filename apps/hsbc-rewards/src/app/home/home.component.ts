7820552dbe5bbafeeadbc960753ec2175ee3c276
Ryane Chia <ryanechia@outlook.com>
2019-08-20 19:39:44 +0800

    fix imports

--- apps/hsbc-rewards/src/app/home/home.component.ts
+++ apps/hsbc-rewards/src/app/home/home.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit, ViewChild, AfterViewInit, ChangeDetectorRef } from '@angular/core';
-import { Observable, of, BehaviorSubject } from 'rxjs';
-import { Router } from '@angular/router';
-import { IReward, RewardsService, LoyaltyService } from '@perx/core';
-import { LoyaltySummaryComponent } from '@perx/core';
-import { ITabConfig } from '@perx/core/dist/perx-core/lib/rewards/rewards-list-tabbed/rewards-list-tabbed.component';
+import {Component, OnInit, ViewChild, AfterViewInit, ChangeDetectorRef} from '@angular/core';
+import {Observable, of, BehaviorSubject} from 'rxjs';
+import {Router} from '@angular/router';
+import {IReward, RewardsService, LoyaltyService} from '@perx/core';
+import {LoyaltySummaryComponent, ITabConfig} from '@perx/core';
 
 const mockTags: ITabConfig[] = [
   {
@@ -37,7 +36,7 @@ export class HomeComponent implements OnInit, AfterViewInit {
   public tags: ITabConfig[];
   public rewards: Observable<IReward[]>;
 
-  @ViewChild('loyaltySummary', { static: false }) public loyaltySummary: LoyaltySummaryComponent;
+  @ViewChild('loyaltySummary', {static: false}) public loyaltySummary: LoyaltySummaryComponent;
 
   constructor(
     private rewardsService: RewardsService,
@@ -55,7 +54,10 @@ export class HomeComponent implements OnInit, AfterViewInit {
   public ngAfterViewInit(): void {
     // @ts-ignore to be verified
     this.loyaltySummary.loyalty$ = new BehaviorSubject({
-      pointsBalance: '100,000', expiringPoints: [{ expireDate: new Date('Jul 17 2017') }], points: 1000, expireDate: new Date('Jul 17 2017')
+      pointsBalance: '100,000',
+      expiringPoints: [{expireDate: new Date('Jul 17 2017')}],
+      points: 1000,
+      expireDate: new Date('Jul 17 2017')
     });
     this.cd.detectChanges();
   }
