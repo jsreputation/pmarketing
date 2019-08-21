a1324e24f293f4984a1a5e28ff393cb7bf264f93
Pierre Enriquez <pierre@perxtech.com>
2019-08-21 17:41:30 +0800

    Revert versions to fix test errors.

--- apps/oracle-pay/src/app/app.component.spec.ts
+++ apps/oracle-pay/src/app/app.component.spec.ts
@@ -1,12 +1,25 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { VouchersModule, VouchersService } from '@perx/core';
+import { of } from 'rxjs';
+
+const vouchersServiceStub = {
+  getAll: () => of([])
+};
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        VouchersModule
+      ],
+      providers: [
+        { provide: VouchersService, useValue: vouchersServiceStub }
       ],
       declarations: [
         AppComponent
@@ -14,20 +27,21 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
   it(`should have as title 'oracle-pay'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('oracle-pay');
+    expect(component.title).toEqual('oracle-pay');
   });
 
   it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to oracle-pay!');
