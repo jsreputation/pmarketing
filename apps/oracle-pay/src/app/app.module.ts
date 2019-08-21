020a5d124aa4a0e47ab2ffd94dc9c44594f01a65
Nicolas Lagier <nicolas@perxtech.com>
2019-08-21 13:04:35 +0800

    feature/oracle-pay

--- apps/oracle-pay/src/app/app.module.ts
+++ apps/oracle-pay/src/app/app.module.ts
@@ -4,15 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { VouchersModule, PuzzlesModule, VouchersService } from '@perx/core';
+
+const vouchersServiceStub = {};
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    VouchersModule,
+    PuzzlesModule
   ],
-  providers: [],
+  providers: [
+    { provide: VouchersService, useValue: vouchersServiceStub }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
