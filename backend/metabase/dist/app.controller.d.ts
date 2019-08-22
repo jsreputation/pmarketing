dcd79f7687fb6742670d50fe75cd030025ce6cb3
pavlovandreyka <andreypavlov1706@gmail.com>
2019-08-22 12:47:01 +0300

    remove metabase

--- backend/metabase/dist/app.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { AppService } from './app.service';
-import { ITokenResponse } from './token';
-export declare class AppController {
-    private readonly appService;
-    constructor(appService: AppService);
-    getToken(id: string): ITokenResponse;
-}
