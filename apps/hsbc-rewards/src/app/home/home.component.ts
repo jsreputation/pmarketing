40d669703e2a0441b3044a94325e1165a9457347
Golotin Denis <d.golotin@dunice.net>
2019-08-22 17:35:40 +0300

    temp solution

--- apps/hsbc-rewards/src/app/home/home.component.ts
+++ apps/hsbc-rewards/src/app/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { IReward, RewardsService, LoyaltyService, ILoyalty } from '@perx/core';
 import { ITabConfig } from '@perx/core';
-import { map, switchMap } from 'rxjs/operators';
+import { map, switchMap, mergeMap } from 'rxjs/operators';
 import { Observable, of, Subject, forkJoin } from 'rxjs';
 
 const tabs: ITabConfig[] = [
@@ -45,7 +45,6 @@ export class HomeComponent implements OnInit {
   public ngOnInit(): void {
     this.getRewardsCollection();
     this.getRewards();
-    this.getTags();
     this.loyalty$ = this.loyaltyService.getLoyalty(100)
       .pipe(map((loyalty: ILoyalty) => {
         loyalty.pointsBalance = 10000;
@@ -62,13 +61,14 @@ export class HomeComponent implements OnInit {
   }
 
   public getRewards(): void {
-    this.getTags().pipe(switchMap((tags: ITabConfig[]) => {
+    this.getTags().pipe(mergeMap((tags: ITabConfig[]) => {
       return forkJoin(tags.map((tab) => {
-        return this.rewardsService.getAllRewards(null, [tab.tabName]);
+        return this.rewardsService.getAllRewards(null, [tab.tabName])
+        .pipe(map((result:IReward[]) => { return { key: tab.tabName, value: result } }));
       }));
     })).subscribe((result) => {
-      result.forEach((rewards: IReward[], index) => {
-        this.staticTab[index].rewardsList = of(rewards);
+      result.forEach((rewards) => {
+        this.staticTab.find((elem)=>rewards.key === elem.tabName).rewardsList = of(rewards.value);
         this.tabs.next(this.staticTab);
       });
     });
@@ -78,6 +78,8 @@ export class HomeComponent implements OnInit {
     // todo: service not implemented yet
     // this.rewardsService.getTags();
     this.staticTab = tabs;
+    this.tabs.next(this.staticTab);
+    console.log('123')
     return of(tabs);
   }
 
