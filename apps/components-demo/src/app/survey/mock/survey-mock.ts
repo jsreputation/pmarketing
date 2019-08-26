67dd72c4097044daecb7aee40dd30541514fe7e1
andrew <andrew@perxtech.com>
2019-08-26 17:20:40 +0800

    lint maintain

--- apps/components-demo/src/app/survey/mock/survey-mock.ts
+++ apps/components-demo/src/app/survey/mock/survey-mock.ts
@@ -201,4 +201,4 @@ export const mock: ISurvey = {
             }
         }
     ]
-}
+};
--- apps/components-demo/src/app/survey/survey.module.ts
+++ apps/components-demo/src/app/survey/survey.module.ts
@@ -1,7 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SurveyModule as PerxSurveyModule} from '@perx/core'
+import { SurveyModule as PerxSurveyModule} from '@perx/core';
 
 import { SurveyRoutingModule } from './survey-routing.module';
 import { SurveyComponent } from './survey.component';
