93d5186d77adb5a151d9eee9d0441ea8a36d80d2
andrew <andrew@perxtech.com>
2019-08-26 11:28:41 +0800

    mock adjustment for test case

--- apps/components-demo/src/app/survey/mock/survey-mock.ts
+++ apps/components-demo/src/app/survey/mock/survey-mock.ts
@@ -4,68 +4,6 @@ export const mock: ISurvey = {
     title: 'Please help us knowing you better',
     questions: [
         {
-            question: 'How likely are you to recommend our service?',
-            description: 'Please rate us',
-            id: '1',
-            required: false,
-            payload: {
-                type: 'rating',
-                color: 'primary',
-                left_label: 'not much',
-                right_label: 'a lot',
-                scale: 5,
-                shape: 'star'
-            }
-        },
-        {
-            question: 'When were you born?',
-            description: 'It\'s between us',
-            id: '2',
-            required: true,
-            payload: {
-                type: 'date'
-            }
-        },
-        {
-            question: 'From when to when was your first job?',
-            description: 'Compliance',
-            id: '3',
-            required: false,
-            payload: {
-                type: 'date',
-                period: true
-            }
-        },
-        {
-            question: 'From when to when was your first job?',
-            description: 'Compliance',
-            id: '4',
-            required: false,
-            payload: {
-                type: 'date',
-                period: true
-            }
-        },
-        {
-            question: 'Tell us more about you',
-            description: 'Be frank',
-            id: '5',
-            required: false,
-            payload: {
-                type: 'long-text',
-                'max-length': 200
-            }
-        },
-        {
-            question: 'Tell us more about us',
-            description: 'No offence taken',
-            id: '6',
-            required: false,
-            payload: {
-                type: 'long-text'
-            }
-        },
-        {
             question: 'Tell us more about us',
             description: 'No offence taken',
             id: '7',
@@ -141,6 +79,68 @@ export const mock: ISurvey = {
                         }
                     }
                 ]
+            }
+        },
+        {
+            question: 'How likely are you to recommend our service?',
+            description: 'Please rate us',
+            id: '1',
+            required: false,
+            payload: {
+                type: 'rating',
+                color: 'primary',
+                left_label: 'not much',
+                right_label: 'a lot',
+                scale: 5,
+                shape: 'star'
+            }
+        },
+        {
+            question: 'When were you born?',
+            description: 'It\'s between us',
+            id: '2',
+            required: true,
+            payload: {
+                type: 'date'
+            }
+        },
+        {
+            question: 'From when to when was your first job?',
+            description: 'Compliance',
+            id: '3',
+            required: false,
+            payload: {
+                type: 'date',
+                period: true
+            }
+        },
+        {
+            question: 'From when to when was your first job?',
+            description: 'Compliance',
+            id: '4',
+            required: false,
+            payload: {
+                type: 'date',
+                period: true
+            }
+        },
+        {
+            question: 'Tell us more about you',
+            description: 'Be frank',
+            id: '5',
+            required: false,
+            payload: {
+                type: 'long-text',
+                'max-length': 20
+            }
+        },
+        {
+            question: 'Tell us more about us',
+            description: 'No offence taken',
+            id: '6',
+            required: false,
+            payload: {
+                type: 'long-text'
             }
         },
         {
