fe826626a152c9e642cad31d49e2c459bcc16cca
Jaye <jaye.tan@flags.fm>
2019-08-27 09:16:43 +0800

    added macaron and count down timer, added unit test

--- apps/starhub/src/app/reward/reward.component.spec.ts
+++ apps/starhub/src/app/reward/reward.component.spec.ts
@@ -1,14 +1,45 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 
 import { RewardComponent } from './reward.component';
 import { MatIconModule } from '@angular/material';
 import { RouterTestingModule } from '@angular/router/testing';
 import { RewardsService } from '@perx/core';
+import { Location } from '@angular/common';
+import { of, Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { Type } from '@angular/core';
 
 describe('RewardComponent', () => {
   let component: RewardComponent;
   let fixture: ComponentFixture<RewardComponent>;
-  const rewardsServiceStub = {};
+
+  const reward = {
+    id: 1,
+    name: 'Get a Free Coke',
+    description: '',
+    subtitle: '',
+    validFrom: null,
+    validTo: null,
+    rewardThumbnail: '',
+    rewardBanner: '',
+    merchantImg: '',
+    merchantName: 'Pizza Hut',
+    termsAndConditions: '',
+    howToRedeem: '',
+    merchantId: 2
+  };
+  const rewardsServiceStub = {
+    getReward: () => of(reward)
+  };
+  const locationStub = {
+    back: () => {}
+  };
+
+  class ActivatedRouteMock {
+    get queryParams(): Observable<any> {
+      return of({id: 1});
+    }
+  }
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -18,7 +49,9 @@ describe('RewardComponent', () => {
         RouterTestingModule
       ],
       providers: [
-        { provide: RewardsService, useValue: rewardsServiceStub }
+        { provide: RewardsService, useValue: rewardsServiceStub },
+        { provide: Location, useValue: locationStub },
+        { provide: ActivatedRoute, useClass: ActivatedRouteMock },
       ]
     })
       .compileComponents();
@@ -32,5 +65,118 @@ describe('RewardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('ngOnInit', () => {
+    it('should get id from params, get reward from reward service', fakeAsync(() => {
+      const activatedRoute = TestBed.get(ActivatedRoute);
+      const activatedRouteSpy = spyOnProperty(activatedRoute, 'queryParams', 'get').and.returnValue(of({id: 1}));
+
+      const rewardsService: RewardsService = fixture.debugElement.injector.get<RewardsService>(RewardsService as Type<RewardsService>);
+      const rewardsServiceSpy = spyOn(rewardsService, 'getReward').and.returnValue(of(reward));
+
+      component.ngOnInit();
+      tick();
+      expect(activatedRouteSpy).toHaveBeenCalled();
+      expect(rewardsServiceSpy).toHaveBeenCalled();
+      expect(component.reward).toBe(reward);
+    }));
+
+    it('should show macaron text and it should be Expiring, save button should be enabled', fakeAsync(() => {
+      const rewardValidFrom = new Date();
+      const rewardValidTo = new Date();
+      const expiringReward = {
+        id: 2,
+        name: 'Get a Free Coke',
+        description: '',
+        subtitle: '',
+        validFrom: new Date(rewardValidFrom.setHours(rewardValidFrom.getHours() + 96)),
+        validTo: new Date(rewardValidTo.setHours(rewardValidTo.getHours() + 36)),
+        rewardThumbnail: '',
+        rewardBanner: '',
+        merchantImg: '',
+        merchantName: 'Pizza Hut',
+        termsAndConditions: '',
+        howToRedeem: '',
+        merchantId: 2
+      };
+      const rewardsService: RewardsService = fixture.debugElement.injector.get<RewardsService>(RewardsService as Type<RewardsService>);
+      const rewardsServiceSpy = spyOn(rewardsService, 'getReward').and.returnValue(of(expiringReward));
+
+      component.ngOnInit();
+      tick();
+      expect(rewardsServiceSpy).toHaveBeenCalled();
+      expect(component.reward).toBe(expiringReward);
+      expect(component.macaronText).toBe('Expiring');
+      expect(component.isExpired).toBe(false);
+      expect(component.isExpiring).toBe(true);
+      expect(component.isButtonDisabled).toBe(false);
+    }));
+
+    it('should show macaron text and it should be Expired, save button should be disabled', fakeAsync(() => {
+      const expiringReward = {
+        id: 3,
+        name: 'Get a Free Coke',
+        description: '',
+        subtitle: '',
+        validFrom: new Date(),
+        validTo: new Date('Sun Aug 20 2019 22:09:08'),
+        rewardThumbnail: '',
+        rewardBanner: '',
+        merchantImg: '',
+        merchantName: 'Pizza Hut',
+        termsAndConditions: '',
+        howToRedeem: '',
+        merchantId: 2
+      };
+      const rewardsService: RewardsService = fixture.debugElement.injector.get<RewardsService>(RewardsService as Type<RewardsService>);
+      const rewardsServiceSpy = spyOn(rewardsService, 'getReward').and.returnValue(of(expiringReward));
+
+      component.ngOnInit();
+      tick();
+      fixture.detectChanges();
+
+      expect(rewardsServiceSpy).toHaveBeenCalled();
+      expect(component.reward).toBe(expiringReward);
+
+      tick(1000);
+      fixture.detectChanges();
+      expect(component.macaronText).toBe('Expired');
+      expect(component.isExpired).toBe(true);
+      expect(component.isButtonDisabled).toBe(true);
+
+    }));
+
+    it('should NOT show macaron text, save button should be enabled', fakeAsync(() => {
+      const rewardValidFrom = new Date();
+      const rewardValidTo = new Date();
+      const expiringReward = {
+        id: 4,
+        name: 'Get a Free Coke',
+        description: '',
+        subtitle: '',
+        validFrom: new Date(rewardValidFrom.setHours(rewardValidFrom.getHours() + 96)),
+        validTo: new Date(rewardValidTo.setHours(rewardValidTo.getHours() + 96)),
+        rewardThumbnail: '',
+        rewardBanner: '',
+        merchantImg: '',
+        merchantName: 'Pizza Hut',
+        termsAndConditions: '',
+        howToRedeem: '',
+        merchantId: 2
+      };
+      const rewardsService: RewardsService = fixture.debugElement.injector.get<RewardsService>(RewardsService as Type<RewardsService>);
+      const rewardsServiceSpy = spyOn(rewardsService, 'getReward').and.returnValue(of(expiringReward));
+
+      component.ngOnInit();
+      tick();
+      expect(rewardsServiceSpy).toHaveBeenCalled();
+      expect(component.reward).toBe(expiringReward);
+
+      expect(component.macaronText).toBe('');
+      expect(component.isExpired).toBe(false);
+      expect(component.isExpiring).toBe(false);
+      expect(component.isButtonDisabled).toBe(false);
+    }));
   });
 });
--- apps/starhub/src/app/reward/reward.component.ts
+++ apps/starhub/src/app/reward/reward.component.ts
@@ -2,7 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 import { IReward, RewardsService } from '@perx/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
-import { filter, switchMap, map } from 'rxjs/operators';
+import { filter, switchMap, map, take } from 'rxjs/operators';
+import { Observable, timer } from 'rxjs';
+
+interface ITime {
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+}
 
 @Component({
   selector: 'app-reward',
@@ -11,14 +19,18 @@ import { filter, switchMap, map } from 'rxjs/operators';
 })
 export class RewardComponent implements OnInit {
   public reward: IReward;
+  public dateTime$: Observable<ITime[]>;
+  public isExpired: boolean = false;
+  public isExpiring: boolean = false;
+  public isButtonDisabled: boolean = false;
+  public macaronText: string = '';
 
   constructor(
     private location: Location,
     private router: Router,
     private rewardsService: RewardsService,
     private activeRoute: ActivatedRoute
-  ) {
-  }
+  ) {}
 
   public ngOnInit(): void {
     this.activeRoute.queryParams
@@ -28,6 +40,50 @@ export class RewardComponent implements OnInit {
         switchMap((id: number) => this.rewardsService.getReward(id))
       )
       .subscribe((reward: IReward) => this.reward = reward);
+
+    const validTo = this.reward.validTo;
+
+    if (!validTo) {
+      return;
+    }
+    // set date 36 hours
+    // const mockDate = new Date(validTo.setHours(validTo.getHours() + 36));
+
+    // set date 10 seconds
+    // const mockDate = new Date(validTo.setSeconds(validTo.getSeconds() + 15));
+    const dateNow = new Date();
+
+    const differenceTime = validTo.valueOf() - dateNow.valueOf();
+    const differenceInHours = differenceTime / 1000 / 60 / 60;
+    let convertedtoSeconds = differenceInHours * 3600;
+
+    if (convertedtoSeconds <= 129600) {
+      this.isExpiring = true;
+      this.macaronText = 'Expiring';
+      this.dateTime$ = timer(0, 1000).pipe(
+        take(129600),
+        map(() => {
+          const time = Date.parse(String(validTo)) - Date.parse(String(new Date()));
+          const seconds = Math.floor( (time / 1000) % 60 );
+          const minutes = Math.floor( (time / 1000 / 60) % 60 );
+          const hours = Math.floor( (time / (1000 * 60 * 60)) % 24 );
+          const days = Math.floor( time / ( 1000 * 60 * 60 * 24) );
+
+          if (Math.round(convertedtoSeconds) <= 0) {
+            this.isExpired = true;
+            this.macaronText = 'Expired';
+            this.isButtonDisabled = true;
+          }
+          convertedtoSeconds--;
+          return [{
+            days,
+            hours,
+            minutes,
+            seconds
+          }];
+        })
+      );
+    }
   }
 
   public back(): void {
