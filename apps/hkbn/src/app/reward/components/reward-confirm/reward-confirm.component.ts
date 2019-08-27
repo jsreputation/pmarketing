4785661e314ae4b8d38209a45266946ecac5c78d
Golotin Denis <d.golotin@dunice.net>
2019-08-27 17:24:26 +0300

    PW-864

--- apps/hkbn/src/app/reward/components/reward-confirm/reward-confirm.component.ts
+++ apps/hkbn/src/app/reward/components/reward-confirm/reward-confirm.component.ts
@@ -1,15 +1,20 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { LoyaltyService } from '@perx/core';
+import { switchMap, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'hkbn-reward-confirm',
   templateUrl: './reward-confirm.component.html',
   styleUrls: ['./reward-confirm.component.scss']
 })
-export class RewardConfirmComponent {
-
-  constructor(private dialogRef: MatDialogRef<RewardConfirmComponent>,
-              @Inject(MAT_DIALOG_DATA) public data: any
+export class RewardConfirmComponent implements OnInit {
+  public loyalty: Observable<number>;
+  constructor(
+    private dialogRef: MatDialogRef<RewardConfirmComponent>,
+    private loyaltyService: LoyaltyService,
+    @Inject(MAT_DIALOG_DATA) public data: any
   ) {
   }
 
@@ -21,4 +26,11 @@ export class RewardConfirmComponent {
     this.dialogRef.close(false);
   }
 
+  ngOnInit() {
+    this.loyalty = this.loyaltyService.getLoyalties().pipe(switchMap((loyaltyes) => {
+      return this.loyaltyService.getLoyalty(loyaltyes[0].id);
+    })).pipe(map((loyalty) => {
+      return loyalty.expiringPoints[0].points;
+    }));
+  }
 }
