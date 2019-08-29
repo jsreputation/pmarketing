50936074b2f22ec6c3772b869ff02e5f66c5b0dd
Golotin Denis <d.golotin@dunice.net>
2019-08-29 17:03:00 +0300

    lint fix

--- libs/perx-core/projects/perx-core/src/lib/loyalty/loyalty-summary/loyalty-summary.component.ts
+++ libs/perx-core/projects/perx-core/src/lib/loyalty/loyalty-summary/loyalty-summary.component.ts
@@ -56,11 +56,11 @@ export class LoyaltySummaryComponent implements OnInit {
       };
     }
 
-    if(!this.summaryExpiringFn) {
+    if (!this.summaryExpiringFn) {
       this.summaryExpiringFn = (loyalty: ILoyalty): string => {
         const expiringPoints = loyalty && loyalty.expiringPoints && loyalty.expiringPoints.length ? loyalty.expiringPoints[0] : null;
         return expiringPoints && expiringPoints.expireDate && expiringPoints.points ?
-        `${expiringPoints.points} points will expire on ${this.datePipe.transform(expiringPoints.expireDate, 'd MMM y')}` : '';
+          `${expiringPoints.points} points will expire on ${this.datePipe.transform(expiringPoints.expireDate, 'd MMM y')}` : '';
       };
     }
     if (!this.profile$) {
