40f16e44f0a82588671d8c8be86eb904341daa0d
winde123 <edwinwan88@gmail.com>
2019-08-30 14:50:29 +0800

    created tc 16-20
    Implemented 16-20

--- apps/candyland/e2e/src/steps/InitiateCreateEngagementCpgn.steps.ts
+++ apps/candyland/e2e/src/steps/InitiateCreateEngagementCpgn.steps.ts
@@ -1,14 +1,16 @@
 import { Before, Given, Then, When, setDefaultTimeout } from 'cucumber';
 import { expect } from 'chai';
 import { element, by, protractor, browser } from 'protractor';
-import { DashboardAppPage } from '../pages/shakeTheTreeFlow.po';
+import { DashboardAppPage, EngagementAppPage } from '../pages/shakeTheTreeFlow.po';
 
 let DashboardPage: DashboardAppPage;
+let EngagementPage: EngagementAppPage;
 // setting step timeout time
 setDefaultTimeout(60 * 1000);
 Before( () => {
   // initializing page objects instances
   DashboardPage = new DashboardAppPage();
+  EngagementPage = new EngagementAppPage();
 });
 // Engagements tab is visible to customer
 Given(/^1_I am on the dashboard page.$/, async () => {
@@ -47,50 +49,44 @@ Then(/^2_I will be redirected to the the engagment page.$/, async () => {
   await browser.wait(ec.urlContains('engagements'), 5000);
 });
 
-        /* Given('{int}_that i am at the engagement page.', function (int) {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
+// Dialog present when customer clicks on create new button.
+
+Given(/^3_that i am at the engagement page.$/, async () => {
+  await EngagementPage.navigateToEngagement();
+});
+
+When(/^3_I click on the create new button.$/, async () => {
+  await element.all(by.css('button')).get(2).click();
+});
+
+Then(/^3_the dialg box is present.$/, async () => {
+  // doing on an assertion on the presence of the dialog box
+  expect(await element(by.className('mat-dialog-content')).isPresent()).to.equal(true);
+});
+
+// The four engagment options are present.
+Given(/^4_I am on the create engagement option dialog$/, async () => {
+  await EngagementPage.navigateToEngagement();
+  await element.all(by.css('button')).get(2).click();
+});
+
+When(/4_I do nothing.$/, () => {});
+
+Then(/^4_There are 4 engagement options available.$/, async () => {
+  // doing an assertion on the number of elements
+  expect(await element.all(by.css('cl-type-item')).count()).to.be.equal(4);
+});
+// Client is able interact with the engagement campaign options
+Given(/^5_I am on the create option dialog box.$/, async () => {
+  await EngagementPage.navigateToEngagement();
+  await element.all(by.css('button')).get(2).click();
          });
 
-         When('{int}_I click on the create new button.', function (int) {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
-\
+When(/^5_I click on the stamps option.$/, async () => {
+  await element.all(by.css('cl-type-item')).get(2).click();
+});
 
-         Then('{int}_the dialg box is present.', function (int) {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
-
-         Given('{int}_I am on the create engagement option dialog', function (int) {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
-
-         When('{int}_I do nothing.', function (int) {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
-
-         Then('{int}_There are {int} engagement options available.', function (int, int2) {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
-
-         Given('{int}_I am on the create option dialog box.', function (int) {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
-
-         When('{int}_I click on the stamps option.', function (int) {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
-
-         Then('{int}_The stamp option is highlighted.', function (int) {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
-
-         */
+Then(/^5_The stamp option is highlighted.$/, async () => {
+  // assertion based on the active item selector
+  expect(await element(by.css('div.type-item.active-item>div.type-wrap-img>img')).getAttribute('alt')).to.equal('Stamp');
+});
