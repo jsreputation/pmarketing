0eb4ffd14b7079cf02cfe462df948daa01de3d55
winde123 <edwinwan88@gmail.com>
2019-08-30 11:56:28 +0800

    added tc 17 and modified script to accomodate the walkme button popup

--- apps/candyland/e2e/src/steps/InitiateCreateEngagementCpgn.steps.ts
+++ apps/candyland/e2e/src/steps/InitiateCreateEngagementCpgn.steps.ts
@@ -10,9 +10,12 @@ Before( () => {
   // initializing page objects instances
   DashboardPage = new DashboardAppPage();
 });
-
+// Engagements tab is visible to customer
 Given(/^1_I am on the dashboard page.$/, async () => {
+  const ec = protractor.ExpectedConditions;
   await DashboardPage.navigateToDashboard();
+  // work around the walk me button presence
+  await browser.wait(ec.elementToBeClickable(element(by.className('walkme-custom-balloon-button-text'))), 5000);
   await element(by.className('walkme-custom-balloon-button-text')).click();
   await DashboardPage.navigateToDashboard();
 });
@@ -26,22 +29,25 @@ Then(/^1_I should see the engagment tab.$/, async () => {
   expect(await element.all(by.css('h3')).get(2).getText()).to.be.equal('Engagements');
 });
 
-        /* Given('{int}_that I am on the dashboard page.', function (int) {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
+// Clicking on the Engagment tabs leads to the engagment page.
+Given(/^2_that I am on the dashboard page.$/, async () => {
+  await DashboardPage.navigateToDashboard();
+  // await browser.sleep(2000);
+});
 
-         When('{int}_I click on the engagement tab.', function (int) {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
+When(/^2_I click on the engagement tab.$/, async () => {
+  const ec = protractor.ExpectedConditions;
+  await browser.wait(ec.elementToBeClickable(element.all(by.css('h3')).get(2)), 5000);
+  await element.all(by.css('h3')).get(2).click();
+});
 
-         Then('{int}_I will be redirected to the the engagment page.', function (int) {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
+Then(/^2_I will be redirected to the the engagment page.$/, async () => {
+  // doing a assertion based on the url path
+  const ec = protractor.ExpectedConditions;
+  await browser.wait(ec.urlContains('engagements'), 5000);
+});
 
-         Given('{int}_that i am at the engagement page.', function (int) {
+        /* Given('{int}_that i am at the engagement page.', function (int) {
            // Write code here that turns the phrase above into concrete actions
            return 'pending';
          });
