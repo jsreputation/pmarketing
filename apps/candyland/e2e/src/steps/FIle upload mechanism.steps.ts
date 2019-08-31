ce1c3f46328f611aa4fe029de63a8ae1d15103f7
winde123 <edwinwan88@gmail.com>
2019-08-31 20:37:17 +0800

    implemented tc 22 to 23

--- apps/candyland/e2e/src/steps/FIle upload mechanism.steps.ts
+++ apps/candyland/e2e/src/steps/FIle upload mechanism.steps.ts
@@ -2,12 +2,11 @@ import { Before, Given, Then, When } from 'cucumber';
 import { expect } from 'chai';
 import { browser, element, by } from 'protractor';
 import * as path from 'path' ;
-// const path = require('path');
-
 import { CreateShakeTheTreeAppPage } from '../pages/shakeTheTreeFlow.po';
 
 let PageShakeTheTree: CreateShakeTheTreeAppPage ;
 
+// Successful file upload for gift box
 Before( () => {
   // initializing page objects instances
   PageShakeTheTree = new CreateShakeTheTreeAppPage();
@@ -18,47 +17,61 @@ Given(/^1_I am on the shake a tree game creation page$/, async () => {
 });
 
 When(/^1_I upload a file$/, async () => {
+  // creating var for url path
  const FileToUpload = './Downloads/testimg.png';
- const absolutePath = path.resolve('/Users/perx', FileToUpload); // __dirname
+ const absolutePath = path.resolve('/Users/perx', FileToUpload); // __dirname when inplementing circle ci later
+ // upload the file to the gift img upload section
  await element.all(by.css('input[type="file"]')).get(0).sendKeys(absolutePath);
-  // await element(by.id('uploadButton')).click();
+
 });
 
 Then(/^1_The file uploaded is present in the upload field under the gift box category .$/, async () => {
+  // doing an assertion based on the attribute of the img obj
   expect(await element(by.css('div.image-wrap.ng-star-inserted>img.image')).getAttribute('alt')).to.be.contain('upload');
 });
 
+// Successful file upload for background image
+Given(/^2_I am on the shake a tree game creation page$/, async () => {
+  await PageShakeTheTree.navigateToShakeTheTree();
+  await browser.sleep(3000);
+});
+
+When(/^2_I upload a file$/, async () => {
+  // creating var for url path
+  const FileToUpload = './Downloads/testimg.png';
+  const absolutePath = path.resolve('/Users/perx', FileToUpload); // __dirname when inplementing circle ci later
+  // upload the file to the gift background upload section
+  await element.all(by.css('input[type="file"]')).get(1).sendKeys(absolutePath);
+});
+
+Then(/^2_The file uploaded is present in the upload field under the background category.$/, async () => {
+  // doing an assertion based on the attribute of the img obj
+  expect(await element(by.css('div.image-wrap.ng-star-inserted>img.image')).getAttribute('alt')).to.be.contain('upload');
+});
+
+// Wrong file upload for gift box
+Given(/^3_I am on the shake a tree game creation page$/, async () => {
+  await PageShakeTheTree.navigateToShakeTheTree();
+  await browser.sleep(3000);
+});
+
+When(/^3_I upload a file with wrong format$/, async () => {
+  // creating var for url path
+  const FileToUpload = './Downloads/testfile.xyz';
+  const absolutePath = path.resolve('/Users/perx', FileToUpload); // __dirname when inplementing circle ci later
+  // upload the file to the gift background upload section
+  await element.all(by.css('input[type="file"]')).get(0).sendKeys(absolutePath);
+});
+
+Then(/^3_my file should not be successfully uploaded.$/, async () =>  {
+  // do an assertion based on the message shown
+  expect(await element(by.css('div.upload-error-wrap.ng-star-inserted>span')).getText()).to.be.equal('Only .JPG or .PNG are supported. ');
+  // do an assertion where there are still 2 empty input fields
+  expect(await element.all(by.css('input[type="file"]')).count()).to.be.equal(2);
+});
+
+// Wrong file upload for background image
 /*Given('I am on the shake a tree game creation page', function() {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
-
-When('I upload a file', function() {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
-
-Then('The file uploaded is present in the upload field under the background category.', function() {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
-
-Given('I am on the shake a tree game creation page', function() {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
-
-When('I upload a file with wrong format', function() {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
-
-Then('my file should not be successfully uploaded.', function() {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
-
-Given('I am on the shake a tree game creation page', function() {
            // Write code here that turns the phrase above into concrete actions
            return 'pending';
          });
