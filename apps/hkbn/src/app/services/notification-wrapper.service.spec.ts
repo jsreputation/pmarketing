28514677663af10599808b00727ffc6121beabb8
Golotin Denis <d.golotin@dunice.net>
2019-09-03 11:30:03 +0300

    unit test for notificationwrapper

--- apps/hkbn/src/app/services/notification-wrapper.service.spec.ts
+++ apps/hkbn/src/app/services/notification-wrapper.service.spec.ts
@@ -1,12 +1,34 @@
 import { TestBed } from '@angular/core/testing';
-
 import { NotificationWrapperService } from './notification-wrapper.service';
+import { NotificationService } from '@perx/core';
 
 describe('NotificationWrapperService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: NotificationWrapperService;
+  let notification: NotificationService;
+  const notificationServiceStub = {
+    $popup: { subscribe: () => ({}) },
+    addPopup: (val) => { val }
+  };
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [{
+      provide: NotificationService, useValue: notificationServiceStub
+    },
+      NotificationWrapperService
+    ]
+  }));
+  beforeEach(() => {
+    notification = TestBed.get(NotificationService);
+    service = TestBed.get(NotificationWrapperService);
+  });
 
   it('should be created', () => {
-    const service: NotificationWrapperService = TestBed.get(NotificationWrapperService);
     expect(service).toBeTruthy();
+  });
+
+  it('should call add popup from Notification', () => {
+    const spy = spyOn(notification, 'addPopup');
+    const config = {title: 'hello'};
+    service.addPopup(config);
+    expect(spy).toHaveBeenCalledWith(config);
   });
 });
