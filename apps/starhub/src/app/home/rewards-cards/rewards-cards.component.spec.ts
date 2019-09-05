a9ee69a79ffc6f52f6d72c7544fc5f1cae33308c
Salman Khalid <salmankhalid1@gmail.com>
2019-09-05 08:52:57 +0500

    Feedback changes

--- apps/starhub/src/app/home/rewards-cards/rewards-cards.component.spec.ts
+++ apps/starhub/src/app/home/rewards-cards/rewards-cards.component.spec.ts
@@ -40,14 +40,14 @@ describe('RewardsCardsComponent', () => {
   describe('isComingSoon & expiring values', () => {
     it('should return true with isComingSoon', () => {
       const currentTime = new Date();
-      const validDateTo = new Date(currentTime.setDate(currentTime.getDate() + 2)); // set to 24hrs
+      const validDateTo = new Date(currentTime.setDate(currentTime.getDate() + 2)); // set to 48hrs
       const comingSoonValue = component.isComingSoon(String(validDateTo));
       expect(comingSoonValue).toBe(true);
     });
 
     it('should return true with isExpiring', () => {
       const currentTime = new Date();
-      const validDateTo = new Date(currentTime.setDate(currentTime.getDate() + 1)); // set to 48hrs
+      const validDateTo = new Date(currentTime.setDate(currentTime.getDate() + 1)); // set to 24hrs
       const result = component.isExpiring(String(validDateTo));
       expect(result).toBe(true);
     });
