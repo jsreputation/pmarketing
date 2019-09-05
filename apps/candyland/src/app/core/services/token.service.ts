67c085aee4e8b4eba03d294e5ad4612903ff0141
Dmitry Yanakov <dima.yanakov.light.it@gmail.com>
2019-09-05 14:21:25 +0300

    fixed bug of absence of redirection after login

--- apps/candyland/src/app/core/services/token.service.ts
+++ apps/candyland/src/app/core/services/token.service.ts
@@ -1,27 +1,39 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { map, skip } from 'rxjs/operators';
 import { LocalStorageService } from './local-storage.service';
 
 @Injectable()
-export class TokenService {
-  private userToken$: BehaviorSubject<any> = new BehaviorSubject<any>(null);
+export class SessionService {
+
+  public isActiveSession$;
+  private authToken$: BehaviorSubject<any>;
 
   constructor(private localStorage: LocalStorageService) {
-    const localToken = this.localStorage.get('authToken') || null;
-    if (!!localToken) {
-      this.userToken$.next(localToken);
-    }
+    const localToken = this.localStorage.get('authToken');
+    this.authToken$ = new BehaviorSubject<any>(localToken);
+    this.authToken$.pipe(skip(1)).subscribe((token) => {
+      if (token) {
+        this.localStorage.set('authToken', token);
+      } else {
+        this.localStorage.remove('authToken');
+      }
+    });
+    this.isActiveSession$ = this.authToken$
+      .pipe(
+        map(token => !!token)
+      );
   }
 
   public get token(): string {
-    return this.userToken$.getValue();
+    return this.authToken$.getValue();
   }
 
   public set token(value: string) {
-    this.userToken$.next(value);
+    this.authToken$.next(value);
   }
 
   public remove(): void {
-    this.userToken$.complete();
+    this.authToken$.next(null);
   }
 }
