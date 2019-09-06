055ff91aa8a62a0630612bd6d34520da587fc4d7
Jaye <jaye.tan@flags.fm>
2019-09-06 14:42:11 +0800

    added voucher component unit test

--- apps/starhub/src/app/voucher/voucher.component.spec.ts
+++ apps/starhub/src/app/voucher/voucher.component.spec.ts
@@ -1,19 +1,45 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 
 import { VoucherComponent } from './voucher.component';
 import { MatIconModule } from '@angular/material';
 import { RouterTestingModule } from '@angular/router/testing';
-import { VouchersService } from '@perx/core';
+import { VouchersService, VoucherState, RedemptionType } from '@perx/core';
 import { LocationShortFormatComponent } from '../location-short-format/location-short-format.component';
 import { RewardDetailComponent } from '../reward/reward-detail/reward-detail.component';
 import { ExpireTimerComponent } from '../reward/expire-timer/expire-timer.component';
+import { ActivatedRoute, Params } from '@angular/router';
+import { of, Subject } from 'rxjs';
 
 describe('VoucherComponent', () => {
   let component: VoucherComponent;
   let fixture: ComponentFixture<VoucherComponent>;
-  const vouchersServiceStub = {};
+
+  const voucher = {
+    id: 1,
+    rewardId: 1,
+    state: VoucherState.issued,
+    name: '',
+    code: '',
+    redemptionType: RedemptionType.pin,
+    thumbnailImg: '',
+    rewardBanner: '',
+    merchantImg: '',
+    merchantName: '',
+    merchantId: 1,
+    expiry: null,
+    redemptionDate: null,
+    description: [],
+    redemptionSuccessTxt: '',
+    redemptionSuccessImg: '',
+  };
+  const vouchersServiceStub = {
+    get: () => of(voucher)
+  };
+  let params: Subject<Params>;
 
   beforeEach(async(() => {
+    params = new Subject<Params>();
+
     TestBed.configureTestingModule({
       declarations: [VoucherComponent, LocationShortFormatComponent, RewardDetailComponent, ExpireTimerComponent],
       imports: [
@@ -21,6 +47,10 @@ describe('VoucherComponent', () => {
         RouterTestingModule
       ],
       providers: [
+        { provide: VouchersService, useValue: vouchersServiceStub },
+        {
+          provide: ActivatedRoute, useValue: { queryParams: params }
+        },
         { provide: VouchersService, useValue: vouchersServiceStub }
       ]
     })
@@ -35,5 +65,31 @@ describe('VoucherComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('onInit', () => {
+    it('should get voucher from service if id is present', fakeAsync(() => {
+      params.next({id: '1'});
+      component.ngOnInit();
+      tick();
+      expect(component.voucher).toBe(voucher);
+    }));
+
+    it('should voucher be undefined if param id is not present', () => {
+      params.next({id: null});
+      expect(component.voucher).toBe(undefined);
+    });
+  });
+
+  describe('setButton', () => {
+    it('should set isButtonEnable to true', () => {
+      component.setButton(true);
+      expect(component.isButtonEnable).toBe(true);
+    });
+
+    it('should set isButtonEnable to false', () => {
+      component.setButton(false);
+      expect(component.isButtonEnable).toBe(false);
+    });
   });
 });
