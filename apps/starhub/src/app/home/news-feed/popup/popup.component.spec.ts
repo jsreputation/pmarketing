b186e420ef7405083be342ffbaf55e8edd965d24
Nicolas Lagier <nicolas@perxtech.com>
2019-09-08 10:15:19 +0800

    STAR-51 fix tests

--- apps/starhub/src/app/home/news-feed/popup/popup.component.spec.ts
+++ apps/starhub/src/app/home/news-feed/popup/popup.component.spec.ts
@@ -1,16 +1,31 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PopupComponent } from './popup.component';
+import { MatIconModule, MAT_DIALOG_DATA } from '@angular/material';
+import { FeedItem } from '@perx/core';
 
 describe('PopupComponent', () => {
   let component: PopupComponent;
   let fixture: ComponentFixture<PopupComponent>;
+  const dataMock: FeedItem = {
+    title: '',
+    description: '',
+    link: '',
+    guid: '',
+    pubDate: new Date()
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PopupComponent ]
+      declarations: [PopupComponent],
+      imports: [
+        MatIconModule
+      ],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: dataMock }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
