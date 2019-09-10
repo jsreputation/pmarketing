121cc151200d436e265f9e8ae08b025d2e54b72d
winde123 <edwinwan88@gmail.com>
2019-09-10 18:18:36 +0800

    Adding a running number to the features ,adding the feature file for the e2e survey engagement flow and steps file for e2e survey flow

---
+++ apps/candyland/e2e/src/steps/6_Successful creation flow of engagement survey.steps.ts
@@ -0,0 +1,40 @@
+/*import { Before, Given, Then, When } from 'cucumber';
+import { expect } from 'chai';
+import { browser, element, by , protractor } from 'protractor';
+import { EngagementAppPage } from '../pages/shakeTheTreeFlow.po';
+
+Given('{int}_I am on the engagment page.', function (int) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Given('{int}_I click on create new button.', function (int) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Given('{int}_I click on survey option.', function (int) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Given('{int}_I click on the next button.', function (int) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Given('{int}_I type the test string.', function (int) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  When('{int}_I press save button.', function (int) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+Then('{int}_Game is present under the engagment category .', function (int) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+  */
