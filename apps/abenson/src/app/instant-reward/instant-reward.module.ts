e930ae02db4b0489bfb60206a4fd6e7fc8fb60e4
Arina Mahmudova <a.mahmudova@dunice.net>
2019-09-11 18:06:07 +0300

    create project skeleton

---
+++ apps/abenson/src/app/instant-reward/instant-reward.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { InstantRewardRoutingModule } from './instant-reward-routing.module';
+import { RewardsModule } from '@perx/core';
+import { RewardComponent } from './reward/reward.component';
+import { MatButtonModule } from '@angular/material';
+
+@NgModule({
+  declarations: [RewardComponent],
+  imports: [
+    CommonModule,
+    RewardsModule,
+    MatButtonModule,
+    InstantRewardRoutingModule
+  ]
+})
+export class InstantRewardModule { }
