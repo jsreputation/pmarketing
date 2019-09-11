2f89d2ca0ec1df4241985f914da213ed089ba5c3
Nicolas Lagier <nicolas@perxtech.com>
2019-09-11 16:35:32 +0800

    PW-37 cleanup interface

--- libs/perx-core/projects/perx-core/src/lib/vouchers/ivoucher.service.ts
+++ libs/perx-core/projects/perx-core/src/lib/vouchers/ivoucher.service.ts
@@ -1,10 +1,10 @@
 import { Observable } from 'rxjs';
-import {IGetVoucherParams, IVoucher} from './models/voucher.model';
+import { IGetVoucherParams, IVoucher, IRedeemOptions } from './models/voucher.model';
 
 export abstract class IVoucherService {
   public abstract getAll(voucherParams?: IGetVoucherParams): Observable<IVoucher[]>;
   public abstract get(id: number, useCache?: boolean): Observable<IVoucher>;
-  public abstract redeemVoucher(id: number): Observable<any>;
+  public abstract redeemVoucher(id: number, options?: IRedeemOptions): Observable<any>;
   public abstract reset(vouchers?: IVoucher[]): void;
   public abstract newVouchersCreatedForReward(rewardId: number, intervalPeriod?: number): Observable<IVoucher[]>;
   public abstract stateChangedForVoucher(voucherId: number, intervalPeriod?: number): Observable<IVoucher>;
