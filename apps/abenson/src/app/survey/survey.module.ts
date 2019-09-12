c4fe7b9bcac9f0c3472af455c09d6275df984197
andrew <andrew@perxtech.com>
2019-09-12 19:17:18 +0800

    change backm not fix in this branch

--- apps/abenson/src/app/survey/survey.module.ts
+++ apps/abenson/src/app/survey/survey.module.ts
@@ -10,7 +10,7 @@ import { survey } from '../mock/survey.mock';
 
 const surveyServiceStub = {
   // @ts-ignore
-  getSurveyFromCampaign: (id: number) => of(survey)
+  getSurvey: (id: number) => of(survey)
 };
 
 @NgModule({
