e0653261eb5d4ce2936800df1691d5e7821112eb
Nicolas Lagier <nicolas@perxtech.com>
2019-09-12 16:17:46 +0800

    PW-1128 fix broken tests

--- apps/blackcomb/src/app/reward/reward.component.spec.ts
+++ apps/blackcomb/src/app/reward/reward.component.spec.ts
@@ -1,16 +1,26 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RewardComponent } from './reward.component';
+import { RewardsModule, RewardsService } from '@perx/core';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('RewardComponent', () => {
   let component: RewardComponent;
   let fixture: ComponentFixture<RewardComponent>;
+  const rewardsServiceStub = {};
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RewardComponent ]
+      declarations: [RewardComponent],
+      imports: [
+        RewardsModule,
+        RouterTestingModule
+      ],
+      providers: [
+        { provide: RewardsService, useValue: rewardsServiceStub }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
