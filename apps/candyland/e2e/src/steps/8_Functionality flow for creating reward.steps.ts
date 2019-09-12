eb6fd9395c83e668908be2e60e8f589f08c9da3c
winde123 <edwinwan88@gmail.com>
2019-09-12 20:53:14 +0800

    implemented tc 10-13

--- apps/candyland/e2e/src/steps/8_Functionality flow for creating reward.steps.ts
+++ apps/candyland/e2e/src/steps/8_Functionality flow for creating reward.steps.ts
@@ -1,6 +1,6 @@
 import { Before, Given, Then, When } from 'cucumber';
 import { expect } from 'chai';
-import { browser, element, by , protractor } from 'protractor';
+import { browser, element, by , protractor, ElementFinder } from 'protractor';
 import { CreateRewardAppPage } from '../pages/shakeTheTreeFlow.po';
 
 let CreateRewardPage: CreateRewardAppPage;
@@ -84,62 +84,90 @@ Then(/^9_I should see four options.$/, async () => {
 });
 
 // Verifiying that there is an upload field when clicking on the option user upload
-/*Given('{int}_that I am on the survey creation page.', function(int) {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
+Given(/^10_that I am on the survey creation page.$/, async () => {
+  await CreateRewardPage.navigateToRewardCreate();
+  await browser.executeScript('document.querySelector("div.page-header.full-with").style.position = "absolute"');
+  // browser.sleep(3000);
+});
 
-When('{int}_I click on the user upload button.', function(int) {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
+When(/^10_I click on the user upload button.$/, async () => {
+  const ec = protractor.ExpectedConditions;
+  // waiting for user upload radio button to load
+  // await browser.actions().sendKeys(protractor.Key.HOME).perform();
+  await browser.wait(ec.elementToBeClickable(element.all(by.className('mat-radio-ripple mat-ripple')).get(4)), 6000);
+  // getting the element finder for the radio button for user upload
+  const elementRadioButton: ElementFinder = element.all(by.css('div.mat-radio-outer-circle')).get(4);
+  browser.executeScript('arguments[0].scrollIntoView(true);', elementRadioButton.getWebElement()).then(function anon(): void {
+    elementRadioButton.click();
+  });
+});
 
-Then('{int}_There should be an upload field.', function(int) {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
+Then(/^10_There should be an upload field.$/, async () => {
+  const ec = protractor.ExpectedConditions;
+  // waiting for the file upload field to be present
+  await browser.wait(ec.presenceOf(element.all(by.css('input[type=file]')).get(1)), 5000);
+  // asserting for the presence of the file upload field
+  expect(await element.all(by.css('input[type=file]')).get(1).isPresent()).to.be.equal(true);
+});
 
-Given('{int}_that I am on the survey creation page.', function(int) {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
+// Verifiying that the slider functionality is working for voucher limits per campaign
+Given(/^11_that I am on the survey creation page.$/, async () => {
+  await CreateRewardPage.navigateToRewardCreate();
+});
 
-When('{int}_I click on the slider .', function(int) {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
+When(/^11_I click on the slider .$/, async () => {
+  const ec = protractor.ExpectedConditions;
+  // waiting for the slider to load for voucher limits per campaign
+  await browser.wait(ec.elementToBeClickable(element.all(by.css('input[type=checkbox]')).get(1)), 6000);
+  // clicking on the slider
+  await element.all(by.className('mat-slide-toggle-thumb-container')).get(0).click();
+});
 
-Then('{int}_I should be type a value in the voucher field and select the frequency.', function(int) {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
+Then(/^11_I should be type a value in the voucher field and select the frequency.$/, async () => {
+  const ec = protractor.ExpectedConditions;
+  // waiting for number field for voucher limits per campaign to load
+  await browser.wait(ec.presenceOf(element.all(by.css('input[type=number]')).get(1)), 6000);
+  // doing on the assertion on the disable attr of the number field
+  expect(await element.all(by.css('input[type=number]')).get(2).getAttribute('disabled')).to.be.equal(null);
+});
 
-Given('{int}_that I am on the survey creation page.', function(int) {
-            // Write code here that turns the phrase above into concrete actions
-            return 'pending';
-          });
+// Verifiying that the slider functionality is working for issuance limits per user
+Given(/^12_that I am on the survey creation page.$/, async () => {
+  await CreateRewardPage.navigateToRewardCreate();
+});
 
-When('{int}_I click on the slider .', function(int) {
-            // Write code here that turns the phrase above into concrete actions
-            return 'pending';
-          });
+When(/^12_I click on the slider .$/, async () => {
+  const ec = protractor.ExpectedConditions;
+  // waiting for the slider to load for voucher limits per campaign
+  await browser.wait(ec.elementToBeClickable(element.all(by.css('input[type=checkbox]')).get(2)), 6000);
+  // clicking on the slider
+  await element.all(by.className('mat-slide-toggle-thumb-container')).get(1).click();
+});
 
-Then('{int}_I should be able to type a value in the times field and select the frequency.', function(int) {
-            // Write code here that turns the phrase above into concrete actions
-            return 'pending';
-          });
+Then(/^12_I should be able to type a value in the times field and select the frequency.$/, async () => {
+  const ec = protractor.ExpectedConditions;
+  // waiting for number field for voucher limits per campaign to load
+  await browser.wait(ec.presenceOf(element.all(by.css('input[type=number]')).get(2)), 6000);
+  // doing on the assertion on the disable attr of the number field
+  expect(await element.all(by.css('input[type=number]')).get(3).getAttribute('disabled')).to.be.equal(null);
+});
 
-Given('{int}_that I am on the survey creation page.', function(int) {
-            // Write code here that turns the phrase above into concrete actions
-            return 'pending';
-          });
+// Verifiying that the slider functionality is working for redemption limits per user
+Given(/^13_that I am on the survey creation page.$/, async () => {
+  await CreateRewardPage.navigateToRewardCreate();
+});
 
-When('{int}_I click on the slider .', function(int) {
-            // Write code here that turns the phrase above into concrete actions
-            return 'pending';
-          });
+When(/^13_I click on the slider .$/, async () => {
+  const ec = protractor.ExpectedConditions;
+  // waiting for the slider to load for voucher limits per campaign
+  await browser.wait(ec.elementToBeClickable(element.all(by.css('input[type=checkbox]')).get(3)), 6000);
+  // clicking on the slider
+  await element.all(by.className('mat-slide-toggle-thumb-container')).get(2).click();
+});
 
-Then('{int}_I should be able to type a value in the times field and select the frequency.', function(int) {
-            // Write code here that turns the phrase above into concrete actions
-            return 'pending';
-          });*/
+Then(/^13_I should be able to type a value in the times field and select the frequency.$/, async () => {
+  const ec = protractor.ExpectedConditions;
+  await browser.wait(ec.presenceOf(element.all(by.css('input[type=number]')).get(4)), 6000);
+  // doing on the assertion on the disable attr of the number field
+  expect(await element.all(by.css('input[type=number]')).get(4).getAttribute('disabled')).to.be.equal(null);
+});
