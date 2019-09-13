c4efae01fafeab2681e8f987a39be00e573d5a7a
Golotin Denis <d.golotin@dunice.net>
2019-09-13 10:07:27 +0300

    fix phone is missed

--- apps/hkbn/src/app/account/containers/change-password/change-password.component.ts
+++ apps/hkbn/src/app/account/containers/change-password/change-password.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthenticationService } from '@perx/core';
-import { IChangePasswordData } from '@perx/core';
+import { AuthenticationService, IProfile, IChangePasswordData, ProfileService } from '@perx/core';
 import { TranslateService } from '@ngx-translate/core';
 import { DataTransferService } from 'src/app/services/data-transfer.service';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'hkbn-change-password',
@@ -13,14 +13,17 @@ import { DataTransferService } from 'src/app/services/data-transfer.service';
 export class ChangePasswordComponent implements OnInit {
   public messageSuccess: string;
   public messageError: string;
+  public profile: IProfile
   constructor(
     private router: Router,
     private authService: AuthenticationService,
     private translate: TranslateService,
-    private dataTransferService: DataTransferService
+    private dataTransferService: DataTransferService,
+    private profileService: ProfileService
   ) {
   }
   public ngOnInit(): void {
+    this.profileService.whoAmI().pipe(take(1)).subscribe((prof)=>this.profile = prof);
     this.translate.get(['PASSWORD_SUCCESS_UPDATE', 'INCORRECT_PASSWORD']).subscribe((dictionary) => {
       this.messageSuccess = dictionary.PASSWORD_SUCCESS_UPDATE;
       this.messageError = dictionary.INCORRECT_PASSWORD;
@@ -28,7 +31,7 @@ export class ChangePasswordComponent implements OnInit {
   }
 
   public changePassword(data: IChangePasswordData): void {
-    this.authService.requestVerificationToken().subscribe(() => {
+    this.authService.requestVerificationToken(this.profile.phone).subscribe(() => {
       this.dataTransferService.newxUpdateData(data);
       this.router.navigate(['account', 'verify_token', 'password']);
     });
