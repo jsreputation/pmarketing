e09ab5fe6c7102c26442a9ef26d6fdc8ecc6e96e
Salman Khalid <salmankhalid1@gmail.com>
2019-09-14 13:03:50 +0500

    2 or more games should be displayed in carousel

--- apps/starhub/src/app/home/campaigns/campaigns.component.ts
+++ apps/starhub/src/app/home/campaigns/campaigns.component.ts
@@ -1,5 +1,7 @@
 import { Component, Output, EventEmitter, OnInit } from '@angular/core';
-import { ICampaign, CampaignType, ICampaignService } from '@perx/core';
+import { ICampaign, CampaignType, ICampaignService, IGameService, IGame } from '@perx/core';
+import { map, switchMap } from 'rxjs/operators';
+import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-campaigns',
@@ -8,24 +10,46 @@ import { ICampaign, CampaignType, ICampaignService } from '@perx/core';
 })
 export class CampaignsComponent implements OnInit {
   public campaigns: ICampaign[];
+  public games: IGame[];
 
   @Output()
   public tapped: EventEmitter<ICampaign> = new EventEmitter();
 
   constructor(
-    private campaignService: ICampaignService
+    private campaignService: ICampaignService,
+    private gameService: IGameService
   ) {}
 
   public ngOnInit(): void {
-    this.campaignService.getCampaigns()
-      .subscribe((campaigns: ICampaign[]) => {
-        if (campaigns.length > 0) {
-          this.campaigns = campaigns.filter((campaign) => campaign.type === CampaignType.game);
-        }
+
+    this.campaignService.getCampaigns().pipe(
+        map((campaigns: ICampaign[]) => campaigns.filter((campaign) => campaign.type === CampaignType.game)),
+        map((campaigns: ICampaign[]) => this.campaigns = campaigns),
+        switchMap((campaigns: ICampaign[]) => combineLatest(...campaigns.map(campaign => this.gameService.getGamesFromCampaign(campaign.id)
+          .pipe(map((games: IGame[]) => { if (games.length > 0) { return games[0]; }})))
+          )))
+        .subscribe((games: IGame[]) => {
+          this.games = games;
+          this.campaigns = this.campaigns.filter(
+            (campaign) => {
+              return ((games.filter((game) => game.campaignId === campaign.id).length) > 0);
+            }
+          );
       });
   }
 
   public selected(campaign: ICampaign): void {
-    this.tapped.emit(campaign);
+    let gameId;
+    this.games.forEach(
+      (game) => {
+        if (game.campaignId === campaign.id) {
+          gameId = game.id;
+        }
+      }
+    );
+
+    if (gameId) {
+      this.tapped.emit(gameId);
+    }
   }
 }
