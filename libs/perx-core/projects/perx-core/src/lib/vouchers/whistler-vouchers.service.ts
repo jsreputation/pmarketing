d1f9487aa33e79be7083bf2ac1bbcb343d1010a6
andrew <andrew@perxtech.com>
2019-09-16 18:02:25 +0800

    get reward detail based on voucher ID

--- libs/perx-core/projects/perx-core/src/lib/vouchers/whistler-vouchers.service.ts
+++ libs/perx-core/projects/perx-core/src/lib/vouchers/whistler-vouchers.service.ts
@@ -2,10 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Config } from '../config/config';
 import { IVoucherService } from './ivoucher.service';
-import { Observable } from 'rxjs';
+import { Observable, combineLatest, of } from 'rxjs';
 import { IVoucher, IGetVoucherParams, VoucherState, RedemptionType } from './models/voucher.model';
 import { IJsonApiListPayload, IJsonApiItem } from '../jsonapi.payload';
-import { map } from 'rxjs/operators';
+import { map, switchMap, last } from 'rxjs/operators';
+import { RewardsService } from '../rewards/rewards.service';
+import { IReward } from '../rewards/models/reward.model';
 
 const enum VoucherStatus {
   assigned = 'assigned'
@@ -18,6 +20,8 @@ interface IWhistlerVoucher {
   code: string;
   assigned_to_id: number;
   status: VoucherStatus;
+  source_id: number;
+  source_type: string;
 }
 
 @Injectable({
@@ -28,7 +32,8 @@ export class WhistlerVouchersService implements IVoucherService {
     // @ts-ignore
     private http: HttpClient,
     // @ts-ignore
-    private config: Config
+    private config: Config,
+    private rewardsService: RewardsService
   ) { }
 
   private static WVoucherStatusToState(stat: VoucherStatus): VoucherState {
@@ -40,7 +45,7 @@ export class WhistlerVouchersService implements IVoucherService {
     }
   }
 
-  private static WVoucherToVoucher(voucher: IJsonApiItem<IWhistlerVoucher>): IVoucher {
+  private static WVoucherToVoucher(voucher: IJsonApiItem<IWhistlerVoucher>, reward: IReward): IVoucher {
     return {
       id: (typeof voucher.id === 'string') ? Number.parseInt(voucher.id, 10) : voucher.id,
       rewardId: -1, // use at \lib\vouchers\vouchers.service.ts
@@ -62,7 +67,15 @@ export class WhistlerVouchersService implements IVoucherService {
     return this.http.get<IJsonApiListPayload<IWhistlerVoucher>>(this.vouchersUrl)
       .pipe(
         map((res) => res.data),
-        map((vouchers: IJsonApiItem<IWhistlerVoucher>[]) => vouchers.map(v => WhistlerVouchersService.WVoucherToVoucher(v)))
+        switchMap((vouchers: IJsonApiItem<IWhistlerVoucher>[]) => combineLatest(...vouchers.map(v => this.getFullVoucher(v))))
+      );
+  }
+
+  private getFullVoucher(voucher: IJsonApiItem<IWhistlerVoucher>): Observable<IVoucher> {
+    return combineLatest(of(voucher), this.rewardsService.getReward(voucher.attributes.source_id))
+      .pipe(
+        last(),
+        map(([v, reward]: [IJsonApiItem<IWhistlerVoucher>, IReward]) => WhistlerVouchersService.WVoucherToVoucher(v, reward))
       );
   }
 
