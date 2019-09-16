8db6d07e8102e58204e21ac3def1a3f60961d433
Arina Mahmudova <a.mahmudova@dunice.net>
2019-09-16 12:46:41 +0300

    create promos route

---
+++ apps/abenson/src/app/promos/promos.component.spec.ts
@@ -0,0 +1,41 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatTabsModule, MatCardModule } from '@angular/material';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { PromosComponent } from './promos.component';
+import { of } from 'rxjs';
+import { FeedReaderService } from '../../../../../libs/perx-core/dist/perx-core';
+
+describe('PromosComponent', () => {
+  let component: PromosComponent;
+  let fixture: ComponentFixture<PromosComponent>;
+
+  const feedReaderServiceStub = {
+    getFromUrl: () => of([])
+  };
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PromosComponent ],
+      imports: [
+        NoopAnimationsModule,
+        MatTabsModule,
+        MatCardModule
+      ],
+      providers: [
+        { provide: FeedReaderService, useValue: feedReaderServiceStub }
+      ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PromosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
