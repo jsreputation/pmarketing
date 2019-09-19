05689d0e3e1e111ed4000ec2c32d5413e8821a22
Vitalii Lysak <v.lysak@dunice.net>
2019-09-19 14:23:50 +0300

    fix lint issues

--- apps/abenson/src/app/card/card.module.ts
+++ apps/abenson/src/app/card/card.module.ts
@@ -7,7 +7,7 @@ import { CardRoutingModule } from './card-routing.module';
 import { CardComponent } from './containers/card/card.component';
 import { ExistingCardComponent } from './containers/existing-card/existing-card.component';
 
-import { SharedModule } from "../shared/shared.module";
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
