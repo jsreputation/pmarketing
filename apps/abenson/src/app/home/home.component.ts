481dd2f3d89e2efa80cb43f4929fb893b86ae155
Golotin Denis <d.golotin@dunice.net>
2019-09-19 17:42:18 +0300

    added filter

--- apps/abenson/src/app/home/home.component.ts
+++ apps/abenson/src/app/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ICampaign, ICampaignService, IVoucherService, VoucherState, Voucher } from '@perx/core';
+import { ICampaign, ICampaignService, IVoucherService, VoucherState, Voucher, CampaignType } from '@perx/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -20,7 +21,8 @@ export class HomeComponent implements OnInit {
   ) { }
 
   public ngOnInit(): void {
-    this.campaigns$ = this.campaignService.getCampaigns();
+    this.campaigns$ = this.campaignService.getCampaigns()
+      .pipe(map((compaing) => compaing.filter(el => el.type === CampaignType.game)));
     this.vouchers$ = this.vouchersService.getAll();
     this.filter = [VoucherState.issued, VoucherState.reserved, VoucherState.released];
   }
