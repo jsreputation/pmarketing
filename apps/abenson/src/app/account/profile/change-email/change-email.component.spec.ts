a4b388709cdcaa0edd0e4d790f672570ba689335
Vitalii Lysak <v.lysak@dunice.net>
2019-09-20 15:13:40 +0300

    update change email tests

--- apps/abenson/src/app/account/profile/change-email/change-email.component.spec.ts
+++ apps/abenson/src/app/account/profile/change-email/change-email.component.spec.ts
@@ -1,6 +1,13 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  async,
+  ComponentFixture,
+  TestBed,
+} from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ChangeEmailComponent } from './change-email.component';
+
+import { SharedModule } from "../../../shared/shared.module";
 
 describe('ChangeEmailComponent', () => {
   let component: ChangeEmailComponent;
@@ -8,7 +15,13 @@ describe('ChangeEmailComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ChangeEmailComponent ]
+      declarations: [
+        ChangeEmailComponent,
+      ],
+      imports: [
+        SharedModule,
+        NoopAnimationsModule,
+      ],
     })
     .compileComponents();
   }));
