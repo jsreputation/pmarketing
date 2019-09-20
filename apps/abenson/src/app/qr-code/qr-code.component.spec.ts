dc28661fd9273fa366e61c329b3252d013443c80
Golotin Denis <d.golotin@dunice.net>
2019-09-20 16:53:44 +0300

    lin unit fix

--- apps/abenson/src/app/qr-code/qr-code.component.spec.ts
+++ apps/abenson/src/app/qr-code/qr-code.component.spec.ts
@@ -1,20 +1,36 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { QrCodeComponent } from './qr-code.component';
+import { QRCodeComponent } from './qr-code.component';
+import { NgxBarcodeModule } from 'ngx-barcode';
+import { VouchersModule, IVoucherService } from '@perx/core';
+import { of } from 'rxjs';
+import { RouterTestingModule } from '@angular/router/testing';
+
+const ivoucherServiceStub = {
+  get: () => of({})
+};
 
 describe('QrCodeComponent', () => {
-  let component: QrCodeComponent;
-  let fixture: ComponentFixture<QrCodeComponent>;
+  let component: QRCodeComponent;
+  let fixture: ComponentFixture<QRCodeComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ QrCodeComponent ]
+      declarations: [QRCodeComponent],
+      imports: [
+        NgxBarcodeModule,
+        VouchersModule,
+        RouterTestingModule
+      ],
+      providers: [
+        { provide: IVoucherService, useValue: ivoucherServiceStub }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(QrCodeComponent);
+    fixture = TestBed.createComponent(QRCodeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- apps/abenson/src/app/qr-code/qr-code.component.ts
+++ apps/abenson/src/app/qr-code/qr-code.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NotificationService, IVoucherService, Voucher } from '@perx/core';
-import { flatMap, tap } from 'rxjs/operators';
+import { flatMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-qr-code',
@@ -9,18 +9,17 @@ import { flatMap, tap } from 'rxjs/operators';
   styleUrls: ['./qr-code.component.scss']
 })
 export class QRCodeComponent implements OnInit {
-  voucher: Voucher;
+  public voucher: Voucher;
   constructor(
     private route: ActivatedRoute,
     private notification: NotificationService,
     private vouchersService: IVoucherService
   ) { }
 
-  ngOnInit() {
-    this.route.params.pipe(flatMap((param)=>this.vouchersService.get(+param.id))).subscribe((val)=>{
-      this.voucher = val;
-    }, (msg)=>{
-      this.notification.addSnack(msg)
-    });
+  public ngOnInit(): void {
+    this.route.params.pipe(flatMap((param) => this.vouchersService.get(+param.id))).subscribe((val) =>
+      this.voucher = val
+      , (msg) =>
+        this.notification.addSnack(msg));
   }
 }
