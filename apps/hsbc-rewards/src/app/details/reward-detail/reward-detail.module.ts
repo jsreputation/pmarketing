96ff074d89bb885f21f6114cae0e2417aa56fe16
Nicolas Lagier <nicolas@perxtech.com>
2019-09-20 14:04:01 +0800

    fix/PW-399

--- apps/hsbc-rewards/src/app/details/reward-detail/reward-detail.module.ts
+++ apps/hsbc-rewards/src/app/details/reward-detail/reward-detail.module.ts
@@ -6,11 +6,12 @@ import { RewardDetailComponent } from './reward-detail.component';
 import { RewardsModule, VouchersModule, ConfigModule, MerchantsModule } from '@perx/core';
 import { DetailHeaderModule } from '../detail-header/detail-header.module';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [RewardDetailComponent],
   imports: [
-    ConfigModule.forRoot({}),
+    ConfigModule.forRoot({...environment}),
     CommonModule,
     RewardDetailRoutingModule,
     MerchantsModule,
