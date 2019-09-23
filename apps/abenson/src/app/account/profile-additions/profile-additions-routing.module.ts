8cdb579c61a8e5cc53a2dda166be123d923681dd
Golotin Denis <d.golotin@dunice.net>
2019-09-23 10:31:15 +0300

    PW-1277

--- apps/abenson/src/app/account/profile-additions/profile-additions-routing.module.ts
+++ apps/abenson/src/app/account/profile-additions/profile-additions-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PrivacyPolicyComponent } from './containers/privacy-policy/privacy-policy.component';
 
 const routes: Routes = [{
-  
+  path: 'privacy-policy',
+  component: PrivacyPolicyComponent
 }];
 
 @NgModule({
--- apps/abenson/src/app/account/profile-additions/profile-additions.module.ts
+++ apps/abenson/src/app/account/profile-additions/profile-additions.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProfileAdditionsRoutingModule } from './profile-additions-routing.module';
+import { PrivacyPolicyComponent } from './containers/privacy-policy/privacy-policy.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [PrivacyPolicyComponent],
   imports: [
     CommonModule,
     ProfileAdditionsRoutingModule
