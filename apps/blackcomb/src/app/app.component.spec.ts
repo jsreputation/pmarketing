88547814497edd91e553b80c7ecbb4a316d86dfd
andrew <andrew@perxtech.com>
2019-09-24 16:31:10 +0800

    unit test maintain

--- apps/blackcomb/src/app/app.component.spec.ts
+++ apps/blackcomb/src/app/app.component.spec.ts
@@ -1,7 +1,10 @@
+import { ThemesService } from '@perx/core';
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { MatDialogModule, MatToolbarModule, MatIconModule } from '@angular/material';
+
+const themesServiceStub = {}
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -15,6 +18,12 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      providers: [
+        {
+          provide: ThemesService,
+          userValue: themesServiceStub
+        }
+      ]
     }).compileComponents();
   }));
 
