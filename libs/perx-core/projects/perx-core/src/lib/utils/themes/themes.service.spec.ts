45f80e2439e09423d380dbec634a8e3bc70289a8
andrew <andrew@perxtech.com>
2019-09-24 16:23:51 +0800

    unit test and lint maintain

--- libs/perx-core/projects/perx-core/src/lib/utils/themes/themes.service.spec.ts
+++ libs/perx-core/projects/perx-core/src/lib/utils/themes/themes.service.spec.ts
@@ -1,9 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ThemesService } from './themes.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ConfigModule } from '../../config/config.module';
 
-describe('ThemesService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+fdescribe('ThemesService', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientTestingModule,
+      ConfigModule.forRoot({})
+    ]
+  }));
 
   it('should be created', () => {
     const service: ThemesService = TestBed.get(ThemesService);
