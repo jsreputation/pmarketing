72f2cb16743b03ee9dba15fc2404e7cd61904cab
Golotin Denis <d.golotin@dunice.net>
2019-09-26 15:44:10 +0300

    app spec full

--- apps/abenson/src/app/app.component.spec.ts
+++ apps/abenson/src/app/app.component.spec.ts
@@ -1,9 +1,29 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, fakeAsync, ComponentFixture, tick } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { MatDialogModule, MatToolbarModule, MatIconModule, MatSnackBarModule } from '@angular/material';
+import { MatDialogModule, MatToolbarModule, MatIconModule, MatSnackBarModule, MatDialog, MatSnackBar } from '@angular/material';
+import { NotificationService } from '@perx/core';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
+import { TermsAndConditionComponent } from './account/profile-additions/containers/terms-and-condition/terms-and-condition.component';
+import { ProfileComponent } from './account/profile/profile.component';
+import { ChangeBarangayComponent } from './account/profile/change-barangay/change-barangay.component';
+import { ChangePasswordComponent } from './account/change-password/change-password.component';
+import { ChangeEmailComponent } from './account/profile/change-email/change-email.component';
+import { ChangeCityComponent } from './account/profile/change-city/change-city.component';
+import { ChangeStreetAddressComponent } from './account/profile/change-street-address/change-street-address.component';
+import { FaqComponent } from './account/profile-additions/containers/faq/faq.component';
+import { PrivacyPolicyComponent } from './account/profile-additions/containers/privacy-policy/privacy-policy.component';
+import { Location } from '@angular/common';
 
-describe('AppComponent', () => {
+fdescribe('AppComponent', () => {
+  let app: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let ntfs: NotificationService;
+  let dialog: MatDialog;
+  let snackBar: MatSnackBar;
+  let location: Location;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -18,10 +38,61 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
-
+  beforeEach(fakeAsync(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.debugElement.componentInstance;
+    ntfs = TestBed.get(NotificationService);
+    dialog = TestBed.get(MatDialog);
+    snackBar = TestBed.get(MatSnackBar);
+    location = TestBed.get(Location);
+    app.ngOnInit();
+  }))
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it('should call dialog service', fakeAsync(() => {
+    app.ngOnInit();
+    const config = { title: 'test' };
+    const spyPopup = spyOn(dialog, 'open');
+    const spySnack = spyOn(snackBar, 'open');
+    ntfs.addPopup(config);
+    ntfs.addSnack('test')
+    tick();
+    expect(spyPopup).toHaveBeenCalled();
+    expect(spySnack).toHaveBeenCalled();
+  }));
+
+  it('should change show log status', () => {
+    app.onActivate(new LoginComponent(null, null, null, null));
+    expect(app.showHeader).toBeFalsy();
+    app.onActivate(new SignUpComponent(null, null, null));
+    expect(app.showHeader).toBeFalsy();
+    app.onActivate(new HomeComponent(null, null, null));
+    expect(app.showToolbar).toBeTruthy();
+    app.onActivate(new TermsAndConditionComponent());
+    expect(app.headerTitle).toBe('Terms & Conditions');
+    app.onActivate(new ProfileComponent(null));
+    expect(app.headerTitle).toBe('Profile');
+    app.onActivate(new ChangeBarangayComponent(null));
+    expect(app.headerTitle).toBe('Change Barangay');
+    app.onActivate(new ChangePasswordComponent(null));
+    expect(app.headerTitle).toBe('Change PIN Code');
+    app.onActivate(new ChangeEmailComponent(null, null, null));
+    expect(app.headerTitle).toBe('Change Email');
+    app.onActivate(new ChangeCityComponent(null, null, null));
+    expect(app.headerTitle).toBe('Change City/Municipality');
+    app.onActivate(new ChangeStreetAddressComponent(null));
+    expect(app.headerTitle).toBe('Change Street Address');
+    app.onActivate(new FaqComponent());
+    expect(app.headerTitle).toBe('FAQ');
+    app.onActivate(new PrivacyPolicyComponent());
+    expect(app.headerTitle).toBe('Privacy Policy');
+  });
+
+  it('should navigate back', ()=>{
+    const spy = spyOn(location, 'back');
+    app.goBack();
+    expect(spy).toHaveBeenCalled();
   });
 });
