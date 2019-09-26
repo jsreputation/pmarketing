54884df121f80e8fe582f3d5853397d002c643fb
Golotin Denis <d.golotin@dunice.net>
2019-09-26 12:37:37 +0300

    fix unit test

--- apps/abenson/src/app/account/change-password/change-password.component.spec.ts
+++ apps/abenson/src/app/account/change-password/change-password.component.spec.ts
@@ -4,6 +4,13 @@ import { ChangePasswordComponent } from './change-password.component';
 import { MatInputModule, MatButtonModule } from '@angular/material';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthenticationService } from '@perx/core';
+import { of } from 'rxjs';
+
+const authenticationServiceStub = {
+  requestVerificationToken: () => of(null)
+}
 
 describe('ChangePasswordComponent', () => {
   let component: ChangePasswordComponent;
@@ -17,7 +24,14 @@ describe('ChangePasswordComponent', () => {
         MatButtonModule,
         FormsModule,
         ReactiveFormsModule,
-        NoopAnimationsModule
+        NoopAnimationsModule,
+        RouterTestingModule.withRoutes([{
+          path: 'account/profile/verify-otp/password',
+          component: ChangePasswordComponent
+        }])
+      ],
+      providers: [
+        { provide: AuthenticationService, useValue: authenticationServiceStub }
       ]
     })
       .compileComponents();
