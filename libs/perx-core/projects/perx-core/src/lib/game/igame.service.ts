8a2e018f000ccdc0dbb01cc86b7b1fd8e3e86667
Nicolas Lagier <jihoun@users.noreply.github.com>
2019-09-30 11:33:38 +0800

    Update igame.service.ts

--- libs/perx-core/projects/perx-core/src/lib/game/igame.service.ts
+++ libs/perx-core/projects/perx-core/src/lib/game/igame.service.ts
@@ -2,7 +2,7 @@ import { Observable } from 'rxjs';
 import { IGame, IPlayOutcome } from './game.model';
 
 export abstract class IGameService {
-    public abstract play(campaignId: number, playId?: number): Observable<IPlayOutcome>;
+    public abstract play(campaignId: number, engagementId?: number): Observable<IPlayOutcome>;
     public abstract getGamesFromCampaign(campaignId: number): Observable<IGame[]>;
     public abstract get(gameId: number): Observable<IGame>;
 }
