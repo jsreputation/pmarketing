a591c704edf0c7ae08015ce6e6a0a51a7c6c8128
andrew <andrew@perxtech.com>
2019-10-02 11:54:04 +0800

    remvoe extra mapping and add url mapping

--- apps/candyland/src/app/core/http-services/limits-https.service.ts
+++ apps/candyland/src/app/core/http-services/limits-https.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { ApiConfig } from '@cl-core/api-config';
 import { Observable } from 'rxjs';
+import { EngagementTypeAPIMapping } from '@cl-core/models/engagement/engagement-type.enum';
 
 @Injectable({
   providedIn: 'root'
@@ -11,14 +12,17 @@ export class LimitsHttpsService {
   }
 
   public getLimits(params: HttpParams, engagementType: string): Observable<IResponseApi<ILimitApi[]>> {
-    return this.http.get<IResponseApi<ILimitApi[]>>(`${ApiConfig.basePath}/${engagementType}/limits`, { params });
+    const eType = EngagementTypeAPIMapping[engagementType];
+    return this.http.get<IResponseApi<ILimitApi[]>>(`${ApiConfig.basePath}/${eType}/limits`, { params });
   }
 
   public updateLimits(id: number, data: any, engagementType: string): Observable<IResponseApi<any>> {
-    return this.http.patch<IResponseApi<any>>(`${ApiConfig.basePath}/${engagementType}/limits` + '/' + id, data);
+    const eType = EngagementTypeAPIMapping[engagementType];
+    return this.http.patch<IResponseApi<any>>(`${ApiConfig.basePath}/${eType}/limits` + '/' + id, data);
   }
 
   public createLimits(data: IResponseApi<any>, engagementType: string): Observable<IResponseApi<any>> {
-    return this.http.post<IResponseApi<any>>(`${ApiConfig.basePath}/${engagementType}/limits`, data);
+    const eType = EngagementTypeAPIMapping[engagementType];
+    return this.http.post<IResponseApi<any>>(`${ApiConfig.basePath}/${eType}/limits`, data);
   }
 }
