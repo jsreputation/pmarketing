4fa8c251e3b0866f31e5d278082a98bd95932638
reynard <reynard93@gmail.com>
2019-10-03 15:54:26 +0800

    v1 of the component demo popup

--- apps/components-demo/src/app/utils/popup/popup.component.ts
+++ apps/components-demo/src/app/utils/popup/popup.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NotificationService } from '@perx/core';
+import { Validators, FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-popup',
@@ -7,13 +8,42 @@ import { NotificationService } from '@perx/core';
   styleUrls: ['./popup.component.scss']
 })
 export class PopupComponent {
-  constructor(private notificationService: NotificationService) { }
-  public pop(): void {
+  public form: FormGroup;
+  constructor(private notificationService: NotificationService, private fb: FormBuilder) {
+    const regexp = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/i;
+
+    this.form = this.fb.group({
+      title: ['', Validators.required],
+      text: ['', Validators.required],
+      btnTxt: ['', [Validators.required, Validators.maxLength(10)]],
+      btnTxt2: ['', [Validators.required, Validators.maxLength(10)]],
+      disableOverlay: ['', Validators.required],
+      imageUrl: ['https://picsum.photos/300/300', [Validators.required, Validators.pattern(regexp)]],
+    }, {updateOn: 'blur'});
+  }
+  public onSubmit({title, text, disableOverlay, btnTxt, btnTxt2, imageUrl}: any): void {
+    // open model
+    // pass in modal data
+    console.log(disableOverlay);
     this.notificationService.addPopup({
-      // tslint:disable-next-line: max-line-length
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      title: 'Lorem ipsum dolor sit amet',
-      buttonTxt: 'Lorem ipsum'
+      title,
+      text,
+      imageUrl,
+      buttonTxt: btnTxt,
+      disableOverlayClose: disableOverlay,
+      buttonTxt2: btnTxt2
     });
   }
+
 }
+// disableOverlayClose
+
+// title
+
+// text
+
+// buttonTxt
+
+// imageUrl (use a default image)
+
+// buttonTxt2
