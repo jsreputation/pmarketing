7d961cad90fdaeb02d7fcdb38c2d40c04948890b
pavlovandreyka <andreypavlov1706@gmail.com>
2019-10-07 10:17:24 +0300

    fixed mistake in the stamp component

--- apps/candyland/src/app/report/containers/stamp/stamp.component.ts
+++ apps/candyland/src/app/report/containers/stamp/stamp.component.ts
@@ -9,7 +9,7 @@ import { untilDestroyed } from 'ngx-take-until-destroy';
   styleUrls: ['./stamp.component.scss']
 })
 export class StampComponent implements OnInit, OnDestroy {
-  public params: {start_data: string, end_date: string} = {
+  public params: {start_date: string, end_date: string} = {
     start_date: '2019-07-01',
     end_date: '2019-08-31'
   };
