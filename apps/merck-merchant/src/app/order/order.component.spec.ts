cf0b44e0e593ad9320abcb0aac3aa0a0649c99bc
Ryane Chia <ryanechia@outlook.com>
2019-10-08 22:30:43 +0800

    update spec to use updated product declaration

--- apps/merck-merchant/src/app/order/order.component.spec.ts
+++ apps/merck-merchant/src/app/order/order.component.spec.ts
@@ -56,42 +56,56 @@ describe('OrderComponent', () => {
       description: '1000mg 60\'s',
       imageUrl: 'https://picsum.photos/200',
       pointsPerUnit: 5,
+      price: 100,
+      currency: 'HKD',
     },
     {
       name: 'Glucophage XR Tab',
       description: '500mg 60\'s',
       imageUrl: 'https://picsum.photos/200',
       pointsPerUnit: 5,
+      price: 50,
+      currency: 'HKD',
     },
     {
       name: 'Glucophage XR Tab',
       description: '750mg 30\'s',
       imageUrl: 'https://picsum.photos/200',
       pointsPerUnit: 5,
+      price: 75,
+      currency: 'HKD',
     },
     {
       name: 'Glucovance Tab',
       description: '2.5mg 30\'s',
       imageUrl: 'https://picsum.photos/200',
       pointsPerUnit: 5,
+      price: 50,
+      currency: 'HKD',
     },
     {
       name: 'Glucovance Tab',
       description: '5mg 30\'s',
       imageUrl: 'https://picsum.photos/200',
       pointsPerUnit: 5,
+      price: 100,
+      currency: 'HKD',
     },
     {
       name: 'Concor Tab',
       description: '2.5mg 30\'s',
       imageUrl: 'https://picsum.photos/200',
       pointsPerUnit: 5,
+      price: 50,
+      currency: 'HKD',
     },
     {
       name: 'Concor Tab',
       description: '5mg 100\'s',
       imageUrl: 'https://picsum.photos/200',
       pointsPerUnit: 5,
+      price: 100,
+      currency: 'HKD',
     }
   ];
   const productServiceStub = {
@@ -145,13 +159,16 @@ describe('OrderComponent', () => {
         description: '1000mg 60\'s',
         imageUrl: 'https://picsum.photos/200',
         pointsPerUnit: 5,
-      },
-        {
-          name: 'Glucophage XR Tab',
-          description: '500mg 60\'s',
-          imageUrl: 'https://picsum.photos/200',
-          pointsPerUnit: 5,
-        }];
+        price: 100,
+        currency: 'HKD',
+      }, {
+        name: 'Glucophage XR Tab',
+        description: '500mg 60\'s',
+        imageUrl: 'https://picsum.photos/200',
+        pointsPerUnit: 5,
+        price: 50,
+        currency: 'HKD',
+      }];
       const productServiceSpy = spyOn(productService, 'getProducts').and.returnValue(of(productsStub));
       component.ngOnInit();
       expect(productServiceSpy).toHaveBeenCalled();
