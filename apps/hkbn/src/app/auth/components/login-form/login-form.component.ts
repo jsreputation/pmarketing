6cff088deed0013470e5afd453ad3ad722b24f34
Jaye <jaye.tan@flags.fm>
2019-10-08 10:13:33 +0800

    added mobile number filtering and email checking

--- apps/hkbn/src/app/auth/components/login-form/login-form.component.ts
+++ apps/hkbn/src/app/auth/components/login-form/login-form.component.ts
@@ -30,11 +30,21 @@ export class LoginFormComponent {
     if (this.loginForm.invalid) {
       return;
     }
+
+    if (!this.isEmail(this.loginForm.value.user)) {
+      this.loginForm.value.user = this.loginForm.value.user.replace(/[^0-9]/g, '');
+    }
+
     this.loginSubmit.emit(this.loginForm.value);
   }
 
   public forgot(): void {
     this.forgotPassword.emit(this.loginForm.value.user || '');
+  }
+
+  private isEmail(email: string): boolean {
+    var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(String(email));
   }
 
 }
