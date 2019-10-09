494c7c7275ee0962d2d61a85e0647296ffb6dd04
Ryane Chia <ryanechia@outlook.com>
2019-10-10 02:41:53 +0800

    implement LoyaltyService.getTransactionHistory()

--- libs/perx-core/projects/perx-core/src/lib/loyalty/v4-loyalty.service.ts
+++ libs/perx-core/projects/perx-core/src/lib/loyalty/v4-loyalty.service.ts
@@ -1,10 +1,18 @@
-import { Injectable, Optional } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-import { map, mergeMap, concatAll, reduce } from 'rxjs/operators';
-import { LoyaltyService } from './loyalty.service';
-import { ILoyalty, ITransaction } from './models/loyalty.model';
-import { Config } from '../config/config';
+import {Injectable, Optional} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable, of} from 'rxjs';
+import {map, mergeMap, concatAll, reduce} from 'rxjs/operators';
+import {LoyaltyService} from './loyalty.service';
+import {
+  ILoyalty,
+  IPurchaseTransactionHistory, IRewardTransactionHistory,
+  ITransaction,
+  ITransactionDetailType,
+  ITransactionHistory
+} from './models/loyalty.model';
+import {Config} from '../config/config';
+import {IV4Reward, IV4Tag} from '../rewards/v4-rewards.service';
+import {ICustomProperties} from '../profile/profile.model';
 
 const DEFAULT_PAGE_COUNT: number = 10;
 
@@ -59,6 +67,53 @@ interface IV4PointHistory {
   properties: {};
 }
 
+interface IV4RewardTransactionHistory {
+  id: number;
+  state: string;
+  voucher_code?: string;
+  reserved_expires_at?: Date;
+  voucher_key?: string;
+  voucher_expires_at: Date;
+  user_account: {
+    identifier: string;
+  };
+  reward: IV4Reward;
+  redemption_location?: string;
+  tags?: IV4Tag[];
+}
+
+interface IV4PurchaseTransactionHistory {
+  id: number;
+  user_account_id: number;
+  updated_at: Date;
+  transaction_type: string;
+  amount: number;
+  transaction_date: Date;
+  currency: string;
+  workflow_id?: number;
+  created_at: Date;
+  properties?: ICustomProperties;
+  transaction_reference: string;
+}
+
+interface IV4TransactionHistory {
+  id: number;
+  name: string;
+  identifier: string;
+  transacted_at: Date;
+  amount: number;
+  transacted_cents?: number; // property will probably be removed
+  properties: ICustomProperties;
+  transaction_details: {
+    type: ITransactionDetailType;
+    data: IV4PurchaseTransactionHistory | IV4RewardTransactionHistory;
+  };
+}
+
+interface IV4TransactionHistoryResponse {
+  data: IV4TransactionHistory[];
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -102,6 +157,56 @@ export class V4LoyaltyService extends LoyaltyService {
       currencyBalance: pointHistory.points_balance_converted_to_currency,
       earnedDate: pointHistory.points_date,
       properties: pointHistory.properties
+    };
+  }
+
+  public static v4TransactionHistoryToTransactionHistory(transactionHistory: IV4TransactionHistory): ITransactionHistory {
+
+    const transactionDetails = transactionHistory.transaction_details.data;
+    let data: IPurchaseTransactionHistory | IRewardTransactionHistory;
+
+    switch (transactionHistory.transaction_details.type) {
+      case ITransactionDetailType.reward:
+        const rthDetails = transactionDetails as IV4RewardTransactionHistory;
+        data = {
+          id: transactionDetails.id,
+          state: rthDetails.state,
+          voucherExpiry: rthDetails.voucher_expires_at,
+          userAccount: rthDetails.user_account.identifier,
+          rewardName: rthDetails.reward.name,
+          redemptionLocation: rthDetails.redemption_location,
+        };
+        break;
+      case ITransactionDetailType.transaction:
+        const pthDetails = transactionDetails as IV4PurchaseTransactionHistory;
+        const pthProps = pthDetails.properties as {
+          merchant_username: string;
+          pharmacy: string;
+          product: string;
+        };
+        data = {
+          id: transactionDetails.id,
+          productName: pthProps.product,
+          pharmacyName: pthProps.pharmacy,
+          issuerName: pthProps.merchant_username,
+          transactionDate: pthDetails.transaction_date,
+          transactionRef: pthDetails.transaction_reference,
+          price: pthDetails.amount,
+          currency: pthDetails.currency,
+        };
+        break;
+    }
+    return {
+      id: transactionHistory.id,
+      name: transactionHistory.name,
+      identifier: transactionHistory.identifier,
+      transactedAt: transactionHistory.transacted_at,
+      pointsAmount: transactionHistory.amount,
+      properties: transactionHistory.properties,
+      transactionDetails: {
+        type: transactionHistory.transaction_details.type,
+        data
+      }
     };
   }
 
@@ -177,6 +282,23 @@ export class V4LoyaltyService extends LoyaltyService {
       }),
       map((loyalty: IV4Loyalty) => loyalty.points_history.map(
         (history: IV4PointHistory) => V4LoyaltyService.v4PointHistoryToPointHistory(history)
+      ))
+    );
+  }
+
+  public getTransactionHistory(page?: number, pageSize?: number): Observable<ITransactionHistory[]> {
+    return this.http.get<IV4TransactionHistoryResponse>(
+      `${this.apiHost}/v4/loyalty/transactions_history`,
+      {
+        params: {
+          page: `${page}`,
+          size: `${pageSize}`
+        }
+      }
+    ).pipe(
+      map((res: IV4TransactionHistoryResponse) => res.data),
+      map((transactionHistories: IV4TransactionHistory[]) => transactionHistories.map(
+        (transactionHistory: IV4TransactionHistory) => V4LoyaltyService.v4TransactionHistoryToTransactionHistory(transactionHistory)
       ))
     );
   }
