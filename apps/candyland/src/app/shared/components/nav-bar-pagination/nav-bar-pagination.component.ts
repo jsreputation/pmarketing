cd5ab792f16c3396f22d8b37968e7869a5510cc2
Cholo De Villa <cholo@meta.us>
2019-10-10 15:05:43 +0800

    Add pagination component attempt

---
+++ apps/candyland/src/app/shared/components/nav-bar-pagination/nav-bar-pagination.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cl-nav-bar-pagination',
+  templateUrl: './nav-bar-pagination.component.html',
+  styleUrls: ['./nav-bar-pagination.component.scss']
+})
+export class NavBarPaginationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
