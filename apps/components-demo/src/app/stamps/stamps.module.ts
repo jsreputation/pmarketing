8ebabdca1a75a3b3b3b258a421a61bccf168211d
reynard <reynard93@gmail.com>
2019-10-10 09:34:38 +0800

    fix build

--- apps/components-demo/src/app/stamps/stamps.module.ts
+++ apps/components-demo/src/app/stamps/stamps.module.ts
@@ -5,7 +5,7 @@ import { StampsRoutingModule } from './stamps-routing.module';
 import { CardComponent } from './card/card.component';
 import { StampsComponent } from './stamps/stamps.component';
 import { PuzzlesModule, RewardsModule } from '@perx/core';
-import { MatSliderModule, MatCheckboxModule, MatTabsModule } from '@angular/material';
+import { MatSliderModule, MatCheckboxModule, MatTabsModule, MatIconModule } from '@angular/material';
 
 @NgModule({
   declarations: [CardComponent, StampsComponent, StampsListComponent ],
@@ -15,7 +15,7 @@ import { MatSliderModule, MatCheckboxModule, MatTabsModule } from '@angular/mate
     MatSliderModule,
     MatCheckboxModule,
     MatIconModule,
-    PuzzlesModule
+    PuzzlesModule,
     MatTabsModule,
     RewardsModule
   ]
