01c4e476eef58b7288663725f834a057d30155d6
AndyG00D <achicunov@gmail.com>
2019-10-10 12:48:36 +0300

    fix lint

--- apps/candyland/src/app/loyalty/containers/loyalty-list-page/loyalty-list-page.component.spec.ts
+++ apps/candyland/src/app/loyalty/containers/loyalty-list-page/loyalty-list-page.component.spec.ts
@@ -1,4 +1,5 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+// tslint:disable-next-line:import-blacklist
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
