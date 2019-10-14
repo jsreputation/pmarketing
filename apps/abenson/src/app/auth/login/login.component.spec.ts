48ecf871a7f905dd04707c906f05a6e0cd934414
Golotin Denis <d.golotin@dunice.net>
2019-10-14 12:36:53 +0300

    login abeson complitly tested

--- apps/abenson/src/app/auth/login/login.component.spec.ts
+++ apps/abenson/src/app/auth/login/login.component.spec.ts
@@ -4,7 +4,7 @@ import { LoginComponent } from './login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule, MatInputModule } from '@angular/material';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { AuthenticationService } from '@perx/core';
+import { AuthenticationService, NotificationService } from '@perx/core';
 import { Type } from '@angular/core';
 import { of, throwError } from 'rxjs';
 import { HttpErrorResponse } from '@angular/common/http';
@@ -13,6 +13,7 @@ describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
   let auth: AuthenticationService;
+  let notificationService: NotificationService;
   const authenticationServiceStub = {
     login: () => of(null)
   };
@@ -42,6 +43,7 @@ describe('LoginComponent', () => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     auth = TestBed.get<AuthenticationService>(AuthenticationService as Type<AuthenticationService>);
+    notificationService = TestBed.get<NotificationService>(NotificationService as Type<NotificationService>);
     fixture.detectChanges();
   });
 
@@ -50,6 +52,7 @@ describe('LoginComponent', () => {
   });
 
   it('login', fakeAsync(() => {
+    (window as any).primaryIdentifier = null;
     component.loginForm.setValue({ mobileNumber: '12345', pinCode: '12345' });
     const spyAuth = spyOn(auth, 'login').and.returnValue(of(null));
     component.onSubmit();
@@ -60,8 +63,39 @@ describe('LoginComponent', () => {
   it('should handle error', fakeAsync(() => {
     component.loginForm.setValue({ mobileNumber: '12345', pinCode: '12345' });
     const spyAuth = spyOn(auth, 'login');
-    spyAuth.and.returnValue(throwError(new HttpErrorResponse({status: 401})));
+    spyAuth.and.returnValue(throwError(new HttpErrorResponse({ status: 401 })));
     component.onSubmit();
     expect(component.errorMessage).toBe('Invalid credentials');
+  }));
+
+  it('expect ignore set primaryIdentifier', fakeAsync(() => {
+    const primaryIdentifier = 'test';
+    (window as any).primaryIdentifier = primaryIdentifier;
+    component.loginForm.setValue({ mobileNumber: '12345', pinCode: '12345' });
+    spyOn(auth, 'login').and.returnValue(of(null));
+    component.onSubmit();
+    tick();
+    expect((window as any).primaryIdentifier).toBe(primaryIdentifier);
+  }));
+
+  it('should handle if error is not a HttpErrorResponse', fakeAsync(() => {
+    component.loginForm.setValue({ mobileNumber: '12345', pinCode: '12345' });
+    spyOn(auth, 'login').and.returnValue(throwError('error'));
+    component.onSubmit();
+    expect(component.errorMessage).toBe('error');
+  }));
+
+  it('should handle unknown status', fakeAsync(() => {
+    component.errorMessage = null;
+    spyOn(auth, 'login').and.returnValue(throwError(new HttpErrorResponse({ status: 403 })));
+    component.onSubmit();
+    expect(component.errorMessage).toBe(null);
+  }));
+
+  it('should handle error with status 0', fakeAsync(() => {
+    spyOn(auth, 'login').and.returnValue(throwError(new HttpErrorResponse({ status: 0 })));
+    const spy = spyOn(notificationService, 'addPopup');
+    component.onSubmit();
+    expect(spy).toHaveBeenCalled();
   }));
 });
