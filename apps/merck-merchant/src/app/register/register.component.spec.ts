a58fb5ab4ddb05a415292f4a9053c925907b79b4
Ryane Chia <ryanechia@outlook.com>
2019-10-17 04:25:40 +0800

    update tests

--- apps/merck-merchant/src/app/register/register.component.spec.ts
+++ apps/merck-merchant/src/app/register/register.component.spec.ts
@@ -1,7 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterComponent } from './register.component';
-import {ResetPasswordComponent} from '../reset-password/reset-password.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
@@ -9,13 +8,18 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatRippleModule} from '@angular/material/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {AuthenticationService} from '@perx/core';
+import {AuthenticationService, IMerchantAdminService} from '@perx/core';
+import {Router} from '@angular/router';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
-
+  const merchantAdminServiceStub = {
+    validateInvite: () => ({ subscribe: () => ({}) })
+  };
   beforeEach(async(() => {
+    const routerStub = { navigateByUrl: () => ({}) };
+
     TestBed.configureTestingModule({
       declarations: [ RegisterComponent ],
       imports: [
@@ -29,10 +33,12 @@ describe('RegisterComponent', () => {
         BrowserAnimationsModule
       ],
       providers: [
+        { provide: Router, useValue: routerStub },
         {
           provide: AuthenticationService,
           useValue: {forgotPassword: () => {}}
-        }
+        },
+        {provide: IMerchantAdminService, useValue: merchantAdminServiceStub},
       ]
     })
       .compileComponents();
