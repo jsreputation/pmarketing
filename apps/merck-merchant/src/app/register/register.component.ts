6c4bc15f232d585cfd16ec2681057e5d08cf19f5
Ryane Chia <ryanechia@outlook.com>
2019-10-17 04:05:07 +0800

    validate merchant invite API

--- apps/merck-merchant/src/app/register/register.component.ts
+++ apps/merck-merchant/src/app/register/register.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit, PLATFORM_ID} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {IMerchantAdminService, NotificationService} from '@perx/core';
+import {isPlatformBrowser} from '@angular/common';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -12,11 +15,30 @@ export class RegisterComponent implements OnInit {
 
   constructor(
     private fb: FormBuilder,
+    private router: Router,
+    private notificationService: NotificationService,
+    private merchantAdminService: IMerchantAdminService,
+    @Inject(PLATFORM_ID) private platformId: object
   ) {
     this.initForm();
   }
 
   public ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const param = location.search;
+      const invitationToken = new URLSearchParams(param).get('invitation_token');
+      const clientId = new URLSearchParams(param).get('client_id');
+
+      this.merchantAdminService.validateInvite(invitationToken, clientId).subscribe(
+        () => {
+          this.router.navigateByUrl('/login');
+          this.notificationService.addSnack('Your password has been saved. Please login');
+        },
+        (err) => {
+          this.notificationService.addSnack(err.message);
+        }
+      );
+    }
   }
 
   private initForm(): void {
@@ -27,6 +49,7 @@ export class RegisterComponent implements OnInit {
   }
 
   public onSubmit(): void {
-
+    // const password: string = this.loginForm.get('password').value;
+    // const confirmPassword: string = this.loginForm.get('password').value;
   }
 }
