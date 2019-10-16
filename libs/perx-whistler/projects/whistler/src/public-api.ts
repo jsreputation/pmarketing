bb63362131c4a7f6ae28d76a9fb2bee3a3f7ad06
Dmitry Yanakov <dima.yanakov.light.it@gmail.com>
2019-10-16 11:29:27 +0300

    move interfece to @perx/whistler

--- libs/perx-whistler/projects/whistler/src/public-api.ts
+++ libs/perx-whistler/projects/whistler/src/public-api.ts
@@ -4,6 +4,7 @@
 
 export * from './lib/whistler.module';
 export { IVoucherStatsApi } from './lib/voucher/stats';
+export { IVouchersApi } from './lib/voucher/vouchers-api';
 export { AssignedStatus, IAssignedAttributes, IAssignRequestAttributes } from './lib/voucher/assigneds';
 export { IRewardEntityAttributes } from './lib/reward/reward';
 export { IInstantOutcomeLimitAttributes, ISurveyLimitAttributes, IGameLimitAttributes } from './lib/limit/limit';
