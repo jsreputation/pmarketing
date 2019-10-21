0f7a267ccfae5398594ef3ba2342c9b46f4b5a2c
Ryane Chia <ryanechia@outlook.com>
2019-10-22 00:31:33 +0800

    update tests

--- libs/perx-core/projects/perx-core/src/lib/config/config.service.spec.ts
+++ libs/perx-core/projects/perx-core/src/lib/config/config.service.spec.ts
@@ -2,10 +2,17 @@ import { TestBed } from '@angular/core/testing';
 
 import { ConfigService } from './config.service';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {AuthenticationService} from '../auth/authentication/authentication.service';
+
+const authenticationServiceStub = { getAppToken: { subscribe: () => ({}) } }
 
 describe('ConfigService', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [HttpClientTestingModule]
+    imports: [HttpClientTestingModule],
+    providers: [{
+      provide: AuthenticationService,
+      useValue: authenticationServiceStub
+    }]
   }));
 
   it('should be created', () => {
