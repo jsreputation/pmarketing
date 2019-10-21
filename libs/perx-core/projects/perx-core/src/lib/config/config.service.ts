8e259749d5ce0a836f0172600b80aa76b4458b31
Ryane Chia <ryanechia@outlook.com>
2019-10-21 23:21:36 +0800

    don't need model mapping

--- libs/perx-core/projects/perx-core/src/lib/config/config.service.ts
+++ libs/perx-core/projects/perx-core/src/lib/config/config.service.ts
@@ -1,9 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
 import {IConfig} from './models/config.model';
-import {map} from 'rxjs/operators';
 import {HttpClient} from '@angular/common/http';
-import {Config} from './config';
 
 @Injectable({
   providedIn: 'root'
@@ -14,21 +12,8 @@ export class ConfigService {
 
   }
 
-  private static configToIConfig(conf: Config): IConfig {
-    return {
-      apiHost: conf.apiHost,
-      preAuth: conf.preAuth,
-      isWhistler: conf.isWhistler,
-      baseHref: conf.baseHref,
-      defaultLang: conf.defaultLang,
-      storageType: conf.storageType
-    };
-  }
-
   public readAppConfig(): Observable<IConfig> {
-    return this.http.get('assets/config/app-config.json').pipe(
-      map((appConf: Config) => ConfigService.configToIConfig(appConf))
-    );
+    return this.http.get<IConfig>('assets/config/app-config.json');
   }
 
 }
