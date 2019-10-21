2948817cc0fe43af145e70aaaa667552a1d12747
Ryane Chia <ryanechia@outlook.com>
2019-10-22 00:37:09 +0800

    missing semicolon

--- libs/perx-core/projects/perx-core/src/lib/config/config.service.spec.ts
+++ libs/perx-core/projects/perx-core/src/lib/config/config.service.spec.ts
@@ -4,7 +4,7 @@ import { ConfigService } from './config.service';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {AuthenticationService} from '../auth/authentication/authentication.service';
 
-const authenticationServiceStub = { getAppToken: { subscribe: () => ({}) } }
+const authenticationServiceStub = { getAppToken: { subscribe: () => ({}) } };
 
 describe('ConfigService', () => {
   beforeEach(() => TestBed.configureTestingModule({
