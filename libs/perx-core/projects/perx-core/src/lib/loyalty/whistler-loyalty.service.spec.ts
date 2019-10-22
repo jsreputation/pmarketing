e7700ecf99e93b0cb0db35e176b1d11a100b8774
andrew <andrew@perxtech.com>
2019-10-22 17:00:13 +0800

    unit test maintain

--- libs/perx-core/projects/perx-core/src/lib/loyalty/whistler-loyalty.service.spec.ts
+++ libs/perx-core/projects/perx-core/src/lib/loyalty/whistler-loyalty.service.spec.ts
@@ -1,9 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { WhistlerLoyaltyService } from './whistler-loyalty.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ConfigModule } from '../../public-api';
 
 describe('WhistlerLoyaltyService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientTestingModule,
+      ConfigModule.forRoot({})
+    ]
+  }));
 
   it('should be created', () => {
     const service: WhistlerLoyaltyService = TestBed.get(WhistlerLoyaltyService);
