6593e4007e99743552c239ff099986491a7b5602
andrew <andrew@perxtech.com>
2019-10-22 18:42:28 +0800

    add control to updateUser

--- apps/candyland/src/app/app.component.ts
+++ apps/candyland/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService, RoutingStateService } from '@cl-core/services';
+import { UserService } from '@cl-core/services/user.service';
+import { LocalStorageService } from '@cl-core/services/local-storage.service';
 
 @Component({
   selector: 'cl-root',
@@ -7,15 +9,22 @@ import { AuthService, RoutingStateService } from '@cl-core/services';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  constructor(private routingState: RoutingStateService,
-              public authService: AuthService) {
+  constructor(
+    private routingState: RoutingStateService,
+    public authService: AuthService,
+    private userService: UserService,
+    private localStorage: LocalStorageService
+  ) {
   }
 
   public ngOnInit(): void {
     this.authService.initAuth();
-    this.authService.updateUser()
-      .subscribe(() => {
-      });
+    const userId = this.userService.userId || this.localStorage.get('userId');
+    if (userId) {
+      this.authService.updateUser()
+        .subscribe(() => {
+        });
+    }
     this.routingState.loadRouting()
       .subscribe(() => {
       });
