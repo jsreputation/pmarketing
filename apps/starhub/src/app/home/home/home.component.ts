7064e7d2b000a0578b6a04f381884784e7d6b4b7
Golotin Denis <d.golotin@dunice.net>
2019-10-22 11:27:14 +0300

    pw-1663

--- apps/starhub/src/app/home/home/home.component.ts
+++ apps/starhub/src/app/home/home/home.component.ts
@@ -1,32 +1,20 @@
 import { Component, OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import { ILoyalty, LoyaltyService, ProfileService, IProfile } from '@perx/core';
-
 import { NoRenewaleInNamePipe } from '../no-renewale-in-name.pipe';
 import { ScrollDispatcher, CdkScrollable } from '@angular/cdk/overlay';
-// import { delay } from 'rxjs/operators';
-import { trigger, transition, animate, style, state } from '@angular/animations';
+import { MatToolbar } from '@angular/material';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
-  animations: [
-    trigger('optionalDisplay', [
-      state("on", style({ top: '0px'})),
-      state("off", style({ top: '-500px'})),
-      transition('on => off', [
-        animate("600ms ease-out", 
-          style({ 'margin-top': "{{pixels}}"})
-        )
-      ], { params: { pixels: "30px" } })
-    ]
-    )]
+  styleUrls: ['./home.component.scss']
 })
 
 export class HomeComponent implements OnInit, AfterViewInit {
-  value: string = 'on';
-  top: number = 0;
-  lastOffset: number = 0;
+  @ViewChild(MatToolbar, { static: false}) 
+  private toolBar: MatToolbar;
+  public top: number = 0;
+  public lastOffset: number = 0;
   @ViewChild('contentScrolled', { static: false })
   public contentScrolled: ElementRef;
   public loyalty: ILoyalty;
@@ -36,7 +24,7 @@ export class HomeComponent implements OnInit, AfterViewInit {
     private noRenewalePipe: NoRenewaleInNamePipe,
     private loyaltyService: LoyaltyService,
     private profileService: ProfileService,
-    private scollService: ScrollDispatcher
+    private scollService: ScrollDispatcher,
   ) { }
 
   public ngOnInit(): void {
@@ -59,29 +47,24 @@ export class HomeComponent implements OnInit, AfterViewInit {
         return 'assets/green-icon.svg';
     }
   }
-  ngAfterViewInit() {
-    this.scollService.scrolled();
 
-    // this.scollService.scrolled().subscribe((data: CdkScrollable) => this.onWindowScroll(data));
-    // this.scollService.ancestorScrolled(this.contentScrolled).subscribe((val)=>{
-    //   console.log('324', val);
-    // })
-    this.onWindowScroll({ } as CdkScrollable)
+  public ngAfterViewInit(): void {
+    this.scollService.scrolled().subscribe((data: CdkScrollable) => this.onWindowScroll(data));
   }
-  private onWindowScroll(data: CdkScrollable) {
-    if(!data.getElementRef) {
-      return ;
-    }
-    const scrollTop = data.getElementRef().nativeElement.scrollTop || 0;
-    const delta = this.lastOffset - scrollTop;
-    if (this.top + delta <= 0 && this.top + delta >= -170) {
-      this.top = this.top + delta;
-    } else if (this.top + delta > 0) {
-      this.top = 0;
-    } else if (this.top + delta <= -170) {
-      this.top = - 170;
-    }
-    this.lastOffset = scrollTop;
 
+  private onWindowScroll(data: CdkScrollable) {
+    const scrollTop = data.getElementRef().nativeElement.scrollTop || 0;
+    requestAnimationFrame(()=>{
+      const delta = this.lastOffset - scrollTop;
+      if (this.top + delta <= 0 && this.top + delta >= -170) {
+        this.top = this.top + delta;
+      } else if (this.top + delta > 0) {
+        this.top = 0;
+      } else if (this.top + delta <= -170) {
+        this.top = - 170;
+      }
+      this.lastOffset = scrollTop;
+      this.toolBar._elementRef.nativeElement.style.transform = `translateY(${this.top}px)`;
+    })
   }
 }
