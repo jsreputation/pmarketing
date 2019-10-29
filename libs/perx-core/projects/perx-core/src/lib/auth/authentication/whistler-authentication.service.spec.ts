b72289b55cbef65b6252182725a4ae3ea3bc07de
Golotin Denis <d.golotin@dunice.net>
2019-10-29 10:04:43 +0300

    store test

--- libs/perx-core/projects/perx-core/src/lib/auth/authentication/whistler-authentication.service.spec.ts
+++ libs/perx-core/projects/perx-core/src/lib/auth/authentication/whistler-authentication.service.spec.ts
@@ -1,11 +1,12 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { WhistlerAuthenticationService } from './whistler-authentication.service';
 import { TokenStorage } from './token-storage.service';
 import { ProfileModule } from '../../profile/profile.module';
 import { ConfigModule } from '../../config/config.module';
+import { Observable } from 'rxjs';
 
-describe('WhistlerAuthenticationService', () => {
+fdescribe('WhistlerAuthenticationService', () => {
   beforeEach(() => TestBed.configureTestingModule({
     imports: [
       HttpClientTestingModule,
@@ -19,4 +20,20 @@ describe('WhistlerAuthenticationService', () => {
     const service: WhistlerAuthenticationService = TestBed.get(WhistlerAuthenticationService);
     expect(service).toBeTruthy();
   });
+
+  it('should create with config', () => {
+    const service: WhistlerAuthenticationService = new WhistlerAuthenticationService({ production: true }, null, null);
+    expect(service).toBeTruthy();
+  });
+
+  it('failedAuth', inject([WhistlerAuthenticationService],(auth: WhistlerAuthenticationService)=>{
+    expect(auth.$failedAuth instanceof Observable).toBeTruthy();
+  }));
+
+  it('check isAuthorized', inject([WhistlerAuthenticationService, TokenStorage], 
+    (auth: WhistlerAuthenticationService, storage: TokenStorage)=>{
+      const spy = spyOn(storage, 'getAppInfoProperty');
+      auth.isAuthorized();
+      expect(spy).toHaveBeenCalledWith('userAccessToken');
+  }));
 });
