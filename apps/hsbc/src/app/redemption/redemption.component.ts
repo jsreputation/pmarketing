dbca19d7a3df78d1f1157f630b456825f84a696f
Jaye <jaye.tan@flags.fm>
2019-10-29 12:18:20 +0800

    added conditional redeem screen if sourceType is hsbc-xmas

--- apps/hsbc/src/app/redemption/redemption.component.ts
+++ apps/hsbc/src/app/redemption/redemption.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ConfigService, IConfig } from '@perx/core';
 
 @Component({
   selector: 'app-redemption',
@@ -8,13 +9,22 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class RedemptionComponent implements OnInit {
   public voucherId: number;
+  public sourceType: string;
+  public voucherCode: string = 'XXXX - XXXX';
 
   constructor(
     private route: ActivatedRoute,
+    private configService: ConfigService,
   ) {
   }
 
   public ngOnInit(): void {
     this.voucherId = parseInt(this.route.snapshot.paramMap.get('id'), 10);
+
+    this.configService.readAppConfig().subscribe(
+      (config: IConfig) => {
+        this.sourceType = config.sourceType as string;
+      }
+    );
   }
 }
