9416be367013845d57d54a62bd37402256ad123e
Vitalii Lysak <v.lysak@dunice.net>
2019-10-31 14:11:52 +0300

    updated tests

--- libs/perx-blackcomb-pages/projects/perx-blackcomb-pages/src/lib/pi/pi.component.spec.ts
+++ libs/perx-blackcomb-pages/projects/perx-blackcomb-pages/src/lib/pi/pi.component.spec.ts
@@ -1,6 +1,22 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  async,
+  ComponentFixture,
+  TestBed,
+} from '@angular/core/testing';
+import {
+  MatFormFieldModule,
+  MatInputModule,
+} from '@angular/material';
+import { ReactiveFormsModule} from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { Config } from '@perx/core';
 
 import { PIComponent } from './pi.component';
+
+const configStub = {
+  preAuth: false
+};
 
 describe('PIComponent', () => {
   let component: PIComponent;
@@ -8,7 +24,18 @@ describe('PIComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PIComponent ]
+      declarations: [
+        PIComponent
+      ],
+      imports: [
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        NoopAnimationsModule,
+      ],
+      providers: [
+        { provide: Config, useValue: configStub },
+      ]
     })
       .compileComponents();
   }));
