fb864411cb1782987cce088cabd8ed83a7d3518b
Nicolas Lagier <nicolas@perxtech.com>
2019-10-31 12:26:18 +0800

    PW-2236 finish tests on merchant service

--- libs/perx-core/projects/perx-core/src/lib/rewards/whistler-rewards.service.spec.ts
+++ libs/perx-core/projects/perx-core/src/lib/rewards/whistler-rewards.service.spec.ts
@@ -1,31 +1,177 @@
 import { TestBed } from '@angular/core/testing';
 
 import { WhistlerRewardsService } from './whistler-rewards.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ConfigModule } from '../config/config.module';
 import { IMerchantsService } from '../merchants/imerchants.service';
 import { of } from 'rxjs';
+import { IJsonApiItem, IJsonApiListPayload } from '../jsonapi.payload';
+import { IRewardEntityAttributes } from '@perx/whistler';
+import { IReward } from './models/reward.model';
+import { IJsonApiItemPayload } from 'perx-core/lib/jsonapi.payload';
+import { IMerchant } from '../merchants/models/merchants.model';
+import { HttpClient } from '@angular/common/http';
+import { takeLast } from 'rxjs/operators';
 
 describe('WhistlerRewardsService', () => {
-
-  const merchantsServiceStub = {
-    getMerchant: () => of()
+  let httpClientSpy: { get: jasmine.Spy };
+  let service: WhistlerRewardsService;
+  const mockMerchant: IMerchant = {
+    id: 42,
+    name: 'merchant 42'
   };
 
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [
-      HttpClientTestingModule,
-      ConfigModule.forRoot({})
-    ],
-    providers: [
-      {
-        provide: IMerchantsService, useValue: merchantsServiceStub
+  const merchantsServiceStub = {
+    getMerchant: () => of(mockMerchant)
+  };
+
+  const environment = {
+    apiHost: 'https://blabla',
+    production: false,
+    isWhistler: false,
+    preAuth: false,
+    baseHref: '/'
+  };
+
+  const mockReward: IJsonApiItem<IRewardEntityAttributes> = {
+    id: '1',
+    type: '',
+    links: { self: '' },
+    attributes: {
+      name: '42',
+      reward_type: '42',
+      category: '42',
+      redemption_type: 'Promo Code',
+      cost_of_reward: 42,
+      tags: [],
+      display_properties: {
+        loyalties: {}
       }
-    ]
-  }));
+    }
+  };
+
+  const mockRewardWithMerchant: IJsonApiItem<IRewardEntityAttributes> = {
+    id: '1',
+    type: '',
+    links: { self: '' },
+    attributes: {
+      name: '42',
+      reward_type: '42',
+      category: '42',
+      redemption_type: 'QR Code',
+      cost_of_reward: 42,
+      tags: [],
+      display_properties: {
+        loyalties: {}
+      },
+      organization_id: '42'
+    }
+  };
+
+  beforeEach(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+
+    TestBed.configureTestingModule({
+      imports: [
+        ConfigModule.forRoot({ ...environment })
+      ],
+      providers: [
+        {
+          provide: IMerchantsService, useValue: merchantsServiceStub
+        },
+        {
+          provide: HttpClient, useValue: httpClientSpy
+        }
+      ]
+    });
+    service = TestBed.get(WhistlerRewardsService);
+  });
 
   it('should be created', () => {
-    const service: WhistlerRewardsService = TestBed.get(WhistlerRewardsService);
     expect(service).toBeTruthy();
+  });
+
+  it('should get a reward from its id without merchant', (done: DoneFn) => {
+    const res: IJsonApiItemPayload<IRewardEntityAttributes> = {
+      data: mockReward
+    };
+    httpClientSpy.get.and.returnValue(of(res));
+
+    service.getReward(42)
+      .subscribe((r: IReward) => {
+        expect(`${r.id}`).toEqual(mockReward.id);
+        expect(r.merchantName).toBeNull();
+        done();
+      });
+
+    expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
+    expect(httpClientSpy.get.calls.argsFor(0)).toEqual(['https://blabla/reward/entities/42']);
+  });
+
+  it('should get a reward from its id with merchant', (done: DoneFn) => {
+    const res: IJsonApiItemPayload<IRewardEntityAttributes> = {
+      data: mockRewardWithMerchant
+    };
+    httpClientSpy.get.and.returnValue(of(res));
+
+    service.getReward(42)
+      .subscribe((r: IReward) => {
+        expect(`${r.id}`).toEqual(mockReward.id);
+        expect(r.merchantName).toEqual(mockMerchant.name);
+        done();
+      });
+
+    expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
+    expect(httpClientSpy.get.calls.argsFor(0)).toEqual(['https://blabla/reward/entities/42']);
+  });
+
+  it('should get a page of rewards', (done: DoneFn) => {
+    const res: IJsonApiListPayload<IRewardEntityAttributes> = {
+      data: [mockReward, mockRewardWithMerchant]
+    };
+    httpClientSpy.get.and.returnValue(of(res));
+
+    service.getRewards(
+      1,
+      10,
+      ['42tags'],
+      ['42categories']
+    )
+      .subscribe((rs: IReward[]) => {
+        expect(rs.length).toEqual(2);
+        done();
+      });
+
+    expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
+    expect(httpClientSpy.get.calls.argsFor(0)).toEqual([
+      'https://blabla/reward/entities',
+      { params: { 'page[number]': '1', 'page[size]': '10', 'filter[tags]': '42tags', 'filter[category]': '42categories' } }
+    ]);
+  });
+
+  it('should get all rewards', (done: DoneFn) => {
+    const res: IJsonApiListPayload<IRewardEntityAttributes> = {
+      data: [mockReward, mockRewardWithMerchant],
+      meta: {
+        page_count: 2
+      }
+    };
+    httpClientSpy.get.and.returnValue(of(res));
+
+    service.getAllRewards(
+      ['42tags'],
+      ['42categories']
+    ).pipe(
+      takeLast(1) // there are 2 pages, so, it should tick twice, make sure that we do not look at the first tick
+    )
+      .subscribe((rs: IReward[]) => {
+        expect(rs.length).toEqual(4);
+        done();
+      });
+
+    expect(httpClientSpy.get.calls.count()).toBe(2, 'two calls');
+    expect(httpClientSpy.get.calls.argsFor(0)).toEqual([
+      'https://blabla/reward/entities',
+      { params: { 'page[number]': '1', 'page[size]': '10', 'filter[tags]': '42tags', 'filter[category]': '42categories' } }
+    ]);
   });
 });
