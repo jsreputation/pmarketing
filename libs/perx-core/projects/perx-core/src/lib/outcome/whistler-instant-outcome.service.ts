4217c0c5923573d7c774f15d3c70eca60c5d900e
andrew <andrew@perxtech.com>
2019-11-01 17:01:44 +0800

    format update for IOutcome

--- libs/perx-core/projects/perx-core/src/lib/outcome/whistler-instant-outcome.service.ts
+++ libs/perx-core/projects/perx-core/src/lib/outcome/whistler-instant-outcome.service.ts
@@ -13,8 +13,11 @@ import {
   IInstantOutcomeTransactionAttributes,
   IInstantOutcomeTxnReq,
   IWInstantOutcomeEngagementAttributes,
+  IWOutcomeDisplayProperties,
   ICampaignAttributes
 } from '@perx/whistler';
+import { IQuestion } from '../../public-api';
+import { MaterialColor } from '../survey/models/survey.model';
 
 interface CampaignProperties {
   engagementId: number;
@@ -55,7 +58,8 @@ export class WhistlerInstantOutcomeService implements InstantOutcomeService {
             `${this.config.apiHost}/instant_outcome/engagements/${campaign.engagementId}`);
         }),
         map(res => res.data.attributes.display_properties),
-        map((outcomeData: IWInstantOutcomeEngagementAttributes) => ({ ...outcomeData.display_properties, ...displayProps }))
+        map((outcomeData: IWOutcomeDisplayProperties) =>
+          ({ ...outcomeData, displayProperties: { ...outcomeData.displayProperties, ...displayProps } }))
       );
   }
 
