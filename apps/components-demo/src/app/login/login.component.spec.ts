32a0e7470dec0f4b2822c41f7d7ee491e96aff19
Jaye <jaye.tan@flags.fm>
2019-11-04 17:41:56 +0800

    use ConfigService instead of loading values from environment.ts

--- apps/components-demo/src/app/login/login.component.spec.ts
+++ apps/components-demo/src/app/login/login.component.spec.ts
@@ -3,12 +3,17 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginComponent } from './login.component';
 import { FormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AuthenticationService } from '@perx/core';
+import { AuthenticationService, ConfigService } from '@perx/core';
+import { of } from 'rxjs';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
   const authenticationServiceStub = {};
+  const configServiceStub = {
+    readAppConfig: () => of()
+  };
+
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -18,7 +23,8 @@ describe('LoginComponent', () => {
       ],
       declarations: [LoginComponent],
       providers: [
-        { provide: AuthenticationService, useValue: authenticationServiceStub }
+        { provide: AuthenticationService, useValue: authenticationServiceStub },
+        { provide: ConfigService, useValue: configServiceStub }
       ]
     })
       .compileComponents();
