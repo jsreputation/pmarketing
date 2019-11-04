92d9249b5531e5799647faa2d02c696d134b4fc9
Nicolas Lagier <nicolas@perxtech.com>
2019-11-04 16:46:51 +0800

    PW-2263 language

--- backend/appauth-server/ctrl/language.ts
+++ backend/appauth-server/ctrl/language.ts
@@ -5,19 +5,19 @@ import { join } from 'path';
 
 const EXPRESS_DIST_FOLDER = join(process.cwd());
 
-export const language = () => async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        // @ts-ignore
-        const langsStr: string = req.headers['accept-language'];
-        let langs = langsStr.split(',');
-        if (req.query.default) {
-            langs.push(req.query.default);
-        }
-        langs.push('en');
-        langs = langs.map(l => l.split(';')[0]);
-        const lang = langs.find(l => fs.existsSync(join(EXPRESS_DIST_FOLDER, `/assets/${l}-json.json`)));
-        res.sendFile(join(EXPRESS_DIST_FOLDER, `/assets/${lang}-json.json`));
-    } catch (e) {
-        next(e);
+export const language = () => (req: Request, res: Response, next: NextFunction) => {
+  try {
+    // @ts-ignore
+    const langsStr: string = req.headers['accept-language'];
+    let langs = langsStr.split(',');
+    if (req.query.default) {
+      langs.push(req.query.default);
     }
+    langs.push('en');
+    langs = langs.map(l => l.split(';')[0]);
+    const lang = langs.find(l => fs.existsSync(join(EXPRESS_DIST_FOLDER, `/assets/${l}-json.json`)));
+    res.sendFile(join(EXPRESS_DIST_FOLDER, `/assets/${lang}-json.json`));
+  } catch (e) {
+    next(e);
+  }
 };
