71025a6560eca3897ffa7d00da514b37a4740a1f
Jaye <jaye.tan@flags.fm>
2019-11-05 16:28:43 +0800

    fixed missing app-config property

--- apps/merck-customer/set-env.ts
+++ apps/merck-customer/set-env.ts
@@ -28,6 +28,7 @@ const appConfigFile = `{
   "preAuth": ${process.env.PREAUTH ? process.env.PREAUTH : false},
   "isWhistler": ${process.env.IS_WHISTLER ? process.env.IS_WHISTLER : false},
   "baseHref": "${process.env.BASE_HREF ? process.env.BASE_HREF : '/'}",
+  "defaultLang": "${process.env.DEFAULT_LANG ? process.env.DEFAULT_LANG : 'en'}",
   "sourceType": "${process.env.SOURCE_TYPE ? process.env.SOURCE_TYPE : ''}"
 }
 `;
