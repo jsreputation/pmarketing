b60f9e88454e92922d447edfee3dce32da8714b9
reynard <reynard93@gmail.com>
2019-11-06 09:26:23 +0800

    signup page

--- libs/perx-core/projects/perx-core/src/lib/auth/authentication/whistler-forms.service.ts
+++ libs/perx-core/projects/perx-core/src/lib/auth/authentication/whistler-forms.service.ts
@@ -1,4 +1,4 @@
-import { ISurvey } from './../../survey/models/survey.model';
+import { ISurvey, IQuestion } from './../../survey/models/survey.model';
 import { HttpClient } from '@angular/common/http';
 import { Config } from '../../config/config';
 import { Observable } from 'rxjs';
@@ -6,7 +6,7 @@ import { IFormsService } from './iforms.service';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { IJsonApiListPayload } from '../../jsonapi.payload';
-import { IWCognitoTenantAttributes } from '@perx/whistler';
+import { IWCognitoTenantAttributes, IWSurveyDisplayProperties } from '@perx/whistler';
 
 @Injectable({
   providedIn: 'root'
@@ -18,12 +18,20 @@ export class WhistlerFormsService implements IFormsService {
     this.baseUrl = config.apiHost as string;
   }
 
+  private static WSurveyToSurvey(survey: IWSurveyDisplayProperties): ISurvey {
+    const iQuestions: IQuestion[] = survey.questions.map((q: any) =>
+      ({id: q.id, question: q.question, required: q.required, payload: q.payload})
+    );
+    return { id: 'signup', title: survey.title, questions: iQuestions};
+  }
+
   public getSignupForm(): Observable<ISurvey> {
     return this.http.get<IJsonApiListPayload<IWCognitoTenantAttributes>>(`${this.baseUrl}/cognito/tenants/`)
       .pipe(
         map(res => res.data),
         map(res => res[0]),
-        map(res => res.attributes.properties.signup)
-      ) as Observable<ISurvey>;
+        map(res => res.attributes.properties.signup),
+        map(res => WhistlerFormsService.WSurveyToSurvey(res))
+      );
   }
 }
--- libs/perx-whistler/projects/whistler/src/lib/cognito/tenants.ts
+++ libs/perx-whistler/projects/whistler/src/lib/cognito/tenants.ts
@@ -1,7 +1,7 @@
-import { IWSurveyAttributes } from './../survey/survey';
+import { IWSurveyDisplayProperties } from './../survey/survey';
 
 export interface IWCognitoTenantAttributes {
   properties: {
-    signup: IWSurveyAttributes
-  }
+    signup: IWSurveyDisplayProperties;
+  };
 }
