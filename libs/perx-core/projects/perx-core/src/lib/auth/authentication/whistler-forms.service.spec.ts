c8dc7b81a8cc780fcbbe2e35e4a375c2b04ce529
Golotin Denis <d.golotin@dunice.net>
2019-11-12 18:29:32 +0300

    auth complite

---
+++ libs/perx-core/projects/perx-core/src/lib/auth/authentication/whistler-forms.service.spec.ts
@@ -0,0 +1,30 @@
+import { TestBed, fakeAsync, inject, tick } from "@angular/core/testing";
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ConfigModule } from '../../config/config.module';
+import { HttpClient } from '@angular/common/http';
+import { WhistlerFormsService } from './whistler-forms.service';
+import { of } from 'rxjs';
+import { SurveyService } from '../../survey/survey.service';
+
+describe('WhistlerFormsService', () => {
+    beforeEach(() => TestBed.configureTestingModule({
+        imports: [
+            HttpClientTestingModule,
+            ConfigModule.forRoot({})
+        ]
+    }));
+    it('should get SignUpForm', fakeAsync(inject([HttpClient, WhistlerFormsService], (http: HttpClient, formService: WhistlerFormsService) => {
+        const spy = spyOn(http, 'get').and.returnValue(of({ data: [{ attributes: { properties: { signup: null } } }] }));
+        formService.getSignupForm().subscribe(() => { });
+        tick();
+        expect(spy).toHaveBeenCalled();
+    })));
+
+    it('should create form ',  fakeAsync(inject([HttpClient, WhistlerFormsService], (http: HttpClient, formService: WhistlerFormsService) => {
+        const spy = spyOn(http, 'get').and.returnValue(of({ data: [{ attributes: { properties: { signup: {} } } }] }));
+        spyOn(SurveyService, 'WSurveyToSurvey').and.returnValue(null)
+        formService.getSignupForm().subscribe(() => { });
+        tick();
+        expect(spy).toHaveBeenCalled();
+    })))
+});
