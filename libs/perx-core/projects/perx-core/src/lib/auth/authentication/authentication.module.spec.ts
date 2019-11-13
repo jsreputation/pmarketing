6bb6d3a088da4c8682f6cace57026cf146df9d24
Golotin Denis <d.golotin@dunice.net>
2019-11-13 10:02:57 +0300

    fix spaces second

--- libs/perx-core/projects/perx-core/src/lib/auth/authentication/authentication.module.spec.ts
+++ libs/perx-core/projects/perx-core/src/lib/auth/authentication/authentication.module.spec.ts
@@ -11,38 +11,38 @@ import { TokenType } from './models/authentication.model';
 import { LocalTokenStorage } from './local-token-storage.service';
 
 describe('should create module', () => {
-	beforeEach(() => TestBed.configureTestingModule({
-		imports: [
-			HttpClientTestingModule
-		],
-		providers: [
-			{
-				provide: TokenStorage,
-				useValue: {},
-			},
-			{
-				provide: ProfileService,
-				useValue: {},
-			}
-		]
-	}));
-	it('should create AuthService', inject([HttpClient, TokenStorage, ProfileService],
-		(http: HttpClient, token: TokenStorage, profile: ProfileService) => {
-			let service = AuthServiceFactory(http, { isWhistler: true }, token, profile);
-			expect(service instanceof WhistlerAuthenticationService);
-			service = AuthServiceFactory(http, { isWhistler: false }, token, profile);
-			expect(service instanceof V4AuthenticationService);
-		}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientTestingModule
+    ],
+    providers: [
+      {
+        provide: TokenStorage,
+        useValue: {},
+      },
+      {
+        provide: ProfileService,
+        useValue: {},
+      }
+    ]
+  }));
+  it('should create AuthService', inject([HttpClient, TokenStorage, ProfileService],
+    (http: HttpClient, token: TokenStorage, profile: ProfileService) => {
+      let service = AuthServiceFactory(http, { isWhistler: true }, token, profile);
+      expect(service instanceof WhistlerAuthenticationService);
+      service = AuthServiceFactory(http, { isWhistler: false }, token, profile);
+      expect(service instanceof V4AuthenticationService);
+    }));
 
-	it('should create formsService', inject([HttpClient], (http: HttpClient) => {
-		const service = FormsServiceFactory({}, http);
-		expect(service instanceof WhistlerFormsService).toBeTruthy();
-	}));
+  it('should create formsService', inject([HttpClient], (http: HttpClient) => {
+    const service = FormsServiceFactory({}, http);
+    expect(service instanceof WhistlerFormsService).toBeTruthy();
+  }));
 
-	it('should create tokenService', () => {
-		let service = TokenStorageServiceFactory({ storageType: TokenType.local });
-		expect(service instanceof LocalTokenStorage).toBeTruthy();
-		service = TokenStorageServiceFactory({ storageType: null });
-		expect(service instanceof LocalTokenStorage).toBeTruthy();
-	});
+  it('should create tokenService', () => {
+    let service = TokenStorageServiceFactory({ storageType: TokenType.local });
+    expect(service instanceof LocalTokenStorage).toBeTruthy();
+    service = TokenStorageServiceFactory({ storageType: null });
+    expect(service instanceof LocalTokenStorage).toBeTruthy();
+  });
 });
