b6a0579d1dc4af14ad94ac89cc028a0d04efee19
Arina Mahmudova <a.mahmudova@dunice.net>
2019-11-13 12:57:05 +0300

    migrating element locators into the CreateShakeTheTreeAppPage class

--- apps/candyland/e2e/src/pages/candylandApp.po.ts
+++ apps/candyland/e2e/src/pages/candylandApp.po.ts
@@ -34,7 +34,135 @@ export class CreateShakeTheTreeAppPage {
   public navigateToShakeTheTree(): Promise<string> {
     return browser.get('engagements/games/new-shake') as Promise<string> ;
  }
-
+  public shakeTreeCreateNewButton(): ElementFinder {
+    return element.all(by.css('button')).get(2);
+  }
+  public shakeTreeTypeOptions(): ElementFinder {
+    return element.all(by.tagName('cl-type-item')).get(1);
+  }
+  public shakeTreeSaveButton(): ElementFinder {
+    return element(by.css('button.btn.mat-flat-button.primary'));
+  }
+  public shakeTreeLaunchButton(): ElementFinder {
+    return element.all(by.css('button.btn.mat-flat-button.primary')).last();
+  }
+  public shakeTreeGetButton(): ElementFinder {
+    return element.all(by.className('btn mat-flat-button primary')).get(1);
+  }
+  public shakeTreeGamesButton(): ElementFinder {
+    return element(by.className('btn mat-flat-button primary'));
+  }
+  public shakeTreeNextButton(): ElementFinder {
+    return element.all(by.css('cl-button')).get(2);
+  }
+  public mobilePreviewCheck(): ElementFinder {
+    return element(by.className('mobile-preview mobile-content-multiple'));
+  }
+  public shakeTreePreviewButton(): ElementFinder {
+    return element(by.className('mobile-preview-btn'));
+  }
+  public shakeTreePreviewHeadline(): ElementFinder {
+    return element(by.className('mobile-preview-headline'));
+  }
+  public shakeTreePreviewEmptyHeadline(): ElementFinder {
+    return element(by.css('p.mobile-preview-headline'));
+  }
+  public shakeTreePreviewSubHeadline(): ElementFinder {
+    return element(by.className('mobile-preview-sub-headline'));
+  }
+  public shakeTreePreviewBackground(): ElementFinder {
+    return element(by.className('mobile-preview-background'));
+  }
+  public shakeTreeDesign(): ElementFinder {
+    return element.all(by.tagName('cl-images-preview')).get(1);
+  }
+  public shakeTreeBackgroundDesign(): ElementFinder {
+    return element.all(by.tagName('cl-images-preview')).last();
+  }
+  public treeDesignPreview(): ElementFinder {
+    return element(by.className('tree__img ng-star-inserted'));
+  }
+  public engagementTitleField(): ElementFinder {
+    return element.all(by.css('input[type=text]')).first();
+  }
+  public headlineField(): ElementFinder {
+    return element.all(by.css('input[type=text]')).get(1);
+  }
+  public subHeadlineField(): ElementFinder {
+    return element.all(by.css('input[type=text]')).get(2);
+  }
+  public buttonTextField(): ElementFinder {
+    return element.all(by.css('input[type=text]')).last();
+  }
+  public giftDesign(): ElementFinder {
+    return element.all(by.className('image-wrap')).get(4);
+  }
+  public firstPresentOption(): ElementArrayFinder {
+    return element.all(by.css('img[alt=game-icon]'));
+  }
+  public secondPresentOption(): ElementFinder {
+    return element(by.css('h2.dialog-title'));
+  }
+  public dropdownList(): ElementFinder {
+     return element(by.css('div.mat-select-arrow'));
+  }
+  public selectOption(): ElementFinder {
+    return element.all(by.className('mat-option ng-star-inserted')).get(1);
+  }
+  public selectedAmountPreview(): ElementArrayFinder {
+    return element.all(by.css('img.gift-img'));
+  }
+  public previewMyChoice(): ElementFinder {
+    return element(by.className('gift-img gift-img__1 ng-star-inserted'));
+  }
+  public uploadGiftImage(): ElementFinder {
+    return element.all(by.css('input[type="file"]')).get(0);
+  }
+  public uploadBackgroundImage(): ElementFinder {
+    return element.all(by.css('input[type="file"]')).get(1);
+  }
+  public uploadedImageObj(): ElementFinder {
+    return element(by.css('div.image-wrap.ng-star-inserted>img.image'));
+  }
+  public errorUploadMessage(): ElementFinder {
+    return element(by.css('div.upload-error-wrap.ng-star-inserted>span'));
+  }
+  public emptyInputFields(): ElementArrayFinder {
+    return element.all(by.css('input[type="file"]'));
+  }
+  public uploadField(): ElementFinder {
+    return element(by.css('div.image-wrap.ng-star-inserted>img'));
+  }
+  public giftPreview(): ElementFinder {
+    return element(by.css('div.gift-wrapper.gift-wrapper__1.hang.ng-star-inserted>img'));
+  }
+  public backgroundPreview(): ElementFinder {
+    return element(by.css('div.mobile-preview-background'));
+  }
+  public mainHeadlineField(): ElementFinder {
+    return element.all(by.css('input[type=text]')).get(0);
+  }
+  public launchLaterBtn(): ElementFinder {
+    return element(by.css('cl-button.actions-close'));
+  }
+  public fileDialog(): ElementFinder {
+    return element(by.css('cl-confirm-modal'));
+  }
+  public itemName(): ElementFinder {
+    return element.all(by.css('p.engagement-item-name')).first();
+  }
+  public itemInfo(): ElementFinder {
+    return element.all(by.css('div.engagement-item-info>p.engagement-item-name')).first();
+  }
+  public transactionDate(): ElementFinder {
+    return element.all(by.css('p.engagement-item-date')).first();
+  }
+  public firstDialogEl(): ElementFinder {
+    return element.all(by.css('cl-button')).get(1);
+  }
+  public secondDialogEl(): ElementFinder {
+    return element.all(by.css('cl-button')).get(2);
+  }
 }
 export class LoginAppPage {
 
