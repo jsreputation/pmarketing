b1b3fab146e3dede53351de0126f196e4dbd6b42
Nicolas Lagier <nicolas@perxtech.com>
2019-11-14 14:12:06 +0800

    PW-2371 cleanup merchants null checks

--- libs/perx-core/projects/perx-core/src/lib/merchants/whistler-merchants.service.ts
+++ libs/perx-core/projects/perx-core/src/lib/merchants/whistler-merchants.service.ts
@@ -32,12 +32,12 @@ export class WhistlerMerchantsService implements IMerchantsService {
       id: (typeof merchant.id === 'string') ? Number.parseInt(merchant.id, 10) : merchant.id,
       name: merchant.attributes.name,
       description: merchant.attributes.description,
-      images: [
+      images: merchant.attributes.properties.logo_image ? [
         {
           type: 'banner',
           url: merchant.attributes.properties.logo_image
         }
-      ]
+      ] : []
     };
   }
 
@@ -46,7 +46,9 @@ export class WhistlerMerchantsService implements IMerchantsService {
     const current = {};
     return new Observable((sub) => {
       this.getMerchantsPage(i).pipe(
-        expand((response) => i < (response.meta && response.meta.page_count) ? this.getMerchantsPage(++i) : EMPTY),
+        expand((response) =>
+          (response.meta && response.meta.page_count && response.meta.page_count > i) ? this.getMerchantsPage(++i) : EMPTY
+        ),
         map((value) => value.data.map((el) => WhistlerMerchantsService.WMerchantToMerchant(el))),
         tap((data) => data.forEach((el) => current[el.id] = el)),
         finalize(() => this.merchants = current)
