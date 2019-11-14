d221788ffb9ce561e4c76ea9f21ab760d96d47bb
reynard <reynard93@gmail.com>
2019-11-14 18:45:39 +0800

    null checks PW-2378 for survey

--- libs/perx-core/projects/perx-core/src/lib/survey/question/group/group.component.ts
+++ libs/perx-core/projects/perx-core/src/lib/survey/question/group/group.component.ts
@@ -48,7 +48,7 @@ export class GroupComponent implements OnChanges {
   }
 
   public updatePoint(point: IPoints): void {
-    this.pointsTracker[point.questionId] = point.point;
+    this.pointsTracker[point.questionId as string] = point.point;
     const currentPoint = this.calculatePoints();
     this.updatePoints.emit(currentPoint);
   }
