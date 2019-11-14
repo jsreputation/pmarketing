317a0eb415e4a4a104c19ec9e835bcd8394623af
HU ZHI <andrewing1012@gmail.com>
2019-11-14 09:17:19 +0800

    typo

--- apps/candyland/src/app/core/http-services/limits-https.service.ts
+++ apps/candyland/src/app/core/http-services/limits-https.service.ts
@@ -14,7 +14,7 @@ export class LimitsHttpsService {
 
   public getLimits(params: HttpParams, engagementType: string):
     Observable<IJsonApiListPayload<IWLimitAttributes>> {
-    const eType = this.getEngegementTypeLink(engagementType);
+    const eType = this.getEngagementTypeLink(engagementType);
     return this.http.get<IJsonApiListPayload<IWLimitAttributes>>(`${ApiConfig.basePath}/${eType}/limits`, { params });
   }
 
@@ -23,7 +23,7 @@ export class LimitsHttpsService {
     data: IJsonApiPayload<IWLimitAttributes>,
     engagementType: string
   ): Observable<IJsonApiPayload<IWLimitAttributes>> {
-    const eType = this.getEngegementTypeLink(engagementType);
+    const eType = this.getEngagementTypeLink(engagementType);
     const url = `${ApiConfig.basePath}/${eType}/limits/${id}`;
     return this.http.patch<IJsonApiPayload<IWLimitAttributes>>(url, data);
   }
@@ -32,7 +32,7 @@ export class LimitsHttpsService {
     data: IJsonApiPayload<IWLimitAttributes>,
     engagementType: string
   ): Observable<IJsonApiPayload<IWLimitAttributes>> {
-    const eType = this.getEngegementTypeLink(engagementType);
+    const eType = this.getEngagementTypeLink(engagementType);
     return this.http.post<IJsonApiPayload<IWLimitAttributes>>(
       `${ApiConfig.basePath}/${eType}/limits`,
       data
@@ -40,13 +40,13 @@ export class LimitsHttpsService {
   }
 
   public deleteLimit(engagementType: string, limitId: number | string): Observable<void> {
-    const eType = this.getEngegementTypeLink(engagementType);
+    const eType = this.getEngagementTypeLink(engagementType);
     return this.http.delete(`${ApiConfig.basePath}/${eType}/limits/${limitId}`)
       // convert to a void observable in case of success
       .pipe(map(() => { return; }));
   }
 
-  private getEngegementTypeLink (type: string): string {
+  private getEngagementTypeLink(type: string): string {
     return EngagementTypeAPIMapping[type].replace('_', '-');
   }
 }
