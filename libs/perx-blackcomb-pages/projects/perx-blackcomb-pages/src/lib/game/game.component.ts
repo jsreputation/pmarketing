f18879c7b2059ae13ec99302e05c8d0b2626c85a
Nicolas Lagier <nicolas.lagier@gmail.com>
2019-11-16 12:07:42 +0800

    PW-2474 cleanup

--- libs/perx-blackcomb-pages/projects/perx-blackcomb-pages/src/lib/game/game.component.ts
+++ libs/perx-blackcomb-pages/projects/perx-blackcomb-pages/src/lib/game/game.component.ts
@@ -36,12 +36,16 @@ export class GameComponent implements OnInit, OnDestroy {
   };
 
   private initTranslate(): void {
-    this.translate.get(this.successPopUp.title).subscribe((text) => this.successPopUp.title = text);
-    this.translate.get(this.successPopUp.text).subscribe((text) => this.successPopUp.text = text);
-    this.translate.get(this.successPopUp.buttonTxt).subscribe((text) => this.successPopUp.buttonTxt = text);
-    this.translate.get(this.noRewardsPopUp.title).subscribe((text) => this.noRewardsPopUp.title = text);
-    this.translate.get(this.noRewardsPopUp.text).subscribe((text) => this.noRewardsPopUp.text = text);
-    this.translate.get(this.noRewardsPopUp.buttonTxt).subscribe((text) => this.noRewardsPopUp.buttonTxt = text);
+    [
+      this.successPopUp.title,
+      this.successPopUp.text,
+      this.successPopUp.buttonTxt,
+      this.noRewardsPopUp.title,
+      this.noRewardsPopUp.text,
+      this.noRewardsPopUp.buttonTxt
+    ]
+      .filter(k => k !== undefined && k !== null)
+      .forEach((k: string) => this.translate.get(k).subscribe((text) => k = text));
   }
 
   constructor(
@@ -100,7 +104,7 @@ export class GameComponent implements OnInit, OnDestroy {
     ).subscribe(
       (gameTransaction: IEngagementTransaction) => {
         this.transactionId = gameTransaction.id;
-        if (gameTransaction.voucherIds.length > 0) {
+        if (gameTransaction.voucherIds && gameTransaction.voucherIds.length > 0) {
           const count = gameTransaction.voucherIds.length.toString();
           this.successPopUp.text =
             this.successPopUp.text ? this.successPopUp.text.replace('{{rewards}}', count) : `You earned ${count} rewards`;
@@ -130,7 +134,9 @@ export class GameComponent implements OnInit, OnDestroy {
         bufferCount(nbSteps),
         first()
       );
-    const userAction$: Observable<void> = this.isAnonymousUser ? of(void 0) : this.gameService.prePlayConfirm(this.transactionId);
+    const userAction$: Observable<void> = this.isAnonymousUser || !this.transactionId ?
+      of(void 0) :
+      this.gameService.prePlayConfirm(this.transactionId);
     combineLatest(processBar$, userAction$).subscribe(
       () => this.redirectUrlAndPopUp(),
       () => this.redirectUrlAndPopUp()
