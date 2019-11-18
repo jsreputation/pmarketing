4cc79d56cb8e8a6d8035de06eedd64951f017dbc
Nicolas Lagier <nicolas@perxtech.com>
2019-11-18 17:06:34 +0800

    PW-2469 change graph type for engagement rate per engagement type

--- libs/perx-chart/projects/perx-chart/src/public-api.ts
+++ libs/perx-chart/projects/perx-chart/src/public-api.ts
@@ -6,3 +6,4 @@ export { PerxChartModule } from './lib/perx-chart.module';
 export { IData, BaseType } from './lib/data.model';
 export { DataService } from './lib/data.service';
 export { EnvConfig } from './lib/env.config';
+export { CardType } from './lib/meta-card/meta-card.component';
