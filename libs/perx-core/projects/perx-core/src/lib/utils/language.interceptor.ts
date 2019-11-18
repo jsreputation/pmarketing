fc583a1ebb5d352c80996e64070646ea0d8e85c7
Golotin Denis <d.golotin@dunice.net>
2019-11-18 16:48:26 +0300

    lint fix

--- libs/perx-core/projects/perx-core/src/lib/utils/language.interceptor.ts
+++ libs/perx-core/projects/perx-core/src/lib/utils/language.interceptor.ts
@@ -1,16 +1,17 @@
-import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { TokenStorage } from './storage/token-storage.service';
+import { Observable } from 'rxjs';
 @Injectable()
 export class LanguageInterceptor implements HttpInterceptor {
-    constructor(
-        private tokenStorage: TokenStorage
-    ) { }
-    intercept(req: HttpRequest<any>, next: HttpHandler) {
-        const lang = this.tokenStorage.getAppInfoProperty('lang');
-        if (lang) {
-            req.headers.set('accept-language', lang)
-        }
-        return next.handle(req);
+  constructor(
+    private tokenStorage: TokenStorage
+  ) { }
+  public intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const lang = this.tokenStorage.getAppInfoProperty('lang');
+    if (lang) {
+      req.headers.set('accept-language', lang);
     }
+    return next.handle(req);
+  }
 }
