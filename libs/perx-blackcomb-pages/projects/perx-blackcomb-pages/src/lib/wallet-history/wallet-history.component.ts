509a52f3422dbc801f2dc05b962f9c21155108b2
salmankhalid187 <salmankhalid1@gmail.com>
2019-11-18 06:04:58 +0500

    New component created for wallet

---
+++ libs/perx-blackcomb-pages/projects/perx-blackcomb-pages/src/lib/wallet-history/wallet-history.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'perx-blackcomb-pages-wallet-history',
+  templateUrl: './wallet-history.component.html',
+  styleUrls: ['./wallet-history.component.scss']
+})
+export class WalletHistoryComponent implements OnInit {
+
+  public ngOnInit(): void {
+  }
+
+}
