17f61b6975659fc213e613f32203ba10ec35f9cb
salmankhalid187 <salmankhalid1@gmail.com>
2019-11-18 14:51:00 +0500

    Vouchers list added in new component

--- libs/perx-blackcomb-pages/projects/perx-blackcomb-pages/src/lib/wallet-history/wallet-history.component.spec.ts
+++ libs/perx-blackcomb-pages/projects/perx-blackcomb-pages/src/lib/wallet-history/wallet-history.component.spec.ts
@@ -1,16 +1,38 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Router } from '@angular/router';
+import { IVoucherService, VouchersModule } from '@perx/core';
+import { RouterTestingModule } from '@angular/router/testing';
 import { WalletHistoryComponent } from './wallet-history.component';
+import { of } from 'rxjs';
+import { MatTabsModule } from '@angular/material';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('WalletHistoryComponent', () => {
   let component: WalletHistoryComponent;
   let fixture: ComponentFixture<WalletHistoryComponent>;
 
+  const router = {
+    navigate: () => {}
+  };
+  const vouchersServiceStub: Partial<IVoucherService> = {
+    getAll: () => of([])
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ WalletHistoryComponent ]
+      declarations: [ WalletHistoryComponent ],
+      imports: [
+        VouchersModule,
+        RouterTestingModule,
+        MatTabsModule,
+        NoopAnimationsModule
+      ],
+      providers: [
+        { provide: Router, useValue: router },
+        { provide: IVoucherService, useValue: vouchersServiceStub }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
