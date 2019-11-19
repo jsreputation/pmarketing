235ba6ed33f0c733c56243ded29190b4c8bc72ed
salmankhalid187 <salmankhalid1@gmail.com>
2019-11-19 17:04:42 +0500

    Config service refactored

---
+++ libs/perx-core/projects/perx-core/src/lib/config/whistler-config.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IConfig, IMicrositeSettings } from './models/config.model';
+import { ConfigService } from './config.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WhistlerConfigService implements ConfigService {
+
+  public readAppConfig(): Observable<IConfig> {
+    throw new Error('WhistlerConfigService readAppConfig Method not implemented.');
+  }
+
+  public getTenantAppSettings(): Observable<IMicrositeSettings> {
+    throw new Error('WhistlerConfigService readAppConfig Method not implemented.');
+  }
+}
