30939bc6ef0c761cba6be6220e61fce8acb9fa35
salmankhalid187 <salmankhalid1@gmail.com>
2019-11-20 11:09:17 +0500

    Fix testing scripts

--- libs/perx-core/projects/perx-core/src/lib/config/v4-config.service.ts
+++ libs/perx-core/projects/perx-core/src/lib/config/v4-config.service.ts
@@ -27,7 +27,7 @@ export class V4ConfigService extends ConfigService {
   constructor(
     private http: HttpClient,
     private authenticationService: AuthenticationService
-  ) { 
+  ) {
     super();
   }
 
