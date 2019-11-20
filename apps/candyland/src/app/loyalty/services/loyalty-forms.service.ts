d1db2b702a838155965459a0ccc5bd2a46caa7ec
AndyG00D <achicunov@gmail.com>
2019-11-20 09:53:29 +0200

    fix merge issues

--- apps/candyland/src/app/loyalty/services/loyalty-forms.service.ts
+++ apps/candyland/src/app/loyalty/services/loyalty-forms.service.ts
@@ -55,7 +55,7 @@ export class LoyaltyFormsService {
       status: StatusLabel.DRAFT,
       details: {
         pointsName: 'Point',
-        imageUrl: 'https://test.com.jpg',
+        imageUrl: null,
         joinMethod: {
           inviteOnly: true
         },
