c484e3bee7011aa92a827bb2f0c9438d59da18e1
salmankhalid187 <salmankhalid1@gmail.com>
2019-11-22 17:33:45 +0500

    getAccountSettings method added in v4ConfigService

--- apps/all-it/set-env.ts
+++ apps/all-it/set-env.ts
@@ -11,6 +11,36 @@ require('dotenv').config();
 
 // Debug environment variables
 
+const displayProperties = `"display_properties": {
+    "account": {
+        "pages": [
+            {
+                "key": "contact-us",
+                "title": "Contact Us",
+                "content_url": "https://cdn.uat.whistler.perxtech.io/dev1/tenants/222222222/762b80fa-e4b3-11e9-81b4-2a2ae2dbcce4"
+            },
+            {
+                "key": "tnc",
+                "title": "Terms and Conditions",
+                "content_url": "https://cdn.uat.whistler.perxtech.io/dev1/tenants/222222222/762b84ba-e4b3-11e9-81b4-2a2ae2dbcce4"
+            }
+        ]
+    },
+    "currency": "VND",
+    "time_zone": 15,
+    "theme.font": "Lato",
+    "theme.logo": "https://cdn.uat.whistler.perxtech.io/dev1/tenants/222222222/b4e04bcc-6dc4-4eea-a9d1-9aa18ca43aa3",
+    "theme.style": "Light",
+    "theme.title": "",
+    "theme.accent": "#0500cc",
+    "theme.primary": "#edcf27",
+    "showHistoryPage": true,
+    "campaign_base_url": "https://generic-blackcomb-dev1.uat.whistler.perxtech.io/loading",
+    "theme.header_color": "#0500cc",
+    "theme.button_text_color": "#ffffff",
+    "theme.button_background_color": "#0500cc"
+  }`;
+
 // `environment.ts` file structure that uses the environment variables
 const envConfigFile = `export const environment = {
   apiHost: '${process.env.APIHOST ? process.env.APIHOST : 'https://api.getperx.io'}',
@@ -34,7 +64,8 @@ const appConfigFile = `{
   "showHistoryPage": ${process.env.SHOW_HISTORY_PAGE ? process.env.SHOW_HISTORY_PAGE : false},
   "showNewsfeedOnHomepage": ${process.env.SHOW_NEWSFEED_HOMEPAGE ? process.env.SHOW_NEWSFEED_HOMEPAGE : true},
   "showQrPageSubtitle": ${process.env.SHOW_QRPAGE_SUBTITLE ? process.env.SHOW_QRPAGE_SUBTITLE : true},
-  "showExpiryOnRewardDetail": ${process.env.SHOW_EXPIRY_REWARD_DETAIL ? process.env.SHOW_EXPIRY_REWARD_DETAIL : false},  
+  "showExpiryOnRewardDetail": ${process.env.SHOW_EXPIRY_REWARD_DETAIL ? process.env.SHOW_EXPIRY_REWARD_DETAIL : false},
+  ${displayProperties}
 }
 `;
 
