45d12583231b3c753a61f5fcdcd4dd1f43c1d96b
reynard <reynard93@gmail.com>
2019-11-22 17:36:46 +0800

    PW-2586/remove lastButton

--- libs/perx-whistler/projects/whistler/src/lib/engagement/games.ts
+++ libs/perx-whistler/projects/whistler/src/lib/engagement/games.ts
@@ -26,7 +26,6 @@ export interface IWPinataDisplayProperties extends IWGameDisplayProperties {
 }
 
 export interface IWScratchDisplayProperties extends IWGameDisplayProperties {
-  lastButtonText?: string;
   pre_scratch_img_url: string;
   post_scratch_success_img_url: string;
   post_scratch_fail_img_url: string;
