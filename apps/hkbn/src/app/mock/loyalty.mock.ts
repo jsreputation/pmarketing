cb145b52bfde24ae40a308ac6de51b3ca8d0f3c4
Vitalii Lysak <v.lysak@dunice.net>
2019-12-02 14:59:40 +0300

    fix test issues

--- apps/hkbn/src/app/mock/loyalty.mock.ts
+++ apps/hkbn/src/app/mock/loyalty.mock.ts
@@ -2,8 +2,16 @@ import { ILoyalty } from '@perx/core';
 
 export const loyalties: ILoyalty[] = [
   {
-    id: 0,
-    name: 'loyalty',
+    beginDate: '2019-08-20T03:28:23.241Z',
+    currency: 'SGD',
+    currencyBalance: 80,
+    description: 'Collect points',
+    endDate: '2029-08-20T03:28:23.241Z',
+    expiringPoints: undefined,
+    id: 100,
+    membershipIdentifier: '8221151080374196',
+    membershipTierName: 'gold',
+    name: 'Starhub Loyalty',
     pointsBalance: 80,
   },
 ];
