d8d68993ccdd21860b6a16010d96a76f5d6b1f6a
Golotin Denis <d.golotin@dunice.net>
2019-12-02 15:30:16 +0300

    pw-2729

--- apps/abenson/src/app/wallet/wallet.component.ts
+++ apps/abenson/src/app/wallet/wallet.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { StatusLabelMapping, Voucher, IVoucherService, VoucherState } from '@perx/core';
+import { StatusLabelMapping, Voucher, IVoucherService, VoucherState, ConfigService } from '@perx/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -11,7 +11,7 @@ import { Router } from '@angular/router';
 })
 export class WalletComponent implements OnInit {
   public issuedVouchers: Observable<Voucher[]>;
-
+  public comingSoon: boolean = true;
   public redeemedVouchers: Observable<Voucher[]>;
 
   public mapping: StatusLabelMapping = {
@@ -24,11 +24,13 @@ export class WalletComponent implements OnInit {
 
   constructor(
     private vouchersService: IVoucherService,
-    private router: Router
+    private router: Router,
+    private configService: ConfigService
   ) { }
 
   public ngOnInit(): void {
     const feed = this.vouchersService.getAll();
+    this.configService.readAppConfig().subscribe((config) => this.comingSoon = config.comingSoon as boolean);
     this.issuedVouchers = feed
       .pipe(
         map((vouchers: Voucher[]) => vouchers.filter(voucher => voucher.state === VoucherState.issued))
