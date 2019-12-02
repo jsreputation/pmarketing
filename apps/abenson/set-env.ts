c6aef834c9d186ccb0f503386fc29ecd56b12a30
Golotin Denis <d.golotin@dunice.net>
2019-12-02 14:36:56 +0300

    pw-2726

--- apps/abenson/set-env.ts
+++ apps/abenson/set-env.ts
@@ -16,7 +16,8 @@ const envConfigFile = `export const environment = {
   production: ${process.env.PRODUCTION ? process.env.PRODUCTION : false},
   preAuth: ${process.env.PREAUTH ? process.env.PREAUTH : false},
   isWhistler: ${process.env.IS_WHISTLER ? process.env.IS_WHISTLER : false},
-  baseHref: '${process.env.BASE_HREF ? process.env.BASE_HREF : '/'}'
+  baseHref: '${process.env.BASE_HREF ? process.env.BASE_HREF : '/'}',
+  comingSoon: '${process.env.COMING_SOON ? process.env.COMING_SOON : false}'
 };
 `;
 
