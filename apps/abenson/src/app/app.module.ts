369663d5577cbb1a01bcd34dcfa7c17999e8c049
Golotin Denis <d.golotin@dunice.net>
2019-12-04 12:14:20 +0300

    pw-2609

--- apps/abenson/src/app/app.module.ts
+++ apps/abenson/src/app/app.module.ts
@@ -48,6 +48,7 @@ import { campaigns } from './mock/campaigns.mock';
 import { SignUpComponent } from './auth/signup/signup.component';
 import { UnauthorizedInterceptor } from './auth/unauthorized.interceptor';
 import { SmsValidationComponent } from './auth/sms-validation/sms-validation.component';
+import { AppTokenInterceptor } from './auth/apptoken.interceptor';
 
 const rewardsServiceStub = {
   getReward: () => of(rewards[0]),
@@ -106,7 +107,8 @@ const campaignServiceStub = {
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: UnauthorizedInterceptor, multi: true },
     { provide: RewardsService, useValue: rewardsServiceStub },
-    { provide: ICampaignService, useValue: campaignServiceStub }
+    { provide: ICampaignService, useValue: campaignServiceStub },
+    { provide: HTTP_INTERCEPTORS, useClass: AppTokenInterceptor, multi: true }
   ],
   bootstrap: [AppComponent]
 })
