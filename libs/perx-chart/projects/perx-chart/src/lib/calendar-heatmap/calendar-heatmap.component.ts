90940a961acc573c7536fa4c27a4e1edc9dae367
reynard <reynard93@gmail.com>
2019-12-05 16:33:18 +0800

    easier way to pass down

--- libs/perx-chart/projects/perx-chart/src/lib/calendar-heatmap/calendar-heatmap.component.ts
+++ libs/perx-chart/projects/perx-chart/src/lib/calendar-heatmap/calendar-heatmap.component.ts
@@ -13,10 +13,6 @@ export class CalendarHeatmapComponent implements OnChanges {
 
   public ngxChartData: ChartData[];
 
-  public colorScheme: {['domain']: string[]} = {
-    domain: ['#e1f5fe', '#b3e5fc', '#81d4fa', '#4fc3f7', '#29b6f6', '#03a9f4', '#039be5', '#0288d1', '#0277bd', '#01579b']
-  };
-
   public ngOnChanges(changes: SimpleChanges): void {
     if (!changes.data) {
       return;
