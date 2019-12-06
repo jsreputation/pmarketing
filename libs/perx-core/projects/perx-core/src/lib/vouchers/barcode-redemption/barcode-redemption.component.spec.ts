bcb1ed0f32e1f9d429379017fedcc25e57acae8c
Vitalii Lysak <v.lysak@dunice.net>
2019-12-06 12:38:57 +0300

    fix tests

--- libs/perx-core/projects/perx-core/src/lib/vouchers/barcode-redemption/barcode-redemption.component.spec.ts
+++ libs/perx-core/projects/perx-core/src/lib/vouchers/barcode-redemption/barcode-redemption.component.spec.ts
@@ -1,14 +1,38 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  async,
+  ComponentFixture,
+  TestBed,
+} from '@angular/core/testing';
+
+import { of } from 'rxjs';
+import { NgxBarcodeModule } from 'ngx-barcode';
 
 import { BarcodeRedemptionComponent } from './barcode-redemption.component';
+
+import {IVoucherService} from '../ivoucher.service';
 
 describe('BarcodeRedemptionComponent', () => {
   let component: BarcodeRedemptionComponent;
   let fixture: ComponentFixture<BarcodeRedemptionComponent>;
 
+  const voucherServiceStub = {
+    get: () => of()
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BarcodeRedemptionComponent ]
+      declarations: [
+        BarcodeRedemptionComponent,
+      ],
+      imports: [
+        NgxBarcodeModule,
+      ],
+      providers: [
+        {
+          provide: IVoucherService,
+          useValue: voucherServiceStub,
+        },
+      ],
     })
     .compileComponents();
   }));
