d3ccf0d65fd7ffec215d0880134c6669a1634cdc
Golotin Denis <d.golotin@dunice.net>
2019-12-11 15:04:17 +0300

    SpinTheWheelComponent

--- libs/perx-core/projects/perx-core/src/lib/game/spin-the-wheel/spin-the-wheel.component.spec.ts
+++ libs/perx-core/projects/perx-core/src/lib/game/spin-the-wheel/spin-the-wheel.component.spec.ts
@@ -1,13 +1,26 @@
 import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 
 import { SpinTheWheelComponent } from './spin-the-wheel.component';
-import { SimpleChange } from '@angular/core';
+import { SimpleChange, DebugElement } from '@angular/core';
 import { ISlice } from '../game.model';
+import { By } from '@angular/platform-browser';
 
 describe('SpinTheWheelComponent', () => {
   let component: SpinTheWheelComponent;
   let fixture: ComponentFixture<SpinTheWheelComponent>;
-
+  let imageOnload: Function[] = [];
+  let debugElement: DebugElement;
+  beforeAll(() => {
+    Object.defineProperty(Image.prototype, 'onload', {
+      get: function () {
+        return this._onload
+      },
+      set: function (fn: Function) {
+        imageOnload.push(fn);
+        this._onload = fn;
+      }
+    })
+  });
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [SpinTheWheelComponent]
@@ -17,6 +30,7 @@ describe('SpinTheWheelComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SpinTheWheelComponent);
+    debugElement = fixture.debugElement;
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -24,9 +38,9 @@ describe('SpinTheWheelComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-
-  it('ngOnChanges', fakeAsync(() => {
+  it('ngOnChanges else', fakeAsync(() => {
     // else flow
+    imageOnload = [];
     component.ngOnChanges({
       wheelImg: {} as SimpleChange,
       pointerImg: {} as SimpleChange
@@ -41,11 +55,47 @@ describe('SpinTheWheelComponent', () => {
       pointerImg: {} as SimpleChange
     });
     tick();
+    imageOnload[0](); // emulate onload
+    expect(component.size).toBeTruthy();
+  }));
+
+  it('ngOnChanges', fakeAsync(() => {
+    imageOnload = [];
+    spyOn(component.ctx, 'createPattern').and.returnValue({setTransform(){}});
+    component.ngOnChanges({
+      wheelImg: {} as SimpleChange,
+      pointerImg: {} as SimpleChange
+    });
+    component.slices = [{
+      id: '1',
+      backgroundImage: 'image.img',
+      label: 'test'
+    } as ISlice];
+    component.ngOnChanges({
+      slices: {} as SimpleChange,
+      wheelImg: {} as SimpleChange,
+      pointerImg: {} as SimpleChange
+    });
+    tick();
+    imageOnload[0]();
+    imageOnload[1]();
     expect(component.size).toBeTruthy();
   }));
 
   it('ngAfterViewInit', () => {
     component.ngAfterViewInit();
     expect(component.size).toBeTruthy();
+  });
+
+  it('handle mouse event', ()=>{
+    const eventMouseUp = new MouseEvent('mouseup');
+    debugElement.query(By.css('#ng-wheel-canvas')).nativeElement.dispatchEvent(eventMouseUp);
+    expect(component.dragging).toBeFalsy();
+    const eventMouseStart = new MouseEvent('mousedown');
+    debugElement.query(By.css('#ng-wheel-canvas')).nativeElement.dispatchEvent(eventMouseStart);
+    expect(component.dragging).toBeTruthy();
+    const eventMouseMove = new MouseEvent('mousemove');
+    debugElement.query(By.css('#ng-wheel-canvas')).nativeElement.dispatchEvent(eventMouseMove);
+    expect(component.dragging).toBeTruthy();
   });
 });
