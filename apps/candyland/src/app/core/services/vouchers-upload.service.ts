9eeb4f38133f4b7acc5ef5dbc110d38201d35c28
HU ZHI <andrewing1012@gmail.com>
2019-12-12 11:04:53 +0800

    add retry logic, need to confirm with backend later

--- apps/candyland/src/app/core/services/vouchers-upload.service.ts
+++ apps/candyland/src/app/core/services/vouchers-upload.service.ts
@@ -1,10 +1,12 @@
 import { IAdvancedUploadFileService, IUploadFileStatus, UploadStatus } from './iadvanced-upload-file.service';
-import { Observable, BehaviorSubject } from 'rxjs';
+import { Observable, BehaviorSubject, of, throwError } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { UploadFileService } from '@cl-core-services';
 import { VouchersService } from './vouchers.service';
-import { map, switchMap } from 'rxjs/operators';
+import { map, switchMap, retryWhen, mergeMap, delay } from 'rxjs/operators';
 import { IWVouchersApi } from '@perx/whistler';
+import { HttpErrorResponse } from '@angular/common/http';
+import { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } from 'constants';
 
 @Injectable({
   providedIn: 'root'
@@ -22,12 +24,29 @@ export class VouchersUploadService extends IAdvancedUploadFileService {
     const subject: BehaviorSubject<IUploadFileStatus> = new BehaviorSubject<IUploadFileStatus>(
       { fileName: file.name, status: UploadStatus.UPLOADING }
     );
+    let maxRetryTimes = 5;
+    let delayUnitTime = 1000;
+    let retryTimes = SSL_OP_SSLEAY_080_CLIENT_DH_BUG;
     this.uploadService.uploadFile(file)
       .pipe(
         map((res: IUploadedFile) => res.url),
         switchMap((url: string) => this.vouchersService.uploadVouchers(url, options.rewardId)),
-        // handle polling
-        // retryWhen()
+        switchMap(
+          (batch: IJsonApiPayload<IWVouchersApi>) =>
+            this.vouchersService.getVouchersBatch(Number.parseInt(batch.data.id)).pipe(
+              retryWhen(
+                (err: Observable<HttpErrorResponse>) => err.pipe(
+                  mergeMap(error => {
+                    if (retryTimes < maxRetryTimes) {
+                      retryTimes++;
+                      return of(error.status).pipe(delay(delayUnitTime));
+                    }
+                    return throwError(error);
+                  })
+                )
+              )
+            )
+        )
       )
       .subscribe(
         (res: IJsonApiPayload<IWVouchersApi>) => {
