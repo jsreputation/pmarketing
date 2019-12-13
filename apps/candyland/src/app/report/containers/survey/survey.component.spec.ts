6def3dc5df5481e62b4177c4f02dad0e7ac341b1
Nicolas Lagier <nicolas@perxtech.com>
2019-12-13 13:12:45 +0800

    PW-241 cleanup lint

--- apps/candyland/src/app/report/containers/survey/survey.component.spec.ts
+++ apps/candyland/src/app/report/containers/survey/survey.component.spec.ts
@@ -6,14 +6,16 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { TranslateModule } from '@ngx-translate/core';
+import { MessageService } from '@cl-core-services';
+import { DatePipe } from '@angular/common';
 
 describe('SurveyComponent', () => {
   let component: SurveyComponent;
   let fixture: ComponentFixture<SurveyComponent>;
-
+  const messageServiceStub: Partial<MessageService> = {};
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SurveyComponent ],
+      declarations: [SurveyComponent],
       schemas: [NO_ERRORS_SCHEMA],
       imports: [
         HttpClientTestingModule,
@@ -21,9 +23,13 @@ describe('SurveyComponent', () => {
           { path: 'dashboard/overview', redirectTo: '/' }
         ]),
         TranslateModule.forRoot(),
+      ],
+      providers: [
+        DatePipe,
+        { provide: MessageService, useValue: messageServiceStub }
       ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
