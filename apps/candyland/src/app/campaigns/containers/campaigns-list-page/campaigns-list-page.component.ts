f4a6f91a861d5378c1fcafdbc716df6001e41e48
Nicolas Lagier <nicolas@perxtech.com>
2019-12-13 11:23:54 +0800

    PW-241 download report as csv

--- apps/candyland/src/app/campaigns/containers/campaigns-list-page/campaigns-list-page.component.ts
+++ apps/candyland/src/app/campaigns/containers/campaigns-list-page/campaigns-list-page.component.ts
@@ -3,14 +3,17 @@ import {
   CustomDataSource,
   DataSourceStates, DataSourceUpdateSchema
 } from '@cl-shared/table/data-source/custom-data-source';
-import { CampaignsService, ConfigService } from '@cl-core/services';
+import { CampaignsService, ConfigService, MessageService } from '@cl-core/services';
 import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material';
-import { ICampaignTableData } from '@cl-core/models/campaign/campaign.interface';
+import { ICampaignTableData, ICampaign } from '@cl-core/models/campaign/campaign';
 import { StatusLabelConfig } from '@cl-shared';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
-import { CampaignAction } from '../../model/campaign-action.enum';
+// import { CampaignAction } from '../../model/campaign-action.enum';
+import { CampaignStatus } from '@cl-core/models/campaign/campaign-status.enum';
+import { EngagementType } from '@cl-core/models/engagement/engagement-type.enum';
+import { DataService } from '@perx/chart';
 
 @Component({
   selector: 'cl-campaigns-list-page',
@@ -24,13 +27,17 @@ export class CampaignsListPageComponent implements OnInit, OnDestroy {
   public displayedColumns: string[] = ['name', 'status', 'begin', 'end', 'audience', 'engagementType', 'actions'];
   public statusLabel: { [key: string]: StatusLabelConfig };
   private destroy$: Subject<void> = new Subject();
-  private campaignStatuses: typeof CampaignAction = CampaignAction;
-  constructor(private campaignsService: CampaignsService,
-              private router: Router,
-              private snack: MatSnackBar,
-              private configService: ConfigService,
-              private cd: ChangeDetectorRef) {
-    this.dataSource = new CustomDataSource<ICampaignTableData>(this.campaignsService, 5, {include: 'pool'});
+
+  constructor(
+    private campaignsService: CampaignsService,
+    private router: Router,
+    private snack: MatSnackBar,
+    private configService: ConfigService,
+    private cd: ChangeDetectorRef,
+    private dataService: DataService,
+    private messageService: MessageService
+  ) {
+    this.dataSource = new CustomDataSource<ICampaignTableData>(this.campaignsService, 5, { include: 'pool' });
   }
 
   public editCampaign(campaign: ICampaignTableData): void {
@@ -46,15 +53,15 @@ export class CampaignsListPageComponent implements OnInit, OnDestroy {
         });
   }
 
-  public activateCampaign(campaign: any): void {
-    this.updateCampaignStatus(campaign.id, this.campaignStatuses.activate);
+  public activateCampaign(campaign: ICampaign): void {
+    this.updateCampaignStatus(campaign.id, CampaignStatus.active);
   }
 
-  public pauseCampaign(campaign: any): void {
-    this.updateCampaignStatus(campaign.id, this.campaignStatuses.pause);
+  public pauseCampaign(campaign: ICampaign): void {
+    this.updateCampaignStatus(campaign.id, CampaignStatus.paused);
   }
 
-  private updateCampaignStatus(campaignId: string, newStatus: CampaignAction): void {
+  private updateCampaignStatus(campaignId: string, newStatus: CampaignStatus): void {
     this.campaignsService.updateCampaignStatus(campaignId, newStatus)
       .subscribe(() => {
         this.dataSource.updateData(DataSourceUpdateSchema.currentPage);
@@ -63,7 +70,21 @@ export class CampaignsListPageComponent implements OnInit, OnDestroy {
   }
 
   public openReport(url: string): void {
-    this.router.navigateByUrl(url);
+    const segments: string[] = url.split('/');
+    // for reports with a dedicated page
+    const hasPage: boolean = [
+      EngagementType.survey,
+      EngagementType.stamp
+    ].some(typ => segments.some(s => s === typ));
+    if (hasPage) {
+      this.router.navigateByUrl(url);
+    } else {
+      this.dataService.getReport('campaign_report', { cid: segments[segments.length - 1] })
+        .subscribe(
+          (res: string) => console.log(res),
+          () => this.messageService.show('Could not download report', 'danger')
+        );
+    }
   }
 
   public ngOnInit(): void {
@@ -82,5 +103,4 @@ export class CampaignsListPageComponent implements OnInit, OnDestroy {
         this.statusLabel = statuses;
       });
   }
-
 }
