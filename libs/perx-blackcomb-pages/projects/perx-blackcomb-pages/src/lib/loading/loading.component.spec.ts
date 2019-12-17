ffdedaf31ff828ad4dc04c607d5ace8370b205e6
Arina Mahmudova <a.mahmudova@dunice.net>
2019-12-17 16:41:33 +0300

    code clean

--- libs/perx-blackcomb-pages/projects/perx-blackcomb-pages/src/lib/loading/loading.component.spec.ts
+++ libs/perx-blackcomb-pages/projects/perx-blackcomb-pages/src/lib/loading/loading.component.spec.ts
@@ -3,7 +3,14 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoadingComponent } from './loading.component';
 import { MatProgressSpinnerModule } from '@angular/material';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AuthenticationService, ICampaignService, IGameService, Config, NotificationService, ConfigService } from '@perx/core';
+import {
+  AuthenticationService,
+  ICampaignService,
+  IGameService,
+  Config,
+  NotificationService,
+  ConfigService
+} from '@perx/core';
 import { of } from 'rxjs';
 import { Router, ActivatedRoute, convertToParamMap } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
