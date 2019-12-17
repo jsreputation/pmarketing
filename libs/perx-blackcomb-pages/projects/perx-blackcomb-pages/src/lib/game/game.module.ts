d3f97218bf6eb1fc4ee0522abcdca43a9799774f
Vitalii Lysak <v.lysak@dunice.net>
2019-12-17 12:03:22 +0300

    remove game components, eprx-blackcomb-pages

--- libs/perx-blackcomb-pages/projects/perx-blackcomb-pages/src/lib/game/game.module.ts
+++ libs/perx-blackcomb-pages/projects/perx-blackcomb-pages/src/lib/game/game.module.ts
@@ -2,28 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatProgressBarModule, MatButtonModule } from '@angular/material';
 
-import {
-  PinataComponent,
-  ShakeTreeComponent,
-  ScratchCardComponent,
-  SpinTheWheelComponent
-} from '@perx/core';
-
 import { TapComponent } from './tap/tap.component';
 import { ShakeComponent } from './shake/shake.component';
 import { ScratchComponent } from './scratch/scratch.component';
 import { GameComponent } from './game.component';
 import {ConfigToMappedSlotPipe, ConfigToSlicesPipe, SpinComponent} from './spin/spin.component';
+import { GameModule as PerxGameModule } from '@perx/core';
 
 @NgModule({
   declarations: [
     TapComponent,
     ShakeComponent,
     ScratchComponent,
-    PinataComponent,
-    ShakeTreeComponent,
-    ScratchCardComponent,
-    SpinTheWheelComponent,
     GameComponent,
     SpinComponent,
     ConfigToSlicesPipe,
@@ -40,7 +30,8 @@ import {ConfigToMappedSlotPipe, ConfigToSlicesPipe, SpinComponent} from './spin/
   imports: [
     CommonModule,
     MatProgressBarModule,
-    MatButtonModule
+    MatButtonModule,
+    PerxGameModule,
   ]
 })
 export class GameModule { }
