2d06bd54c8e602980710caed6a4cb14c899435f5
Jaye <jaye.tan@flags.fm>
2019-12-19 18:15:56 +0800

    removed abenson environment.ts

--- apps/abenson/src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  apiHost: 'https://api.perxtech.io',
-  production: false,
-  preAuth: false,
-  isWhistler: false,
-  baseHref: '/',
-  custom: {comingSoon: 'true'}
-};
