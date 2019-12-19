148d2fdbe39cbe3551278833b343fb73ad073d0d
Golotin Denis <d.golotin@dunice.net>
2019-12-19 17:33:03 +0300

    pw-2932

--- apps/all-it/src/app/app.component.ts
+++ apps/all-it/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { MatDialog, MatSnackBar } from '@angular/material';
-import { PopupComponent, NotificationService, IPopupConfig, ITheme, AuthenticationService } from '@perx/core';
+import { PopupComponent, NotificationService, IPopupConfig, ITheme, AuthenticationService, ConfigService } from '@perx/core';
 import {
   HomeComponent,
   HistoryComponent,
@@ -10,8 +10,9 @@ import {
 } from '@perx/blackcomb-pages';
 import { Location } from '@angular/common';
 import { Router, NavigationEnd, Event } from '@angular/router';
-import { filter, map } from 'rxjs/operators';
+import { filter, map, switchMap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -24,7 +25,7 @@ export class AppComponent implements OnInit {
   public leftIcon: string = '';
   public preAuth: boolean;
   public theme: ITheme;
-
+  public loaded: boolean = false;
   constructor(
     private notificationService: NotificationService,
     private dialog: MatDialog,
@@ -32,12 +33,17 @@ export class AppComponent implements OnInit {
     private location: Location,
     private router: Router,
     private authService: AuthenticationService,
-    private cd: ChangeDetectorRef
+    private cd: ChangeDetectorRef,
+    private translate: TranslateService,
+    private config: ConfigService
   ) {
     this.preAuth = environment.preAuth;
   }
 
   public ngOnInit(): void {
+    this.config.readAppConfig().pipe(switchMap((conf)=>this.translate.getTranslation(conf.defaultLang as string))).subscribe(()=>{
+      this.loaded = true;
+    });
     this.authService.$failedAuth.subscribe(
       res => {
         if (res) {
