e2ffd26a069fa3abd9f2b3e00fad467dedbaae58
Cholo De Villa <cholo@meta.us>
2019-12-23 09:24:21 +0800

    Dismiss and Show toggle

--- apps/candyland/src/app/dashboard/containers/dashboard-page/dashboard-page.component.ts
+++ apps/candyland/src/app/dashboard/containers/dashboard-page/dashboard-page.component.ts
@@ -2,7 +2,6 @@ import { DOCUMENT } from '@angular/common';
 import { Component, OnInit, ChangeDetectionStrategy, OnDestroy, Inject, Renderer2 } from '@angular/core';
 import { DashboardService } from '@cl-core/services';
 import { FormControl } from '@angular/forms';
-
 import { Observable, Subject } from 'rxjs';
 import { map, takeUntil } from 'rxjs/operators';
 
@@ -23,6 +22,7 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
   public dateRange: FormControl = new FormControl();
   public gameCard$: Observable<DashboardGameCard[]>;
   public userName$: string;
+  public isOpen: boolean = true;
   public navLinks: { path: string, label: string }[] = [
     {
       path: 'overview',
@@ -81,5 +81,9 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
 
   private getGameCard(): void {
     this.gameCard$ = this.dashboardService.getDashboardGameCard();
+  }
+
+  public toggle(): void {
+    this.isOpen = !this.isOpen;
   }
 }
