06bf1025e0fdf0523f1d33e5b3fb2d917d01bafa
HU ZHI <andrewing1012@gmail.com>
2019-12-23 18:04:30 +0800

    add more types in campaign display properties

--- libs/perx-whistler/projects/whistler/src/lib/campaign/campaign.ts
+++ libs/perx-whistler/projects/whistler/src/lib/campaign/campaign.ts
@@ -33,6 +33,8 @@ export interface IWCampaignAttributes {
 
 export interface IWCampaignDisplayProperties {
   informationCollectionSetting?: WInformationCollectionSettingType;
+  weblink: boolean;
+  sms: boolean;
   noRewardsPopUp?: IWProperties;
   successPopUp?: IWProperties;
 }
