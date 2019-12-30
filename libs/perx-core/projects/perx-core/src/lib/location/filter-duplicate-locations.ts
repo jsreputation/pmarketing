caa4f2fb0c29aa377f0f748ff9e0c20bb1937d61
Arina Mahmudova <a.mahmudova@dunice.net>
2019-12-30 13:14:52 +0300

    fix lint issue

--- libs/perx-core/projects/perx-core/src/lib/location/filter-duplicate-locations.ts
+++ libs/perx-core/projects/perx-core/src/lib/location/filter-duplicate-locations.ts
@@ -1,6 +1,5 @@
 import { ILocation } from './ilocation';
 
-export const filterDuplicateLocations = (locations: ILocation[]): ILocation[] => {
-  return locations.filter((item: ILocation, i: number, array: ILocation[]) =>
+export const filterDuplicateLocations = (locations: ILocation[]): ILocation[] =>
+  locations.filter((item: ILocation, i: number, array: ILocation[]) =>
     array.map(mapObj => mapObj.locationId).indexOf(item.locationId) === i);
-};
