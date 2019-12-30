3602b6af7e434bc7a185590a5f3752f713d818a9
Golotin Denis <d.golotin@dunice.net>
2019-12-30 15:55:39 +0300

    guard

--- apps/blackcomb/src/app/pre-login.guard.ts
+++ apps/blackcomb/src/app/pre-login.guard.ts
@@ -5,22 +5,22 @@ import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 
 export class PreLoginGuard implements CanActivate {
-    constructor(private auth: AuthenticationService) { }
+  constructor(private auth: AuthenticationService) { }
 
-    public canActivate(route: ActivatedRouteSnapshot): Observable<boolean> {
-        return this.auth.getAccessToken().pipe(map((token) => {
-            if (token) {
-                return true;
-            }
-            if (route.queryParams.token) {
-                this.auth.saveUserAccessToken(route.queryParams.token);
-                return true;
-            }
-            return false;
-        }));
-    }
+  public canActivate(route: ActivatedRouteSnapshot): Observable<boolean> {
+    return this.auth.getAccessToken().pipe(map((token) => {
+      if (token) {
+        return true;
+      }
+      if (route.queryParams.token) {
+        this.auth.saveUserAccessToken(route.queryParams.token);
+        return true;
+      }
+      return false;
+    }));
+  }
 }
