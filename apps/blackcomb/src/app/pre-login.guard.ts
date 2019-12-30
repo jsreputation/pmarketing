ad3d9a696ae3c294fd590167c91f889361c32c2b
Ryane Chia <ryanechia@outlook.com>
2019-12-31 02:12:09 +0800

    use getUserAccessToken instead

--- apps/blackcomb/src/app/pre-login.guard.ts
+++ apps/blackcomb/src/app/pre-login.guard.ts
@@ -1,26 +1,25 @@
-import { AuthenticationService } from '@perx/core';
-import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot } from '@angular/router';
-import { map } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import {AuthenticationService} from '@perx/core';
+import {Injectable} from '@angular/core';
+import {CanActivate, ActivatedRouteSnapshot} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class PreLoginGuard implements CanActivate {
-  constructor(private auth: AuthenticationService) { }
+  constructor(private auth: AuthenticationService) {
+  }
 
-  public canActivate(route: ActivatedRouteSnapshot): Observable<boolean> {
-    return this.auth.getAccessToken().pipe(map((token) => {
-      if (token) {
-        return true;
-      }
-      if (route.queryParams.token) {
-        this.auth.saveUserAccessToken(route.queryParams.token);
-        return true;
-      }
-      return false;
-    }));
+  public canActivate(route: ActivatedRouteSnapshot): boolean {
+    const token = this.auth.getUserAccessToken();
+    if (token) {
+      return true;
+    }
+    console.log(route.queryParams.token);
+    if (route.queryParams.token) {
+      this.auth.saveUserAccessToken(route.queryParams.token);
+      return true;
+    }
+    return false;
   }
 }
