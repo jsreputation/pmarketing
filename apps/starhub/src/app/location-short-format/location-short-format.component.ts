0a8766f07567d308efac33035401f38c1c37fd32
Golotin Denis <d.golotin@dunice.net>
2020-01-10 14:58:54 +0300

    PW-3171

--- apps/starhub/src/app/location-short-format/location-short-format.component.ts
+++ apps/starhub/src/app/location-short-format/location-short-format.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ILocation, LocationsService, filterDuplicateLocations } from '@perx/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-location-short-format',
@@ -16,18 +17,20 @@ export class LocationShortFormatComponent implements OnInit {
 
   @Input()
   public merchantId: number;
-  @Input()
-  public rewardId: number;
 
+  public voucherId?: number;
   constructor(
-    private locationService: LocationsService
+    private locationService: LocationsService,
+    private route: ActivatedRoute
   ) { }
 
   public ngOnInit(): void {
     if (!this.merchantId) {
       return;
     }
-
+    this.route.queryParams.subscribe((param) => {
+      this.voucherId = param.id;
+    });
     this.locations$ = this.locationService.getFromMerchant(this.merchantId).pipe(map(filterDuplicateLocations));
 
     this.displayLocation$ = this.locations$.pipe(
