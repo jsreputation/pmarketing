3b81a1820288f1b27052987b96be405ab4e28f39
HU ZHI <andrewing1012@gmail.com>
2020-01-15 11:12:47 +0800

    unit test maintain

--- apps/candyland/src/app/audience/containers/send-message-popup/send-message-popup.component.spec.ts
+++ apps/candyland/src/app/audience/containers/send-message-popup/send-message-popup.component.spec.ts
@@ -3,12 +3,11 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { SendMessagePopupComponent } from './send-message-popup.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { MatDialogModule, MatIconModule, MatDatepickerModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
-import { DatePickerModule, ButtonModule } from '@cl-shared/';
+import { DatePickerModule, ButtonModule } from '@cl-shared';
 import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { TranslateModule } from '@ngx-translate/core';
-import { expect } from 'chai';
 
 describe('SendMessagePopupComponent', () => {
   let component: SendMessagePopupComponent;
