7ca00f49e80ac3fc20521b56b44bc48c91308a49
Golotin Denis <d.golotin@dunice.net>
2020-01-20 10:56:35 +0300

    remove mock provider

--- apps/components-demo/src/app/stamps/stamps.module.ts
+++ apps/components-demo/src/app/stamps/stamps.module.ts
@@ -4,23 +4,9 @@ import { CommonModule } from '@angular/common';
 import { StampsRoutingModule } from './stamps-routing.module';
 import { CardComponent } from './card/card.component';
 import { StampsComponent } from './stamps/stamps.component';
-import { PuzzlesModule, RewardsModule, ThemesService, CampaignModule, StampModule, ICampaignService, StampService } from '@perx/core';
+import { PuzzlesModule, RewardsModule, CampaignModule, StampModule } from '@perx/core';
 import { MatSliderModule, MatCheckboxModule, MatTabsModule, MatIconModule } from '@angular/material';
-import { mockTheme } from './theme';
-import { of } from 'rxjs';
-import { campaigns } from './campaign.mock';
-import { stampCard, stamp } from './stamps.mock';
 
-const themesServiceStub: Partial<ThemesService> = {
-  getThemeSetting: () => of(mockTheme)
-};
-
-const campaignServiceStub: Partial<ICampaignService> = { getCampaigns: () => of(campaigns) };
-const stampsServiceStub: Partial<StampService> = {
-  getCurrentCard: () => of(stampCard),
-  getStamps: () => of(),
-  putStamp: () => of(stamp)
-};
 @NgModule({
   declarations: [CardComponent, StampsComponent, StampsListComponent],
   imports: [
@@ -34,11 +20,6 @@ const stampsServiceStub: Partial<StampService> = {
     RewardsModule,
     CampaignModule,
     StampModule
-  ],
-  providers: [
-    { provide: ThemesService, useValue: themesServiceStub },
-    { provide: ICampaignService, useValue: campaignServiceStub },
-    { provide: StampService, useValue: stampsServiceStub }
   ]
 })
 export class StampsModule { }
