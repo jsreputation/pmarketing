49f18c18dece1cb0ab0c9a3ce68b47a5c91fd58a
reynard <reynard93@gmail.com>
2020-01-30 17:24:48 +0800

    fix strange test breaking beacuse of capitalisation

--- apps/candyland/src/app/engagements/games/containers/new-snake-page/new-snake-page.component.spec.ts
+++ apps/candyland/src/app/engagements/games/containers/new-snake-page/new-snake-page.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NewSnakePageComponent } from './new-Snake-page.component';
+import { NewSnakePageComponent } from './new-snake-page.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
