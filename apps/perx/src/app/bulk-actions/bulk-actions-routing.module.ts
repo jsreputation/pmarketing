cdcde8fed8c9a9a9a728a83b1706207fb17df6bd
Nicolas Lagier <nicolas.lagier@gmail.com>
2020-01-31 10:48:03 +0800

    Bulk actions

--- apps/perx/src/app/bulk-actions/bulk-actions-routing.module.ts
+++ apps/perx/src/app/bulk-actions/bulk-actions-routing.module.ts
@@ -3,7 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { BulkActionsComponent } from './bulk-actions.component';
 
-const routes: Routes = [{ path: '', component: BulkActionsComponent }, { path: 'list', loadChildren: () => import('./containers/list/list.module').then(m => m.ListModule) }];
+const routes: Routes = [
+  { path: '', component: BulkActionsComponent, children:[
+    {path: '', redirectTo:'list'},
+  { path: 'list', loadChildren: () => import('./containers/list/list.module').then(m => m.ListModule) }]}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
