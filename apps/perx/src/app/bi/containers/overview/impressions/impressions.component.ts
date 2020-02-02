dc5e61a20801d8ee9f3793e306b184a82b865104
Nicolas Lagier <nicolas.lagier@gmail.com>
2020-02-02 13:14:32 +0800

    Perx with sub-routes

---
+++ apps/perx/src/app/bi/containers/overview/impressions/impressions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-impressions',
+  templateUrl: './impressions.component.html',
+  styleUrls: ['./impressions.component.css']
+})
+export class ImpressionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ apps/perx/src/app/bi/containers/overview/impressions/impressions.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ImpressionsRoutingModule } from './impressions-routing.module';
+import { ImpressionsComponent } from './impressions.component';
+
+
+@NgModule({
+  declarations: [ImpressionsComponent],
+  imports: [
+    CommonModule,
+    ImpressionsRoutingModule
+  ]
+})
+export class ImpressionsModule { }
