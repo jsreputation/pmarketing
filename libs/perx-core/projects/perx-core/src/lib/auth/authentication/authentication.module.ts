93783d20d0f9c51165924f350b6f78ef9b2d47a5
Ryane Chia <ryanechia@outlook.com>
2020-02-06 20:22:43 +0800

    use config service in authentication service

--- libs/perx-core/projects/perx-core/src/lib/auth/authentication/authentication.module.ts
+++ libs/perx-core/projects/perx-core/src/lib/auth/authentication/authentication.module.ts
@@ -18,18 +18,20 @@ import { V4FormsService } from './v4-forms.service';
 
 import { TokenStorage } from '../../utils/storage/token-storage.service';
 import { UtilsModule } from '../../utils/utils.module';
+import { ConfigService } from '../../config/config.service';
 
 export function AuthServiceFactory(
   http: HttpClient,
   config: Config,
   tokenStorage: TokenStorage,
-  profileService: ProfileService
+  profileService: ProfileService,
+  configService: ConfigService
 ): AuthenticationService {
   // Make decision on what to instantiate based on config
   if (config.isWhistler) {
     return new WhistlerAuthenticationService(config, http, tokenStorage);
   }
-  return new V4AuthenticationService(config, http, tokenStorage, profileService);
+  return new V4AuthenticationService(configService, http, tokenStorage, profileService);
 }
 
 export function FormsServiceFactory(config: Config, http: HttpClient): IFormsService {
@@ -49,12 +51,12 @@ export function FormsServiceFactory(config: Config, http: HttpClient): IFormsSer
     {
       provide: AuthenticationService,
       useFactory: AuthServiceFactory,
-      deps: [HttpClient, Config, TokenStorage, ProfileService]
+      deps: [HttpClient, Config, TokenStorage, ProfileService, ConfigService]
     },
     {
       provide: AUTH_SERVICE,
       useFactory: AuthServiceFactory,
-      deps: [HttpClient, Config, TokenStorage, ProfileService]
+      deps: [HttpClient, Config, TokenStorage, ProfileService, ConfigService]
     },
     { provide: IFormsService, useFactory: FormsServiceFactory, deps: [Config, HttpClient] }
   ]
