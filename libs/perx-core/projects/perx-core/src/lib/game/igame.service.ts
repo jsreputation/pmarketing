737846cf85c2d819fb024a92d3b80a3fa163dfdd
reynard <reynard93@gmail.com>
2020-02-14 11:23:04 +0800

    lint spaces

--- libs/perx-core/projects/perx-core/src/lib/game/igame.service.ts
+++ libs/perx-core/projects/perx-core/src/lib/game/igame.service.ts
@@ -4,8 +4,9 @@ import {IGame, IPlayOutcome, IEngagementTransaction, IGameOutcome} from './game.
 export abstract class IGameService {
   public abstract play(engagementId: number, campaignId?: number): Observable<IPlayOutcome>;
   public abstract prePlay(engagementId: number, campaignId?: number): Observable<IEngagementTransaction>;
-  public abstract prePlayConfirm(transactionId: number,
-                                 informationCollectionSetting?: string): Observable<IEngagementTransaction | IPlayOutcome | void>;
+  public abstract prePlayConfirm(
+    transactionId: number,
+    informationCollectionSetting?: string): Observable<IEngagementTransaction | IPlayOutcome | void>;
   public abstract getGamesFromCampaign(campaignId: number): Observable<IGame[]>;
   public abstract get(gameId: number, campaignId?: number): Observable<IGame>;
   public abstract getActiveGames(): Observable<IGame[]>;
