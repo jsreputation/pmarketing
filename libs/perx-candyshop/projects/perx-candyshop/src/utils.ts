4186c18d51dfb48b7c483dac9ec558bbc3e210fe
prettier <nicolas@perxtech.com>
2020-02-21 13:28:23 +0800

    PW-3285 finish migrating candyshop (hopefully...)

--- libs/perx-candyshop/projects/perx-candyshop/src/utils.ts
+++ libs/perx-candyshop/projects/perx-candyshop/src/utils.ts
@@ -1,10 +1,5 @@
-// import _isEqual from 'lodash.isequal';
-import * as _isEqual from 'lodash.isequal';
+import _isEqual from 'lodash.isequal';
 
-// import _transform from 'lodash.transform';
-// import _isEmpty from 'lodash.isempty';
-
-// tslint:disable
 export default class Utils {
   public static isEqual(value: any, other: any): boolean {
     return _isEqual(value, other);
