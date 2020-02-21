74622100017972b639bd081edf63e0d02474314f
prettier <nicolas@perxtech.com>
2020-02-21 13:30:21 +0800

    PW-3285 it was not finished...

--- libs/perx-candyshop/projects/perx-candyshop/src/utils.ts
+++ libs/perx-candyshop/projects/perx-candyshop/src/utils.ts
@@ -1,5 +1,6 @@
 import _isEqual from 'lodash.isequal';
 
+// eslint-disable-next-line @typescript-eslint/tslint/config
 export default class Utils {
   public static isEqual(value: any, other: any): boolean {
     return _isEqual(value, other);
