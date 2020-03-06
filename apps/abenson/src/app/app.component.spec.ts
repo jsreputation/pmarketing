39b391815c834a153473ec5e971ef113e0989942
Ryane Chia <ryanechia@outlook.com>
2020-03-06 23:24:00 +0800

    update tests

--- apps/abenson/src/app/app.component.spec.ts
+++ apps/abenson/src/app/app.component.spec.ts
@@ -105,7 +105,7 @@ describe('AppComponent', () => {
     const locationTest = {} as Location;
     const shared = {} as SharedDataService;
     const matDialog = {} as MatDialog;
-    app.onActivate(new LoginComponent(router, form, auth, notifi));
+    app.onActivate(new LoginComponent(router, form, auth, notifi, configService));
     expect(app.showHeader).toBeFalsy();
     // , profile, ntfs //for below signupcomponent but remove temp on disable card section
     app.onActivate(new SignUpComponent(form, auth, router, notifi, config));
