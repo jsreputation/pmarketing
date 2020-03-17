44724c2bf51962a90e508aae157922908200a864
Ryane Chia <ryanechia@outlook.com>
2020-03-17 17:39:31 +0800

    PW-2727: eslint likes it this way

--- apps/merck-merchant/src/app/order/order.component.ts
+++ apps/merck-merchant/src/app/order/order.component.ts
@@ -99,7 +99,7 @@ export class OrderComponent implements OnInit {
                   partDataRequests.push(this.merchantAdminService.createTransaction(
                     this.payload.id, merchantUsername, product.price, product.currency,
                     'purchase', `${dateStamp}-${this.payload.id}`, merchantName,
-                    product.name + ' ' + product.description));
+                    `${product.name} ${product.description}`));
                 }
               }
               return forkJoin(partDataRequests);
