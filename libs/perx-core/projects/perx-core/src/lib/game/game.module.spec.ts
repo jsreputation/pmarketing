c1c1eec1aef22fcfe5b4ebc9f0dd25d53faab44c
andrewing <andrewing1012@gmail.com>
2020-03-17 17:20:48 +0800

    unit test maintain

--- libs/perx-core/projects/perx-core/src/lib/game/game.module.spec.ts
+++ libs/perx-core/projects/perx-core/src/lib/game/game.module.spec.ts
@@ -5,22 +5,28 @@ import { IVoucherService } from '../vouchers/ivoucher.service';
 import { gameServiceFactory } from './game.module';
 import { V4GameService } from './v4-game.service';
 import { WhistlerGameService } from './whist-game.service';
+import { ConfigService } from '../../public-api';
+import { of } from 'rxjs';
 
 describe('GameModule', () => {
+  const configServiceStub: Partial<ConfigService> = {
+    readAppConfig: () => of()
+  };
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
         HttpClientTestingModule
       ],
       providers: [
-        { provide: IVoucherService, useValue: {} }
+        { provide: IVoucherService, useValue: {} },
+        { provide: ConfigService, useValue: configServiceStub }
       ]
     });
   });
 
-  it('gameServiceFactory', inject([HttpClient, IVoucherService],
-    (http: HttpClient, voucherService: IVoucherService) => {
-      expect(gameServiceFactory(http, { isWhistler: false }, voucherService) instanceof V4GameService).toBeTruthy();
-      expect(gameServiceFactory(http, { isWhistler: true }, voucherService) instanceof WhistlerGameService).toBeTruthy();
+  it('gameServiceFactory', inject([HttpClient, IVoucherService, ConfigService],
+    (http: HttpClient, voucherService: IVoucherService, configService: ConfigService) => {
+      expect(gameServiceFactory(http, { isWhistler: false }, configService, voucherService) instanceof V4GameService).toBeTruthy();
+      expect(gameServiceFactory(http, { isWhistler: true }, configService, voucherService) instanceof WhistlerGameService).toBeTruthy();
     }));
 });
