1c089a433297c9d52dcb2392b224119cd22ab1d1
andrewing <andrewing1012@gmail.com>
2020-03-19 14:41:12 +0800

    add mapping for rewards categories

--- libs/perx-blackcomb-pages/projects/perx-blackcomb-pages/src/lib/home/home.component.ts
+++ libs/perx-blackcomb-pages/projects/perx-blackcomb-pages/src/lib/home/home.component.ts
@@ -154,17 +154,7 @@ export class HomeComponent implements OnInit, OnDestroy {
   }
 
   private getTabs(): Observable<ITabConfigExtended[]> {
-    return this.rewardsService.getCategories()
-      .pipe(
-        tap(console.log),
-        switchMap((stubTabs: ITabConfigExtended[]) => this.translate
-          .get(stubTabs.map(tab => tab.tabName))
-          .pipe(map((translation) => stubTabs.map((tab) => {
-            tab.tabName = translation[tab.tabName];
-            return tab;
-          })))
-        )
-      );
+    return this.rewardsService.getCategories();
   }
 
   public goToCampaignPage(campaign: ICampaign): void {
