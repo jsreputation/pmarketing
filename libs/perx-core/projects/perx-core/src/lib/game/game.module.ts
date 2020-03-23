57fde539132d0c0f36649c4726bfe43ee2791ae8
reynard <reynard93@gmail.com>
2020-03-23 11:09:19 +0800

    PW-3467/cache-module-campaigns+games

--- libs/perx-core/projects/perx-core/src/lib/game/game.module.ts
+++ libs/perx-core/projects/perx-core/src/lib/game/game.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, Optional } from '@angular/core';
+import { ModuleWithProviders, NgModule, Optional } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { V4GameService } from './v4-game.service';
@@ -30,6 +30,7 @@ export function gameServiceFactory(
   }
   return new V4GameService(http, configService);
 }
+
 // https://github.com/ng-packagr/ng-packagr/issues/641
 // @dynamic
 @NgModule({
@@ -44,20 +45,7 @@ export function gameServiceFactory(
   imports: [
     CommonModule
   ],
-  providers: [
-    {
-      provide: IGameService,
-      useFactory: gameServiceFactory,
-      deps: [
-        HttpClient,
-        Config,
-        ConfigService,
-        IVoucherService,
-        [new Optional(), ICampaignService],
-        [new Optional(), AuthenticationService]
-      ]
-    }
-  ],
+  providers: [],
   exports: [
     ShakeTreeComponent,
     PinataComponent,
@@ -67,5 +55,36 @@ export function gameServiceFactory(
     MineSweeperComponent
   ]
 })
-export class GameModule {
+export class GameModule {}
+
+// For the forRoot and forChild methods, although they are functions,
+// it is highly recommended that they only have "return", without implementing more logic.
+// for aot
+@NgModule({
+})
+export class GameServiceModule {
+  public static forRoot(): ModuleWithProviders<GameModule> {
+    return {
+      ngModule: GameModule,
+      providers: [
+        {
+          provide: IGameService,
+          useFactory: gameServiceFactory,
+          deps: [
+            HttpClient,
+            Config,
+            ConfigService,
+            IVoucherService,
+            [new Optional(), ICampaignService],
+            [new Optional(), AuthenticationService]
+          ]
+        }
+      ]
+    };
+  }
+  public static forChild(): ModuleWithProviders<GameModule> {
+    return {
+      ngModule: GameModule
+    };
+  }
 }
