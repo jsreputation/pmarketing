0a14e40ee871c8d42bf4673ce9b3d7726097ab5f
nicolas <nicolas@perxtech.com>
2020-03-24 14:48:23 +0800

    HH-16 forgot password component wrapup

--- libs/perx-blackcomb-pages/projects/perx-blackcomb-pages/src/lib/forgot-password/forgot-password.component.spec.ts
+++ libs/perx-blackcomb-pages/projects/perx-blackcomb-pages/src/lib/forgot-password/forgot-password.component.spec.ts
@@ -1,15 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Type } from '@angular/core';
-import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { Router } from '@angular/router';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { TranslateModule } from '@ngx-translate/core';
-import { AuthenticationService } from '@perxtech/core';
-import { of } from 'rxjs';
+import { AuthenticationService, UtilsModule } from '@perxtech/core';
+import { Observable, of } from 'rxjs';
 import { ForgotPasswordComponent } from './forgot-password.component';
+
+class ActivatedRouteMock {
+  public get queryParams(): Observable<any> {
+    return of(true);
+  }
+}
 
 describe('ForgotPasswordComponent', () => {
   let component: ForgotPasswordComponent;
@@ -17,37 +26,31 @@ describe('ForgotPasswordComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ForgotPasswordComponent],
       imports: [
-        FormsModule,
+        CommonModule,
         ReactiveFormsModule,
-        MatSelectModule,
+        RouterTestingModule,
         MatFormFieldModule,
         MatInputModule,
-        BrowserAnimationsModule,
+        MatButtonModule,
+        MatSelectModule,
+        UtilsModule,
+        NoopAnimationsModule,
         TranslateModule.forRoot()
       ],
+      declarations: [ForgotPasswordComponent],
       providers: [
         {
-          provide: AuthenticationService,
-          useValue: {
-            forgotPassword: () => of(),
-            getAppToken: () => of()
-          }
+          provide: ActivatedRoute, useClass: ActivatedRouteMock,
         },
         {
-          provide: Router,
-          useValue: {
-            navigate: () => { },
-            getCurrentNavigation: () => (
-              {
-                extras: {
-                  state: {
-                    mobileNo: '1234', country: ''
-                  }
-                }
-              }
-            )
+          provide: AuthenticationService, useValue: {
+            forgotPassword: () => of(true),
+            resendOTP: () => of(true),
+            verifyOTP: () => of(true),
+            changePassword: () => of(true),
+            resetPassword: () => of(true),
+            login: () => of(true)
           }
         }
       ]
@@ -58,26 +61,74 @@ describe('ForgotPasswordComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ForgotPasswordComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
 
-  it('should redirect to enter-pin/password on fogrot password submit', fakeAsync(() => {
-    const authenticationService: AuthenticationService = fixture.debugElement.injector.get<AuthenticationService>
-      (AuthenticationService as Type<AuthenticationService>);
-    const authSpy = spyOn(authenticationService, 'forgotPassword').and.returnValue(
-      of({
-        message: 'success'
-      })
+  it('should init identifier from queryParams, if it exist', () => {
+    const activatedRoute = TestBed.get(ActivatedRoute);
+    jest.spyOn(activatedRoute, 'queryParams', 'get').mockReturnValue(of({ identifier: '63987654' }));
+    fixture.detectChanges();
+    expect(component.phoneStepForm.value).toEqual({ phone: '63987654', countryCode: null });
+  });
+
+  it('phoneHandler should move to step 2', () => {
+    fixture.detectChanges();
+    component.phoneStepForm.setValue({ phone: '63987654', countryCode: null });
+    component.phoneHandler();
+    fixture.detectChanges();
+    expect(component.currentStep).toEqual(2);
+  });
+
+  it('should do nothing if phoneStepForm is invalid', () => {
+    fixture.detectChanges();
+    component.phoneHandler();
+    expect(component.currentStep).toEqual(1);
+  });
+
+  it('pinHandle should move to step 3', () => {
+    fixture.detectChanges();
+    component.handlePin('334245');
+    expect(component.currentStep).toEqual(3);
+  });
+
+  it('should call resendOTP of authenticationService when call resend method', () => {
+    fixture.detectChanges();
+    const authenticationService = TestBed.get<AuthenticationService>(AuthenticationService as Type<AuthenticationService>);
+    const resendOTPSpy = spyOn(authenticationService, 'resendOTP').and.callThrough();
+    component.resend();
+    expect(resendOTPSpy).toHaveBeenCalled();
+  });
+
+  it('should do nothing if password form invalid', () => {
+    fixture.detectChanges();
+    const authenticationService = TestBed.get<AuthenticationService>(AuthenticationService as Type<AuthenticationService>);
+    const resetPasswordSpy = spyOn(authenticationService, 'resetPassword').and.returnValue(of({ message: 'password reset' }));
+    component.changePassword();
+    expect(resetPasswordSpy.calls.count()).toBe(0);
+  });
+
+  it('should reset user password, when call changePassword method and data is valid', () => {
+    fixture.detectChanges();
+    const authenticationService = TestBed.get<AuthenticationService>(AuthenticationService as Type<AuthenticationService>);
+    const resetPasswordSpy = spyOn(authenticationService, 'resetPassword').and.returnValue(of({ message: 'password reset' }));
+    const loginSpy = spyOn(authenticationService, 'login').and.returnValue(of({ bearer_token: 'SWWERW' }));
+
+    component.phoneStepForm.setValue({ phone: '63987654', countryCode: '65' });
+    component.phoneHandler();
+    fixture.detectChanges();
+
+    component.handlePin('334245');
+
+    component.newPasswordForm.setValue({ newPassword: 'qwerty123', passwordConfirmation: 'qwerty123' });
+    component.changePassword();
+    expect(resetPasswordSpy).toHaveBeenCalledWith(
+      { phone: '+6563987654', otp: '334245', newPassword: 'qwerty123', passwordConfirmation: 'qwerty123' }
     );
-    const router: Router = fixture.debugElement.injector.get(Router);
-    const routerSpy = spyOn(router, 'navigate');
-    component.onSubmit();
-    tick();
-    expect(authSpy).toHaveBeenCalled();
-    expect(routerSpy).toHaveBeenCalledWith(['enter-pin/password'], { state: { mobileNo: '+1234' } });
-  }));
+
+    expect(loginSpy).toHaveBeenCalledWith('+6563987654', 'qwerty123');
+  });
 });
--- libs/perx-blackcomb-pages/projects/perx-blackcomb-pages/src/lib/forgot-password/forgot-password.component.ts
+++ libs/perx-blackcomb-pages/projects/perx-blackcomb-pages/src/lib/forgot-password/forgot-password.component.ts
@@ -1,71 +1,140 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { Component } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators, ValidationErrors } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AuthenticationService, NotificationService } from '@perxtech/core';
+import { Subject } from 'rxjs';
+import { filter, mergeMap, takeUntil, map } from 'rxjs/operators';
 
 @Component({
-  selector: 'mc-forgot-password',
+  selector: 'perx-blackcomb-pages-password',
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.scss']
 })
-export class ForgotPasswordComponent {
-  public selectedCountry: string = '852';
-  public resetPasswordForm: FormGroup;
+export class ForgotPasswordComponent implements OnInit, OnDestroy {
+  public currentStep: number = 1;
+  public usersPhone: string;
 
-  public get mobileNo(): AbstractControl | null {
-    return this.resetPasswordForm.get('mobileNo');
-  }
+  public phoneStepForm: FormGroup = new FormGroup({
+    phone: new FormControl(null, [
+      Validators.required,
+      Validators.pattern('^[0-9]+$'),
+      Validators.minLength(2),
+      Validators.maxLength(10)]),
+    countryCode: new FormControl('852', [])
+  });
 
-  public get countryCode(): AbstractControl | null {
-    return this.resetPasswordForm.get('countryCode');
-  }
+  public newPasswordForm: FormGroup = new FormGroup({
+    newPassword: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+    passwordConfirmation: new FormControl(null, [Validators.required, Validators.minLength(6)])
+  }, [ForgotPasswordComponent.equalityValidator('newPassword', 'passwordConfirmation')]);
+
+  private otp: string;
+  private identifier: string;
+  private destroy$: Subject<void> = new Subject<void>();
+  public countryCodesOptions: { value: string, label: string }[] = [
+    { value: '852', label: 'HongKong' },
+    { value: '853', label: 'Macau' },
+    { value: '86', label: 'China' },
+    { value: '65', label: 'Singapore' }
+  ];
 
   constructor(
+    private authenticationService: AuthenticationService,
     private router: Router,
-    private fb: FormBuilder,
-    private authService: AuthenticationService,
+    private route: ActivatedRoute,
     private notificationService: NotificationService
-  ) {
-    this.initForm();
+  ) { }
+
+  public ngOnInit(): void {
+    this.route.queryParams
+      .pipe(
+        filter((params) => !!params.identifier),
+        map((params) => params.identifier),
+        takeUntil(this.destroy$)
+      )
+      .subscribe((phone: string) => {
+        if (phone[0] === '+') {
+          phone = phone.slice(1);
+        }
+        const countryCodes = this.countryCodesOptions.map(op => op.value);
+        const countryCode = countryCodes.find(code => phone.startsWith(code)) || null;
+        if (countryCode !== null) {
+          phone = phone.slice(countryCode.length);
+        }
+        this.phoneStepForm.setValue({ phone, countryCode });
+      });
   }
 
-  private initForm(): void {
-    let receivedMobileNo = '';
-    const navigation = this.router.getCurrentNavigation();
-    if (navigation !== null && navigation.extras.state) {
-      receivedMobileNo = navigation.extras.state.mobileNo;
-      this.selectedCountry = navigation.extras.state.country;
-    }
-    this.resetPasswordForm = this.fb.group({
-      mobileNo: [receivedMobileNo, Validators.required],
-      countryCode: [this.selectedCountry, Validators.required]
-    });
+  public ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
-  public onSubmit(): void {
-    const mobileNumber: string = (this.resetPasswordForm.value.mobileNo as string);
-    const countryCode: string = (this.resetPasswordForm.value.countryCode as string);
-    const codeAndMobile = `${countryCode}${mobileNumber}`.trim();
-    // remove non numeric and special characters except leading '+'
-    const cleanedMobileNo = `+${codeAndMobile.replace(/[^0-9]/g, '')}`;
-
-    try {
-      this.authService.forgotPassword(cleanedMobileNo).subscribe(
-        () => this.router.navigate(['otp/password'], { state: { mobileNo: cleanedMobileNo } }),
-        err => {
-          if (err instanceof HttpErrorResponse) {
-            if (err.status === 0) {
-              this.notificationService.addSnack('We could not reach the server');
-            } else if (err.status === 401) {
-              this.notificationService.addSnack('Invalid mobile number.');
-            } else {
-              this.notificationService.addSnack(err.error.message);
-            }
-          }
-        });
-    } catch (error) {
-      console.log(error);
+  public phoneHandler(): void {
+    if (this.phoneStepForm.invalid) {
+      return;
     }
+    const phone: string = `${this.phoneStepForm.value.countryCode}${this.phoneStepForm.value.phone}`.trim();
+    this.identifier = `+${phone.replace(/[^0-9]/g, '')}`;
+    this.usersPhone = this.identifier.slice(-2);
+
+    this.authenticationService.forgotPassword(this.identifier)
+      .subscribe(
+        () => this.currentStep = 2,
+        (err) => this.handleError(err)
+      );
+  }
+
+  public resend(): void {
+    this.authenticationService.resendOTP(this.otp).subscribe(() => { });
+  }
+
+  public handlePin(otp: string): void {
+    this.currentStep = 3;
+    this.otp = otp;
+  }
+
+  public changePassword(): void {
+    if (this.newPasswordForm.invalid) {
+      return;
+    }
+    const value = this.newPasswordForm.value;
+    this.authenticationService.resetPassword({ phone: this.identifier, otp: this.otp, ...value })
+      .pipe(mergeMap(() => this.authenticationService.login(this.identifier, value.newPassword)))
+      .subscribe(() => this.router.navigate(['/']));
+  }
+
+  private handleError(err: any): void {
+    if (err instanceof HttpErrorResponse) {
+      if (err.status === 0) {
+        this.notificationService.addSnack('We could not reach the server');
+      } else if (err.status === 401) {
+        this.notificationService.addSnack('Invalid mobile number.');
+      } else {
+        this.notificationService.addSnack(err.error.message);
+      }
+    }
+  }
+
+  private static equalityValidator(firstFieldPath: string, secondFieldPath: string): ValidatorFn {
+    // eslint-disable-next-line
+    const validator = function (control: AbstractControl): ValidationErrors | null {
+      const firstField = control.get(firstFieldPath);
+      const secondField = control.get(secondFieldPath);
+      if (firstField === null || secondField === null) {
+        return null;
+      }
+      const firstValue = firstField.value;
+      const secondValue = secondField.value;
+
+      if (firstValue !== secondValue) {
+        secondField.setErrors({ notEqual: true });
+        return { notEqual: true };
+      }
+      return null;
+    };
+    // eslint-disable-next-line
+    return validator;
   }
 }
