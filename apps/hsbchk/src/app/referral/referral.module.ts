c32a460d6c7e877379495ff16b0545287ce720dd
nicolas <nicolas@perxtech.com>
2020-03-25 13:57:37 +0800

    Lint

--- apps/hsbchk/src/app/referral/referral.module.ts
+++ apps/hsbchk/src/app/referral/referral.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PerxBlackcombPagesModule, ReferralComponent } from '@perxtech/blackcomb-pages';
 
