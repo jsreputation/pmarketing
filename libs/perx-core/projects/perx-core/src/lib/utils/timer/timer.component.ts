0da9405113c2260898486dcd067545e0bc2e3325
nicolas <nicolas@perxtech.com>
2020-03-31 05:59:18 +0800

    First fix the timer logic

--- libs/perx-core/projects/perx-core/src/lib/utils/timer/timer.component.ts
+++ libs/perx-core/projects/perx-core/src/lib/utils/timer/timer.component.ts
@@ -1,91 +1,86 @@
-import { Component, EventEmitter, Input, OnChanges, Output, Pipe, PipeTransform, SimpleChanges } from '@angular/core';
-import { defer, of, Subject, timer, Observable } from 'rxjs';
-import { finalize, map, repeatWhen, switchMap, takeUntil, tap } from 'rxjs/operators';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output, Pipe, PipeTransform } from '@angular/core';
+import { Observable, of, Subject, timer } from 'rxjs';
+import { catchError, delay, map, repeatWhen, takeUntil } from 'rxjs/operators';
 
 @Pipe({
   name: 'lengthForce',
 }) // can make reusable by tasking in param the length
 export class ForceLengthPipe implements PipeTransform {
   public transform(value: string | null = null, x: number): string | null {
-    if (value && value.length > x) {
+    if (value === null) {
+      return value;
+    }
+    if (value.length > x) {
       return value.substr(0, x);
     }
-    if (value && value.length < x) {
-      return value.padEnd(x, '0');
-    }
-    return value;
+    return value.padEnd(x, '0');
   }
 }
 
+// @dynamic required to avoid error because of usage of lambda function in static method
 @Component({
   selector: 'perx-core-timer',
   templateUrl: './timer.component.html',
   styleUrls: ['./timer.component.scss']
 })
-export class TimerComponent implements OnChanges {
-  private INTERVAL: number = 100;
-  private startTime!: number;
-
+export class TimerComponent implements OnInit, OnDestroy {
   @Input()
   public timeToRun: number = 3000; // input in milliseconds
 
   @Output()
-  public questionDone: EventEmitter<{ timeTaken: number }> = new EventEmitter();
+  public done: EventEmitter<void> = new EventEmitter(); // will emit when time is up
 
   @Input('reset')
-  public reset$: Observable<void> = new Subject(); // jz for utility when questionID has changed and inform timeSrc
+  public reset$: Observable<void>; // when ticking, the timer will reset and restart
 
+  // stream of formatted time values
   public timer$: Observable<string>;
+  private destroy$: Subject<void> = new Subject();
+  // refresh interval
+  private INTERVAL: number = 100;
 
-  private startAgain$: Subject<boolean> = new Subject<boolean>();
-  // anyway -> should be countdown
-  private timeSource$: Observable<string>;
-
-  constructor() {
-    this.timeSource$ = timer(0, this.INTERVAL)
+  public ngOnInit(): void {
+    this.timer$ = this.timeSource()
       .pipe(
-        map((time: number) => {
-          const date = time === 0 ? new Date(this.INTERVAL) : new Date(0); // first one needs to be offset
+        // unfortunately we have to slightly delay the repeat, because we want to make sure that 
+        // timeSource also had time to stop
+        repeatWhen(() => this.reset$.pipe(delay(50))),
+        takeUntil(this.destroy$)
+      );
+  }
+
+  public ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  private timeSource(): Observable<string> {
+    return timer(0, this.INTERVAL)
+      .pipe(
+        map((ticks: number) => {
+          // first one needs to be offset
+          const date = ticks === 0 ? new Date(this.INTERVAL) : new Date(0);
+          // bcz start at 0 the emission
+          const ellapasedTime = (ticks + 1) * this.INTERVAL;
           // wont exceed the timetoRun, time + 1 so one stpe in afacance to close properly at 0.00,
-          if (this.timeToRun > (time + 1) * this.INTERVAL) {
-            // bcz start at 0 the emission
-            date.setUTCMilliseconds(this.timeToRun - ((time + 1) * this.INTERVAL));
+          if (this.timeToRun > ellapasedTime) {
+            date.setUTCMilliseconds(this.timeToRun - ellapasedTime);
+          } else {
+            throw 'done';
           }
           const dateString = date.toLocaleTimeString('en-GB', {
             timeZone: 'UTC',
             minute: '2-digit',
             second: '2-digit'
           });
-          // use a pipe instead ->
-          // console.log(Number(dateString.split(':')[1] + `.${date.getUTCMilliseconds()}`).toFixed(2));
-          return `${dateString}.${date.getUTCMilliseconds()}`;
+          return `${dateString}:${date.getUTCMilliseconds()}`;
         }),
-        finalize(() => this.questionDone.emit({ timeTaken: Date.now() - this.startTime }))
-      );
-    this.timer$ = defer(() => timer(0, this.timeToRun)
-      .pipe(
-        tap((time) => {
-          if (time === 0) {
-            this.startTime = Date.now();
-          }
-        }),
-        // because swithcmap runs it twice, for each emission, runn in 2 concurrent timers
-        switchMap((time) => {
-          if (time === 0) {
-            return this.timeSource$;
-          }
+        catchError(() => {
+          // when we reach the end, we notify any potential listener
+          this.done.emit();
           return of('00:00:00');
-          // return of(); // useless here, the logic is to only switchmap once for each timer$ kicked off
         }),
-        takeUntil(this.reset$),
-        repeatWhen(() => this.startAgain$)
-      ));
-  }
-
-  public ngOnChanges(changes: SimpleChanges): void {
-    if (changes.reset$ && this.reset$) {
-      this.reset$
-        .subscribe(() => this.startAgain$.next(true));
-    }
+        takeUntil(this.reset$)
+      );
   }
 }
