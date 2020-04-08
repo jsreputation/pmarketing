4dd2eb6bb3fe85fe890d26d01a1aa3062d3b80f1
nicolas <nicolas@perxtech.com>
2020-04-08 12:25:36 +0800

    HH-53 improve validations for usability on signup component

--- apps/hsbchk/src/app/sign-up/sign-up/i-sign-up.component.ts
+++ apps/hsbchk/src/app/sign-up/sign-up/i-sign-up.component.ts
@@ -1,8 +1,9 @@
 import { OnDestroy } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ValidatorFn } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AuthenticationService, ISignUpData, ITheme, NotificationService, ThemesService } from '@perxtech/core';
-import { Observable, Subject } from 'rxjs';
+import { AuthenticationService, ISignUpData, ITheme, NotificationService, ThemesService, equalityValidator } from '@perxtech/core';
+import { Observable, Subject, combineLatest } from 'rxjs';
+import { distinctUntilChanged, takeUntil, startWith } from 'rxjs/operators';
 
 export abstract class ISignUpComponent implements OnDestroy {
   public signupForm: FormGroup;
@@ -45,18 +46,56 @@ export abstract class ISignUpComponent implements OnDestroy {
   }
 
   protected initForm(): void {
+    const hkidValidators: ValidatorFn[] = [Validators.minLength(4), Validators.maxLength(4), Validators.pattern('[0-9]*')];
     this.signupForm = this.fb.group({
       nickname: ['', Validators.required],
       referralCode: [''],
       fullName: [''],
-      hkid: ['', [Validators.minLength(4), Validators.maxLength(4), Validators.pattern('[0-9]*')]],
+      hkid: ['', hkidValidators],
       mobileNo: ['', Validators.required],
       email: ['', Validators.email],
       password: ['', [Validators.required, Validators.minLength(6)]],
       confirmPassword: ['', [Validators.required, Validators.minLength(6)]],
       accept_terms: [false, Validators.required],
       accept_marketing: [false]
-    });
+    }, { validators: [equalityValidator('password', 'confirmPassword')] });
+
+    combineLatest([
+      this.signupForm.controls.hkid.valueChanges.pipe(startWith('')),
+      this.signupForm.controls.fullName.valueChanges.pipe(startWith('')),
+      this.signupForm.controls.accept_marketing.valueChanges.pipe(startWith(false))
+    ])
+      .pipe(
+        distinctUntilChanged((a: [string, string, boolean], b: [string, string, boolean]) => {
+          if (a.length !== b.length) {
+            return false;
+          }
+          for (let i = 0; i < a.length; i++) {
+            if (a[i] !== b[i]) {
+              return false;
+            }
+          }
+          return true;
+        }),
+        takeUntil(this.destroy$)
+      )
+      .subscribe(([hkid, name, checkbox]) => {
+        if (name !== '' || hkid !== '' || checkbox !== false) {
+          // as soon as one of the fields is not empty make the 3 fields mandatory
+          this.signupForm.controls.hkid.setValidators([...hkidValidators, Validators.required]);
+          this.signupForm.controls.fullName.setValidators([Validators.required]);
+          this.signupForm.controls.accept_marketing.setValidators([Validators.required]);
+        } else {
+          // if the fields are empty make the 3 fields optional
+          this.signupForm.controls.hkid.setValidators(hkidValidators);
+          this.signupForm.controls.fullName.clearValidators();
+          this.signupForm.controls.accept_marketing.clearValidators();
+        }
+        this.signupForm.controls.hkid.updateValueAndValidity();
+        this.signupForm.controls.fullName.updateValueAndValidity();
+        this.signupForm.controls.accept_marketing.updateValueAndValidity();
+        console.log(this.signupForm.controls.accept_marketing);
+      });
   }
 
   protected fetchTheme(): void {
@@ -79,6 +118,9 @@ export abstract class ISignUpComponent implements OnDestroy {
       this.errorMessage = 'Unknown error occured.';
       return;
     }
+    if (!this.signupForm.valid) {
+      return;
+    }
 
     const passwordString = this.signupForm.value.password;
     const confirmPassword = this.signupForm.value.confirmPassword;
@@ -93,6 +135,12 @@ export abstract class ISignUpComponent implements OnDestroy {
     const mobileNumber = this.mobileNumber;
     const emailValue = this.signupForm.value.email;
     const lastName = this.signupForm.value.fullName ? this.signupForm.value.fullName : nickname;
+    // for some strange reason, the automated addition of the validator on accept_marketing field does not seem to work.
+    // so we do it now
+    if ((name !== '' || hkid !== '') && this.signupForm.value.accept_marketing === false) {
+      this.signupForm.controls.accept_marketing.setErrors({ required: true });
+      return;
+    }
 
     const signUpData: ISignUpData = {
       lastName,
