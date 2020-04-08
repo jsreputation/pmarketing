ef80d47a69e9662570c15648e295019dad34819b
Ryane Chia <ryanechia@outlook.com>
2020-04-08 17:21:09 +0800

    HH-48: remove + from the model. move it to display wherever needed

--- libs/perx-core/projects/perx-core/src/lib/utils/general-static-data/country-code.ts
+++ libs/perx-core/projects/perx-core/src/lib/utils/general-static-data/country-code.ts
@@ -8,241 +8,241 @@ export const countryCodes: ICountryCode[] = [
   {
     id: 1,
     name: 'Afghanistan',
-    code: '+93'
+    code: '93'
   },
   {
     id: 2,
     name: 'Armenia',
-    code: '+374'
+    code: '374'
   },
   {
     id: 3,
     name: 'Azerbaijan',
-    code: '+994'
+    code: '994'
   },
   {
     id: 4,
     name: 'Bahrain',
-    code: '+973'
+    code: '973'
   },
   {
     id: 5,
     name: 'Bangladesh',
-    code: '+880'
+    code: '880'
   },
   {
     id: 6,
     name: 'Bhutan',
-    code: '+975'
+    code: '975'
   },
   {
     id: 7,
     name: 'Brunei',
-    code: '+673'
+    code: '673'
   },
   {
     id: 8,
     name: 'Cambodia',
-    code: '+855'
+    code: '855'
   },
   {
     id: 9,
     name: 'China',
-    code: '+86'
+    code: '86'
   },
   {
     id: 10,
     name: 'Georgia',
-    code: '+995'
+    code: '995'
   },
   {
     id: 11,
     name: 'Hong Kong',
-    code: '+852'
+    code: '852'
   },
   {
     id: 12,
     name: 'India',
-    code: '+91'
+    code: '91'
   },
   {
     id: 13,
     name: 'Indonesia',
-    code: '+62'
+    code: '62'
   },
   {
     id: 14,
     name: 'Iran',
-    code: '+98'
+    code: '98'
   },
   {
     id: 15,
     name: 'Iraq',
-    code: '+964'
+    code: '964'
   },
   {
     id: 16,
     name: 'Israel',
-    code: '+972'
+    code: '972'
   },
   {
     id: 17,
     name: 'Japan',
-    code: '+81'
+    code: '81'
   },
   {
     id: 18,
     name: 'Jordan',
-    code: '+962'
+    code: '962'
   },
   {
     id: 19,
     name: 'Kazakhstan',
-    code: '+7'
+    code: '7'
   },
   {
     id: 20,
     name: 'Kuwait',
-    code: '+965'
+    code: '965'
   },
   {
     id: 21,
     name: 'Kyrgyzstan',
-    code: '+996'
+    code: '996'
   },
   {
     id: 22,
     name: 'Laos',
-    code: '+856'
+    code: '856'
   },
   {
     id: 23,
     name: 'Lebanon',
-    code: '+961'
+    code: '961'
   },
   {
     id: 24,
     name: 'Macau',
-    code: '+853'
+    code: '853'
   },
   {
     id: 25,
     name: 'Malaysia',
-    code: '+60'
+    code: '60'
   },
   {
     id: 26,
     name: 'Maldives',
-    code: '+960'
+    code: '960'
   },
   {
     id: 27,
     name: 'Mongolia',
-    code: '+976'
+    code: '976'
   },
   {
     id: 28,
     name: 'Myanmar [Burma]',
-    code: '+95'
+    code: '95'
   },
   {
     id: 29,
     name: 'Nepal',
-    code: '+977'
+    code: '977'
   },
   {
     id: 30,
     name: 'North Korea',
-    code: '+850'
+    code: '850'
   },
   {
     id: 31,
     name: 'Oman',
-    code: '+968'
+    code: '968'
   },
   {
     id: 32,
     name: 'Pakistan',
-    code: '+92'
+    code: '92'
   },
   {
     id: 33,
     name: 'Philippines',
-    code: '+63'
+    code: '63'
   },
   {
     id: 34,
     name: 'Qatar',
-    code: '+974'
+    code: '974'
   },
   {
     id: 35,
     name: 'Saudi Arabia',
-    code: '+966'
+    code: '966'
   },
   {
     id: 36,
     name: 'Singapore',
-    code: '+65'
+    code: '65'
   },
   {
     id: 37,
     name: 'South Korea',
-    code: '+82'
+    code: '82'
   },
   {
     id: 38,
     name: 'Sri Lanka',
-    code: '+94'
+    code: '94'
   },
   {
     id: 39,
     name: 'Syria',
-    code: '+963'
+    code: '963'
   },
   {
     id: 40,
     name: 'Taiwan',
-    code: '+886'
+    code: '886'
   },
   {
     id: 41,
     name: 'Tajikistan',
-    code: '+992'
+    code: '992'
   },
   {
     id: 42,
     name: 'Thailand',
-    code: '+66'
+    code: '66'
   },
   {
     id: 43,
     name: 'Turkey',
-    code: '+90'
+    code: '90'
   },
   {
     id: 44,
     name: 'Turkmenistan',
-    code: '+993'
+    code: '993'
   },
   {
     id: 45,
     name: 'United Arab Emirates',
-    code: '+971'
+    code: '971'
   },
   {
     id: 46,
     name: 'Uzbekistan',
-    code: '+998'
+    code: '998'
   },
   {
     id: 47,
     name: 'Vietnam',
-    code: '+84'
+    code: '84'
   },
   {
     id: 48,
     name: 'Yemen',
-    code: '+967'
+    code: '967'
   }
 ];
