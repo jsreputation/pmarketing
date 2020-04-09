5e35caa1b1b4f252bf602bd8c14050100dd1ed99
reynard <reynard93@gmail.com>
2020-04-09 16:19:21 +0800

    PW-3686/profile-summary-onaccount-comp

---
+++ libs/perx-core/projects/perx-core/src/lib/profile/profile-summary/profile-summary.component.ts
@@ -0,0 +1,51 @@
+import {Component, HostListener, Input, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {IProfile} from '../profile.model';
+import {ILoyalty} from '../../loyalty/models/loyalty.model';
+
+
+@Component({
+  selector: 'perx-core-profile-summary',
+  templateUrl: './profile-summary.component.html',
+  styleUrls: ['./profile-summary.component.scss']
+})
+export class ProfileSummaryComponent implements OnInit {
+  @Input()
+  public profile: IProfile;
+
+  @Input()
+  public profileImg: string;
+
+  @Input()
+  public loyalty: ILoyalty;
+
+  @Input()
+  public titleFn: (profile: IProfile) => string;
+
+  @HostListener('click', ['$event'])
+  public gotoProfile(_: Event): void {
+    this.router.navigateByUrl('profile');
+  }
+
+  public constructor(private router: Router) {
+  }
+
+  public ngOnInit(): void {
+    console.log(this.profile, 'waht is my progile');
+    if (!this.titleFn) {
+      this.titleFn = (profile?: IProfile): string => {
+        if (profile && profile.customProperties && profile.customProperties.nickName) {
+          return `${profile.customProperties.nickName}`;
+        }
+        if (profile && profile.firstName) {
+          return `${profile.firstName}`;
+        }
+        if (profile && profile.lastName) {
+          return `${profile.lastName}`;
+        }
+        return '';
+      };
+    }
+  }
+
+}
