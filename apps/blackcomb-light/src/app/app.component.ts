d3544a152c8cc57ac068226d44fdf18b3765f664
nicolas <nicolas@perxtech.com>
2020-04-30 05:39:36 +0800

    PW-3738 cleanup app.component

--- apps/blackcomb-light/src/app/app.component.ts
+++ apps/blackcomb-light/src/app/app.component.ts
@@ -1,45 +1,9 @@
-import {
-  Component,
-  OnInit,
-  ChangeDetectorRef,
-} from '@angular/core';
-import {
-  MatDialog,
-  MatSnackBar,
-} from '@angular/material';
-import {
-  Router,
-  NavigationEnd,
-  Event,
-  ActivatedRoute,
-  Params,
-} from '@angular/router';
-import { Location } from '@angular/common';
-
-import {
-  filter,
-  map,
-  switchMap,
-} from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatSnackBar } from '@angular/material';
+import { ActivatedRoute, Params } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
-
-import {
-  PopupComponent,
-  NotificationService,
-  IPopupConfig,
-  ConfigService,
-  IConfig,
-  AuthenticationService,
-} from '@perxtech/core';
-import {
-  HomeComponent,
-  HistoryComponent,
-  AccountComponent,
-  SignIn2Component,
-  WalletComponent
-} from '@perxtech/blackcomb-pages';
-
-import { BACK_ARROW_URLS } from './app.constants';
+import { AuthenticationService, ConfigService, IConfig, IPopupConfig, NotificationService, PopupComponent } from '@perxtech/core';
+import { filter, map, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -47,22 +11,12 @@ import { BACK_ARROW_URLS } from './app.constants';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  public showHeader: boolean;
-  public showToolbar: boolean;
-  public backArrowIcon: string = '';
   public preAuth: boolean;
   public translationLoaded: boolean = false;
-
-  private initBackArrow(url: string): void {
-    this.backArrowIcon = BACK_ARROW_URLS.some(test => url.startsWith(test)) ? 'arrow_backward' : '';
-  }
 
   constructor(
     private notificationService: NotificationService,
     private dialog: MatDialog,
-    private location: Location,
-    private router: Router,
-    private cd: ChangeDetectorRef,
     private snack: MatSnackBar,
     private config: ConfigService,
     private translate: TranslateService,
@@ -75,7 +29,7 @@ export class AppComponent implements OnInit {
     this.activeRoute.queryParams
       .pipe(
         filter((params: Params) => params.token),
-        map((params: Params) => params.token)
+        map((params: Params) => params.token),
       )
       .subscribe((token: string) => this.authenticationService.saveUserAccessToken(token));
 
@@ -91,34 +45,11 @@ export class AppComponent implements OnInit {
         (data: IPopupConfig) => this.dialog.open(PopupComponent, { data }),
         (err) => console.error(err)
       );
+
     this.notificationService.$snack
       .subscribe(
         (msg: string) => this.snack.open(msg, 'x', { duration: 2000 }),
         (err) => console.error(err)
       );
-
-    this.router.events
-      .pipe(
-        filter((event: Event) => event instanceof NavigationEnd),
-        map((event: NavigationEnd) => event.urlAfterRedirects)
-      )
-      .subscribe(url => this.initBackArrow(url));
-    this.initBackArrow(this.router.url);
   }
-
-  public onActivate(ref: any): void {
-    this.showHeader = !(ref instanceof SignIn2Component);
-    this.showToolbar = ref instanceof HomeComponent ||
-      ref instanceof HistoryComponent ||
-      ref instanceof AccountComponent ||
-      ref instanceof WalletComponent;
-    this.cd.detectChanges();
-  }
-
-  public backArrowClick(): void {
-    if (this.backArrowIcon !== '') {
-      this.location.back();
-    }
-  }
-
 }
