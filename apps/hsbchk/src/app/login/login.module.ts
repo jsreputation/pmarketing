82caebf37b069f849ee146c441c65d7af42e0451
reynard <reynard93@gmail.com>
2020-04-30 16:53:54 +0800

    PW-3749/fix-cc-toggle

--- apps/hsbchk/src/app/login/login.module.ts
+++ apps/hsbchk/src/app/login/login.module.ts
@@ -4,7 +4,8 @@ import { SignIn2Component, SignIn2Module } from '@perxtech/blackcomb-pages';
 
 const routes: Routes = [{
   path: '',
-  component: SignIn2Component
+  component: SignIn2Component,
+  data: { countryList: ['Hong Kong', 'Singapore']}
 }];
 
 @NgModule({
