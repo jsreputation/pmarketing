e7c45d4291a71bcfa488ec3c3837566888f953db
reynard <reynard93@gmail.com>
2020-04-30 16:05:08 +0800

    remove default obj from page and move to display component test as stub

--- libs/perx-blackcomb-pages/projects/perx-blackcomb-pages/src/lib/leaderboard-page/leaderboard-page.component.ts
+++ libs/perx-blackcomb-pages/projects/perx-blackcomb-pages/src/lib/leaderboard-page/leaderboard-page.component.ts
@@ -18,18 +18,7 @@ import {Observable, of} from 'rxjs';
   styleUrls: ['./leaderboard-page.component.scss']
 })
 export class LeaderboardPageComponent {
-  public leaderboardRanks: UserRanking[] = [
-    {id: 1, rank: 1, displayName: 'john', value: 1},
-    {id: 1, rank: 2, displayName: 'oliver', value: 1},
-    {id: 1, rank: 3, displayName: 'jimmy', value: 1},
-    {id: 1, rank: 4, displayName: 'kimmel', value: 1},
-    {id: 1, rank: 5, displayName: 'fallon', value: 1},
-    {id: 1, rank: 6, displayName: 'craig', value: 1},
-    {id: 1, rank: 7, displayName: 'ferguson', value: 1},
-    {id: 1, rank: 8, displayName: 'graham', value: 1},
-    {id: 1, rank: 9, displayName: 'norton', value: 1},
-    {id: 1, rank: 10, displayName: 'ellen', value: 1},
-  ];
+  public leaderboardRanks: UserRanking[];
   public userMiniRankData: UserRanking = {
     id: 1,
     rank: 12,
