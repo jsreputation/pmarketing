ca5455750fa092f9a76eedf5f85334487a7ee91a
nicolas <nicolas@perxtech.com>
2020-05-04 05:40:45 +0800

    PW-3747 bring quiz to blackcomb

--- apps/blackcomb/src/app/campaign-landing/campaign-landing.module.ts
+++ apps/blackcomb/src/app/campaign-landing/campaign-landing.module.ts
@@ -1,6 +1,13 @@
+<<<<<<< HEAD
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CampaignLandingPageComponent, CampaignLandingPageModule } from '@perxtech/blackcomb-pages';
+=======
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CampaignLandingPageComponent, PerxBlackcombPagesModule } from '@perxtech/blackcomb-pages';
+>>>>>>> PW-3747 bring quiz to blackcomb
 
 const routes: Routes = [{
   path: '',
@@ -9,8 +16,16 @@ const routes: Routes = [{
 
 @NgModule({
   imports: [
+<<<<<<< HEAD
     CampaignLandingPageModule,
     RouterModule.forChild(routes)
+=======
+    CommonModule,
+    PerxBlackcombPagesModule,
+    RouterModule.forChild(routes)
+  ],
+  providers: [
+>>>>>>> PW-3747 bring quiz to blackcomb
   ]
 })
 export class CampaignLandingModule { }
