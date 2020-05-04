5920b97d8e398b380109f4e3ce0c4e7321cbbe66
andrewing <andrewing1012@gmail.com>
2020-05-04 15:18:33 +0800

    MER-159 move filter into service, if not need to put into onchanges of component

--- libs/perx-core/projects/perx-core/src/lib/location/locations-list/locations-list.component.ts
+++ libs/perx-core/projects/perx-core/src/lib/location/locations-list/locations-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { ILocation } from '../ilocation';
-import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'perx-core-locations-list',
@@ -21,17 +20,5 @@ export class LocationsListComponent implements OnInit {
     if (!this.headerFn) {
       this.headerFn = (location) => of(location.merchantName ? location.merchantName : location.name);
     }
-    if (this.locations) {
-      this.locations.pipe(
-        map((locations: ILocation[]) => locations
-          .sort((locationA, locationB) => {
-            if (locationA.merchantName && locationB.merchantName) {
-              return locationA.merchantName < locationB.merchantName ? -1 : 1;
-            }
-            return 0;
-          })
-        )).subscribe();
-    }
-
   }
 }
