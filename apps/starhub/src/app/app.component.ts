4eb785ee7d754d8f1945b09803a2442bc81955f8
Ryane Chia <ryanechia@outlook.com>
2020-05-22 23:25:06 +0800

    VS-3564: comment linting

--- apps/starhub/src/app/app.component.ts
+++ apps/starhub/src/app/app.component.ts
@@ -64,7 +64,7 @@ export class AppComponent implements OnInit {
   // public selectedCampaign: ICampaign;
   public game?: IGame;
   public theme: ITheme;
-  public holdingGateOpened: boolean = false;
+  public holdingGateOpened: boolean = true;
 
   constructor(
     // private authenticationService: AuthenticationService,
