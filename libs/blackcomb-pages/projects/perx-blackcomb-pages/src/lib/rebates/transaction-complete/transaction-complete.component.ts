e3add3f6d8e6c4974064a5d278466ae4773fd23e
reynard <reynard93@gmail.com>
2020-05-27 13:05:40 +0800

    remove type declarations on transc-complete component

--- libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/rebates/transaction-complete/transaction-complete.component.ts
+++ libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/rebates/transaction-complete/transaction-complete.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransactionCompleteComponent implements OnInit {
 
-  public constructor(): void { }
+  constructor() { }
 
   public ngOnInit(): void {
   }
