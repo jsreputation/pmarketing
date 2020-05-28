9b812e058a7f55b669b00d562af959875a57d1a8
reynard <reynard93@gmail.com>
2020-05-28 18:58:41 +0800

    complete rebates-complete page

--- libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/rebates/transaction-complete/transaction-complete.component.ts
+++ libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/rebates/transaction-complete/transaction-complete.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-// should it be required in future
+import {Router} from '@angular/router';
+
+type TransactionReceipt = {
+  rebateGained: string,
+  transactionAmount: string,
+  rebateBurned: string,
+  name: string,
+  logo: string
+}
+
 @Component({
   selector: 'perx-blackcomb-pages-transaction-complete',
   templateUrl: './transaction-complete.component.html',
   styleUrls: ['./transaction-complete.component.scss']
 })
 export class TransactionCompleteComponent implements OnInit {
+  constructor(
+    private router: Router
+  ) {}
 
-  constructor() { }
-
+  public transactionObject: TransactionReceipt;
   public ngOnInit(): void {
+    this.transactionObject = history.state;
+  }
+
+  public backHome(): void {
+    this.router.navigate(['home']);
   }
 
 }
