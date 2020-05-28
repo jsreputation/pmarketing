cb8da31ad5b612e29bbf9fbbd0caad039088ed6b
reynard <reynard93@gmail.com>
2020-05-28 18:59:40 +0800

    modify qrscanner component and refactor merchants-list component (no onScroll implemented) -> after demo phase to do

--- libs/core/projects/perx-core/src/lib/rebates/rebates-list/rebates-list.component.ts
+++ libs/core/projects/perx-core/src/lib/rebates/rebates-list/rebates-list.component.ts
@@ -1,5 +1,17 @@
 import { Component, EventEmitter, Output, Input } from '@angular/core';
-import { Observable } from 'rxjs';
+import {Observable} from 'rxjs';
+import {ITheme} from '../../utils/themes/themes.model';
+import {Colors} from '../../perx-core.constants';
+import {IPrice, IReward} from '../../rewards/models/reward.model';
+import {ThemesService} from '../../utils/themes/themes.service';
+
+export type MerchantData = {
+  merchantId: number;
+  name: string;
+  description: string;
+  imgUrl: string;
+  rebateAmount: string;
+}
 
 // part of rebates-wallet page
 @Component({
@@ -8,11 +20,42 @@ import { Observable } from 'rxjs';
   styleUrls: ['./rebates-list.component.scss']
 })
 export class RebatesListComponent {
-  @Output() public tapped: EventEmitter<any> = new EventEmitter<any>();
+  @Input('data') public merchants$: Observable<MerchantData[]>;
 
-  @Input('data') public merchants$: Observable<any[]>;
+  public theme: ITheme | null = null;
+  public colorPrimary: Colors = Colors.Primary;
 
-  public onClick(merchant: any): void {
+  @Input('rewardsList')
+  public rewards$: Observable<IReward[]>;
+
+  @Input()
+  public defaultImg: string;
+
+  @Input()
+  public displayPriceFn: (rewardPrice: IPrice) => string;
+
+  @Output()
+  public tapped: EventEmitter<MerchantData> = new EventEmitter<MerchantData>();
+
+  public get themeFontColor(): string | null {
+    return this.theme ? this.theme.properties['--font_color'] : null;
+  }
+
+  private initTheme(): void {
+    this.themesService.getThemeSetting().subscribe(
+      theme => this.theme = theme
+    );
+  }
+
+  constructor(
+    private themesService: ThemesService,
+  ) { }
+
+  public ngOnInit(): void {
+    this.initTheme();
+  }
+
+  public merchantClickedHandler(merchant: MerchantData): void {
     this.tapped.emit(merchant);
   }
 }
