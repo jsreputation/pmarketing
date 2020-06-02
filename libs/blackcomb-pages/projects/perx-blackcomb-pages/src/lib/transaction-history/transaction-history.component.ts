33746ece1846f7523fc21d92c35ff17595d3a2e6
Ryane Chia <ryanechia@outlook.com>
2020-06-02 16:36:02 +0800

    VS-3699: expand ternary statements to handle undefined state

--- libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/transaction-history/transaction-history.component.ts
+++ libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/transaction-history/transaction-history.component.ts
@@ -6,9 +6,11 @@ import {
   LoyaltyService,
   ITransactionHistory,
   IRewardTransactionHistory,
-  IPurchaseTransactionHistory,
-  TransactionPipe
+  // IPurchaseTransactionHistory,
+  TransactionPipe,
+  // ITransactionProperties
 } from '@perxtech/core';
+import { oc } from 'ts-optchain';
 
 // import { ShowTitleInHeader } from '../layout/layout.component';
 
@@ -37,17 +39,27 @@ export class TransactionHistoryComponent implements OnInit/*, ShowTitleInHeader 
   public ngOnInit(): void {
     this.transactions = this.loyaltyService.getTransactionHistory(this.pageNumber, this.pageSize);
 
-    this.purchasesTitleFn = (tr: ITransactionHistory) =>
-      `${(tr.transactionDetails && tr.transactionDetails.data) ?
-        (tr.transactionDetails.data as IPurchaseTransactionHistory).pharmacyName : ''}`;
+    this.purchasesTitleFn = (tr: ITransactionHistory) => {
+      let text = '';
+      const properties = oc(tr).transactionDetails.data.properties();
+      if (properties) {
+        text = properties.storeName ? properties.storeName : '';
+      }
+      return text;
+    }
 
     this.redemptionsTitleFn = (tr: ITransactionHistory) =>
       `${(tr.transactionDetails && tr.transactionDetails.data) ?
         (tr.transactionDetails.data as IRewardTransactionHistory).rewardName : ''}`;
 
-    this.descFn = (tr: ITransactionHistory) =>
-      `${tr.transactionDetails && tr.transactionDetails.data &&
-      (tr.transactionDetails.data as IPurchaseTransactionHistory).productName}`;
+    this.descFn = (tr: ITransactionHistory) => {
+      let text = '';
+      const properties = oc(tr).transactionDetails.data.properties();
+      if (properties) {
+        text = properties.productName ? properties.productName : '';
+      }
+      return text;
+    }
 
     this.subTitleFn = (tr: ITransactionHistory) => `${this.datePipe.transform(tr.transactedAt, 'dd/MM/yyyy')}`;
     this.priceLabelFn = (tr: ITransactionHistory) => `${this.transactionPipe.transform(tr.pointsAmount || 0)}`;
