5bccbc121c509e5590740e8988d0a9a1a39ac097
Ryane Chia <ryanechia@outlook.com>
2020-06-19 18:43:09 +0800

    VS-3808: create a pipe to show cashback texts instead of points

--- libs/core/projects/perx-core/src/public-api.ts
+++ libs/core/projects/perx-core/src/public-api.ts
@@ -140,6 +140,7 @@ export {
   ITransactionProperties
 } from './lib/loyalty/models/loyalty.model';
 export { TransactionPipe } from './lib/loyalty/loyalty-transactions-list/transaction.pipe';
+export { CashbackTransactionPipe } from './lib/loyalty/loyalty-transactions-list/cashback-transaction.pipe';
 export { LoyaltySummaryComponent } from './lib/loyalty/loyalty-summary/loyalty-summary.component';
 export { LoyaltyTransactionsListComponent } from './lib/loyalty/loyalty-transactions-list/loyalty-transactions-list.component';
 /**
