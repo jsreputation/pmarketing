2bd1de8a1d5342a82eece8934b63ff8fabc201ad
Ryane Chia <ryanechia@outlook.com>
2020-06-22 20:56:27 +0800

    VS-3835: initialise the rebate transaction page with API data or merchant passed from previous screen

--- libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/rebates/transaction/transaction.component.ts
+++ libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/rebates/transaction/transaction.component.ts
@@ -1,8 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, NavigationExtras, Router} from '@angular/router';
 import {FormControl} from '@angular/forms';
-import {MerchantData} from '../rebates.types';
 import {CurrencyPipe} from '@angular/common';
+import {
+  DecimalPointsPipe,
+  ILoyalty,
+  LoyaltyService
+} from '@perxtech/core';
+import {
+  map,
+  switchMap
+} from 'rxjs/operators';
+import {
+  iif,
+  of
+} from 'rxjs';
 
 @Component({
   selector: 'perx-blackcomb-pages-transaction',
@@ -10,8 +22,8 @@ import {CurrencyPipe} from '@angular/common';
   styleUrls: ['./transaction.component.scss']
 })
 export class TransactionComponent implements OnInit {
-  private rebatesData?: MerchantData[];
-  public matchingMerchant: MerchantData | undefined;
+  // private rebatesData?: MerchantData[];
+  public matchingMerchant: ILoyalty | undefined;
   public rebateGained: number = 0;
   public consumedRebates: number = 0;
   public transactionAmount: number = 0;
@@ -19,12 +31,15 @@ export class TransactionComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private currencyPipe: CurrencyPipe,
-    private router: Router
+    private router: Router,
+    private loyaltyService: LoyaltyService,
+    private decimalPointsPipe: DecimalPointsPipe
   ) {
     this.costControl.valueChanges.subscribe(
       (currValue) => {
         if (this.matchingMerchant) {
-          const parsedRebateAmount = TransactionComponent.convertStringCurrencyToFloat(this.matchingMerchant.rebateAmount);
+          // const parsedRebateAmount = TransactionComponent.convertStringCurrencyToFloat(this.matchingMerchant.rebateAmount);
+          const parsedRebateAmount = parseFloat(this.decimalPointsPipe.transform(this.matchingMerchant.pointsBalance, 2));
           if (parsedRebateAmount >= currValue) {
             this.consumedRebates = currValue;
           } else if (parsedRebateAmount > 0) {
@@ -48,22 +63,30 @@ export class TransactionComponent implements OnInit {
   }
 
   public ngOnInit(): void {
-    let merchantRebateId;
-    this.route.params.subscribe((routeData) => {
-      merchantRebateId = routeData.id;
-    });
-    this.rebatesData = JSON.parse(localStorage.getItem('merchantsRebates') as string);
-    this.matchingMerchant = this.rebatesData ?
-      this.rebatesData.find(data => data.merchantId === parseInt(merchantRebateId, 10)) : undefined;
-    if (this.matchingMerchant && history.state.price) {
-      this.transactionAmount = TransactionComponent.convertStringCurrencyToFloat(history.state.price);
-      this.costControl.disable();
-    }
+    this.route.params.pipe(
+      map((routeData) => routeData.id as number),
+      switchMap((merchantId: number) => iif(
+        () => history.state.merchant,
+        of(history.state.merchant),
+        this.loyaltyService.getLoyalty(merchantId))
+      )
+    ).subscribe((merchant: ILoyalty) => {
+        this.matchingMerchant = merchant;
+        if (this.matchingMerchant && history.state.price) {
+          this.transactionAmount = history.state.price;
+          this.costControl.disable();
+        }
+      });
+    // this.rebatesData = JSON.parse(localStorage.getItem('merchantsRebates') as string);
+    // this.matchingMerchant = this.rebatesData ?
+    //   this.rebatesData.find(data => data.merchantId === parseInt(merchantRebateId, 10)) : undefined;
+
+
   }
 
-  public validateMatchingMerchant(obj: any): obj is MerchantData {
-    return 'merchantId' in obj;
-  }
+  // public validateMatchingMerchant(obj: any): obj is MerchantData {
+  //   return 'merchantId' in obj;
+  // }
 
   public cancelTransaction(): void {
     this.router.navigate(['rebates/transaction']);
@@ -71,25 +94,28 @@ export class TransactionComponent implements OnInit {
 
   public confirmTransaction(): void {
     // deduct rebate and credit rebate
-    if (this.rebatesData && this.matchingMerchant) {
-      this.rebatesData = this.rebatesData.map(merchant => {
-        if (this.validateMatchingMerchant(this.matchingMerchant) && merchant.merchantId === this.matchingMerchant.merchantId) {
-          let rebateAmount = TransactionComponent.convertStringCurrencyToFloat(merchant.rebateAmount);
-          if (this.consumedRebates) {
-            rebateAmount -= this.consumedRebates; // consume rebate
-          }
-          rebateAmount += this.rebateGained;
-          return {
-            ...merchant,
-            rebateAmount: `${this.currencyPipe.transform(
-              rebateAmount,
-              '$')}`
-          };
-        }
-        return merchant;
-      });
-    }
-    localStorage.setItem('merchantsRebates', JSON.stringify(this.rebatesData));
+    // if (this.rebatesData && this.matchingMerchant) {
+    //   this.rebatesData = this.rebatesData.map(merchant => {
+    //     if (this.validateMatchingMerchant(this.matchingMerchant) && merchant.merchantId === this.matchingMerchant.merchantId) {
+    //       let rebateAmount = TransactionComponent.convertStringCurrencyToFloat(merchant.rebateAmount);
+    //       if (this.consumedRebates) {
+    //         rebateAmount -= this.consumedRebates; // consume rebate
+    //       }
+    //       rebateAmount += this.rebateGained;
+    //       return {
+    //         ...merchant,
+    //         rebateAmount: `${this.currencyPipe.transform(
+    //           rebateAmount,
+    //           '$')}`
+    //       };
+    //     }
+    //     return merchant;
+    //   });
+    // }
+    // localStorage.setItem('merchantsRebates', JSON.stringify(this.rebatesData));
+
+
+
     // navigate to success - think about when will fail, just to display success/failure? not useful, combine
     // wont have failure cause not api, just go to success page, pass down info required
     const navigationExtras: NavigationExtras = {
@@ -102,7 +128,7 @@ export class TransactionComponent implements OnInit {
         transactionAmount: this.currencyPipe.transform(this.transactionAmount, '$'),
         rebateBurned: this.currencyPipe.transform(this.consumedRebates, '$'),
         name: this.matchingMerchant ? this.matchingMerchant.name : '',
-        logo: this.matchingMerchant ? this.matchingMerchant.logo : ''
+        // logo: this.matchingMerchant ? this.matchingMerchant.logo : ''
       }
     };
     this.router.navigate(['rebates/show'], navigationExtras);
