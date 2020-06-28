f37c039836b928b82d94002726ac960beabb9bc0
Ryane Chia <ryanechia@outlook.com>
2020-06-28 15:43:57 +0800

    VS-3916: make a copy of BCPHomeModule but use the local home component for all-it

--- apps/all-it/src/app/home/home.module.ts
+++ apps/all-it/src/app/home/home.module.ts
@@ -1,16 +1,62 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent, HomeModule as BCPHomeModule } from '@perxtech/blackcomb-pages';
-import { RewardPopupComponent } from '@perxtech/core';
+import { MatButtonModule, MatCardModule, MatDialogModule } from '@angular/material';
+import {
+  RouterModule,
+  Routes
+} from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import {
+  CampaignModule,
+  CampaignServiceModule,
+  LoyaltyModule,
+  OutcomeModule,
+  RewardPopupComponent,
+  RewardsModule,
+  UtilsModule,
+  QuizModule,
+
+} from '@perxtech/core';
+import {
+  CampaignsCollectionComponent,
+  CatalogsComponent,
+  GamesCollectionComponent
+} from '@perxtech/blackcomb-pages';
+
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { HomeComponent } from './home.component';
+
 const routes: Routes = [{
   path: '',
-  component: HomeComponent
+  component: HomeComponent,
 }];
 
 @NgModule({
+  declarations: [
+    HomeComponent,
+    CampaignsCollectionComponent,
+    GamesCollectionComponent,
+    CatalogsComponent
+  ],
   imports: [
-    BCPHomeModule,
-    RouterModule.forChild(routes)
+    CommonModule,
+    RouterModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDialogModule,
+    LoyaltyModule,
+    UtilsModule,
+    RewardsModule,
+    TranslateModule.forChild(),
+    InfiniteScrollModule,
+    CampaignModule,
+    CampaignServiceModule.forChild(),
+    QuizModule,
+    OutcomeModule,
+    RouterModule.forChild(routes),
+  ],
+  exports: [
+    HomeComponent
   ],
   entryComponents: [
     RewardPopupComponent
