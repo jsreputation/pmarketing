091d606261207cd3009645041b669f1d8bc64dcb
Ryane Chia <ryanechia@outlook.com>
2020-06-29 22:14:33 +0800

    VS-3914: make premium member specific text functions

--- apps/all-it/src/app/transaction-history/transaction-history.component.ts
+++ apps/all-it/src/app/transaction-history/transaction-history.component.ts
@@ -17,7 +17,10 @@ import {
   // ILoyaltyTransactionProperties
 } from '@perxtech/core';
 import { oc } from 'ts-optchain';
-import { map } from 'rxjs/operators';
+import {
+  map,
+  switchMap
+} from 'rxjs/operators';
 
 // import { ShowTitleInHeader } from '../layout/layout.component';
 
@@ -27,16 +30,18 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./transaction-history.component.scss']
 })
 export class TransactionHistoryComponent implements OnInit/*, ShowTitleInHeader */ {
-  public transactions: Observable<ILoyaltyTransactionHistory[]> | Observable<ITransaction[]>;
-  public purchasesTitleFn: (tr: ILoyaltyTransactionHistory) => string;
+  public transactions: Observable<ILoyaltyTransactionHistory[] | ITransaction[]>;
+  public purchasesTitleFn: (tr: ILoyaltyTransactionHistory | ITransaction) => string;
   public redemptionsTitleFn: (tr: ILoyaltyTransactionHistory) => string;
-  public descFn: (tr: ILoyaltyTransactionHistory) => string;
-  public subTitleFn: (tr: ILoyaltyTransactionHistory) => string;
-  public priceLabelFn: (tr: ILoyaltyTransactionHistory) => string;
+  public descFn: (tr: ILoyaltyTransactionHistory | ITransaction) => string;
+  public subTitleFn: (tr: ILoyaltyTransactionHistory | ITransaction) => string;
+  public priceLabelFn: (tr: ILoyaltyTransactionHistory | ITransaction) => string;
 
   private pageNumber: number = 2;
-  private pageSize: number = 10;
+  private pageSize: number = 25;
   private complitePagination: boolean = false;
+  public isPremiumMember: boolean = false;
+
   constructor(
     private loyaltyService: LoyaltyService,
     private settingsService: SettingsService,
@@ -59,10 +64,9 @@ export class TransactionHistoryComponent implements OnInit/*, ShowTitleInHeader 
             this.loyaltyService.getTransactionHistory(this.pageNumber - 1, this.pageSize)
           );
         }
-      }
-    );
-
-    this.settingsService.getRemoteFlagsSettings().subscribe((flags: IFlags) => {
+      }),
+    switchMap(() => this.settingsService.getRemoteFlagsSettings())
+    ).subscribe((flags: IFlags) => {
       if (flags.rebateDemoFlow) {
         this.priceLabelFn = (tr: ILoyaltyTransactionHistory) => `${this.cashbackTransactionPipe.transform(tr.pointsAmount || 0)}`;
         this.descFn = (tr: ILoyaltyTransactionHistory) => {
@@ -81,6 +85,10 @@ export class TransactionHistoryComponent implements OnInit/*, ShowTitleInHeader 
           }
           return text;
         };
+      } else if (this.isPremiumMember) {
+        this.priceLabelFn = (tr: ITransaction) => `${tr.currency ? tr.currency : ''}${tr.amount}`;
+        this.descFn = (tr: ITransaction) => `${tr.properties.storeName}`;
+        this.purchasesTitleFn = (tr: ITransaction) => `${tr.properties.productName}`;
       } else {
         this.priceLabelFn = (tr: ILoyaltyTransactionHistory) => `${this.transactionPipe.transform(tr.pointsAmount || 0)}`;
         this.descFn = (tr: ILoyaltyTransactionHistory) => {
@@ -106,14 +114,17 @@ export class TransactionHistoryComponent implements OnInit/*, ShowTitleInHeader 
       `${(tr.transactionDetails && tr.transactionDetails.data) ?
         (tr.transactionDetails.data as IRewardTransactionHistory).rewardName : ''}`;
 
-    this.subTitleFn = (tr: ILoyaltyTransactionHistory) => `${this.datePipe.transform(tr.transactedAt, 'dd/MM/yyyy')}`;
+    this.subTitleFn = (tr: ILoyaltyTransactionHistory | ITransaction) => `${this.datePipe.transform(tr.transactedAt, 'dd/MM/yyyy')}`;
   }
 
   public onScroll(): void {
     if (this.complitePagination) {
       return;
     }
-    forkJoin(this.transactions, this.loyaltyService.getTransactionHistory(this.pageNumber, this.pageSize))
+    forkJoin(this.transactions,
+      iif(() => this.isPremiumMember,
+        this.transactionsService.getTransactions(this.pageNumber, this.pageSize),
+        this.loyaltyService.getTransactionHistory(this.pageNumber, this.pageSize)))
       .subscribe((val) => {
         if (val[1].length < this.pageSize) {
           this.complitePagination = true;
