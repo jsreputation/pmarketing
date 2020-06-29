2c71f90e0104683cf00cfb939e73c97ecea7c39d
Ryane Chia <ryanechia@outlook.com>
2020-06-29 14:30:12 +0800

    VS-3914: set up skeleton for switching between two different transaction histories for all-it premium member

--- apps/all-it/src/app/transaction-history/transaction-history.component.ts
+++ apps/all-it/src/app/transaction-history/transaction-history.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DatePipe } from '@angular/common';
-import { Observable, forkJoin, of } from 'rxjs';
+import { Observable, forkJoin, of, iif } from 'rxjs';
 
 import {
   LoyaltyService,
@@ -11,9 +11,11 @@ import {
   CashbackTransactionPipe,
   SettingsService,
   IFlags,
+  ILoyalty,
   // ITransactionProperties
 } from '@perxtech/core';
 import { oc } from 'ts-optchain';
+import { map } from 'rxjs/operators';
 
 // import { ShowTitleInHeader } from '../layout/layout.component';
 
@@ -42,7 +44,22 @@ export class TransactionHistoryComponent implements OnInit/*, ShowTitleInHeader 
   ) { }
 
   public ngOnInit(): void {
-    this.transactions = this.loyaltyService.getTransactionHistory(this.pageNumber - 1, this.pageSize);
+
+    // if premium member hide stuff.
+    this.loyaltyService.getLoyalties().pipe(
+      map( (loyalties: ILoyalty[]) => loyalties[0]),
+    ).subscribe(
+      (loyalty: ILoyalty) => {
+        if (loyalty && loyalty.tiers !== undefined) {
+          // @ts-ignore we are already doing null|undefined checks. todo: typescript upgrade due.
+          this.transactions = iif(() => loyalty.tiers.filter((tier) => tier.name === 'Premium').length > 0,
+            of(), // todo: change to transactions API
+            this.loyaltyService.getTransactionHistory(this.pageNumber - 1, this.pageSize)
+          );
+        }
+      }
+    );
+
     this.settingsService.getRemoteFlagsSettings().subscribe((flags: IFlags) => {
       if (flags.rebateDemoFlow) {
         this.priceLabelFn = (tr: ITransactionHistory) => `${this.cashbackTransactionPipe.transform(tr.pointsAmount || 0)}`;
