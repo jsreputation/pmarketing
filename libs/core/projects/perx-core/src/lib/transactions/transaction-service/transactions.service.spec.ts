f0b62f9934576ef956ab100b4a5dd39034da9740
Ryane Chia <ryanechia@outlook.com>
2020-06-29 23:03:53 +0800

    VS-3914: initialise unit tests

--- libs/core/projects/perx-core/src/lib/transactions/transaction-service/transactions.service.spec.ts
+++ libs/core/projects/perx-core/src/lib/transactions/transaction-service/transactions.service.spec.ts
@@ -1,9 +1,23 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TransactionsService } from './transactions.service';
+import { of } from 'rxjs';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('TransactionsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+
+  const transactionServiceStub: Partial<TransactionsService> = {
+    getTransactions: () => of()
+  };
+
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientTestingModule
+    ],
+    providers: [
+      { provide: TransactionsService, useValue: transactionServiceStub }
+    ]
+  }));
 
   it('should be created', () => {
     const service: TransactionsService = TestBed.get(TransactionsService);
--- libs/core/projects/perx-core/src/lib/transactions/transaction-service/v4-transactions.service.spec.ts
+++ libs/core/projects/perx-core/src/lib/transactions/transaction-service/v4-transactions.service.spec.ts
@@ -1,9 +1,29 @@
 import { TestBed } from '@angular/core/testing';
 
 import { V4TransactionsService } from './v4-transactions.service';
+import { TransactionsService } from './transactions.service';
+import { of } from 'rxjs';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ConfigService } from '../../config/config.service';
 
 describe('V4TransactionsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+
+  const transactionServiceStub: Partial<TransactionsService> = {
+    getTransactions: () => of()
+  };
+  const configServiceStub: Partial<ConfigService> = {
+    readAppConfig: () => of()
+  };
+
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientTestingModule
+    ],
+    providers: [
+      { provide: TransactionsService, useValue: transactionServiceStub },
+      { provide: ConfigService, useValue: configServiceStub }
+    ]
+  }));
 
   it('should be created', () => {
     const service: V4TransactionsService = TestBed.get(V4TransactionsService);
