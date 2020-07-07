7df13af26cd6475533b4cd9b27fd6d89fbc641da
andrewing <andrewing1012@gmail.com>
2020-07-07 15:41:07 +0800

    PW-3763 Rank translation

--- libs/core/projects/perx-core/src/lib/rank/leaderboard/leaderboard.component.ts
+++ libs/core/projects/perx-core/src/lib/rank/leaderboard/leaderboard.component.ts
@@ -1,3 +1,4 @@
+import { Observable, of } from 'rxjs';
 import { Component, Input, OnInit } from '@angular/core';
 import { UserRanking } from '../models/rank.model';
 
@@ -7,7 +8,7 @@ import { UserRanking } from '../models/rank.model';
   styleUrls: ['./leaderboard.component.scss']
 })
 export class LeaderboardComponent implements OnInit {
-  @Input() public rank1to3Images: [string, string, string] =  [
+  @Input() public rank1to3Images: [string, string, string] = [
     'https://cdn.perxtech.io/content/hsbc-hk/images/firstplace.png',
     'https://cdn.perxtech.io/content/hsbc-hk/images/secondplace.png',
     'https://cdn.perxtech.io/content/hsbc-hk/images/thirdplace.png'
@@ -16,11 +17,16 @@ export class LeaderboardComponent implements OnInit {
   public metric: string = 'score';
   public columnsToDisplay: ['rank', 'displayName', 'value'] = ['rank', 'displayName', 'value'];
   @Input() public dataArray: UserRanking[];
+  @Input()
+  public nickNameTxtFn: () => Observable<string>
   // @Input() public dataNumberToDisplay: number = 10;
 
   public ngOnInit(): void {
     // if (this.dataArray.length > this.dataNumberToDisplay) {
     //   this.dataArray = this.dataArray.slice(0, this.dataNumberToDisplay);
     // }
+    if (!this.nickNameTxtFn) {
+      this.nickNameTxtFn = () => of('NICKNAME');
+    }
   }
 }
--- libs/core/projects/perx-core/src/lib/rank/mini-rank/mini-rank.component.ts
+++ libs/core/projects/perx-core/src/lib/rank/mini-rank/mini-rank.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Component, Input, OnInit } from '@angular/core';
 import { UserRanking } from '../models/rank.model';
 
 @Component({
@@ -6,9 +7,22 @@ import { UserRanking } from '../models/rank.model';
   templateUrl: './mini-rank.component.html',
   styleUrls: ['./mini-rank.component.scss']
 })
-export class MiniRankComponent {
+export class MiniRankComponent implements OnInit {
   @Input()
   public metric: string = 'score';
   @Input()
   public userGameInfo: UserRanking;
+  @Input()
+  public rantTextFn: () => Observable<string>;
+  @Input()
+  public pointTextFn: () => Observable<string>;
+
+  public ngOnInit(): void {
+    if (!this.rantTextFn) {
+      this.rantTextFn = () => of('Your Rank');
+    }
+    if (!this.pointTextFn) {
+      this.pointTextFn = () => of(' points');
+    }
+  }
 }
