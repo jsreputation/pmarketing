45323f56554e7820bec68f6d15fd37503dcb8f18
Dawson Chung <dawson1096@gmail.com>
2020-07-13 17:25:20 +0800

    Changed if condition to account for disabled campaigns

--- libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/home/campaigns-collection/campaigns-collection.component.ts
+++ libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/home/campaigns-collection/campaigns-collection.component.ts
@@ -142,7 +142,8 @@ export class CampaignsCollectionComponent implements OnInit {
   }
 
   public selectCampaign(campaign: ICampaign): void {
-    if (!this.isCampaignComplete(campaign.id)) {
+    if (!this.isCampaignComplete(campaign.id) && (this.isCampaignDisabled[campaign.id] === undefined || !this.isCampaignDisabled[campaign.id])) {
+      console.log('this.isCampaignDisabled[campain.id')
       this.selected.emit(campaign);
     }
   }
