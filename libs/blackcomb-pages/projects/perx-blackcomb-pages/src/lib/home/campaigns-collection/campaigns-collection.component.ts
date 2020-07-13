8dd6a0b5efb73d54c02113c46f7299462f740bc8
Dawson Chung <dawson1096@gmail.com>
2020-07-13 18:02:41 +0800

    Removed trailing white space

--- libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/home/campaigns-collection/campaigns-collection.component.ts
+++ libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/home/campaigns-collection/campaigns-collection.component.ts
@@ -143,7 +143,7 @@ export class CampaignsCollectionComponent implements OnInit {
 
   public selectCampaign(campaign: ICampaign): void {
     if (
-      !this.isCampaignComplete(campaign.id) && 
+      !this.isCampaignComplete(campaign.id) &&
       (this.isCampaignDisabled[campaign.id] === undefined ||
       !this.isCampaignDisabled[campaign.id])
     ) {
