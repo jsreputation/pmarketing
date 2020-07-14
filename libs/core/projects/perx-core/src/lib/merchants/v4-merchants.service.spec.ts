70b377c6388f790a5c6fce92946e82003bbbbf91
winde123 <edwinwan88@gmail.com>
2020-07-14 21:49:52 +0800

    Pw-3792 increasing coverage for perx-core/merchant.service

--- libs/core/projects/perx-core/src/lib/merchants/v4-merchants.service.spec.ts
+++ libs/core/projects/perx-core/src/lib/merchants/v4-merchants.service.spec.ts
@@ -1,11 +1,53 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed,fakeAsync, inject, tick } from '@angular/core/testing';
 
 import { V4MerchantsService } from './v4-merchants.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ConfigModule } from '../config/config.module';
+// import { IMerchant } from '../merchants/models/merchants.model';
+import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
+
+interface IMerchant {
+  id: number;
+  name: string;
+  description?: string;
+  website?: string;
+  tags?: ITag[];
+  images?: IImage[];
+  outlets?: IOutlet[] | null;
+}
+interface ITag {
+  id: number;
+  name: string;
+}
+
+interface IImage {
+  type: string;
+  url: string;
+}
+
+interface IOutlet {
+  outletId: number;
+  outletName: string;
+  outletAddress1: string;
+  outletAddress2?: string;
+  outletAddress3?: string;
+  postalCode?: string;
+  tel: string;
+  coordinates: { lat: number, lng: number, distance?: number, unitOfMeasure: string };
+  tags?: ITag[];
+}
+
+const mockMerchant: IMerchant = {
+  id: 1,
+  name: 'test',
+  description: 'test',
+  website: 'test',
+  outlets: null
+};
 
 describe('V4MerchantsService', () => {
-
+  let service: V4MerchantsService;
   beforeEach(() => TestBed.configureTestingModule({
     imports: [
       HttpClientTestingModule,
@@ -13,8 +55,20 @@ describe('V4MerchantsService', () => {
     ]
   }));
 
+  beforeEach(() => {
+    service = TestBed.get(V4MerchantsService);
+  });
+
   it('should be created', () => {
-    const service: V4MerchantsService = TestBed.get(V4MerchantsService);
+    // const service: V4MerchantsService = TestBed.get(V4MerchantsService);
     expect(service).toBeTruthy();
   });
+
+  it('should get all merchants', fakeAsync(inject([HttpClient], (http: HttpClient) =>{
+    const spy = jest.spyOn(service,'getAllMerchants')
+    jest.spyOn(http, 'put').mockReturnValue(of({ data: mockMerchant }));
+    service.getAllMerchants().subscribe(() => {});
+    tick();
+    expect(spy).toHaveBeenCalled();
+  })));
 });
