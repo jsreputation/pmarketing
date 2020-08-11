fa836ffdd9eca4b9732fe86c03f99c4c2fbccf70
Dawson Chung <dawson1096@gmail.com>
2020-08-11 15:18:28 +0800

    configured test

--- apps/all-it/src/app/tnc/tnc.component.spec.ts
+++ apps/all-it/src/app/tnc/tnc.component.spec.ts
@@ -1,25 +1,64 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatToolbarModule, MatIconModule } from '@angular/material';
+import { Location } from '@angular/common';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ThemesService, ITheme } from '@perxtech/core';
+import { of } from 'rxjs';
 
 import { TncComponent } from './tnc.component';
 
 describe('TncComponent', () => {
+  
+  const mockTheme: ITheme = {
+    name: 'theme',
+    properties: {
+      '--background': 'red',
+      '--font_color': 'black'
+    }
+  };
   let component: TncComponent;
   let fixture: ComponentFixture<TncComponent>;
+  let location: Location;
+
+  const themesServiceStub: Partial<ThemesService> = {
+    getThemeSetting: () => of(mockTheme)
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TncComponent ]
+      imports: [
+        RouterTestingModule.withRoutes([
+          { path: 'c/tnc', redirectTo: '/' }
+        ]),
+        MatToolbarModule,
+        MatIconModule,
+      ],
+      declarations: [ TncComponent ],
+      providers: [
+        {
+          provide: ThemesService,
+          useValue: themesServiceStub
+        }
+      ]
     })
       .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TncComponent);
+    location = TestBed.get(Location);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should location back', () => {
+    component.backArrowIcon = 'back';
+    spyOn(location, 'back');
+    component.backArrowClick();
+    expect(location.back).toHaveBeenCalled();
   });
 });
