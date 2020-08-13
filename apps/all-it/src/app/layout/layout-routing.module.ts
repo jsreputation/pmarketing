9768d02646c1423646927ebb7952eacee1220692
Dawson Chung <dawson1096@gmail.com>
2020-08-13 14:24:58 +0800

    Removed previous routes to tnc and contactus

--- apps/all-it/src/app/layout/layout-routing.module.ts
+++ apps/all-it/src/app/layout/layout-routing.module.ts
@@ -75,10 +75,6 @@ const routes: Routes = [
       {
         path: 'booking/:id',
         loadChildren: () => import('../rewards-booking/rewards-booking.module').then((mod) => mod.RewardsBookingModule)
-      },
-      {
-        path: 'c/contact-us',
-        loadChildren: () => import('../contact-us/contact-us.module').then((mod) => mod.ContactUsModule)
       }
     ]
   }
