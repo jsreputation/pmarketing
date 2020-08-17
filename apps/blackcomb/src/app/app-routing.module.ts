a35437d7148cd84c23c869f79880cee34ea33533
Ryane Chia <ryanechia@outlook.com>
2020-08-17 12:48:48 +0800

    Revert "VS-4266/preload-modules"
    This reverts commit 5a35ddb1b9b718890e12f5b9a8d6c8619c5e52eb.

--- apps/blackcomb/src/app/app-routing.module.ts
+++ apps/blackcomb/src/app/app-routing.module.ts
@@ -3,46 +3,36 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProtectedGuard, PublicGuard } from 'ngx-auth';
 import { PreLoginGuard } from './pre-login.guard';
-import { AppPreloadingStrategy } from './preload-strat';
 
 const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then((mod) => mod.LoginModule),
-    canActivate: [PublicGuard, PreAuthGuard],
-    data: { preload: true, delay: false }
+    canActivate: [PublicGuard, PreAuthGuard]
   },
   {
     path: 'signup',
     loadChildren: () => import('./sign-up/sign-up.module').then((mod) => mod.SignUpModule),
-    canActivate: [PublicGuard, PreAuthGuard],
-    data: { preload: false, delay: false }
+    canActivate: [PublicGuard, PreAuthGuard]
   },
-  { path: 'otp/:type',
-    loadChildren: () => import('./otp/otp.module').then((mod) => mod.OtpModule),
-    data: { preload: false, delay: false }
-  },
+  { path: 'otp/:type', loadChildren: () => import('./otp/otp.module').then((mod) => mod.OtpModule) },
   {
     path: 'forgot-password',
     loadChildren: () => import('./forgot-password/forgot-password.module').then((mod) => mod.ForgotPasswordModule),
-    canActivate: [PublicGuard, PreAuthGuard],
-    data: { preload: false, delay: false }
+    canActivate: [PublicGuard, PreAuthGuard]
   },
   {
     path: 'loading',
-    loadChildren: () => import('./loading/loading.module').then((mod) => mod.LoadingModule),
-    data: { preload: false, delay: false }
+    loadChildren: () => import('./loading/loading.module').then((mod) => mod.LoadingModule)
   },
   {
     path: '',
     loadChildren: () => import('./layout/layout.module').then((mod) => mod.LayoutModule),
-    canActivate: [PreLoginGuard, ProtectedGuard],
-    data: { preload: true, delay: false }
+    canActivate: [PreLoginGuard, ProtectedGuard]
   },
   {
     path: 'c/:key',
     loadChildren: () => import('./content/content.module').then(mod => mod.ContentModule),
-    data: { preload: false, delay: false }
     // content page can be accessed both logged-in and logged-out
     // canActivate: [ProtectedGuard]
   },
@@ -51,7 +41,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {preloadingStrategy: AppPreloadingStrategy})],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
