63686ec5104e369ffb2f28e34f0aab8b4950e48a
Ryane Chia <ryanechia@outlook.com>
2020-08-19 18:22:17 +0800

    VS-4341: update function signature in the abstract game service

--- libs/core/projects/perx-core/src/lib/game/igame.service.ts
+++ libs/core/projects/perx-core/src/lib/game/igame.service.ts
@@ -1,5 +1,6 @@
 import { Observable } from 'rxjs';
 import { IGame, IPlayOutcome, IEngagementTransaction } from './game.model';
+import { ICampaign } from '../campaign/models/campaign.model';
 
 export abstract class IGameService {
   public abstract play(engagementId: number, campaignId?: number): Observable<IPlayOutcome>;
@@ -7,7 +8,7 @@ export abstract class IGameService {
   public abstract prePlayConfirm(
     transactionId: number,
     informationCollectionSetting?: string): Observable<IEngagementTransaction | IPlayOutcome | void>;
-  public abstract getGamesFromCampaign(campaignId: number): Observable<IGame[]>;
+  public abstract getGamesFromCampaign(campaign: ICampaign): Observable<IGame[]>;
   public abstract get(gameId: number, campaignId?: number): Observable<IGame>;
   public abstract getActiveGames(): Observable<IGame[]>;
 }
