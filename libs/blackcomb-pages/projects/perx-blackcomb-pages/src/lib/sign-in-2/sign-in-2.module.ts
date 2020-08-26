9458a4aa51fa807ca24bd99b992cfb5d16b279a7
Ryane Chia <ryanechia@outlook.com>
2020-08-26 23:32:22 +0800

    HSBCPH-405: re-import mat modules from their specific folders to aid tree-shaking

--- libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/sign-in-2/sign-in-2.module.ts
+++ libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/sign-in-2/sign-in-2.module.ts
@@ -1,12 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule, MatFormFieldModule, MatInputModule, MatProgressSpinnerModule, MatSelectModule } from '@angular/material';
 import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { AuthenticationModule, ConfigModule, UtilsModule } from '@perxtech/core';
 import { SignIn2Component } from './sign-in-2.component';
 import { MatOptionModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
