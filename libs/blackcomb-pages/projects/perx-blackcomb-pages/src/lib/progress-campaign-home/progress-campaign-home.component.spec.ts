e0bcda94fb5c8f538aefbccc65885b8da75d89f7
Ryane Chia <ryanechia@outlook.com>
2020-09-18 15:59:09 +0800

    RAZ-4: import providers for progress campaign home spec

--- libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/progress-campaign-home/progress-campaign-home.component.spec.ts
+++ libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/progress-campaign-home/progress-campaign-home.component.spec.ts
@@ -1,6 +1,38 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProgressCampaignHomeComponent } from './progress-campaign-home.component';
+import { PageComponentsModule } from '../page-components/page-components.module';
+import { RouterTestingModule } from '@angular/router/testing';
+import {
+  CampaignServiceModule,
+  ConfigService,
+  ICampaignService,
+  IGameService,
+  QuizService,
+  SettingsService,
+  SurveyService
+} from '@perxtech/core';
+import { of } from 'rxjs';
+import { TranslateModule } from '@ngx-translate/core';
+
+const configServiceStub: Partial<ConfigService> = {
+  readAppConfig: () => of()
+};
+const campaignServiceStub: Partial<ICampaignService> = {
+  getCampaigns: () => of()
+};
+const settingsServiceStub: Partial<SettingsService> = {
+  getRemoteFlagsSettings: () => of()
+};
+const gameServiceStub: Partial<IGameService> = {
+  getActiveGames: () => of([])
+};
+const quizServiceStub: Partial<QuizService> = {
+  getQuizFromCampaign: () => of()
+};
+const surveyServiceStub: Partial<SurveyService> = {
+  getSurveyFromCampaign: () => of()
+};
 
 describe('ProgressCampaignHomeComponent', () => {
   let component: ProgressCampaignHomeComponent;
@@ -8,7 +40,21 @@ describe('ProgressCampaignHomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProgressCampaignHomeComponent ]
+      declarations: [ ProgressCampaignHomeComponent ],
+      imports: [
+        PageComponentsModule,
+        RouterTestingModule,
+        CampaignServiceModule.forChild(),
+        TranslateModule.forRoot()
+      ],
+      providers: [
+        { provide: ConfigService, useValue: configServiceStub },
+        { provide: ICampaignService, useValue: campaignServiceStub },
+        { provide: QuizService, useValue: quizServiceStub },
+        { provide: IGameService, useValue: gameServiceStub },
+        { provide: SettingsService, useValue: settingsServiceStub },
+        { provide: SurveyService, useValue: surveyServiceStub },
+      ]
     })
     .compileComponents();
   }));
