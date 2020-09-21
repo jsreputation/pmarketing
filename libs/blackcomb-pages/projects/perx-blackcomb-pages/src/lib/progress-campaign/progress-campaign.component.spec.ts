6b9709a79c010449257ed2e1d13b99dd5937375c
Ryane Chia <ryanechia@outlook.com>
2020-09-21 13:37:28 +0800

    RAZ-18: add stubs and providers to the progress campaign component spec file

--- libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/progress-campaign/progress-campaign.component.spec.ts
+++ libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/progress-campaign/progress-campaign.component.spec.ts
@@ -1,6 +1,27 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProgressCampaignComponent } from './progress-campaign.component';
+import {
+  ConfigService,
+  ICampaignService,
+  RewardsModule,
+  SettingsService
+} from '@perxtech/core';
+import {
+  ActivatedRoute,
+  convertToParamMap
+} from '@angular/router';
+import { of } from 'rxjs';
+
+const campaignServiceStub: Partial<ICampaignService> = {
+  getCampaign: () => of(),
+};
+const settingsServiceStub: Partial<SettingsService> = {
+  getRemoteFlagsSettings: () => of()
+};
+const configServiceStub: Partial<ConfigService> = {
+  readAppConfig: () => of()
+};
 
 describe('ProgressCampaignComponent', () => {
   let component: ProgressCampaignComponent;
@@ -8,7 +29,20 @@ describe('ProgressCampaignComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProgressCampaignComponent ]
+      declarations: [ ProgressCampaignComponent ],
+      imports: [
+        RewardsModule
+      ],
+      providers: [
+        {
+          provide: ActivatedRoute, useValue: {
+            paramMap: of(convertToParamMap({ id: 1 }))
+          }
+        },
+        { provide: ICampaignService, useValue: campaignServiceStub},
+        { provide: ConfigService, useValue: configServiceStub },
+        { provide: SettingsService, useValue: settingsServiceStub },
+      ]
     })
     .compileComponents();
   }));
