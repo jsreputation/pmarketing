5be49f02e0f6a93f50ce687525cd295824dfba45
Ryane Chia <ryanechia@outlook.com>
2020-09-21 13:55:28 +0800

    RAZ-18: add stubs and providers to the rewards large list component spec file

--- libs/core/projects/perx-core/src/lib/rewards/rewards-large-list/rewards-large-list.component.spec.ts
+++ libs/core/projects/perx-core/src/lib/rewards/rewards-large-list/rewards-large-list.component.spec.ts
@@ -1,16 +1,32 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RewardsLargeListComponent } from './rewards-large-list.component';
+import {
+  ThemesService,
+  UtilsModule
+} from '@perxtech/core';
+import { MatCardModule } from '@angular/material/card';
+import { of } from 'rxjs';
 
 describe('RewardsLargeListComponent', () => {
   let component: RewardsLargeListComponent;
   let fixture: ComponentFixture<RewardsLargeListComponent>;
+  const themesServiceStub: Partial<ThemesService> = {
+    getThemeSetting: () => of()
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RewardsLargeListComponent ]
-    })
-    .compileComponents();
+      declarations: [ RewardsLargeListComponent ],
+      imports: [
+        UtilsModule,
+        MatCardModule
+      ],
+      providers: [
+        { provide: ThemesService, useValue: themesServiceStub },
+
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
