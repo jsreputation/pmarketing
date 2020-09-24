7afb9f1f26b965321575cec1481ecda7470cf34b
reynard <reynard93@gmail.com>
2020-09-24 09:45:57 +0800

    progress bar improvements so it's evenly spaced isntead of accurate, interface for progressbar

--- libs/core/projects/perx-core/src/public-api.ts
+++ libs/core/projects/perx-core/src/public-api.ts
@@ -66,7 +66,8 @@ export {
   CampaignState,
   ICampaign,
   CampaignLandingPage,
-  IReferral
+  IReferral,
+  ProgressBarFields
 } from './lib/campaign/models/campaign.model';
 export { ExpireTimerComponent } from './lib/campaign/reward-popup/expire-timer/expire-timer.component';
 export { RewardPopupComponent } from './lib/campaign/reward-popup/reward-popup.component';
