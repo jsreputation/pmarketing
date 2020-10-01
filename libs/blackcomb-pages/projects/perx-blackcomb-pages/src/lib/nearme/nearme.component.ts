675a4e75a1a4b3bdbd4dc528b02b539879ae5295
Dawson Chung <dwc291@nyu.edu>
2020-10-01 17:22:30 -0400

    changed updateMarkers to take lat and lng instead of position

--- libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/nearme/nearme.component.ts
+++ libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/nearme/nearme.component.ts
@@ -21,7 +21,7 @@ import {
   Subject,
   from
 } from 'rxjs';
-import { takeUntil, mergeMap, filter, tap } from 'rxjs/operators';
+import { take, mergeMap, filter, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'perx-blackcomb-pages-nearme',
@@ -38,6 +38,8 @@ export class NearmeComponent implements OnInit, OnDestroy {
   public currentPrice: IPrice | null;
   private destroy$: Subject<void> = new Subject();
   public position: Position;
+  public rad: number = 100000000;
+  public firstLoad: boolean = true;
 
   constructor(
     private rewardsService: RewardsService,
@@ -46,7 +48,6 @@ export class NearmeComponent implements OnInit, OnDestroy {
   ) { }
 
   public ngOnInit(): void {
-
     this.loadScript()
       .then(() => {
         const mapProp: google.maps.MapOptions = {
@@ -57,11 +58,11 @@ export class NearmeComponent implements OnInit, OnDestroy {
         this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
         this.map.addListener('click', () => this.current = null);
         this.geoLocationService.positions()
-          .pipe(takeUntil(this.destroy$))
+          .pipe(take(1))
           .subscribe((position: Position) => {
             this.position = position;
+            this.updateMarkers(this.position.coords.latitude, this.position.coords.longitude, this.rad);
           });
-        this.updateMarkers(this.position);
       });
   }
 
@@ -81,7 +82,7 @@ export class NearmeComponent implements OnInit, OnDestroy {
     script.innerHTML = '';
     let url = 'https://maps.googleapis.com/maps/api/js';
     if (this.key) {
-      url += `?key=${this.key}`;
+      url += `?key=${this.key}&libraries=geometry`;
     }
 
     script.src = url;
@@ -91,21 +92,16 @@ export class NearmeComponent implements OnInit, OnDestroy {
     return p;
   }
 
-
   public clearMarkers(): void {
     this.markersArray.forEach(item => {
       item.setMap(null);
     });
   }
 
-  private updateMarkers(position: Position): void {
-    if (!position) {
-      return;
-    }
-
-    const rad = 10000;
-    this.rewardsService.nearMe(rad, position).pipe(
-      takeUntil(this.destroy$),
+  private updateMarkers(latitude: number, longitude: number, rad: number): void {
+    this.rewardsService.nearMe(rad, latitude, longitude).pipe(
+      take(1),
+      tap(rewards => console.log(rewards)),
       mergeMap((rewards: IReward[]) =>
         from(rewards).pipe(
           mergeMap(reward => this.vouchersService.getRewardLocations(reward.id).pipe(
@@ -114,10 +110,7 @@ export class NearmeComponent implements OnInit, OnDestroy {
                 filter((location: IVoucherLocation) => {
                   const lat = location.latitude !== null ? parseFloat(location.latitude) : 0;
                   const lng = location.longitude !== null ? parseFloat(location.longitude) : 0;
-                  if (lat === 0 || lng === 0) {
-                    return false;
-                  }
-                  return this.isNearMe(rad, lat, lng);
+                  return lat === 0 || lng === 0 ? false : true;
                 }),
                 tap((location: IVoucherLocation) => {
                   const lat = location.latitude !== null ? parseFloat(location.latitude) : 0;
@@ -132,17 +125,18 @@ export class NearmeComponent implements OnInit, OnDestroy {
                     this.currentPrice = reward.rewardPrice ? reward.rewardPrice[0] : null;
                   });
                   this.markersArray.push(marker);
-                  this.updateBoundingBox();
-                  console.log(reward);
                 })
               )
             )
           ))
         )
       )
-    ).subscribe();
-  }
-
+    ).subscribe(() => {
+      if (this.firstLoad) {
+        this.firstLoad = false;
+        this.updateBoundingBox();
+      }
+    });
   }
 
   public updateBoundingBox(): void {
@@ -153,14 +147,13 @@ export class NearmeComponent implements OnInit, OnDestroy {
         bbox = bbox.extend(position);
       }
     });
-    if (this.userMarker) {
-      const position = this.userMarker.getPosition();
-      if (position) {
-        bbox.extend(position);
-      }
+    if (this.position) {
+      const position = new google.maps.LatLng(this.position.coords.latitude, this.position.coords.longitude);
+      bbox.extend(position);
     }
     this.map.fitBounds(bbox);
   }
+
 
   public ngOnDestroy(): void {
     this.destroy$.next();
