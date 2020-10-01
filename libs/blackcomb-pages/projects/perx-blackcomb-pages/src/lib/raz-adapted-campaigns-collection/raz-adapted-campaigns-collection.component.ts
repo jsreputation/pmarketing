8b91e263a822f1c5dfa3bdd356d6a95c2d728c4a
reynard <reynard93@gmail.com>
2020-10-01 11:11:54 +0800

    fix test regarding typing and extraneoustap import

--- libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/raz-adapted-campaigns-collection/raz-adapted-campaigns-collection.component.ts
+++ libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/raz-adapted-campaigns-collection/raz-adapted-campaigns-collection.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Observable, zip } from 'rxjs';
 import { CampaignType, ICampaign, ICampaignService, LoyaltyService, ProgressBarFields, StampService } from '@perxtech/core';
-import { concatMap, map, switchMap, tap, withLatestFrom } from 'rxjs/operators';
+import { concatMap, map, switchMap, withLatestFrom } from 'rxjs/operators';
 import { listAnimation } from '../home/games-collection/games-collection.animation';
 
 @Component({
@@ -118,7 +118,7 @@ export class RazAdaptedCampaignsCollectionComponent implements OnInit {
             const campaignsWithoutUltimateCampaign = campaigns.filter(campaign => campaign.name !== 'Ultimate Task');
             return [...campaignsWithoutUltimateCampaign, ultimateCampaign].map((campaign, index) =>
               // index skip over ultimateCampaign
-              ({ ...campaign, progress: progress[index > ultimateCampaignInd ? index + 1 : index] as ProgressBarFields }))
+              ({ ...campaign as ICampaign, progress: progress[index > ultimateCampaignInd ? index + 1 : index] as ProgressBarFields }))
           }
         ),
         // tap to see transformed
