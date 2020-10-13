7f891c127f6ffcac3cf494630f2bb977a8d19d2b
Ryane Chia <ryanechia@outlook.com>
2020-10-13 15:23:20 +0800

    GLOB-2: add new globe one app based on blackcomb

---
+++ apps/globeone/src/app/loading/loading.module.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoadingComponent, PerxBlackcombPagesModule } from '@perxtech/blackcomb-pages';
+import { SharedModule } from '../shared/shared.module';
+
+const routes: Routes = [{
+  path: '',
+  component: LoadingComponent
+}];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    SharedModule,
+    PerxBlackcombPagesModule
+  ]
+})
+export class LoadingModule { }
