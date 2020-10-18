df886308193f8b988cb615d2052d2214b35d45a8
Ryane Chia <ryanechia@outlook.com>
2020-10-19 02:13:15 +0800

    add radix to parseInt calls

--- libs/core/projects/perx-core/src/lib/utils/progress-info/progress-info.pipe.ts
+++ libs/core/projects/perx-core/src/lib/utils/progress-info/progress-info.pipe.ts
@@ -15,15 +15,15 @@ export class ProgressInfoPipe implements PipeTransform {
     switch (rewardMode) {
       case CampaignRewardMode.TransactionQuantity:
         if (campaignName === 'Getting Started' || campaignName === 'Ultimate Reward') {
-          return `You've completed <strong>${parseInt(value)}</strong> tasks`;
+          return `You've completed <strong>${parseInt(value, 10)}</strong> tasks`;
         }
-        return `You've made <strong>${parseInt(value)}</strong> transaction${(+value > 1 || +value === 0)? 's' : ''}`;
+        return `You've made <strong>${parseInt(value, 10)}</strong> transaction${(+value > 1 || +value === 0) ? 's' : ''}`;
       case CampaignRewardMode.TransactionAmount:
         return `You've spent <strong>$${value}</strong>`;
       case CampaignRewardMode.Referral:
-        return `You've recruited <strong>${parseInt(value)} users</strong>`;
+        return `You've recruited <strong>${parseInt(value, 10)} users</strong>`;
       default:
-        return `You have made ${parseInt(value)} point${+value > 1 ? 's' : ''}`;
+        return `You have made ${parseInt(value, 10)} point${+value > 1 ? 's' : ''}`;
 
     }
     return null;
