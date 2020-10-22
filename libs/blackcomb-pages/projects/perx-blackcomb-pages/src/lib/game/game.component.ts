9e9774d8e54e293cfdb727b70d9a5fd1e4ad13d5
reynard <reynard93@gmail.com>
2020-10-22 13:26:19 +0800

    stop prettier from overformatting stuff

--- libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/game/game.component.ts
+++ libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/game/game.component.ts
@@ -185,7 +185,8 @@ export class GameComponent implements OnInit, OnDestroy {
             // null is recognised as infinite from dashboard
             this.notificationService.addPopup({
               title: 'No more tries',
-              text: "Come back when you've earned more tries!",
+              // prettier-ignore
+              text: 'Come back when you\'ve earned more tries!',
               buttonTxt: this.isEmbedded ? null : 'Close',
               afterClosedCallBack: this,
               disableOverlayClose: true,
