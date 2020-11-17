1c368f305f94e1fa2dcdf831773f0fc64baea811
Ryane Chia <ryanechia@outlook.com>
2020-11-17 17:43:46 +0800

    VS-4947: home module should continue to use forChild router

--- apps/dbshk-agent/src/app/home/home.module.ts
+++ apps/dbshk-agent/src/app/home/home.module.ts
@@ -1,7 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule, MatCardModule, MatDialogModule, MatRippleModule } from '@angular/material';
-import { RouterModule } from '@angular/router';
+import {
+  RouterModule,
+  Routes
+} from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import {
   CampaignModule,
@@ -18,13 +21,18 @@ import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { HomeComponent } from './home.component';
 import { PageComponentsModule } from '@perxtech/blackcomb-pages';
 
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent
+}];
+
 @NgModule({
   declarations: [
     HomeComponent,
   ],
   imports: [
     CommonModule,
-    RouterModule,
+    RouterModule.forChild(routes),
     MatCardModule,
     MatButtonModule,
     MatRippleModule,
