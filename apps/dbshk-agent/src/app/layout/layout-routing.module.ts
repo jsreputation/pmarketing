0d6cbcbbe99bb70320ffb2de70731bbf0c678b11
Ryane Chia <ryanechia@outlook.com>
2020-11-17 16:31:51 +0800

    VS-4947: switch to a custom copy for layout component in dbshk

--- apps/dbshk-agent/src/app/layout/layout-routing.module.ts
+++ apps/dbshk-agent/src/app/layout/layout-routing.module.ts
@@ -1,7 +1,7 @@
 import { ProtectedGuard } from 'ngx-auth';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LayoutComponent } from '@perxtech/blackcomb-pages';
+import { LayoutComponent } from './layout.component';
 
 const routes: Routes = [
   {
