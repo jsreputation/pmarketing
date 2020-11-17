1750121a591ed122884c1600b07d9ba4765b33ff
Ryane Chia <ryanechia@outlook.com>
2020-11-17 17:45:39 +0800

    VS-4947: add the performance nav button to dbshk app

--- apps/dbshk-agent/src/app/layout/layout.module.ts
+++ apps/dbshk-agent/src/app/layout/layout.module.ts
@@ -1,12 +1,36 @@
 import { NgModule } from '@angular/core';
-import { LayoutModule as BCPLayoutModule } from '@perxtech/blackcomb-pages';
 import { LayoutRoutingModule } from './layout-routing.module';
+import { LayoutComponent } from './layout.component';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import {
+  ConfigModule,
+  RewardsModule,
+  UtilsModule,
+  VouchersModule
+} from '@perxtech/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
+  declarations:[
+    LayoutComponent
+  ],
   imports: [
     // LayoutRoutingModule must be listed first to use its '' routing paths
     LayoutRoutingModule,
-    BCPLayoutModule
+    CommonModule,
+    RouterModule,
+    // ReactiveFormsModule,
+    // FormsModule,
+    TranslateModule.forChild(),
+    UtilsModule,
+    ConfigModule.forChild(),
+    VouchersModule,
+    RewardsModule,
+    MatIconModule,
+    MatToolbarModule
   ]
 })
 export class LayoutModule { }
