cba51f4f01c0fd67d6e482c494f263633157edc4
Awad Maharoof <awadmaharoof@gmail.com>
2020-11-23 14:00:49 +0800

    [VS-4989] Fix test

--- apps/dbshk-agent/src/app/referral/referral.component.spec.ts
+++ apps/dbshk-agent/src/app/referral/referral.component.spec.ts
@@ -3,15 +3,23 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { ReferralComponent } from './referral.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { ICampaignService } from '@perxtech/core';
-import {of} from 'rxjs';
+import { BehaviorSubject, of } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 const campaignServiceStub: Partial<ICampaignService> = {
   getCampaigns: () => of([])
 };
 
 describe('ReferralComponent', () => {
+  const testId = 584;
   let component: ReferralComponent;
   let fixture: ComponentFixture<ReferralComponent>;
+  const params = new BehaviorSubject({ type: 'id' });
+  const activatedRouteStub: Partial<ActivatedRoute> = {
+    params,
+    queryParams: of({ id: testId })
+  };
+
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -21,7 +29,8 @@ describe('ReferralComponent', () => {
         TranslateModule.forRoot()
       ],
       providers: [
-        { provide: ICampaignService, useValue: campaignServiceStub }
+        { provide: ICampaignService, useValue: campaignServiceStub },
+        { provide: ActivatedRoute, useValue: activatedRouteStub }
       ]
     })
       .compileComponents();
