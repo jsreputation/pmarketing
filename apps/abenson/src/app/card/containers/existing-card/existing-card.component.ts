d82b055e3daead1dbc5549d81299a1f0f4aa94db
reynard <reynard93@gmail.com>
2020-11-26 17:37:51 +0800

    slight refactor

--- apps/abenson/src/app/card/containers/existing-card/existing-card.component.ts
+++ apps/abenson/src/app/card/containers/existing-card/existing-card.component.ts
@@ -82,11 +82,9 @@ export class ExistingCardComponent implements OnInit {
               this.notificationService.addSnack('Invalid card number.');
             } else if (err.status === 404) {
               this.notificationService.addSnack('Card number not found.');
+            } else if (err.error && err.error.message) {
+              this.notificationService.addSnack(err.error.message);
             } else {
-              if (err.error && err.error.message) {
-                this.notificationService.addSnack(err.error.message);
-                return;
-              }
               this.notificationService.addSnack(err.statusText);
             }
           }
