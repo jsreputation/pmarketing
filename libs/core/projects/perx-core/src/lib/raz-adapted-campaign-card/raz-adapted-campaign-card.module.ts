2b58a69643b04d8f8cbfae49fc9036083e8876df
reynard <reynard93@gmail.com>
2020-12-17 17:33:24 +0800

    VS-5095/ getting started pipes, campaign card component and exporting components

---
+++ libs/core/projects/perx-core/src/lib/raz-adapted-campaign-card/raz-adapted-campaign-card.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RazAdaptedCampaignCardComponent } from './raz-adapted-campaign-card.component';
+import { ProgressBarModule } from '../progress-bar/progress-bar.module';
+import { MatCardModule } from '@angular/material';
+
+@NgModule({
+  declarations: [
+    RazAdaptedCampaignCardComponent
+  ],
+  exports: [
+    RazAdaptedCampaignCardComponent
+  ],
+  imports: [
+    CommonModule,
+    ProgressBarModule,
+    CommonModule,
+    MatCardModule,
+  ]
+})
+export class RazAdaptedCampaignCardModule { }
