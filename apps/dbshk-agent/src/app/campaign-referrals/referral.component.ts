432ebb49d6d01f38cc1cc6accef363a95963c5b8
Ryane Chia <ryanechia@outlook.com>
2020-12-18 16:01:17 +0800

    VS-4955: update dbshk share URL hardcode link

--- apps/dbshk-agent/src/app/campaign-referrals/referral.component.ts
+++ apps/dbshk-agent/src/app/campaign-referrals/referral.component.ts
@@ -27,7 +27,7 @@ export class ReferralComponent {
   // todo to be replaced with the proper content when api is available
   public shareTitle: string;
   // todo to be replaced with the proper content when api is available
-  public shareUrl: string = 'https://retailbank.hsbc.com.hk/ins/';
+  public shareUrl: string = 'https://landing.dbshk.static.perxtech.net';
   public copyToClipboardTxt: string;
   public clipboardErrorTxt: string;
   public popupTitle: string;
