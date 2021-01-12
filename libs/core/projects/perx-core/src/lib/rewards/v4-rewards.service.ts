9104bb674ecb213806a2180085ce7f2d06e15747
Ryane Chia <ryanechia@outlook.com>
2021-01-12 18:42:06 +0800

    AI-200: line too long lint

--- libs/core/projects/perx-core/src/lib/rewards/v4-rewards.service.ts
+++ libs/core/projects/perx-core/src/lib/rewards/v4-rewards.service.ts
@@ -179,7 +179,9 @@ export class V4RewardsService extends RewardsService {
       rewardTotalLimit: v4Invent.reward_total_limit !== undefined ? v4Invent.reward_total_limit : null,
       rewardLimitPerUserBalance: v4Invent.reward_limit_per_user_balance !== undefined && v4Invent.reward_limit_per_user_balance !== null ?
         v4Invent.reward_limit_per_user_balance.available_amount : null,
-      rewardLimitPerUserPerPeriodBalance: v4Invent.reward_limit_per_user_per_period !== undefined && v4Invent.reward_limit_per_user_per_period_balance !== null ?
+      rewardLimitPerUserPerPeriodBalance:
+        v4Invent.reward_limit_per_user_per_period !== undefined &&
+        v4Invent.reward_limit_per_user_per_period_balance !== null ?
         v4Invent.reward_limit_per_user_per_period_balance.available_amount : null,
     } : undefined;
     return {
