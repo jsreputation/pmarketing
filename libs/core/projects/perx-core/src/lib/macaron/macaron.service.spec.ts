9d4b49990b968606e50eb0ac28491553151fb065
Ryane Chia <ryanechia@outlook.com>
2021-02-22 15:46:11 +0800

    VS-5473: add translation service stub

--- libs/core/projects/perx-core/src/lib/macaron/macaron.service.spec.ts
+++ libs/core/projects/perx-core/src/lib/macaron/macaron.service.spec.ts
@@ -1,9 +1,20 @@
 import { TestBed } from '@angular/core/testing';
 
 import { MacaronService } from './macaron.service';
+import { TranslateService } from '@ngx-translate/core';
+import { of } from 'rxjs';
 
 describe('MacaronService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+
+  const translateServiceStub: Partial<TranslateService> = {
+    get: () => of()
+  };
+
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      { provide: TranslateService, useValue: translateServiceStub }
+    ]
+  }));
 
   it('should be created', () => {
     const service: MacaronService = TestBed.get(MacaronService);
