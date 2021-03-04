d77f0eccfeedfce2a659b1b133243dbab208cd80
Awad Maharoof <awadmaharoof@gmail.com>
2021-03-04 12:24:32 +0800

    [STAR-460] Enable theme based on env variable

--- apps/starhub/src/main.ts
+++ apps/starhub/src/main.ts
@@ -8,5 +8,9 @@ if (environment.production) {
   enableProdMode();
 }
 
+if (environment.newTheme) {
+  document.body.classList.add('electric-green');
+}
+
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch((err: any) => console.error(err));
