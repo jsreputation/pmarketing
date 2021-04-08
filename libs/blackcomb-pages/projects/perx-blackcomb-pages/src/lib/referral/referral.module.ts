976ca99d34b1f81a1f78b4540b7d1106ea4bbd5a
Awad Maharoof <awadmaharoof@gmail.com>
2021-04-08 13:47:23 +0800

    VS-5943: Add misssing provider

--- libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/referral/referral.module.ts
+++ libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/referral/referral.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material';
 import { TranslateModule } from '@ngx-translate/core';
-import { CampaignServiceModule, UtilsModule } from '@perxtech/core';
+import { CampaignServiceModule, ConfigService, IRankService, rankServiceFactory, UtilsModule } from '@perxtech/core';
 import { ReferralComponent } from './referral.component';
 import { MatButtonModule } from '@angular/material/button';
+import { HttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [ReferralComponent],
@@ -17,5 +18,12 @@ import { MatButtonModule } from '@angular/material/button';
     UtilsModule,
     CampaignServiceModule.forChild(),
   ],
+  providers: [
+    {
+      provide: IRankService,
+      useFactory: rankServiceFactory,
+      deps: [HttpClient, ConfigService]
+    }
+  ]
 })
-export class ReferralModule {}
+export class ReferralModule { }
