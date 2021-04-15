477b70a385d553614c4465a05f63a2ceca9f0ece
Ryane Chia <ryanechia@outlook.com>
2021-04-15 21:53:29 +0800

    TRAN-14: port BCP signup module to transcycle

--- apps/transcycle/src/app/sign-up/sign-up.module.ts
+++ apps/transcycle/src/app/sign-up/sign-up.module.ts
@@ -3,20 +3,61 @@ import {
   RouterModule,
   Routes
 } from '@angular/router';
+import { SignUpComponent } from './sign-up.component';
+import { CommonModule } from '@angular/common';
 import {
-  SignUpComponent,
-  SignUpModule as BCPSignUpModule
-} from '@perxtech/blackcomb-pages';
+  FormsModule,
+  ReactiveFormsModule
+} from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
+import {
+  AuthenticationModule,
+  ConfigModule,
+  GameModule,
+  SettingsModule,
+  SurveyModule,
+  UtilsModule
+} from '@perxtech/core';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatProgressSpinnerModule,
+  MatSelectModule,
+  MatSnackBarModule
+} from '@angular/material';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: SignUpComponent },
   { path: '*', redirectTo: '' }
 ];
 
+
 @NgModule({
+  declarations: [SignUpComponent],
+  exports: [SignUpComponent],
   imports: [
-    BCPSignUpModule,
-    RouterModule.forChild(routes)
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    RouterModule,
+    TranslateModule.forChild(),
+    RouterModule.forChild(routes),
+    AuthenticationModule,
+    SurveyModule,
+    GameModule,
+    UtilsModule,
+    ConfigModule.forChild(),
+    SettingsModule,
+
+    MatSnackBarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatProgressSpinnerModule,
+    MatIconModule,
   ]
 })
 export class SignUpModule { }
