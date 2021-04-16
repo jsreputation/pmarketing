d2d3eb8db84e2961b5f8d88e82389ca1307d59e0
Ryane Chia <ryanechia@outlook.com>
2021-04-16 15:51:44 +0800

    TRAN-14: create a transcycle mock signup payload

---
+++ apps/transcycle/src/app/sign-up/sign-up.mock.ts
@@ -0,0 +1,13 @@
+import { ISignUpData } from '@perxtech/core';
+
+export const transcycleSignUpDataMock: ISignUpData = {
+  lastName: 'Doe',
+  birthDay: '2021-04-06T00:00:00+08:00', // ISO date
+  identifier: '1',
+  phone: '22',
+  password: 'pass',
+  passwordConfirmation: 'pass',
+  customProperties: {
+    engine_number: '1ENG'
+  }
+}
