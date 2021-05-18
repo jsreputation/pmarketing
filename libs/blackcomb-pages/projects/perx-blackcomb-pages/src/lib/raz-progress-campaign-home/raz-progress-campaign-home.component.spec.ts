8113914b0facd58e521a75c23d20503eb7a83962
Ryane Chia <ryanechia@outlook.com>
2021-05-18 20:21:24 +0800

    update spec imports

--- libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/raz-progress-campaign-home/raz-progress-campaign-home.component.spec.ts
+++ libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/raz-progress-campaign-home/raz-progress-campaign-home.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { RazProgressCampaignHomeComponent } from './progress-campaign-home.component';
+import { RazProgressCampaignHomeComponent } from './raz-progress-campaign-home.component';
 import { PageComponentsModule } from '../page-components/page-components.module';
 import { RouterTestingModule } from '@angular/router/testing';
 import {
