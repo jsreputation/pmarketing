ba7396b4ca249a653822d7392707057f0d19ca82
Ryane Chia <ryanechia@outlook.com>
2021-05-20 00:56:23 +0800

    VS-6205: update mock content to be as expected from dashboard

--- libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/mock/campaigns.mock.ts
+++ libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/mock/campaigns.mock.ts
@@ -61,7 +61,7 @@ export const campaigns: ICampaign[] = [
     displayProperties: {
       questDetails: {
         title: 'How far can you go?',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        description: 'Estimated time to complete - more than enough',
         body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         imageUrl: 'https://picsum.photos/300/200?random=3',
         successImageUrl: 'https://picsum.photos/300/200?random=2'
