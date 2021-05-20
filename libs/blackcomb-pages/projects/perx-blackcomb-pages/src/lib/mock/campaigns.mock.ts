cf773e6201bfe79010fab6fbed016fd083fbc715
Ryane Chia <ryanechia@outlook.com>
2021-05-20 19:29:29 +0800

    VS-6205: temporarily hard code success icon

--- libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/mock/campaigns.mock.ts
+++ libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/mock/campaigns.mock.ts
@@ -66,7 +66,7 @@ export const campaigns: ICampaign[] = [
         description: 'Estimated time to complete - more than enough',
         body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         imageUrl: 'https://picsum.photos/300/200?random=3',
-        successImageUrl: 'https://picsum.photos/300/200?random=2'
+        successImageUrl: 'assets/done_current.png'
       }
     },
     enrolled: true
