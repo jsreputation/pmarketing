64010edd69d75b7c8ffc43c2bf2fdc7880f49585
Awad Maharoof <awadmaharoof@gmail.com>
2021-05-25 11:09:02 +0800

    VS-6030: Wireup navigation for badge detail popup

--- libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/badges/badge-list/badge-list.component.ts
+++ libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/badges/badge-list/badge-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { BadgeDetailPopupComponent, IBadge } from '@perxtech/core';
 import { Observable } from 'rxjs';
@@ -12,7 +13,7 @@ import { Observable } from 'rxjs';
 export class BadgeListComponent {
   @Input() public badges: Observable<IBadge[]>;
 
-  constructor(private dialog: MatDialog, private translate: TranslateService) { }
+  constructor(private dialog: MatDialog, private translate: TranslateService, private router: Router) { }
 
   public viewBadgeDetail(badge: IBadge): void {
     this.translate.get('BADGES.VIEW_WALLET').subscribe(viewWallet => {
@@ -22,10 +23,15 @@ export class BadgeListComponent {
           imageUrl: badge.image.value.image_url,
           description: badge.description,
           buttonTxt: viewWallet,
-          active: badge.active
+          active: badge.active,
+          afterClosedCallBack: this
         },
         minWidth: '35.5rem'
       });
     });
+  }
+
+  public dialogClosed(): void {
+    this.router.navigate(['/wallet']);
   }
 }
