112013a436f2325bc41c3fa8499b691cd281d373
Ryane Chia <ryanechia@outlook.com>
2021-05-31 21:26:41 +0800

    VS-6269: show operating hours on game cards

--- libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/home/games-collection/games-collection.component.ts
+++ libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/home/games-collection/games-collection.component.ts
@@ -1,7 +1,7 @@
 import { listAnimation } from './games-collection.animation';
 import { Component, Input, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ConfigService, IConfig, IGame, ITheme, ThemesService } from '@perxtech/core';
+import { ConfigService, IConfig, IGame, IOperatingHours, ITheme, ThemesService } from '@perxtech/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -52,5 +52,56 @@ export class GamesCollectionComponent implements OnInit {
     if(!this.isCampaignDisabled[game.id]) {
       this.router.navigate([ `/game/${game.campaignId}` ]);
     }
+  }
+
+  public getOperatingHours(operatingHours: IOperatingHours): string {
+    // Date obj that we only need the time from
+
+    const openTime: Date = new Date(operatingHours.opensAt);
+    const closeTime: Date = new Date(operatingHours.closesAt);
+
+    const daysMapArr = [ false, false, false, false, false, false, false ]; // index 0 is sunday
+
+    for (const dayIndex in operatingHours.days) {
+      if (dayIndex) { // guard-for-in
+        daysMapArr[operatingHours.days[dayIndex]] = true;
+      }
+    }
+    const days: string = this.dayArrToIntuitiveStringDayRange(daysMapArr);
+    const hours: string =
+      `${openTime.getHours()}:${openTime.getMinutes()} - ${closeTime.getHours()}:${closeTime.getMinutes()}`;
+    return `Campaign available during: ${days}, ${hours}`;
+  }
+
+  private dayOfWeekAsString(dayIndex: number): string {
+    return [ 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat' ][dayIndex];
+  }
+
+  // works but can't wrap sat and sun
+  private dayArrToIntuitiveStringDayRange(daysMapArr: boolean[]): string {
+    let dayRange = '', multiDayRange = '';
+    let findingRange = false;
+
+    for (let i = 0; i <= daysMapArr.length; i++) {
+      if (daysMapArr[i]) {
+        if (dayRange.length > 0 && !findingRange) {
+          findingRange = true;
+        } else if (dayRange.length === 0) { // first item in current range.
+          dayRange = `${this.dayOfWeekAsString(i)}`;
+        }
+      } else if (dayRange.length > 0 && ! daysMapArr[i]) { // first part of range already identified
+        if (this.dayOfWeekAsString(i - 1) !== dayRange) {
+          dayRange = `${dayRange} - ${this.dayOfWeekAsString(i - 1)}`;
+        }
+        if (multiDayRange.length === 0) {
+          multiDayRange = dayRange;
+        } else {
+          multiDayRange = `${multiDayRange}, ${dayRange}`;
+        }
+        dayRange = ''; // reset for more ranges;
+        findingRange = false;
+      }
+    }
+    return multiDayRange;
   }
 }
