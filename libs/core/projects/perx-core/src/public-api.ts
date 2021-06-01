692dac8ad2ce1c93b4403f8be8571889472c32df
Amrutha Mohan <75286912+amrutha-mohan-1@users.noreply.github.com>
2021-06-01 16:50:58 +0800

    fix for failed test job

--- libs/core/projects/perx-core/src/public-api.ts
+++ libs/core/projects/perx-core/src/public-api.ts
@@ -162,8 +162,7 @@ export {
   IPurchaseTransactionHistory,
   TransactionDetailType,
   IJoinMethod,
-  IGameTransactionHistory,
-  IStampTransactionHistory
+  IGameTransactionHistory
 } from './lib/loyalty/models/loyalty.model';
 export { TransactionPipe } from './lib/loyalty/loyalty-transactions-list/transaction.pipe';
 export { CashbackTransactionPipe } from './lib/loyalty/loyalty-transactions-list/cashback-transaction.pipe';
