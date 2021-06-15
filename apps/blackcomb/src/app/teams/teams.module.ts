a49e4c8408e51ccd364f5879cbeca04b9b8293f2
Ryane Chia <ryanechia@outlook.com>
2021-06-15 21:51:38 +0800

    VS-6390: add routes for teams to blackcomb layout router

---
+++ apps/blackcomb/src/app/teams/teams.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import {
+  CreateTeamComponent,
+  JoinTeamComponent,
+  PendingTeamComponent,
+  TeamsModule as BCPTeamsModule
+} from '@perxtech/blackcomb-pages';
+
+const routes: Routes = [
+  {
+    path: 'create',
+    component: CreateTeamComponent
+  },
+  {
+    path: 'join',
+    component: JoinTeamComponent
+  },
+  {
+    path: 'pending',
+    component: PendingTeamComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'create'
+  },
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BCPTeamsModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class TeamsModule {
+}
