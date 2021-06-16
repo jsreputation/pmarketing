eb12d95f38e507649ddb4c20a1824c01ae777ba1
Ryane Chia <ryanechia@outlook.com>
2021-06-16 21:14:04 +0800

    VS-6390: add modules and stubs for teams specs

--- libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/teams/pending-team/pending-team.component.spec.ts
+++ libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/teams/pending-team/pending-team.component.spec.ts
@@ -1,14 +1,41 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PendingTeamComponent } from './pending-team.component';
+import { ICampaignService, UtilsModule } from '@perxtech/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { TranslateModule } from '@ngx-translate/core';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
+import { of } from 'rxjs';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatIconModule } from '@angular/material/icon';
 
 describe('PendingTeamComponent', () => {
   let component: PendingTeamComponent;
   let fixture: ComponentFixture<PendingTeamComponent>;
 
+  const campaignServiceStub: Partial<ICampaignService> = {
+    getCampaigns: () => of([]),
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PendingTeamComponent ]
+      declarations: [ PendingTeamComponent ],
+      imports: [
+        UtilsModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatProgressBarModule,
+        MatIconModule,
+        TranslateModule.forRoot()
+      ],
+      providers: [
+        { provide: ICampaignService, useValue: campaignServiceStub },
+        { provide: ActivatedRoute, useValue: { paramMap: of(convertToParamMap({ id: '1' })) } }
+      ]
     })
     .compileComponents();
   });
