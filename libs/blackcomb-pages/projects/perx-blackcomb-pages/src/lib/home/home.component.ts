db5755c3849524499dd3b40cea86896ae0bea45f
Ryane Chia <ryanechia@outlook.com>
2021-06-16 17:58:29 +0800

    VS-6390: stamp teamsize will not be null if in team mode

--- libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/home/home.component.ts
+++ libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/home/home.component.ts
@@ -241,7 +241,7 @@ export class HomeComponent implements OnInit, OnDestroy {
       return;
     }
 
-    if (campaign.type === 'stamp' && !campaign.teamSize) {
+    if (campaign.type === 'stamp' && campaign.teamSize) {
       this.router.navigate([`campaign-welcome/${campaign.id}`]);
       return;
     }
