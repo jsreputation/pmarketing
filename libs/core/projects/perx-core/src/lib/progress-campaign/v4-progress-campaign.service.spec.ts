9043252e481a536f876467929b062e54d87909b4
Ryane Chia <ryanechia@outlook.com>
2021-06-21 20:40:00 +0800

    VS-6480: add spec minimum modules for progress campaign service

--- libs/core/projects/perx-core/src/lib/progress-campaign/v4-progress-campaign.service.spec.ts
+++ libs/core/projects/perx-core/src/lib/progress-campaign/v4-progress-campaign.service.spec.ts
@@ -1,12 +1,26 @@
 import { TestBed } from '@angular/core/testing';
 
 import { V4ProgressCampaignService } from './v4-progress-campaign.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ConfigService } from '@perxtech/core';
+import { of } from 'rxjs';
 
 describe('V4ProgressCampaignService', () => {
   let service: V4ProgressCampaignService;
 
+  const configServiceStub: Partial<ConfigService> = {
+    readAppConfig: () => of()
+  };
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+      ],
+      providers: [
+        { provide: ConfigService, useValue: configServiceStub },
+      ]
+    });
     service = TestBed.inject(V4ProgressCampaignService);
   });
 
