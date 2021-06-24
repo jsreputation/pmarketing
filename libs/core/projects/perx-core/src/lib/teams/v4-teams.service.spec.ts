9c66b0ea72f325c5ef209f1c50d89ebe5c9e71e2
Ryane Chia <ryanechia@outlook.com>
2021-06-24 23:07:35 +0800

    VS-6391: add providers for teams service spec

--- libs/core/projects/perx-core/src/lib/teams/v4-teams.service.spec.ts
+++ libs/core/projects/perx-core/src/lib/teams/v4-teams.service.spec.ts
@@ -1,12 +1,25 @@
 import { TestBed } from '@angular/core/testing';
 
 import { V4TeamsService } from './v4-teams.service';
+import { ConfigService } from '@perxtech/core';
+import { of } from 'rxjs';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('V4TeamsService', () => {
   let service: V4TeamsService;
 
+  const configServiceStub: Partial<ConfigService> = {
+    readAppConfig: () => of()
+  };
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+      ],
+      providers: [
+        { provide: ConfigService, useValue: configServiceStub },
+      ]
+    });
     service = TestBed.inject(V4TeamsService);
   });
 
