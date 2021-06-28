84cec4a38e3b784a161b844f24c31c1033be6482
Truong Vo Nhat <truong.vo@saigontechnology.com>
2021-06-28 12:53:43 +0700

    [STAR-541] prevent the quiz question refreshes the count down timer

--- libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/quiz/quiz.component.ts
+++ libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/quiz/quiz.component.ts
@@ -8,7 +8,7 @@ import {
   ViewChild
 } from '@angular/core';
 import {
-  ActivatedRoute,
+  ActivatedRoute, NavigationStart,
   ParamMap,
   Router
 } from '@angular/router';
@@ -36,7 +36,7 @@ import {
   iif,
   Observable,
   of,
-  Subject,
+  Subject, Subscription,
   throwError
 } from 'rxjs';
 import {
@@ -48,6 +48,8 @@ import {
   tap
 } from 'rxjs/operators';
 import { TranslateService } from '@ngx-translate/core';
+
+export const browserRefresh = false;
 
 @Component({
   selector: 'perx-blackcomb-quiz',
@@ -87,6 +89,8 @@ export class QuizComponent implements OnInit, OnDestroy {
   };
   private submitErrorTxt: string;
   public disableNextButton: boolean = true;
+  public subscription: Subscription;
+  public isRefreshBrowser: boolean = false;
 
   constructor(
     private router: Router,
@@ -99,6 +103,12 @@ export class QuizComponent implements OnInit, OnDestroy {
     private translate: TranslateService
   ) {
     this.hideArrow = this.hideArrow.bind(this);
+
+    window.addEventListener('beforeunload',  (e: BeforeUnloadEvent) => {
+      const confirmationMessage = 'Do you wanna refresh!';
+      e.returnValue = confirmationMessage;
+      return confirmationMessage;
+    });
   }
 
   public ngOnInit(): void {
