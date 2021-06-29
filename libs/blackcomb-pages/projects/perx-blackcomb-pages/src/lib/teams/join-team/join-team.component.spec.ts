bd66a630ba1e2d2afa20ef7adb5385e65af4f351
Ryane Chia <ryanechia@outlook.com>
2021-06-29 12:19:00 +0800

    VS-6392: add jointeampage stubs and providers

--- libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/teams/join-team/join-team.component.spec.ts
+++ libs/blackcomb-pages/projects/perx-blackcomb-pages/src/lib/teams/join-team/join-team.component.spec.ts
@@ -1,13 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { JoinTeamComponent } from './join-team.component';
-import { ICampaignService, UtilsModule } from '@perxtech/core';
+import { ICampaignService, TeamsService, UtilsModule } from '@perxtech/core';
 import { of } from 'rxjs';
 import { ActivatedRoute, convertToParamMap } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { TranslateModule } from '@ngx-translate/core';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('JoinTeamComponent', () => {
   let component: JoinTeamComponent;
@@ -15,6 +16,9 @@ describe('JoinTeamComponent', () => {
 
   const campaignServiceStub: Partial<ICampaignService> = {
     getCampaigns: () => of([]),
+  };
+  const teamsServiceStub: Partial<TeamsService> = {
+    joinATeamForCampaign: () => of()
   };
 
   beforeEach(async () => {
@@ -26,12 +30,14 @@ describe('JoinTeamComponent', () => {
         ReactiveFormsModule,
         MatFormFieldModule,
         MatInputModule,
+        RouterTestingModule,
         TranslateModule.forRoot()
 
       ],
       providers: [
         { provide: ICampaignService, useValue: campaignServiceStub },
         { provide: ActivatedRoute, useValue: { paramMap: of(convertToParamMap({ id: '1' })) } },
+        { provide: TeamsService, useValue: teamsServiceStub }
 
       ]
     })
