8af91d4315850b4b81fa3a43481af502646f952c
Amrutha Mohan <75286912+amrutha-mohan-1@users.noreply.github.com>
2021-07-08 12:31:09 +0800

    added unit tests for plinko

--- libs/core/projects/perx-core/src/lib/game/plinko/plinko.component.spec.ts
+++ libs/core/projects/perx-core/src/lib/game/plinko/plinko.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PlinkoComponent } from './plinko.component';
+import { PlinkoComponent, LOSE_PATHS, WIN_PATHS} from './plinko.component';
 
 describe('PlinkoComponent', () => {
   let component: PlinkoComponent;
@@ -21,5 +21,26 @@ describe('PlinkoComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should not start game animation if not enabled', () => {
+    component.startGame = false;
+    spyOn(component, 'finishedGame');
+    component.startGameAnimation();
+    expect(component.finishedGame).not.toHaveBeenCalled();
+  });
+
+  it('should animation path taken from win path if willWin is true', () => {
+    component.startGame = true;
+    component.willWin = true;
+    component.startGameAnimation();
+    expect(WIN_PATHS).toContain(component.ballPath);
+  });
+
+  it('should animation path taken from lose path if willWin is false', () => {
+    component.startGame = true;
+    component.willWin = false;
+    component.startGameAnimation();
+    expect(LOSE_PATHS).toContain(component.ballPath);
   });
 });
