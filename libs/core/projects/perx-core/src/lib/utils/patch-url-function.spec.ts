136fa76e0a783713e41d27c17193fc7f375d32fd
Truong Vo Nhat <truong.vo@saigontechnology.com>
2021-07-28 16:17:33 +0700

    [VS-6696] fix the github test stage error continue

--- libs/core/projects/perx-core/src/lib/utils/patch-url-function.spec.ts
+++ libs/core/projects/perx-core/src/lib/utils/patch-url-function.spec.ts
@@ -4,7 +4,7 @@ describe('patchUrl', () => {
   it('should patch url properly', () => {
     [
       { input: 'https://cdn.perxtech.io/bla', output: 'https://cdn.perxtech.io/bla' },
-      { input: 'http://cdn.perxtech.net/bla', output: 'https://cdn.perxtech.net/bla' },
+      { input: 'https://cdn.perxtech.net/bla', output: 'https://cdn.perxtech.net/bla' },
       { input: 'https://cdn.perxtech.io/bla', output: 'https://cdn.perxtech.io/bla' },
       { input: 'https://cdn.perxtech.net/bla', output: 'https://cdn.perxtech.net/bla' }
     ].forEach((ca) => {
