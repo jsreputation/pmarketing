edca44ec1954bb936ed3f3df09d80c242e55dcd6
Ryane Chia <ryanechia@outlook.com>
2021-09-01 01:30:23 +0800

    MER-296: make merck-merchant app toolbar dashboard configurable

--- apps/merck-merchant/src/app/header/header.component.ts
+++ apps/merck-merchant/src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Location } from '@angular/common';
+import { ITheme, ThemesService } from '@perxtech/core';
 
 @Component({
   selector: 'app-header',
@@ -10,10 +11,18 @@ export class HeaderComponent implements OnInit {
 
   @Input()
   public showBackButton: boolean = false;
+  public theme: ITheme;
 
-  constructor(private location: Location) {}
+  constructor(
+    private location: Location,
+    private themesService: ThemesService,
+  ) {}
 
-  public ngOnInit(): void {}
+  public ngOnInit(): void {
+    this.themesService.getThemeSetting().subscribe((theme) => {
+      this.theme = theme;
+    });
+  }
 
   public onLeftActionClick(): void {
     this.location.back();
