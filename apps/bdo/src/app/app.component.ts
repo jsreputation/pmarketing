a16bf7ac8c6d11a66f76da59ae8564da0ba2e8f8
Ryane Chia <ryanechia@outlook.com>
2021-09-10 23:39:13 +0800

    BDO-17: bdo app skeleton

---
+++ apps/bdo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'perxtech-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'bdo';
+}
