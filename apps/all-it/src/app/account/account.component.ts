969897b60f698cda1e9a5eba14ef2d2e8658cb82
Ryane Chia <ryanechia@outlook.com>
2021-09-20 21:59:54 +0800

    AI-306: hollow out all-it extension for account page

--- apps/all-it/src/app/account/account.component.ts
+++ apps/all-it/src/app/account/account.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { take, tap, map, filter } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 
 import { TranslateService } from '@ngx-translate/core';
@@ -13,7 +12,6 @@ import {
   IConfig,
   AuthenticationService,
   Config,
-  PagesObject,
   AccountPageObject,
   ITheme,
   ThemesService,
@@ -31,7 +29,7 @@ import { AccountComponent as BCPAccountComponent} from '@perxtech/blackcomb-page
   changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrls: ['./account.component.scss'],
 })
-export class AccountComponent extends BCPAccountComponent implements OnInit {
+export class AccountComponent extends BCPAccountComponent {
   public profile$: Observable<IProfile> | null = null;
   public loyalty$: Observable<ILoyalty>;
   public pages!: AccountPageObject[];
@@ -69,48 +67,5 @@ export class AccountComponent extends BCPAccountComponent implements OnInit {
     );
     this.preAuth = config.preAuth || false;
 
-  }
-
-  public ngOnInit(): void {
-    this.theme = this.themesService.getThemeSetting();
-    this.settingsService
-      .getAccountSettings()
-      .pipe(
-        map((settings: PagesObject) => settings.pages),
-        tap((pages: AccountPageObject[]) => (this.pages = pages)),
-        filter((pages: AccountPageObject[]) => pages.length > 0)
-      )
-      .subscribe();
-    this.settingsService.getRemoteFlagsSettings().subscribe((flags: IFlags) => {
-      this.remoteFlags = flags;
-    });
-    this.appConfig$ = this.configService.readAppConfig();
-    this.profile$ = this.profileService.whoAmI().pipe(take(1));
-    this.loyalty$ = this.loyaltyService.getLoyalty();
-    this.memberFn = (membershipTierName: string) =>
-      this.translate
-        .get([membershipTierName, 'HOME.MEMBER'])
-        .pipe(map((res) => `${res[membershipTierName]}${res['HOME.MEMBER']}`));
-    this.getAchievedBadgeCount();
-  }
-
-  public logout(): void {
-    this.authenticationService.logout();
-    this.router.navigate(['/login']);
-  }
-
-  public goToPage(page: AccountPageObject): void {
-    if (page.key) {
-      this.router.navigate(['/c', page.key]);
-    } else {
-      const a = document.createElement('a');
-      a.href = page.content_url;
-      a.target = '_blank';
-      a.click();
-    }
-  }
-
-  protected getAchievedBadgeCount(): void {
-    this.acquiredBadges = this.badgeService.getAchievedBadgeCount();
   }
 }
