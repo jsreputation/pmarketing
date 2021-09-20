e94425b8e3362c6b19d9c2e017a1fcf4ce711d42
Ryane Chia <ryanechia@outlook.com>
2021-09-20 22:39:17 +0800

    AI-306: use a popup to show renew membership UX in all-it

--- apps/all-it/src/app/account/account.component.ts
+++ apps/all-it/src/app/account/account.component.ts
@@ -1,7 +1,7 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { Observable } from 'rxjs';
+import { forkJoin, Observable } from 'rxjs';
 
 import { TranslateService } from '@ngx-translate/core';
 
@@ -19,9 +19,11 @@ import {
   ILoyalty,
   SettingsService,
   IFlags,
-  BadgeService,
+  BadgeService, ICustomProperties, NotificationService,
 } from '@perxtech/core';
 import { AccountComponent as BCPAccountComponent} from '@perxtech/blackcomb-pages';
+import { HttpClient } from '@angular/common/http';
+import { map, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'allit-account',
@@ -51,8 +53,9 @@ export class AccountComponent extends BCPAccountComponent {
      themesService: ThemesService,
      settingsService: SettingsService,
      badgeService: BadgeService,
+     private http: HttpClient,
+     private notificationService: NotificationService
   ) {
-
     super(
       config,
       profileService,
@@ -66,6 +69,59 @@ export class AccountComponent extends BCPAccountComponent {
       badgeService
     );
     this.preAuth = config.preAuth || false;
-
   }
+
+  public confirmRenewalPopup() {
+    this.notificationService.addPopup({
+      title: 'Renew Memebership',
+      text: 'Would you like to renew your membership using points?',
+      buttonTxt: 'Renew',
+      buttonTxt2: 'Cancel',
+      hideCloseButton: true,
+      afterClosedCallBack: {
+        dialogClosed: (): void => {
+          forkJoin([ this.loyalty$, this.appConfig$ ]).pipe(
+            switchMap(([ loyalty, appconfig ]) => this.postMembershipExtend(loyalty, appconfig))
+          ).subscribe(
+            (loyaltyTransaction: IV4LoyaltyTransaction) => {
+              this.notificationService.addPopup({
+                title: 'Success',
+                text: `Thank you! Your membership has been extended using ${Math.abs(loyaltyTransaction.points)} points`
+              });
+            },
+            () => {
+              this.notificationService.addPopup({
+                title: 'Failed',
+                text: `Sorry, could not process your request.`
+              });
+            }
+          );
+        }
+      }
+    });
+  }
+
+  private postMembershipExtend(loyalty: ILoyalty, appconfig: IConfig<void>): Observable<IV4LoyaltyTransaction>{
+    return this.http.post(
+      `${appconfig.apiHost}/v4/custom/allit/membership_accounts/extend`,
+      {
+        id: loyalty.id
+      }
+    ).pipe(
+      map( (res: IV4AllItMembershipExtendResponse) => res.data)
+
+    );
+  }
+}
+
+interface IV4AllItMembershipExtendResponse {
+  data: IV4LoyaltyTransaction;
+}
+
+interface IV4LoyaltyTransaction {
+  id: number;
+  transacted_at: Date;
+  properties: ICustomProperties;
+  points: number;
+  loyalty_program_id: number;
 }
