04e177d5cc8365c713308c1d2368e570168eca88
Ryane Chia <ryanechia@outlook.com>
2021-09-30 21:30:42 +0800

    BDO-53: open route for /login with auth guard

--- apps/bdo/src/app/app-routing.module.ts
+++ apps/bdo/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PublicGuard } from 'ngx-auth';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./main/main.module').then(mod => mod.MainModule) },
+  {
+    path: '', loadChildren: () => import('./main/main.module').then(mod => mod.MainModule),
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then((mod) => mod.LoginModule),
+    canActivate: [PublicGuard]
+  },
 ];
 
 @NgModule({
