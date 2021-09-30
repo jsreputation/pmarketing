ad815540332b126c0a83ae0542359dfc8d394b84
Ryane Chia <ryanechia@outlook.com>
2021-09-30 22:07:17 +0800

    BDO-53: port BCP forgot password page over

--- apps/bdo/src/app/app-routing.module.ts
+++ apps/bdo/src/app/app-routing.module.ts
@@ -16,6 +16,11 @@ const routes: Routes = [
     loadChildren: () => import('./sign-up/sign-up.module').then((mod) => mod.SignUpModule),
     canActivate: [PublicGuard]
   },
+  {
+    path: 'forgot-password',
+    loadChildren: () => import('./forgot-password/forgot-password.module').then((mod) => mod.ForgotPasswordModule),
+    canActivate: [PublicGuard]
+  },
 ];
 
 @NgModule({
