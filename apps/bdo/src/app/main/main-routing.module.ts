487bd27121ef566b522122a15f2da639362b9a36
Tan.Vo <tan.vo@perxtech.com>
2021-10-05 10:25:50 +0700

    BDO-36: Nearby deals page

--- apps/bdo/src/app/main/main-routing.module.ts
+++ apps/bdo/src/app/main/main-routing.module.ts
@@ -4,24 +4,29 @@ import { MainComponent } from './main.component';
 import { HomeComponent } from './home/home.component';
 import { SearchComponent } from './search/search.component';
 import { DealLandingComponent } from './deal-landing/deal-landing.component';
+import { NearbyDealsComponent } from './nearby-deals/nearby-deals.component';
 import { TreatWelcomeComponent } from './treat-welcome/treat-welcome.component';
 import { TreatEnrollPageComponent } from './treat-enroll-page/treat-enroll-page.component';
 
 const routes: Routes = [
   {
-    path: '', component: MainComponent, children: [
+    path: '',
+    component: MainComponent,
+    children: [
       { path: '', pathMatch: 'full', redirectTo: 'home' },
       { path: 'home', component: HomeComponent },
-      { path: 'search/:text', component: SearchComponent},
-      { path: 'deal-welcome/:rid', component: DealLandingComponent},
-      { path: 'treat-welcome/:id', component: TreatWelcomeComponent},
-      {path: 'treat-enroll/:id',component:TreatEnrollPageComponent}
-    ]
-  }
+      { path: 'search/:text', component: SearchComponent },
+      { path: 'deal-welcome/:rid', component: DealLandingComponent },
+      { path: 'treat-welcome/:id', component: TreatWelcomeComponent },
+      { path: 'treat-enroll/:id',component:TreatEnrollPageComponent },
+      { path: 'deal-welcome/:rid', component: DealLandingComponent },
+      { path: 'nearby-deals', component: NearbyDealsComponent },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class MainRoutingModule { }
+export class MainRoutingModule {}
---
+++ apps/bdo/src/app/mock-data/location.mock.ts
@@ -0,0 +1,22 @@
+import { IVoucherLocation } from '@perxtech/core';
+
+export const LOCATIONS: IVoucherLocation[] = [
+  {
+    id: 1,
+    name: '40% OFF at New World Makati Hotel',
+    latitude: '1.39188',
+    longitude: '103.87504'
+  },
+  {
+    id: 2,
+    name: 'Win up to P5,000 Shopping Money at The SM Store',
+    latitude: '1.40000',
+    longitude: '103.76004'
+  },
+  {
+    id: 3,
+    name: 'Win up to P5,000 Shopping Money at The SM Store',
+    latitude: '1.90000',
+    longitude: '106.76004'
+  }
+];
