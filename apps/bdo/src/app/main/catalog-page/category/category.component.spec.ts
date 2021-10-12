e8bf753a21280f82ebaaccaef85f5066b2d4b7d3
DuyCao <duy.cao@saigontechnology.com>
2021-10-12 16:36:47 +0700

    BDO-26 update unit test category page

--- apps/bdo/src/app/main/catalog-page/category/category.component.spec.ts
+++ apps/bdo/src/app/main/catalog-page/category/category.component.spec.ts
@@ -1,12 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { APP_BASE_HREF } from '@angular/common';
 import { CategoryComponent } from './category.component';
-// import {MatSelectModule} from '@angular/material/select';
-// import { MatExpansionModule } from '@angular/material/expansion';
-
-// import { MatToolbarModule } from '@angular/material/toolbar';
-// import { MatSidenavModule } from '@angular/material/sidenav';
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 describe('SortComponent', () => {
   let component: CategoryComponent;
   let fixture: ComponentFixture<CategoryComponent>;
@@ -16,11 +10,6 @@ describe('SortComponent', () => {
         CategoryComponent
       ],
       imports: [
-        // MatSelectModule,
-        // BrowserAnimationsModule,
-        // MatToolbarModule,
-        // MatSidenavModule,
-        // MatExpansionModule,
       ],
       providers: [
         {
