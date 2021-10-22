9591e15c6c25a1b3e0cd7b7e060a34ab48375d14
Tan.Vo <tan.vo@perxtech.com>
2021-10-22 16:52:51 +0700

    BDO-89: Add footer

--- apps/bdo/src/app/main/home/footer/footer.component.spec.ts
+++ apps/bdo/src/app/main/home/footer/footer.component.spec.ts
@@ -1,18 +1,14 @@
 import { RewardsService } from '@perxtech/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HomeComponent } from './home.component';
-import { PrimaryCatalogComponent } from './primary-catalog/primary-catalog.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MainRoutingModule } from '../main-routing.module';
+import { MainRoutingModule } from '../../main-routing.module';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { FeatureDealsComponent } from './featured-deals/featured-deals.component';
-import { SecondaryCatalogComponent } from './secondary-catalog/secondary-catalog.component';
 import { MatIconModule } from '@angular/material/icon';
-import { TaggedItemComponent } from '../../shared/components/tagged-item/tagged-item.component';
+import { TaggedItemComponent } from '../../../shared/components/tagged-item/tagged-item.component';
 import { Router } from '@angular/router';
 import { of } from 'rxjs';
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from './footer.component';
 class MockRewardsService {
  getRewards() {
    return of();
@@ -21,19 +17,15 @@ class MockRewardsService {
 class MockRouter {
   navigate() {}
 }
-describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+describe('FooterComponent', () => {
+  let component: FooterComponent;
+  let fixture: ComponentFixture<FooterComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        HomeComponent,
-        PrimaryCatalogComponent,
-        SecondaryCatalogComponent,
-        FeatureDealsComponent,
-        TaggedItemComponent,
-        FooterComponent
+        FooterComponent,
+        TaggedItemComponent
       ],
       imports: [
         MainRoutingModule,
@@ -49,7 +41,7 @@ describe('HomeComponent', () => {
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HomeComponent);
+    fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
