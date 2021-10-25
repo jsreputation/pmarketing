1e840376fabf205cbdb8080eae8f3fb21f9104f8
DuyCao <duy.cao@saigontechnology.com>
2021-10-25 16:04:30 +0700

    BDO-84 format code in treat welcome

--- apps/bdo/src/app/main/treat-welcome/treat-welcome.component.ts
+++ apps/bdo/src/app/main/treat-welcome/treat-welcome.component.ts
@@ -6,20 +6,20 @@ import { switchMap } from 'rxjs/operators';
 @Component({
   selector: 'bdo-treat-welcome',
   templateUrl: './treat-welcome.component.html',
-  styleUrls: ['./treat-welcome.component.scss']
+  styleUrls: ['./treat-welcome.component.scss'],
 })
-export class TreatWelcomeComponent implements OnInit{
-  lstDeal = [
-    1,2,3,4
-  ];
+export class TreatWelcomeComponent implements OnInit {
+  lstDeal = [1, 2, 3, 4];
   campaign: ICampaign;
-  constructor(private campaignService : ICampaignService,private activeRoute: ActivatedRoute){
-
+  constructor(
+    private campaignService: ICampaignService,
+    private activeRoute: ActivatedRoute
+  ) {}
+  ngOnInit() {
+    this.activeRoute.params
+      .pipe(switchMap((param) => this.campaignService.getCampaign(param.id)))
+      .subscribe((item) => {
+        this.campaign = item;
+      });
   }
-  ngOnInit(){
-    this.activeRoute.params.pipe(switchMap((param) => this.campaignService.getCampaign(param.id))).subscribe( item=>{
-      this.campaign = item;
-    });
-  }
-  
 }
