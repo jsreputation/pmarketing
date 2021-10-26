9f0711968dcb7c810066e3465153ba82c6fbaac3
Tan.Vo <tan.vo@perxtech.com>
2021-10-26 13:33:40 +0700

    BDO-93: Update unit test

--- apps/bdo/src/app/main/search/search.component.spec.ts
+++ apps/bdo/src/app/main/search/search.component.spec.ts
@@ -4,29 +4,27 @@ import { SearchComponent } from './search.component';
 import { of } from 'rxjs';
 import { RewardsService } from '@perxtech/core';
 import { Router } from '@angular/router';
-class MockRewardService {
-  getTrending() {
-    return of();
-  }
-}
+import { rewards } from './../../../../../starhub/src/app/rewards.mock';
 
-class MockRouter {
-  navigate(){
-    
-  }
-}
 describe('SearchComponent', () => {
 
   let component: SearchComponent;
   let fixture: ComponentFixture<SearchComponent>;
+  const rewardsServiceBdo: Partial<RewardsService> = {
+    getRewards: () => of(rewards),
+    getTrending: () => of([])
+  };
 
+  const routerBdo: Partial<Router> = {
+    navigate: () => Promise.resolve(true)
+  };
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ SearchComponent ],
       providers:
       [
-        { provide: RewardsService, useClass: MockRewardService },
-        { provide: Router, useClass: MockRouter }
+        { provide: RewardsService, useValue: rewardsServiceBdo},
+        { provide: Router, useValue:routerBdo}
       ]
     })
     .compileComponents();
