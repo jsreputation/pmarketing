7263f229d7859f980d5f7494d79a744a162239aa
Ryane Chia <ryanechia@outlook.com>
2021-10-28 00:11:00 +0800

    BDO-94: autoformat

--- apps/bdo/src/app/main/search/search.component.ts
+++ apps/bdo/src/app/main/search/search.component.ts
@@ -5,27 +5,26 @@ import { ITrending, RewardsService } from '@perxtech/core';
 @Component({
   selector: 'bdo-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: [ './search.component.scss' ]
 })
-export class SearchComponent implements OnInit{
+export class SearchComponent implements OnInit {
   public trendingList: string[] = [];
   public searchHistories: string[] = [];
 
-  constructor(private rewardsService: RewardsService, private route:Router) { }
+  constructor(private rewardsService: RewardsService, private route: Router) {
+  }
 
-  ngOnInit(): void {
-    this.rewardsService
-      .getTrending()
-      .subscribe((searchHistory: ITrending[]) => {
-        this.trendingList = searchHistory.filter(trending=>trending.value.trim()).map((item) => item.value);
-      });
-    this.rewardsService.getSearchHistory().subscribe((searchHistory: ISearchHistory[])=>{
-      this.searchHistories = searchHistory.map(item=> item.value);
+  public ngOnInit(): void {
+    this.rewardsService.getTrending().subscribe((searchHistory: ITrending[]) => {
+      this.trendingList = searchHistory.filter(trending => trending.value.trim()).map((item) => item.value);
+    });
+    this.rewardsService.getSearchHistory().subscribe((searchHistory: ISearchHistory[]) => {
+      this.searchHistories = searchHistory.map(item => item.value);
     });
   }
 
-  onClick(trending:string) {
+  public onClick(trending: string): void {
     const queryParams: Params = { search: trending };
-    this.route.navigate([`result`], { queryParams:queryParams });
+    this.route.navigate([ `result` ], { queryParams: queryParams });
   }
 }
