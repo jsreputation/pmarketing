355ddeecb24efb5a312643da4ed8f7c7a7714aed
Ryane Chia <ryanechia@outlook.com>
2021-11-09 03:33:29 +0800

    BDO-153: autoformat

--- apps/bdo/src/app/main/home/featured-deals/featured-deals.component.ts
+++ apps/bdo/src/app/main/home/featured-deals/featured-deals.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output} from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { IReward } from '@perxtech/core';
 import { mapRewardsToListItem } from '../../../shared/utilities/mapping.util';
 import { IListItemModel } from '../../../shared/models/list-item.model';
@@ -6,14 +6,14 @@ import { IListItemModel } from '../../../shared/models/list-item.model';
 @Component({
   selector: 'bdo-featured-deals',
   templateUrl: './featured-deals.component.html',
-  styleUrls: ['./featured-deals.component.scss']
+  styleUrls: [ './featured-deals.component.scss' ]
 })
 export class FeatureDealsComponent {
-  urlImageDefault= "assets/images/light-gray-color-default-image.png"
+  public urlImageDefault = 'assets/images/light-gray-color-default-image.png';
   @Input() deals: IReward[] = [];
   @Output() eventItemClick: EventEmitter<IListItemModel> = new EventEmitter<IListItemModel>();
- 
-  itemClick(featureDealsItem:IReward){
-    this.eventItemClick.emit(mapRewardsToListItem([featureDealsItem])[0]);
+
+  public itemClick(featureDealsItem: IReward): void {
+    this.eventItemClick.emit(mapRewardsToListItem([ featureDealsItem ])[0]);
   }
 }
