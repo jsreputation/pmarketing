0c301ea3f5c56753abe69c2ed1b84dc3eff61c3b
Ryane Chia <ryanechia@outlook.com>
2021-11-09 01:46:40 +0800

    BDO-165: use error message service to customise error message for enrol failure

--- apps/bdo/src/app/main/treat-enroll-page/treat-enroll-page.component.ts
+++ apps/bdo/src/app/main/treat-enroll-page/treat-enroll-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params, Router } from '@angular/router';
-import { ICampaign, ICampaignService, NotificationService } from '@perxtech/core';
+import { ErrorMessageService, ICampaign, ICampaignService, NotificationService } from '@perxtech/core';
 import { switchMap } from 'rxjs/operators';
 
 @Component({
@@ -17,7 +17,8 @@ export class TreatEnrollPageComponent implements OnInit {
     private campaignService: ICampaignService,
     private activeRoute: ActivatedRoute,
     private route: Router,
-    private notificationService: NotificationService
+    private notificationService: NotificationService,
+    private errorMessageService: ErrorMessageService
   ) {
   }
 
@@ -42,13 +43,25 @@ export class TreatEnrollPageComponent implements OnInit {
     if (this.enrollForm.valid) {
       this.campaignService
         .bdoCampaignEnrol(this.campaign.id, this.enrollForm.get('promoId').value)
-        .subscribe((item) => {
-          if (item) {
-            this.route.navigate([ `treat-enroll/${this.campaign.id}/complete` ], { state: { promoId: this.enrollForm.get('promoId').value } });
-          } else {
-            this.notificationService.addSnack('Could not enrol in campaign');
-          }
-        });
+        .subscribe(
+          (item) => {
+            if (item) {
+              this.route.navigate([ `treat-enroll/${this.campaign.id}/complete` ], { state: { promoId: this.enrollForm.get('promoId').value } });
+            } else {
+              this.notificationService.addSnack('Could not enrol in campaign');
+            }
+          },
+          ((err) => {
+            if (err.error) {
+              console.log(err);
+              this.errorMessageService.getErrorMessageByErrorCode(err.error.code, err.error.message, err.status)
+                .subscribe(
+                  (errMessage: string) => {
+                    this.notificationService.addSnack(errMessage);
+                  });
+            }
+          })
+        );
     } else {
       Object.keys(this.enrollForm.controls).forEach((controlName) =>
         this.enrollForm.controls[controlName].markAsTouched()
