546ff5d6ea24868e7623a2bc7a4aeb9ebcec5101
Tan.Vo <tan.vo@perxtech.com>
2021-11-12 13:24:31 +0700

    BDO-152: Map linked from Deals Details page should direct to an embedded webview instead of GMap

--- apps/bdo/src/app/main/main-routing.module.ts
+++ apps/bdo/src/app/main/main-routing.module.ts
@@ -11,6 +11,7 @@ import { TreatEnrollPageComponent } from './treat-enroll-page/treat-enroll-page.
 import { TreatEnrollCompletePageComponent } from './treat-enroll-complete-page/treat-enroll-complete-page.component';
 import { SearchComponent } from './search/search.component';
 import { DealLocationPageComponent } from './deal-landing/deal-location-page/deal-location-page.component';
+import { LocationLandingComponent } from './location-landing/location-landing.componen';
 
 const routes: Routes = [
   {
@@ -23,6 +24,7 @@ const routes: Routes = [
       { path: 'result', component: ResultComponent },
       { path: 'deal-welcome/:rid', component: DealLandingComponent },
       { path: 'deal-welcome/:rid/location', component: DealLocationPageComponent },
+      { path: 'deal-welcome/:rid/location/map', component: LocationLandingComponent },
       { path: 'treat-welcome/:id', component: TreatWelcomeComponent },
       { path: 'treat-enroll/:id',component:TreatEnrollPageComponent },
       { path: 'nearby', component: NearbyDealsComponent },
