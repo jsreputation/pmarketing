b47b4d1d40760f8764bfccf36c7535f0e4c1dc56
Ryane Chia <ryanechia@outlook.com>
2021-12-15 23:08:16 +0800

    BDO-271: remove non-applicable test

--- apps/bdo/src/app/main/home/primary-catalog/primary-catalog.component.spec.ts
+++ apps/bdo/src/app/main/home/primary-catalog/primary-catalog.component.spec.ts
@@ -1,8 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PrimaryCatalogComponent } from './primary-catalog.component';
-import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
-import { HOME_LIST_CATEGORY_CONFIGURATIONS } from '../../../shared/constants/home-category-configuration.const';
 
 describe('PrimaryCatalogComponent', () => {
   let component: PrimaryCatalogComponent;
@@ -19,18 +17,10 @@ describe('PrimaryCatalogComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PrimaryCatalogComponent);
     component = fixture.componentInstance;
-    component.categories = HOME_LIST_CATEGORY_CONFIGURATIONS;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
-
-  it('should display a list of input elements', () => {
-    component.categories = HOME_LIST_CATEGORY_CONFIGURATIONS;
-    fixture.detectChanges();
-    const elements = fixture.debugElement.queryAll(By.css('.item'));
-    expect(elements.length).toEqual(HOME_LIST_CATEGORY_CONFIGURATIONS.length);
   });
 });
