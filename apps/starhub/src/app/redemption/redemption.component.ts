d65e092f7bf67a5fd436fee7edc14213cb0b2345
Philip Cesar B. Garay <philipgaray2@gmail.com>
2022-01-12 16:05:37 +0800

    Update  Toast message

--- apps/starhub/src/app/redemption/redemption.component.ts
+++ apps/starhub/src/app/redemption/redemption.component.ts
@@ -142,7 +142,7 @@ export class RedemptionComponent implements OnInit {
   public copyCode(inputElement: HTMLInputElement): void {
     inputElement.select();
     document.execCommand('copy');
-    this.notficationService.addSnack('Code Copied.');
+    this.notficationService.addSnack('Copied to your clipboard!');
   }
 
   public updatePin(): void {
