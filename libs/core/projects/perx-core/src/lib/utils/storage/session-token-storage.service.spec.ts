1f9136c0bcea0151a4ca941feae7cc1fb9179978
Ryane Chia <ryanechia@outlook.com>
2022-01-28 21:32:06 +0800

    CITI-14: add http testing module to session storage service spec

--- libs/core/projects/perx-core/src/lib/utils/storage/session-token-storage.service.spec.ts
+++ libs/core/projects/perx-core/src/lib/utils/storage/session-token-storage.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SessionTokenStorageService } from './session-token-storage.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('SessionTokenStorageService', () => {
   let service: SessionTokenStorageService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
+      providers: []
+    });
     service = TestBed.inject(SessionTokenStorageService);
   });
 
