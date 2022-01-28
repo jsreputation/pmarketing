9ef7219044f310a4e333e1d2aeca59c5175f6a98
Ryane Chia <ryanechia@outlook.com>
2022-01-28 21:01:40 +0800

    CITI-14: add session token storage service

--- libs/core/projects/perx-core/src/lib/utils/storage/storage.module.ts
+++ libs/core/projects/perx-core/src/lib/utils/storage/storage.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { Config } from '../../config/config';
 import { TokenStorage } from './token-storage.service';
 import { LocalTokenStorage } from './local-token-storage.service';
+import { SessionTokenStorageService } from './session-token-storage.service';
+import { HttpClient } from '@angular/common/http';
+import { TokenType } from './models/token-storage.model';
 
 export function TokenStorageServiceFactory(
+  http: HttpClient,
   config: Config
 ): TokenStorage {
   switch (config.storageType) {
-    case 'local':
+    case TokenType.local:
       return new LocalTokenStorage(config);
+    case TokenType.session:
+      return new SessionTokenStorageService(http);
     default:
       return new LocalTokenStorage(null);
   }
@@ -18,7 +24,7 @@ export function TokenStorageServiceFactory(
   providers: [{
     provide: TokenStorage,
     useFactory: TokenStorageServiceFactory,
-    deps: [Config]
+    deps: [HttpClient, Config]
   }]
 })
 export class StorageModule { }
