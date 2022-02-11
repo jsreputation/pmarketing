9eec000cf2d5943fec832ea2ccc85948e6f82c24
Amrutha Mohan <75286912+amrutha-mohan-1@users.noreply.github.com>
2022-02-11 12:09:33 +0800

    BDO-280 turn on show ordered catalog items flag for production

--- apps/bdo/src/app/main/catalog-page/catalog-page.component.ts
+++ apps/bdo/src/app/main/catalog-page/catalog-page.component.ts
@@ -11,7 +11,7 @@ import { mapCampaignsToListItem, mapRewardsToListItem, mapCampaignToListItem, ma
 import { SelfDestruct } from '../../shared/utilities/self-destruct.component';
 import { buildParams, mapQueryParamsToFilterObject } from '../../shared/utilities/filter.util';
 
-const ORDERED_CATALOG_NAME = 'Bdo deals';
+const ORDERED_CATALOG_NAME = 'bdo deals';
 
 interface IBDOConfig {
   showOrderedCatalogItems: boolean;
@@ -50,7 +50,7 @@ export class CatalogPageComponent extends SelfDestruct implements OnInit {
       this.rewardsService.getAllCategories(),
       this.rewardsService.getCatalogs(1, this.requestPageSize)
     ]).subscribe(([params, categories, catalogs]) => {
-      this.catalogId = catalogs?.find((catalog: ICatalog) => catalog.name === ORDERED_CATALOG_NAME)?.id;
+      this.catalogId = catalogs?.find((catalog: ICatalog) => catalog.name.toLowerCase() === ORDERED_CATALOG_NAME)?.id;
       const filterData = FILTER_DATA;
       filterData.categories = filterData.categories
         .map(item => {
