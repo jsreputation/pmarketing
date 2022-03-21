79b3462739ce9ee3fb27c4b544e243000ea5e427
Philip Cesar B. Garay <philipgaray2@gmail.com>
2022-03-21 21:52:14 +0800

    [MEG-65] megaworld microsite add email as a required field

--- apps/megaworld/src/app/sign-up/sign-up.module.ts
+++ apps/megaworld/src/app/sign-up/sign-up.module.ts
@@ -1,64 +1,49 @@
-import { NgModule } from '@angular/core';
-import { SignupRoutingModule } from './signup-routing.module';
-import { SignupComponent } from './sign-up.component';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatDatepickerModule } from '@angular/material/datepicker';
+
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatCardModule } from '@angular/material/card';
-import {
-  MAT_MOMENT_DATE_ADAPTER_OPTIONS,
-  MAT_MOMENT_DATE_FORMATS,
-  MatMomentDateModule,
-  MomentDateAdapter
-} from '@angular/material-moment-adapter';
-import {
-  FormsModule,
-  ReactiveFormsModule
-} from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatButtonModule } from '@angular/material/button';
+import { AuthenticationModule, GameModule, SurveyModule, UtilsModule, ConfigModule, SettingsModule } from '@perxtech/core';
+import { SignUpComponent } from './sign-up.component';
+import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
-import {
-  DateAdapter,
-  MAT_DATE_FORMATS,
-  MAT_DATE_LOCALE
-} from '@angular/material/core';
+import { MatCardModule } from '@angular/material/card';
+
+import { SignupRoutingModule } from './signup-routing.module';
 
 @NgModule({
-  declarations: [SignupComponent],
+  declarations: [SignUpComponent],
+  exports: [SignUpComponent],
   imports: [
     CommonModule,
     SignupRoutingModule,
+
+    ReactiveFormsModule,
+    FormsModule,
+    RouterModule,
+    TranslateModule.forChild(),
+
+    AuthenticationModule,
+    SurveyModule,
+    GameModule,
+    UtilsModule,
+    ConfigModule.forChild(),
+    SettingsModule,
+
+    MatSnackBarModule,
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
-    MatCheckboxModule,
-    MatIconModule,
-    MatMomentDateModule,
-    MatDatepickerModule,
-    MatProgressBarModule,
     MatProgressSpinnerModule,
-    MatCardModule,
-    ReactiveFormsModule,
-    FormsModule,
-    TranslateModule
-  ],
-  providers: [
-    { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
-    { provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS },
-    {
-      provide: MAT_MOMENT_DATE_ADAPTER_OPTIONS,
-      useValue: {
-        useUtc: true,
-        strict: true
-      }
-    }
+    MatIconModule,
+    MatCardModule
   ]
 })
 export class SignUpModule { }
--- apps/megaworld/src/app/sign-up/signup-routing.module.ts
+++ apps/megaworld/src/app/sign-up/signup-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SignupComponent } from './sign-up.component';
+import { SignUpComponent } from './sign-up.component';
 
 
 const routes: Routes = [
-  { path: '', component: SignupComponent, data: { countryList: ['Malaysia', 'Singapore'] } },
+  { path: '', component: SignUpComponent },
 ];
 
 @NgModule({
