f1536cff829dec6daad6788cb3251c4d6600114f
Ryane Chia <ryanechia@outlook.com>
2022-03-25 07:13:39 +0800

    CITI-22: add token storage stub to access verify component

--- apps/blackcomb/src/app/access-verify/access-verify.component.spec.ts
+++ apps/blackcomb/src/app/access-verify/access-verify.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AccessVerifyComponent } from './access-verify.component';
-import { AuthenticationService } from '@perxtech/core';
+import { AuthenticationService, TokenStorage } from '@perxtech/core';
 import { of } from 'rxjs';
 
 describe('AccessVerifyComponent', () => {
@@ -12,12 +12,20 @@ describe('AccessVerifyComponent', () => {
     getExchangeToken: () => of()
   };
 
+  const tokenStorageStub = {
+    clearAppInfoProperty: () => {}
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ AccessVerifyComponent ],
       imports: [ RouterTestingModule ],
       providers: [
-        { provide: AuthenticationService, useValue: authenticationServiceStub }
+        { provide: AuthenticationService, useValue: authenticationServiceStub },
+        {
+          provide: TokenStorage,
+          useValue: tokenStorageStub
+        }
       ]
     })
     .compileComponents();
