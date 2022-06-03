9b8052d6c1d455d5a8e86428bb6752b86faf5a5b
Philip Cesar Garay <philipgaray2@gmail.com>
2022-06-03 12:02:20 +0800

    [HS-60] initial commit

--- apps/hangseng/src/app/campaign-stamps/campaign-stamps.module.ts
+++ apps/hangseng/src/app/campaign-stamps/campaign-stamps.module.ts
@@ -1,6 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CampaignStampsComponent, CampaignStampsModule as BCPCampaignStampsModule } from '@perxtech/blackcomb-pages';
+import { CampaignStampsModule as BCPCampaignStampsModule } from '@perxtech/blackcomb-pages';
+import { ConfigModule, PipeUtilsModule, PuzzlesModule, StampModule, UtilsModule } from '@perxtech/core';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { CampaignStampsComponent } from './campaign-stamps.component';
 
 const routes: Routes = [{
   path: '',
@@ -9,7 +13,14 @@ const routes: Routes = [{
 
 @NgModule({
   imports: [
+    CommonModule,
+    UtilsModule,
+    InfiniteScrollModule,
+    PipeUtilsModule,
+    StampModule,
+    PuzzlesModule,
     BCPCampaignStampsModule,
+    ConfigModule,
     RouterModule.forChild(routes),
   ]
 })
--- apps/hangseng/src/app/stamp/stamp.module.ts
+++ apps/hangseng/src/app/stamp/stamp.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { StampCardComponent, StampCardModule } from '@perxtech/blackcomb-pages';
+import { StampCardComponent } from '@perxtech/blackcomb-pages';
+import { StampCardModule } from './stamp-card/stamp-card.module';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: StampCardComponent },
