36805f3e416d1995bb4d86c734bd8d35983cf32f
Philip Cesar Garay <philipgaray2@gmail.com>
2022-06-09 13:02:12 +0800

    [HS-83] Load scratch game on a custom scratch game container

--- apps/hangseng/src/app/game/scratch/scratch.component.ts
+++ apps/hangseng/src/app/game/scratch/scratch.component.ts
@@ -1,15 +1,132 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+import {
+  IFlags,
+  IGame,
+  IOperatingHours,
+  IScratch,
+  ITheme,
+  SettingsService,
+  ThemesService,
+} from '@perxtech/core';
 
 @Component({
   selector: 'hangseng-game-scratch',
   templateUrl: './scratch.component.html',
-  styleUrls: ['./scratch.component.scss']
+  styleUrls: ['./scratch.component.scss'],
 })
 export class ScratchComponent implements OnInit {
+  @Input() public willWin: boolean = false;
 
-  constructor() { }
+  @Input() public game: IGame;
 
-  ngOnInit(): void {
+  @Output() public broken: EventEmitter<void> = new EventEmitter();
+  @Output() public loaded: EventEmitter<boolean> = new EventEmitter();
+
+  public headerStyle: { [key: string]: string } = {};
+  public subheaderStyle: { [key: string]: string } = {};
+  public buttonStyle: { [key: string]: string } = {};
+  public showOperatingHours: boolean = false;
+
+  public isEnabled: boolean = false;
+
+  constructor(
+    private themesService: ThemesService,
+    private settingsService: SettingsService
+  ) {}
+
+  public ngOnInit(): void {
+    this.loaded.emit();
+    this.buttonStyle.visibility = 'hidden';
+    this.themesService.getThemeSetting().subscribe((theme: ITheme) => {
+      this.buttonStyle['background-color'] = this.game.texts.buttonColour
+        ? this.game.texts.buttonColour
+        : theme.properties['--button_background_color']
+        ? theme.properties['--button_background_color']
+        : '';
+      this.buttonStyle.color = this.game.texts.buttonTextColour
+        ? this.game.texts.buttonTextColour
+        : theme.properties['--button_text_color']
+        ? theme.properties['--button_text_color']
+        : '';
+      this.buttonStyle.visibility = 'visible';
+    });
+    this.headerStyle.color = this.game.texts.headerColour
+      ? this.game.texts.headerColour
+      : '';
+    this.subheaderStyle.color = this.game.texts.subheaderColour
+      ? this.game.texts.subheaderColour
+      : '';
+
+    this.settingsService.getRemoteFlagsSettings().subscribe((flags: IFlags) => {
+      this.showOperatingHours = flags.showHappyHourOperatingHours
+        ? flags.showHappyHourOperatingHours
+        : false;
+    });
   }
 
+  public get config(): IScratch {
+    return this.game.config as IScratch;
+  }
+
+  public gameCompleted(): void {
+    this.broken.emit();
+  }
+
+  public onClick(): void {
+    this.isEnabled = true;
+    this.buttonStyle.visibility = 'hidden';
+  }
+
+  public getOperatingHours(operatingHours: IOperatingHours): string {
+    const daysMapArr = [false, false, false, false, false, false, false]; // index 0 is sunday
+
+    for (const dayIndex in operatingHours.days) {
+      if (dayIndex) {
+        // guard-for-in
+        daysMapArr[operatingHours.days[dayIndex]] = true;
+      }
+    }
+    const days: string = this.dayArrToIntuitiveStringDayRange(daysMapArr);
+    const hours: string = `${operatingHours.opensAt?.substr(
+      0,
+      5
+    )} - ${operatingHours.closesAt?.substr(0, 5)}`;
+    return `Play this game during: ${days}, ${hours} ${operatingHours.formattedOffset}`;
+  }
+
+  private dayOfWeekAsString(dayIndex: number): string {
+    return ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'][dayIndex];
+  }
+
+  // works but can't wrap sat and sun
+  private dayArrToIntuitiveStringDayRange(daysMapArr: boolean[]): string {
+    let dayRange = '',
+      multiDayRange = '';
+    let findingRange = false;
+
+    for (let i = 0; i <= daysMapArr.length; i++) {
+      if (daysMapArr[i]) {
+        if (dayRange.length > 0 && !findingRange) {
+          findingRange = true;
+        } else if (dayRange.length === 0) {
+          // first item in current range.
+          dayRange = `${this.dayOfWeekAsString(i)}`;
+        }
+      } else if (dayRange.length > 0 && !daysMapArr[i]) {
+        // first part of range already identified
+        if (this.dayOfWeekAsString(i - 1) !== dayRange) {
+          dayRange = `${dayRange} - ${this.dayOfWeekAsString(i - 1)}`;
+        }
+        if (multiDayRange.length === 0) {
+          multiDayRange = dayRange;
+        } else {
+          multiDayRange = `${multiDayRange}, ${dayRange}`;
+        }
+        dayRange = ''; // reset for more ranges;
+        findingRange = false;
+      }
+    }
+    return multiDayRange;
+  }
 }
