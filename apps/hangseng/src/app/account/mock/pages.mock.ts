80c7da3ad80e766cc9f16d7b3b7d4de2113e3033
Philip Cesar Garay <philipgaray2@gmail.com>
2022-06-13 22:12:08 +0800

    [HS-49] pullout Account page from BCP and use it as Settings Page for HASE

---
+++ apps/hangseng/src/app/account/mock/pages.mock.ts
@@ -0,0 +1,11 @@
+import { PagesObject } from '@perxtech/core';
+
+export const pagesObject: PagesObject = {
+  pages: [
+    {
+      title: 'title',
+      content_url: 'http://content_url.ru',
+      key: 'key',
+    }
+  ]
+};
