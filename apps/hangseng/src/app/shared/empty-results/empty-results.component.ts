2320c4dc22fd7bc2664f62e2f58185aa4859524f
Philip Cesar B. Garay <philipgaray2@gmail.com>
2022-06-17 20:44:53 +0800

    [HS-123] HS microsite - empty state for campaign filter

--- apps/hangseng/src/app/shared/empty-results/empty-results.component.ts
+++ apps/hangseng/src/app/shared/empty-results/empty-results.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'hangseng-empty-results',
   templateUrl: './empty-results.component.html',
   styleUrls: ['./empty-results.component.scss']
 })
-export class EmptyResultsComponent implements OnInit {
+export class EmptyResultsComponent {
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
